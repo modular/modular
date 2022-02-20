a5ac3596e4640ffcc095b473088119d883e97b18
Chris Lattner <clattner@nondot.org>
2022-02-20 11:45:00 -0800

    Build out the bef-executor command line interface and debugging allocators.
    TODO: Add tests for debugging allocators when more infra is up.
    MODULAR_ORIG_COMMIT_REV_ID: 49c326218fce98f75b7e598521e1773e773275ff

---
+++ LLCL/lib/Runtime/DebuggingAllocators.cpp
@@ -0,0 +1,111 @@
+//===- DebuggingAllocators.cpp - Allocators for debugging -----------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file defines wrapper allocators that keep track of extra metadata.
+//
+//===----------------------------------------------------------------------===//
+
+#include "LLCL/Allocator.h"
+#include "LLCL/Atomics.h"
+#include <atomic>
+
+using namespace LLCL;
+
+//===----------------------------------------------------------------------===//
+// Leak Checking Allocator
+//===----------------------------------------------------------------------===//
+
+namespace {
+class LeakCheckAllocator : public Allocator {
+public:
+  explicit LeakCheckAllocator(std::unique_ptr<Allocator> baseAllocator)
+      : baseAllocator(std::move(baseAllocator)) {}
+  ~LeakCheckAllocator() override { checkLeak(); }
+
+  void *allocateBytes(size_t size, size_t alignment) override {
+    ++numAllocations;
+    numBytesAllocated.fetch_add(size);
+    return baseAllocator->allocateBytes(size, alignment);
+  }
+
+  void deallocateBytes(void *ptr, size_t size) override {
+    auto curNumAllocations = --numAllocations;
+    assert(curNumAllocations >= 0 && "deallocation imbalance");
+    numBytesAllocated.fetch_sub(size);
+    baseAllocator->deallocateBytes(ptr, size);
+  }
+
+  /// Print a message and exit(1) when memory leak is detected.
+  void checkLeak() {
+    if (numBytesAllocated.load() != 0) {
+      printf("Memory leak detected: %lld alive allocations, %lld alive bytes\n",
+             (long long)numAllocations.load(),
+             (long long)numBytesAllocated.load());
+      printf("Run with other allocators to debug what happened.\n");
+      fflush(stdout);
+      exit(1);
+    }
+  }
+
+  /// This keeps track of how many bytes/allocations are currently alive.
+  std::atomic<ssize_t> numBytesAllocated{0}, numAllocations{0};
+
+private:
+  std::unique_ptr<Allocator> baseAllocator;
+};
+} // end anonymous namespace.
+
+/// Create a wrapper allocator that checks to make sure all memoory is
+/// deallocated when the allocator itself is destroyed.
+std::unique_ptr<Allocator>
+LLCL::createLeakCheckAllocator(std::unique_ptr<Allocator> baseAllocator) {
+  return std::make_unique<LeakCheckAllocator>(std::move(baseAllocator));
+}
+
+//===----------------------------------------------------------------------===//
+// Profiling Allocator
+//===----------------------------------------------------------------------===//
+
+namespace {
+class ProfilingAllocator : public LeakCheckAllocator {
+public:
+  explicit ProfilingAllocator(std::unique_ptr<Allocator> baseAllocator)
+      : LeakCheckAllocator(std::move(baseAllocator)) {}
+
+  void *allocateBytes(size_t size, size_t alignment) override {
+    void *result = LeakCheckAllocator::allocateBytes(size, alignment);
+    ++totalAllocations;
+
+    atomicMax(maxAllocations, numAllocations.load());
+    atomicMax(maxBytesAllocated, numBytesAllocated.load());
+    return result;
+  }
+
+  ~ProfilingAllocator() override {
+    printf("LLCL::Allocator profile:\n");
+    printf("Max number of allocations = %lld\n",
+           (long long)maxAllocations.load());
+    printf("Total number of allocations = %lld\n",
+           (long long)totalAllocations.load());
+    printf("Max number of bytes ever allocated = %lld\n",
+           (long long)maxBytesAllocated.load());
+    fflush(stdout);
+
+    // If we still have active memory alive, print an error.
+    checkLeak();
+  }
+
+  std::atomic<ssize_t> maxAllocations{0}, maxBytesAllocated{0};
+  std::atomic<int64_t> totalAllocations{0};
+};
+} // end anonymous namespace.
+
+/// Create a wrapper allocator that prints memory profiling information when it
+/// is destroyed.  This also performs leak checks.
+std::unique_ptr<Allocator>
+LLCL::createProfilingAllocator(std::unique_ptr<Allocator> baseAllocator) {
+  return std::make_unique<ProfilingAllocator>(std::move(baseAllocator));
+}
