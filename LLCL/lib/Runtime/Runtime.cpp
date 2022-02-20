d6f65397614390f289627a022a55a79d6eacdff0
Chris Lattner <clattner@nondot.org>
2022-02-20 15:54:12 -0800

    [LLCL] Implement CompactRuntimePtr.
    This is allows us to identify an instance of a LLCL::Runtime while
    only using 8 bits.
    
    MODULAR_ORIG_COMMIT_REV_ID: 473a6d4c3abb779a06b203ce30a7c17d4fa6bac8

--- LLCL/lib/Runtime/Runtime.cpp
+++ LLCL/lib/Runtime/Runtime.cpp
@@ -16,8 +16,33 @@ using namespace LLCL;
 void WorkQueue::vtableAnchor() {}
 void Allocator::vtableAnchor() {}
 
+/// The `CompactRuntimePtr` type provides a pointer compressed version of
+/// `Runtime*` that fits in 8 bits.  This allows every AsyncValue to carry a
+/// backpointer to the Runtime that allocated them, and allows deallocating the
+/// memory for the AsyncValue through the Runtime's allocator.
+///
+/// This is implemented with a static array of Runtime pointers that are given
+/// unique IDs.
+static std::atomic<uint8_t> nextRuntimeIndex{0};
+static Runtime *allRuntimes[CompactRuntimePtr::kInvalidIndex];
+
+CompactRuntimePtr::CompactRuntimePtr(Runtime *runtime)
+    : CompactRuntimePtr(runtime->getCompactPtr()) {}
+
+Runtime *CompactRuntimePtr::get() const {
+  assert(index != kInvalidIndex);
+  return allRuntimes[index];
+}
+
 Runtime::Runtime(std::unique_ptr<Allocator> allocator,
                  std::unique_ptr<WorkQueue> workQueue)
-    : allocator(std::move(allocator)), workQueue(std::move(workQueue)) {}
+    : allocator(std::move(allocator)), workQueue(std::move(workQueue)),
+      runtimeIndex(nextRuntimeIndex.fetch_add(1)) {
+  // We provide a dense numbering of runtime instances right now, but we could
+  // make this fancier to allow deallocating and reusing indexes if needbe.
+  assert(runtimeIndex < CompactRuntimePtr::kInvalidIndex &&
+         "Created too many Runtimes");
+  allRuntimes[runtimeIndex] = this;
+}
 
-Runtime::~Runtime() {}
+Runtime::~Runtime() { allRuntimes[runtimeIndex] = nullptr; }
