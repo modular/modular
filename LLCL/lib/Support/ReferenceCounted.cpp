16ae3c9a0cfb7e5260a825f3f298f5c7f5b05e97
Chris Lattner <clattner@nondot.org>
2022-02-22 12:31:51 -0800

    [LLCL] Add a ReferenceCounted<T> CRTP base class for object ownership.
    MODULAR_ORIG_COMMIT_REV_ID: b1378ea89f4dd042aeb2fdc511fa0a2c7d45e2f9

---
+++ LLCL/lib/Support/ReferenceCounted.cpp
@@ -0,0 +1,32 @@
+//===- ReferenceCounted.cpp -----------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file implements ReferenceCounted class.
+//
+//===----------------------------------------------------------------------===//
+
+#include "LLCL/Support/ReferenceCounted.h"
+#include <cstdlib>
+using namespace LLCL;
+
+#ifndef NDEBUG
+/// In debug builds we keep track of the number of reference counted objects,
+/// which enables clients to check that none are alive at key moments.  This is
+/// a low-tech way to find certain classes of memory leaks.
+std::atomic<size_t> LLCL::currentReferenceCountedObjects{0};
+#endif
+
+/// Verify that there are no live ReferenceCounted objects that are currently
+/// alive and print the specified message and abort if there are.
+void LLCL::verifyNoLiveReferenceCountedObjects(const char *errorMessage) {
+  if (!currentReferenceCountedObjects.load(std::memory_order_relaxed))
+    return;
+
+  // Otherwise print an error and crash.
+  puts(errorMessage);
+  fflush(stdout);
+  abort();
+}
