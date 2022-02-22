a3bd2bdaf01d60f5312e05a954a3fa0392d9e3df
Chris Lattner <clattner@nondot.org>
2022-02-21 18:24:27 -0800

    [LLCL] Add an RCRef<T> helper smart pointer.
    This makes working with reference counted values a lot safer.
    
    MODULAR_ORIG_COMMIT_REV_ID: d67e371697f6d541ddec13ec3cb417bf5952b782

---
+++ LLCL/include/LLCL/Support/ReferenceCount.h
@@ -0,0 +1,138 @@
+//===- LLCL/Support/ReferenceCount.h ----------------------------*- C++ -*-===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file provides classes that are helpful for working with
+// domain-independent reference counted objects.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLCL_SUPPORT_REFERENCE_COUNT_H
+#define LLCL_SUPPORT_REFERENCE_COUNT_H
+
+#include <atomic>
+
+namespace LLCL {
+
+/// This is a smart pointer that keeps the specified reference counted value
+/// around.  It is move-only to avoid accidental copies, but it can be copied
+/// explicitly.
+template <typename T>
+class RCRef {
+public:
+  RCRef() : pointer(nullptr) {}
+  RCRef(RCRef &&other) : pointer(other.pointer) { other.pointer = nullptr; }
+
+  /// This constructor forms a reference to the specified pointer, increasing
+  /// the underlying reference count by 1.
+  static RCRef copy(T *pointer) {
+    RCRef<T> ref;
+    ref.pointer = pointer;
+    if (pointer)
+      pointer->addRef();
+    return ref;
+  }
+
+  /// This constructor forms a reference to the specified pointer, taking
+  /// ownership it, and thus not increasing the reference count.
+  static RCRef take(T *pointer) {
+    RCRef<T> ref;
+    ref.pointer = pointer;
+    return ref;
+  }
+
+  // Support implicit conversion from RCRef<Derived> to RCRef<Base>.
+  template <typename U,
+            typename = std::enable_if_t<std::is_base_of<T, U>::value>>
+  RCRef(RCRef<U> &&u) : pointer(u.pointer) { // NOLINT
+    u.pointer = nullptr;
+  }
+
+  ~RCRef() {
+    if (pointer)
+      pointer->dropRef();
+  }
+
+  RCRef &operator=(RCRef &&other) {
+    if (pointer)
+      pointer->dropRef();
+    pointer = other.pointer;
+    other.pointer = nullptr;
+    return *this;
+  }
+
+  /// Manually drop the reference in this RCRef, setting it to null.
+  void reset() {
+    if (pointer)
+      pointer->dropRef();
+    pointer = nullptr;
+  }
+
+  /// Take ownership of the underlying pointer away from the RCRef and reset it
+  /// to null.
+  T *release() {
+    T *tmp = pointer;
+    pointer = nullptr;
+    return tmp;
+  }
+
+  T &operator*() const {
+    assert(pointer && "null RCRef");
+    return *pointer;
+  }
+
+  T *operator->() const {
+    assert(pointer && "null RCRef");
+    return pointer;
+  }
+
+  // Return a raw pointer.
+  T *get() const { return pointer; }
+
+  /// Make an explicit copy of this RCRef, increasing the refcount by one.
+  RCRef<T> copy() const { return RCRef<T>::copy(get()); }
+
+  /// Test for null.
+  explicit operator bool() const { return pointer != nullptr; }
+
+  void swap(RCRef &other) {
+    using std::swap;
+    swap(pointer, other.pointer);
+  }
+
+private:
+  // Not implicity copyable, use the copy() method for an explicit copy of
+  // this reference.
+  RCRef(const RCRef &) = delete;
+  RCRef &operator=(const RCRef &) = delete;
+
+  T *pointer;
+};
+
+// These global functions help make type inference work better.
+
+/// Forms a reference to the specified pointer, increasing the underlying
+/// reference count by 1.
+template <typename T>
+inline RCRef<T> copyRCRef(T *ptr) {
+  return RCRef<T>::copy(ptr);
+}
+
+/// Form a reference to the specified pointer, taking ownership it, and thus not
+/// increasing the reference count.
+template <typename T>
+inline RCRef<T> takeRCRef(T *ptr) {
+  return RCRef<T>::take(ptr);
+}
+
+// For ADL style swap.
+template <typename T>
+inline void swap(RCRef<T> &a, RCRef<T> &b) {
+  a.swap(b);
+}
+
+} // namespace LLCL
+
+#endif // LLCL_SUPPORT_REFERENCE_COUNT_H
