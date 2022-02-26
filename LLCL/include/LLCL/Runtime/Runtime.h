54c4f811622a9683904b0d08d045f2f6ae7e9181
Chris Lattner <clattner@nondot.org>
2022-02-26 14:14:18 -0800

    [WorkQueue] implement support for await() functionality.
    MODULAR_ORIG_COMMIT_REV_ID: 27770cfa91974ec28a40c0272a9cff2bfbc7f12d

--- LLCL/include/LLCL/Runtime/Runtime.h
+++ LLCL/include/LLCL/Runtime/Runtime.h
@@ -18,7 +18,6 @@
 #include "LLCL/Runtime/Allocator.h"
 #include "LLCL/Runtime/CompactRuntimePtr.h"
 #include "LLCL/Runtime/WorkQueue.h"
-#include "llvm/ADT/FunctionExtras.h"
 
 namespace LLCL {
 class Allocator;
@@ -48,36 +47,36 @@ public:
   /// Get direct access to the low level allocator.
   Allocator *getAllocator() { return allocator.get(); }
 
-  // Allocate the specified number of bytes with the specified alignment.
+  /// Allocate the specified number of bytes with the specified alignment.
   void *allocateBytes(size_t size, size_t alignment) {
     return allocator->allocateBytes(size, alignment);
   }
 
-  // Deallocate the specified pointer that had the specified size.
+  /// Deallocate the specified pointer that had the specified size.
   void deallocateBytes(void *ptr, size_t size) {
     return allocator->deallocateBytes(ptr, size);
   }
 
-  // Allocate memory for one or more entries of type T.
+  /// Allocate memory for one or more entries of type T.
   template <typename T>
   T *allocate(size_t numElements = 1) {
     return static_cast<T *>(allocateBytes(sizeof(T) * numElements, alignof(T)));
   }
 
-  // Deallocate the memory for one or more entries of type T.
+  /// Deallocate the memory for one or more entries of type T.
   template <typename T>
   void deallocate(T *ptr, size_t numElements) {
     deallocateBytes(ptr, sizeof(T) * numElements);
   }
 
-  // Allocate and initialize an object of type T.
+  /// Allocate and initialize an object of type T.
   template <typename T, typename... Args>
   T *construct(Args &&...args) {
     T *buf = allocate<T>();
     return new (buf) T(std::forward<Args>(args)...);
   }
 
-  // Destruct and deallocate space for one or more object of type T.
+  /// Destruct and deallocate space for one or more object of type T.
   template <typename T>
   void destroyAndDeallocate(T *ptr, size_t numElements = 1) {
     for (size_t i = 0; i != numElements; ++i)
@@ -89,14 +88,16 @@ public:
   // Concurrency
   //===--------------------------------------------------------------------===//
 
-  // Enqueue a block of work. Thread-safe.
+  /// Enqueue a block of work. Thread-safe.
   void addTask(TaskFunction work) { workQueue->addTask(std::move(work)); }
 
-  // TODO: Await.
+  /// Block until the specified values are ready.  This should not be called by
+  /// a thread managed by our work queue.
+  void await(llvm::ArrayRef<RCRef<AsyncValue>> values);
 
-  // Block until the system is quiescent (no pending work and no inflight work).
-  //
-  // This should not be called by a thread managed by the work queue.
+  /// Block until the system is quiescent (no pending/inflight work).
+  ///
+  /// This should not be called by a thread managed by the work queue.
   void quiesce() { workQueue->quiesce(); }
 
   //===--------------------------------------------------------------------===//
--- LLCL/include/LLCL/Runtime/WorkQueue.h
+++ LLCL/include/LLCL/Runtime/WorkQueue.h
@@ -12,6 +12,8 @@
 #ifndef LLCL_RUNTIME_WORKQUEUE_H
 #define LLCL_RUNTIME_WORKQUEUE_H
 
+#include "LLCL/ForwardDecls.h"
+#include "Support/LLVM.h"
 #include "llvm/ADT/FunctionExtras.h"
 
 namespace LLCL {
@@ -19,22 +21,31 @@ class LLCLAllocator;
 
 using TaskFunction = llvm::unique_function<void()>;
 
+/// This is an interface to various implementations of workqueues: different
+/// execution methods which are often current.  These implementations may be
+/// very domain or host system specific, but the interface to them is kept
+/// intentionally simple to just `addTask` (which adds a block of work to be
+/// done as a C++ closure), `await` which blocks until some specific values are
+/// ready to go, and `quiesce` which blocks until all work is done.
 class WorkQueue {
 public:
   virtual ~WorkQueue() {}
 
 protected:
-  // Clients should access WorkQueue's methods via LLCL::Runtime.
+  /// Clients should access WorkQueue's methods via LLCL::Runtime.
   friend class Runtime;
 
-  // Enqueue a block of work. Thread-safe.
+  /// Enqueue a block of work. Thread-safe.
   virtual void addTask(TaskFunction work) = 0;
 
-  // TODO: Await.
+  /// Block until the specified values are ready.  This should not be called by
+  /// a thread managed by our work queue.
+  virtual void await(llvm::ArrayRef<RCRef<AsyncValue>> values) = 0;
 
-  // Block until the system is quiescent (no pending work and no inflight work).
-  //
-  // This should not be called by a thread managed by the work queue.
+  /// Block until the system is quiescent (no pending work and no inflight
+  /// work).
+  ///
+  /// This should not be called by a thread managed by the work queue.
   virtual void quiesce() = 0;
 
   WorkQueue() = default;
@@ -45,8 +56,8 @@ private:
   void operator=(const WorkQueue &) = delete;
 };
 
-// Create a thread pool that only uses the host donor thread, involving no
-// synchronization.
+/// Create a thread pool that only uses the host donor thread, involving no
+/// synchronization.
 std::unique_ptr<WorkQueue> createSingleThreadWorkQueue();
 
 } // namespace LLCL
