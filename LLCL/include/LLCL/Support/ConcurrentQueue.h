7a1efe2dfe5fd844e1fee0e487550c3696daa0ed
bzcheeseman <12992886+bzcheeseman@users.noreply.github.com>
2022-03-11 13:11:47 -0800

    [LLCL] Add concurrent-safe queue
    MODULAR_ORIG_COMMIT_REV_ID: 164ef1eaf55b5327674f727bc24cd7ff635d9e9f

---
+++ LLCL/include/LLCL/Support/ConcurrentQueue.h
@@ -0,0 +1,94 @@
+//===- ConcurrentQueue.h ----------------------------------------*- C++ -*-===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLCL_SUPPORT_CONCURRENTQUEUE_H
+#define LLCL_SUPPORT_CONCURRENTQUEUE_H
+
+#include <mutex>
+
+namespace LLCL {
+/// This class provides a concurrent-safe ordered queue. Items in the queue
+/// cannot be re-ordered.
+template <typename T>
+class ConcurrentQueue {
+public:
+  ConcurrentQueue() : head(nullptr), tail(nullptr) {}
+  ~ConcurrentQueue() { assert(empty() && "Cannot destroy a non-empty queue!"); }
+
+  /// Enqueue takes ownership of the object and ties its lifetime to the
+  /// lifetime of the queue.
+  void enqueue(T &&obj);
+
+  /// Dequeue returns failure if the queue is empty, otherwise returns the
+  /// stored value to the caller and relinquishes ownership.
+  mlir::FailureOr<T> dequeue();
+
+  /// This function checks if the queue is empty. It holds the lock, so be
+  /// careful where you call it from.
+  bool empty() {
+    std::lock_guard lock(m);
+    return emptyImpl();
+  }
+
+private:
+  ConcurrentQueue(const ConcurrentQueue &other) = delete;
+  ConcurrentQueue &operator=(const ConcurrentQueue &other) = delete;
+
+  /// Linked list node used for this queue.
+  struct Item {
+    Item *next;
+    T data;
+  };
+
+  /// The caller must hold the lock for this function, which is why it's
+  /// private.
+  bool emptyImpl() const { return head == nullptr && head == tail; }
+
+  std::mutex m;
+  Item *head;
+  Item *tail;
+};
+
+//===----------------------------------------------------------------------===//
+// Queue function implementations
+//===----------------------------------------------------------------------===//
+
+template <typename T>
+void ConcurrentQueue<T>::enqueue(T &&obj) {
+  auto *newItem = new Item{.next = nullptr, .data = std::move(obj)};
+  assert(newItem != nullptr);
+
+  std::lock_guard lock(m);
+  Item *prevTail = tail;
+  // If the queue is empty then set head and tail to the new item.
+  if (emptyImpl())
+    head = newItem;
+  else
+    prevTail->next = newItem;
+
+  tail = newItem;
+}
+
+template <typename T>
+mlir::FailureOr<T> ConcurrentQueue<T>::dequeue() {
+  std::lock_guard lock(m);
+  if (emptyImpl())
+    return mlir::failure();
+
+  Item *out = head;
+  head = out->next;
+  // If head has caught up to tail, then set tail to nullptr as well.
+  if (head == nullptr)
+    tail = nullptr;
+
+  T outData = std::move(out->data);
+  delete out;
+  return outData;
+}
+
+} // namespace LLCL
+
+#endif // LLCL_SUPPORT_CONCURRENTQUEUE_H
