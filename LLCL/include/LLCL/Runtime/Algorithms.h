25258e274458a3fd21efc7f8416ca1d72e3361d6
Chris Lattner <clattner@nondot.org>
2022-03-12 22:47:26 -0800

    [Algorithms] Add a parallel for loop.
    This adds the version that takes a per-element function, a completion
    handler, and an arbitrary amount of state to capture.  We should also
    eventually have one that completes a chain when done (instead of
    calling a completion handler), but this is a starting point.
    
    MODULAR_ORIG_COMMIT_REV_ID: 4bce49e95e66d35321fc11b9907deac93d2b7194

--- LLCL/include/LLCL/Runtime/Algorithms.h
+++ LLCL/include/LLCL/Runtime/Algorithms.h
@@ -64,6 +64,66 @@ LLVM_NODISCARD inline static AsyncValueRef<ResultTy> addTask(Runtime &runtime,
   return result;
 }
 
+//===----------------------------------------------------------------------===//
+// parallelForEachN
+//===----------------------------------------------------------------------===//
+
+/// This version of parallelForEachN takes an element closure and a completion
+/// handler callback.
+template <typename... CaptureTys, typename ElementFn, typename CompletionFn>
+static inline void
+parallelForEachN(Runtime &runtime, size_t totalCount, ElementFn &&elementFn,
+                 CompletionFn &&completionFn, CaptureTys &&...captures) {
+  // If there is nothing to do, then we're already done.
+  if (totalCount == 0)
+    return;
+
+  struct ParallelState {
+    /// This is the number of elements left to finish executing.  When this
+    /// drops to zero, the completion handler is run.
+    std::atomic<size_t> numElementsLeft;
+
+    /// This is the function to execute on each element.
+    ElementFn elementFn;
+
+    /// This is the function to execute once all elements are done.
+    CompletionFn completionFn;
+
+    /// This is the state captured by the computation, it is passed to both the
+    /// per-element computation as well as to the completion function.
+    std::tuple<CaptureTys...> capturesList;
+  };
+
+  // Allocate the parallel state on the heap since it will out-live the call to
+  // this function.  We will deallocate it after invoking the completion
+  // handler when the last element completes.
+  auto state = new ParallelState{totalCount,
+                                 std::forward<ElementFn>(elementFn),
+                                 std::forward<CompletionFn>(completionFn),
+                                 {std::forward<CaptureTys>(captures)...}};
+
+  // Enqueue each element of work!
+  for (size_t elementIdx = 0; elementIdx != totalCount; ++elementIdx) {
+    addTask(runtime, [state, elementIdx]() {
+      // Invoke the per-element function with the index and all of the captured
+      // state.
+      std::apply([&](auto &&...args) { state->elementFn(elementIdx, args...); },
+                 state->capturesList);
+      // Once that is done we can decrement the count and trigger completion
+      // when the last element is done.
+      if (--state->numElementsLeft != 0)
+        return;
+
+      // Invoke the completion function, since we're done.
+      std::apply([&](auto &&...args) { state->completionFn(args...); },
+                 state->capturesList);
+
+      // All uses of the state are done, so we can deallocate it.
+      delete state;
+    });
+  }
+}
+
 } // namespace LLCL
 
 #endif // LLCL_RUNTIME_ALGORITHMS_H
