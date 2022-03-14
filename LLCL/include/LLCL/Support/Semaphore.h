db3cb0c39e449da537f9bd17d900b139a3aa09b9
bzcheeseman <12992886+bzcheeseman@users.noreply.github.com>
2022-03-14 09:26:10 -0700

    [LLCL] Add platform-independent semaphore
    MODULAR_ORIG_COMMIT_REV_ID: 9931940e1c6d31000c6fca98a4fa90d1145c534a

---
+++ LLCL/include/LLCL/Support/Semaphore.h
@@ -0,0 +1,37 @@
+//===- Semaphore.h --------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLCL_SUPPORT_SEMAPHORE_H
+#define LLCL_SUPPORT_SEMAPHORE_H
+
+#include <memory>
+
+namespace LLCL {
+/// This is an interface to a basic semaphore with post and timed wait
+/// functionality. This is essentially a lowest-common-denominator interface
+/// that is meant to be able to be backed by a GCD semaphore, or a POSIX
+/// semaphore, or in the worst case a counter protected by a mutex.
+class Semaphore {
+public:
+  /// Create and destroy a semaphore.
+  Semaphore();
+  ~Semaphore();
+
+  /// Increments the semaphore.
+  void post();
+
+  /// Attempts to decrement the semaphore, but waits for `ns` nanoseconds.
+  /// Returns true if the decrement timed out. Conceptually similar to
+  /// POSIX sem_timedwait. Passing -1 here means to wait forever.
+  bool wait(int64_t ns);
+
+private:
+  class Impl;
+  std::unique_ptr<Impl> impl;
+};
+} // namespace LLCL
+
+#endif // LLCL_SUPPORT_SEMAPHORE_H
