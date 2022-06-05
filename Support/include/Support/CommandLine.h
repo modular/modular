f02e9d4912263d542a83164d3ba8be3b2dc1ca2e
Chris Lattner <lattner@users.noreply.github.com>
2022-06-04 21:53:08 -0700

    [Driver] Eliminate 'using namespace llvm' particularly for cl::opts. (#855)
    This introduces a new "Support/CommandLine.h" header which wraps
    llvm/Support/CommandLine.h but imports the normally used types and
    functions into the M namespace.  This eliminates the llvm::cl::opt
    noise, allowing us to get rid of 'using namespace llvm' entirely.
    
    MODULAR_ORIG_COMMIT_REV_ID: 67ae57dde03aaf2e386acc951092e197eda66ba6

---
+++ Support/include/Support/CommandLine.h
@@ -0,0 +1,50 @@
+//===- Support/CommandLine.h ----------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file is a convenience wrapper around llvm/Support/CommandLine.h.  In
+// addition to including it, this defines an `M::cl` namespace analogous to the
+// `llvm::cl` namespace with the important types and functions imported.  This
+// avoids having massively llvm::cl::opt sorts of qualifications in Modular
+// code.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef SUPPORT_COMMANDLINE_H
+#define SUPPORT_COMMANDLINE_H
+
+#include "llvm/Support/CommandLine.h"
+
+namespace M {
+
+namespace cl {
+
+using alias = llvm::cl::alias;
+
+template <class DataType, class StorageClass = bool,
+          class ParserClass = llvm::cl::parser<DataType>>
+using list = llvm::cl::list<DataType, StorageClass, ParserClass>;
+
+template <class DataType, bool ExternalStorage = false,
+          class ParserClass = llvm::cl::parser<DataType>>
+using opt = llvm::cl::opt<DataType, ExternalStorage, ParserClass>;
+
+using desc = llvm::cl::desc;
+using value_desc = llvm::cl::value_desc;
+
+template <class Ty>
+inline llvm::cl::initializer<Ty> init(const Ty &Val) {
+  return llvm::cl::init(Val);
+}
+template <typename... OptsTy>
+inline llvm::cl::ValuesClass values(OptsTy... Options) {
+  return llvm::cl::values(Options...);
+}
+
+} // namespace cl
+
+} // end namespace M
+
+#endif
