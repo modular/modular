29585334d0e9471c8e7a5632f978f40566d7d9c9
Chris Lattner <lattner@users.noreply.github.com>
2022-06-12 21:40:24 -0700

    [ParamExprAttr] Introduce expression canonicalization. (#959)
    This ensures that expressions produces by parameter specifications
    are canonical, which ensures pointer equivalence.  This is particularly
    helpful for things like parameterized types, were we want things like
    array<n*2> to be the same type as array<n+n>.  This handles all the
    affine cases very well, there are a few more arithmetic simplifications
    that could be added for more complex operators.
    
    MODULAR_ORIG_COMMIT_REV_ID: 9fcf5c859adb9e282378fbd37344a0c49cf2c895

--- KGEN/lib/KGENDialect/KGENAttrs.cpp
+++ KGEN/lib/KGENDialect/KGENAttrs.cpp
@@ -258,6 +258,9 @@ ParamExprAttr::verify(llvm::function_ref<mlir::InFlightDiagnostic()> emitError,
     if (operands.size() < 1)
       return emitError()
              << "associative operator must have at least one operand";
+    type = operands[0].getType();
+    if (!type.isSignedInteger() && !type.isUnsignedInteger())
+      return emitError() << "operator requires a signful integer type";
     break;
 
   // Binary expressions.
@@ -267,9 +270,312 @@ ParamExprAttr::verify(llvm::function_ref<mlir::InFlightDiagnostic()> emitError,
   case PEO::Mod:
     if (operands.size() != 2)
       return emitError() << "binary operators must have two operands";
+    type = operands[0].getType();
+    if (!type.isSignedInteger() && !type.isUnsignedInteger())
+      return emitError() << "operator requires a signful integer type";
     break;
   }
   return success();
+}
+
+/// If the specified attribute is a ParamExprAttr with the specified opcode,
+/// return it.  Otherwise return null.
+static ParamExprAttr dyn_castPE(PEO opcode, Attribute value) {
+  if (auto expr = value.dyn_cast<ParamExprAttr>())
+    if (expr.getOpcode() == opcode)
+      return expr;
+  return {};
+}
+
+/// This implements a < comparison for two operands to an associative operation
+/// imposing an ordering upon them.
+///
+/// The ordering provided puts more complex things to the start of the list,
+/// from left to right:
+///    expressions :: decl.refs :: constant
+///
+static bool paramExprOperandSortPredicate(Attribute lhs, Attribute rhs) {
+  // Simplify the code below - we never have to care about exactly equal values.
+  if (lhs == rhs)
+    return false;
+
+  // All expressions are "less than" a constant, since they appear on the right.
+  if (rhs.isa<IntegerAttr>()) {
+    // We don't bother to order constants w.r.t. each other since they will be
+    // folded - they can all compare equal.
+    return !lhs.isa<IntegerAttr>();
+  }
+  if (lhs.isa<IntegerAttr>())
+    return false;
+
+  // Next up are named parameters.
+  if (auto rhsParam = rhs.dyn_cast<ParamDeclRefAttr>()) {
+    // Parameters are sorted lexically w.r.t. each other.
+    if (auto lhsParam = lhs.dyn_cast<ParamDeclRefAttr>())
+      return lhsParam.getName().getValue() < rhsParam.getName().getValue();
+    // They otherwise appear on the right of other things.
+    return true;
+  }
+  if (lhs.isa<ParamDeclRefAttr>())
+    return false;
+
+  // The only thing left are nested expressions.
+  auto lhsExpr = lhs.cast<ParamExprAttr>(), rhsExpr = rhs.cast<ParamExprAttr>();
+  // Sort by the string form of the opcode, e.g. add, .. mul,... then xor.
+  if (lhsExpr.getOpcode() != rhsExpr.getOpcode())
+    return stringifyPEO(lhsExpr.getOpcode()) <
+           stringifyPEO(rhsExpr.getOpcode());
+
+  // If they are the same opcode, then sort by arity: more complex to the left.
+  ArrayRef<Attribute> lhsOperands = lhsExpr.getOperands(),
+                      rhsOperands = rhsExpr.getOperands();
+  if (lhsOperands.size() != rhsOperands.size())
+    return lhsOperands.size() > rhsOperands.size();
+
+  // We know the two subexpressions are different (they'd otherwise be pointer
+  // equivalent) so just go compare all of the elements.
+  for (size_t i = 0, e = lhsOperands.size(); i != e; ++i) {
+    if (paramExprOperandSortPredicate(lhsOperands[i], rhsOperands[i]))
+      return true;
+    if (paramExprOperandSortPredicate(rhsOperands[i], lhsOperands[i]))
+      return false;
+  }
+
+  llvm_unreachable("expressions should never be equivalent");
+  return false;
+}
+
+/// Given a fully associative variadic integer operation, constant fold any
+/// constant operands and move them to the right.  If the whole expression is
+/// constant, then return that, otherwise update the operands list.
+static Attribute simplifyAssocOp(
+    PEO opcode, SmallVector<Attribute, 4> &operands,
+    llvm::function_ref<APInt(const APInt &, const APInt &)> calculateFn,
+    llvm::function_ref<bool(const APInt &)> identityConstantFn,
+    llvm::function_ref<bool(const APInt &)> destructiveConstantFn = {}) {
+  auto type = operands[0].getType();
+  if (operands.size() == 1)
+    return operands[0];
+
+  // Flatten any of the same operation into the operand list:
+  // `(add x, (add y, z))` => `(add x, y, z)`.
+  for (size_t i = 0, e = operands.size(); i != e; ++i) {
+    if (auto subexpr = dyn_castPE(opcode, operands[i])) {
+      std::swap(operands[i], operands.back());
+      operands.pop_back();
+      --e;
+      --i;
+      operands.append(subexpr.getOperands().begin(),
+                      subexpr.getOperands().end());
+    }
+  }
+
+  // Impose an ordering on the operands, pushing subexpressions to the left and
+  // constants to the right, with ParamRefs in the middle - but predictably
+  // ordered w.r.t. each other.
+  llvm::stable_sort(operands, paramExprOperandSortPredicate);
+
+  // Merge any constants, they will appear at the back of the operand list now.
+  if (operands.back().isa<IntegerAttr>()) {
+    while (operands.size() >= 2 &&
+           operands[operands.size() - 2].isa<IntegerAttr>()) {
+      APInt c1 = operands.pop_back_val().cast<IntegerAttr>().getValue();
+      APInt c2 = operands.pop_back_val().cast<IntegerAttr>().getValue();
+      auto resultConstant = IntegerAttr::get(type, calculateFn(c1, c2));
+      operands.push_back(resultConstant);
+    }
+
+    auto resultCst = operands.back().cast<IntegerAttr>();
+
+    // If the resulting constant is the destructive constant (e.g. `x*0`), then
+    // return it.
+    if (destructiveConstantFn && destructiveConstantFn(resultCst.getValue()))
+      return resultCst;
+
+    // Remove the constant back to our operand list if it is the identity
+    // constant for this operator (e.g. `x*1`) and there are other operands.
+    if (identityConstantFn(resultCst.getValue()) && operands.size() != 1)
+      operands.pop_back();
+  }
+
+  return operands.size() == 1 ? operands[0] : Attribute();
+}
+
+/// Analyze an operand to an add.  If it is a multiplication by a constant (e.g.
+/// `(a*b*42)` then split it into the non-constant and the constant portions
+/// (e.g. `a*b` and `42`).  Otherwise return the operand as the first value and
+/// null as the second (standin for "multiplication by 1").
+static std::pair<Attribute, Attribute> decomposeAddend(Attribute operand) {
+  if (auto mul = dyn_castPE(PEO::Mul, operand))
+    if (auto cst = mul.getOperands().back().dyn_cast<IntegerAttr>()) {
+      auto nonCst = ParamExprAttr::get(PEO::Mul, mul.getOperands().drop_back());
+      return {nonCst, cst};
+    }
+  return {operand, Attribute()};
+}
+
+static Attribute getOneOfType(Type type) {
+  return IntegerAttr::get(type, APInt(type.getIntOrFloatBitWidth(), 1));
+}
+
+static Attribute simplifyAdd(SmallVector<Attribute, 4> &operands) {
+  if (auto result = simplifyAssocOp(
+          PEO::Add, operands, [](auto a, auto b) { return a + b; },
+          /*identityCst*/ [](auto cst) { return cst.isZero(); }))
+    return result;
+
+  // Canonicalize the add by splitting all addends into their variable and
+  // constant factors.
+  SmallVector<std::pair<Attribute, Attribute>> decomposedOperands;
+  llvm::SmallDenseSet<Attribute> nonConstantParts;
+  for (auto &op : operands) {
+    decomposedOperands.push_back(decomposeAddend(op));
+
+    // Keep track of non-constant parts we've already seen.  If we see multiple
+    // uses of the same value, then we can fold them together with a multiply.
+    // This handles things like `(a+b+a)` => `(a*2 + b)` and `(a*2 + b + a)` =>
+    // `(a*3 + b)`.
+    if (!nonConstantParts.insert(decomposedOperands.back().first).second) {
+      // The thing we multiply will be the common expression.
+      Attribute mulOperand = decomposedOperands.back().first;
+
+      // Find the index of the first occurrence.
+      size_t i = 0;
+      while (decomposedOperands[i].first != mulOperand)
+        ++i;
+      // Remove both occurrences from the operand list.
+      operands.erase(operands.begin() + (&op - &operands[0]));
+      operands.erase(operands.begin() + i);
+
+      auto type = mulOperand.getType();
+      auto c1 = decomposedOperands[i].second,
+           c2 = decomposedOperands.back().second;
+      // Fill in missing constant multiplicands with 1.
+      if (!c1)
+        c1 = getOneOfType(type);
+      if (!c2)
+        c2 = getOneOfType(type);
+      // Re-add the "a"*(c1+c2) expression to the operand list and
+      // re-canonicalize.
+      auto constant = ParamExprAttr::get(PEO::Add, c1, c2);
+      auto mulCst = ParamExprAttr::get(PEO::Mul, mulOperand, constant);
+      operands.push_back(mulCst);
+      return ParamExprAttr::get(PEO::Add, operands);
+    }
+  }
+
+  return {};
+}
+
+static Attribute simplifyMul(SmallVector<Attribute, 4> &operands) {
+  if (auto result = simplifyAssocOp(
+          PEO::Mul, operands, [](auto a, auto b) { return a * b; },
+          /*identityCst*/ [](auto cst) { return cst.isOne(); },
+          /*destructiveCst*/ [](auto cst) { return cst.isZero(); }))
+    return result;
+
+  // We always build a sum-of-products representation, so if we see an addition
+  // as a subexpr, we need to pull it out: (a+b)*c*d ==> (a*c*d + b*c*d).
+  for (size_t i = 0, e = operands.size(); i != e; ++i) {
+    if (auto addSubExpr = dyn_castPE(PEO::Add, operands[i])) {
+      // Pull the `c*d` operands out - it is whatever operands remain after
+      // removing the `(a+b)` term.
+      operands.erase(operands.begin() + i);
+
+      // Build each add operand.
+      SmallVector<Attribute> addOperands;
+      for (auto addOperand : addSubExpr.getOperands()) {
+        operands.push_back(addOperand);
+        addOperands.push_back(ParamExprAttr::get(PEO::Mul, operands));
+        operands.pop_back();
+      }
+      // Canonicalize and form the add expression.
+      return ParamExprAttr::get(PEO::Add, addOperands);
+    }
+  }
+
+  return {};
+}
+
+static Attribute simplifyAnd(SmallVector<Attribute, 4> &operands) {
+  return simplifyAssocOp(
+      PEO::And, operands, [](auto a, auto b) { return a & b; },
+      /*identityCst*/ [](auto cst) { return cst.isAllOnes(); },
+      /*destructiveCst*/ [](auto cst) { return cst.isZero(); });
+}
+
+static Attribute simplifyOr(SmallVector<Attribute, 4> &operands) {
+  return simplifyAssocOp(
+      PEO::Or, operands, [](auto a, auto b) { return a | b; },
+      /*identityCst*/ [](auto cst) { return cst.isZero(); },
+      /*destructiveCst*/ [](auto cst) { return cst.isAllOnes(); });
+}
+
+static Attribute simplifyXor(SmallVector<Attribute, 4> &operands) {
+  return simplifyAssocOp(
+      PEO::Xor, operands, [](auto a, auto b) { return a ^ b; },
+      /*identityCst*/ [](auto cst) { return cst.isZero(); });
+}
+
+/// Given a binary function, if the two operands are known constant integers,
+/// use the specified fold functions to compute the result.
+static Attribute
+foldBinaryOp(ArrayRef<Attribute> operands,
+             llvm::function_ref<APInt(const APInt &, const APInt &)> unsignedfn,
+             llvm::function_ref<APInt(const APInt &, const APInt &)> signedFn) {
+  assert(operands.size() == 2 && "binary operator always has two operands");
+  if (auto lhs = operands[0].dyn_cast<IntegerAttr>())
+    if (auto rhs = operands[1].dyn_cast<IntegerAttr>()) {
+      auto result = (lhs.getType().isSignedInteger() ? signedFn : unsignedfn)(
+          lhs.getValue(), rhs.getValue());
+      return IntegerAttr::get(lhs.getType(), std::move(result));
+    }
+  return {};
+}
+
+static Attribute simplifyShl(SmallVector<Attribute, 4> &operands) {
+  // Canonicalize `x << cst` => `x * (1<<cst)` to compose correctly with
+  // add/mul canonicalization (also handles constant folding).
+  if (auto rhs = operands[1].dyn_cast<IntegerAttr>()) {
+    auto rhsCst = APInt::getOneBitSet(rhs.getValue().getBitWidth(),
+                                      rhs.getValue().getZExtValue());
+    return ParamExprAttr::get(PEO::Mul, operands[0],
+                              IntegerAttr::get(rhs.getType(), rhsCst));
+  }
+  return {};
+}
+
+static Attribute simplifyShr(SmallVector<Attribute, 4> &operands) {
+  if (auto rhs = operands[1].dyn_cast<IntegerAttr>())
+    if (rhs.getValue().isZero())
+      return operands[0]; // `x >> 0 = x`.
+  // TODO: 0 >> x, -1 >>> x
+
+  return foldBinaryOp(
+      operands, [](auto a, auto b) { return a.lshr(b); },
+      [](auto a, auto b) { return a.ashr(b); });
+}
+
+static Attribute simplifyDiv(SmallVector<Attribute, 4> &operands) {
+  // Implement support for identities like `x/1 = x`.
+  if (auto rhs = operands[1].dyn_cast<IntegerAttr>())
+    if (rhs.getValue().isOne())
+      return operands[0];
+
+  return foldBinaryOp(
+      operands, [](auto a, auto b) { return a.udiv(b); },
+      [](auto a, auto b) { return a.sdiv(b); });
+}
+
+static Attribute simplifyMod(SmallVector<Attribute, 4> &operands) {
+  // Implement support for identities like `x%1 = 0`.
+  if (auto rhs = operands[1].dyn_cast<IntegerAttr>())
+    if (rhs.getValue().isOne())
+      return IntegerAttr::get(rhs.getType(), 0);
+
+  return foldBinaryOp(
+      operands, [](auto a, auto b) { return a.urem(b); },
+      [](auto a, auto b) { return a.srem(b); });
 }
 
 /// Build a parameter expression.  This automatically canonicalizes and
@@ -281,7 +587,45 @@ Attribute ParamExprAttr::get(PEO opcode, ArrayRef<Attribute> operandsIn) {
   assert(llvm::all_of(operandsIn.drop_front(),
                       [&](auto op) { return op.getType() == type; }));
 
-  return Base::get(operandsIn[0].getContext(), opcode, operandsIn, type);
+  SmallVector<Attribute, 4> operands(operandsIn.begin(), operandsIn.end());
+
+  // Verify and canonicalize parameter expressions.
+  Attribute result;
+  switch (opcode) {
+  case PEO::Add:
+    result = simplifyAdd(operands);
+    break;
+  case PEO::Mul:
+    result = simplifyMul(operands);
+    break;
+  case PEO::And:
+    result = simplifyAnd(operands);
+    break;
+  case PEO::Or:
+    result = simplifyOr(operands);
+    break;
+  case PEO::Xor:
+    result = simplifyXor(operands);
+    break;
+  case PEO::Shl:
+    result = simplifyShl(operands);
+    break;
+  case PEO::Shr:
+    result = simplifyShr(operands);
+    break;
+  case PEO::Div:
+    result = simplifyDiv(operands);
+    break;
+  case PEO::Mod:
+    result = simplifyMod(operands);
+    break;
+  }
+
+  // If we folded to an operand, return it.
+  if (result)
+    return result;
+
+  return Base::get(operandsIn[0].getContext(), opcode, operands, type);
 }
 
 /// Builder used by the generic parser.
