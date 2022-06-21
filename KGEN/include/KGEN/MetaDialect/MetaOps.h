2c2a191e4e2664ec8e50eefc2e243e238ac1c7e3
abdul dakkak <adakkak@modular.ai>
2022-06-21 08:50:49 -0700

    [KGEN]: Add `meta.buffer.size` (#1145)
    This adds `meta.buffer.size` to the Meta dialect which gets the size of a `meta.buffer` type
    
    MODULAR_ORIG_COMMIT_REV_ID: 746b409da774383da2228fbb4fb5f0dde20bdda0

---
+++ KGEN/include/KGEN/MetaDialect/MetaOps.h
@@ -0,0 +1,21 @@
+//===- MetaOps.h ----------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file declares the operation classes for the Meta dialect.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef KGEN_KGENDIALECT_METAOPS_H
+#define KGEN_KGENDIALECT_METAOPS_H
+
+#include "KGEN/MetaDialect/MetaTypes.h"
+#include "Support/LLVMCompilerForwardDecls.h"
+#include "mlir/Interfaces/SideEffectInterfaces.h"
+
+#define GET_OP_CLASSES
+#include "KGEN/MetaDialect/Meta.h.inc"
+
+#endif // KGEN_KGENDIALECT_METAOPS_H
