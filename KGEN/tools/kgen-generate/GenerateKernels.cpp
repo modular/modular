eb9a98f15398ed8c3c92b114882c99fedfc5cb8d
Chris Lattner <lattner@users.noreply.github.com>
2022-06-24 16:22:50 +0200

    [kgen-generator] Build collection of all generator impls of interfaces. (#1253)
    This builds the complete set of interface implementations in both the
    primary and library modules together and does type checking to make
    sure the interfaces agree in their implementations.  The diagnostics
    are not great and should be improved.
    
    MODULAR_ORIG_COMMIT_REV_ID: 86f65f9eefcf543be906ab4345431984a30c53b5

--- KGEN/tools/kgen-generate/GenerateKernels.cpp
+++ KGEN/tools/kgen-generate/GenerateKernels.cpp
@@ -9,13 +9,109 @@
 //===----------------------------------------------------------------------===//
 
 #include "Internals.h"
+#include "KGEN/KGENDialect/KGENOps.h"
 #include "Support/LLVMCompilerForwardDecls.h"
 #include "mlir/IR/BuiltinOps.h"
 using namespace M;
+using namespace KGEN;
+
+namespace {
+class KernelGenerator {
+public:
+  KernelGenerator(ModuleOp primary, ModuleOp library)
+      : primaryModule(primary), libraryModule(library) {}
+
+  /// Scan the primary and library module to collect all the interfaces,
+  /// verifying that any common interfaces are the same.
+  ParseResult collectInterfaces();
+
+private:
+  /// These are the two modules we start with.  The primary module is mutated by
+  /// our algorithm, the library module is immutable.
+  ModuleOp primaryModule, libraryModule;
+
+  /// This collects all of the generator implementations of generator
+  /// interfaces, across both the primary module and the library.
+  DenseMap<StringAttr, SmallVector<GeneratorOp, 4>> interfaceImpls;
+};
+} // end anonymous namespace
+
+/// Given a generator interface that is declared in both the primary module and
+/// the library module, check to see if they agree on all signature components.
+/// If not, emit an error indicating the problem.
+static LogicalResult
+diagnoseGeneratorInterfaceMismatches(GeneratorInterfaceOp primaryItf,
+                                     GeneratorInterfaceOp libraryItf) {
+  // Check the SSA arguments line up.
+  if (primaryItf.getFunctionTypeAttr() != libraryItf.getFunctionTypeAttr())
+    return primaryItf.emitError("interface declared with type ")
+           << primaryItf.getFunctionTypeAttr() << " but library expects type "
+           << libraryItf.getFunctionTypeAttr();
+
+  // Check the parameters line up.
+  if (primaryItf.getParamDeclsAttr() != libraryItf.getParamDeclsAttr())
+    return primaryItf.emitError("interface declared with parameters ")
+           << primaryItf.getParamDeclsAttr() << " but library expects "
+           << libraryItf.getParamDeclsAttr();
+
+  if (primaryItf.getNumInputParameters() != libraryItf.getNumInputParameters())
+    return primaryItf.emitError("generator has ")
+           << primaryItf.getNumInputParameters()
+           << " input parameters, but library expects "
+           << libraryItf.getNumInputParameters();
+
+  return success();
+}
+
+/// Scan the primary and library module to collect all the interfaces,
+/// verifying that any common interfaces are the same.
+ParseResult KernelGenerator::collectInterfaces() {
+  // Collect all the generator interfaces in the library module, which will
+  // allow cross checking them below.
+  DenseMap<StringAttr, GeneratorInterfaceOp> libraryInterfaces;
+  for (auto itf : libraryModule.getOps<GeneratorInterfaceOp>())
+    libraryInterfaces[itf.getNameAttr()] = itf;
+
+  // Collect all the kernel generators that implement a given interface,
+  // starting with the library.  These will already have been type checked
+  // within the library.
+  for (auto generator : libraryModule.getOps<GeneratorOp>()) {
+    if (auto interface = generator.getImplementsAttr())
+      interfaceImpls[interface.getAttr()].push_back(generator);
+  }
+
+  // Collect the kernel generators from the primary module.  Start by checking
+  // that any generator implementations that exist in both modules match in
+  // signature exactly.
+  for (auto itf : primaryModule.getOps<GeneratorInterfaceOp>()) {
+    auto it = libraryInterfaces.find(itf.getNameAttr());
+    if (it == libraryInterfaces.end())
+      continue;
+    if (failed(diagnoseGeneratorInterfaceMismatches(itf, it->second)))
+      return failure();
+  }
+
+  // If they all match up, collect the generator implementations from the
+  // primary module.
+  for (auto generator : primaryModule.getOps<GeneratorOp>())
+    if (auto interface = generator.getImplementsAttr())
+      interfaceImpls[interface.getAttr()].push_back(generator);
+
+  return success();
+}
 
 /// Generate kernels in the specified module, incorporating implementation logic
 /// from the specified library.
-LogicalResult M::generateKernels(ModuleOp module, ModuleOp library) {
-  // TODO!
-  return success();
+LogicalResult M::generateKernels(ModuleOp primary, ModuleOp library) {
+  // We currently rely on pointer equivalence between attributes etc when
+  // matching across modules, so the modules must be in the same context.  We
+  // could relax this restriction in the future if there were a reason to.
+  if (primary.getContext() != library.getContext())
+    return primary.emitError() << "Cannot generate kernels when primary and "
+                                  "library are in different MLIR contexts";
+  KernelGenerator generator(primary, library);
+
+  // Scan the primary and library module to collect all the interfaces,
+  // verifying that any common interfaces are the same.
+  return generator.collectInterfaces();
 }
