7846c554c0f89b90dad6606c87f7c16380ba78fc
Chris Lattner <lattner@users.noreply.github.com>
2022-06-26 08:22:12 +0200

    lattner/pr branch 58 (#1280)
    * [kgen-generate] add top level algorithm structure.
    
    After concretizing each kernel in term, the algorithm cleans the
    generators + interfaces out of the primary file, leaving just the
    concretized kernels that were generated.
    
    * [kgen-generate] add top level algorithm structure.
    
    After concretizing each kernel in term, the algorithm cleans the
    generators + interfaces out of the primary file, leaving just the
    concretized kernels that were generated.
    
    MODULAR_ORIG_COMMIT_REV_ID: 15e57cfaa56b3aead749070d6debcd40292a8536

--- KGEN/test/kgen-generate/kgen-generate.mlir
+++ KGEN/test/kgen-generate/kgen-generate.mlir
@@ -1,12 +1,11 @@
 // RUN: kgen-generate %s -library=%S/library.mlir | FileCheck %s
 
-// CHECK-LABEL: kgen.generator @trivial_generator(%arg0: si32) -> si32 {
+// CHECK-NOT: kgen.generator @trivial_generator
 kgen.generator @trivial_generator(%arg0: si32) -> si32 {
-  // CHECK-NEXT: kgen.return %arg0 : si32
   kgen.return %arg0 : si32
 }
 
-// CHECK-LABEL: kgen.generator.interface @unary_add<size>(si32) -> si32
+// CHECK-NOT: kgen.generator.interface @unary_add
 kgen.generator.interface @unary_add<size>(si32) -> si32
 
 kgen.generator @unary_add_impl1<size>(%arg0: si32) -> si32
@@ -19,7 +18,18 @@ kgen.generator @unary_add_impl1<size>(%arg0: si32) -> si32
   kgen.return %arg0 : si32
 }
 
-kgen.kernel @test1(%arg0: si32, %arg1: si32) -> (si32, si32, si32) {
+// CHECK: kgen.kernel @test0() -> index {
+// CHECK-NEXT: %0 = kgen.param.value = <1>
+// CHECK-NEXT:  kgen.return %0 : index
+// CHECK-NEXT: }
+kgen.kernel @test0() -> index {
+  %0 = kgen.param.value = <1>
+  kgen.return %0 : index
+}
+
+
+// TODO: enable this as a kernel test eventually.
+kgen.generator @test1(%arg0: si32, %arg1: si32) -> (si32, si32, si32) {
   // Can invoke the generator directly.
   %0 = kgen.call @trivial_generator(%arg0) : (si32) -> si32
 
--- KGEN/tools/kgen-generate/GenerateKernels.cpp
+++ KGEN/tools/kgen-generate/GenerateKernels.cpp
@@ -15,6 +15,10 @@
 using namespace M;
 using namespace KGEN;
 
+//===----------------------------------------------------------------------===//
+// KernelGenerator class definition
+//===----------------------------------------------------------------------===//
+
 namespace {
 class KernelGenerator {
 public:
@@ -24,6 +28,15 @@ public:
   /// Scan the primary and library module to collect all the interfaces,
   /// verifying that any common interfaces are the same.
   ParseResult collectInterfaces();
+
+  /// Concretize a kernel in the primary file.
+  ParseResult processKernel(KernelOp kernel);
+
+  /// Concretize all kernels in the primary file.
+  ParseResult processKernels();
+
+  /// Remove generators and generator interfaces from the file to clean it up.
+  void removeGenerators();
 
 private:
   /// These are the two modules we start with.  The primary module is mutated by
@@ -35,6 +48,10 @@ private:
   DenseMap<StringAttr, SmallVector<GeneratorOp, 4>> interfaceImpls;
 };
 } // end anonymous namespace
+
+//===----------------------------------------------------------------------===//
+// collectInterfaces and cleanup helpers
+//===----------------------------------------------------------------------===//
 
 /// Scan the primary and library module to collect all the interfaces,
 /// verifying that any common interfaces are the same.
@@ -74,6 +91,45 @@ ParseResult KernelGenerator::collectInterfaces() {
   return success();
 }
 
+/// Remove generators and generator interfaces from the file to clean it up.
+void KernelGenerator::removeGenerators() {
+  for (Operation &op : llvm::make_early_inc_range(primaryModule.getOps())) {
+    if (isa<GeneratorOp, GeneratorInterfaceOp>(op))
+      op.erase();
+  }
+}
+
+//===----------------------------------------------------------------------===//
+// Core Kernel Generator Algorithm
+//===----------------------------------------------------------------------===//
+
+/// Concretize a kernel in the primary file.
+ParseResult KernelGenerator::processKernel(KernelOp kernel) {
+  // TODO: Implement.
+  return success();
+}
+
+ParseResult KernelGenerator::processKernels() {
+  bool didFail = false;
+  SmallVector<KernelOp, 16> kernelsToGenerate;
+
+  // Collect all the kernels to generate in a prepass, because we will be
+  // creating new kernels in the primary file that are already concretized and
+  // we don't want to reprocess them.
+  for (auto kernel : primaryModule.getOps<KernelOp>())
+    kernelsToGenerate.push_back(kernel);
+
+  // Process each kernel.
+  for (auto kernel : kernelsToGenerate)
+    didFail |= failed(processKernel(kernel));
+
+  return failure(didFail);
+}
+
+//===----------------------------------------------------------------------===//
+// generateKernels Driver
+//===----------------------------------------------------------------------===//
+
 /// Generate kernels in the specified module, incorporating implementation logic
 /// from the specified library.
 LogicalResult M::generateKernels(ModuleOp primary, ModuleOp library) {
@@ -87,5 +143,9 @@ LogicalResult M::generateKernels(ModuleOp primary, ModuleOp library) {
 
   // Scan the primary and library module to collect all the interfaces,
   // verifying that any common interfaces are the same.
-  return generator.collectInterfaces();
+  if (generator.collectInterfaces() || generator.processKernels())
+    return failure();
+
+  generator.removeGenerators();
+  return success();
 }
