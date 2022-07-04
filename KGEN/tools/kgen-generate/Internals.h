223b04c4d9c296688cc20a8f7ffd01ffa2d8248f
Chris Lattner <lattner@users.noreply.github.com>
2022-07-03 20:52:07 -0700

    [KGEN] Introduce a helper function for cloning an operation. (#1381)
    This is a clone (ha) of `Operation::clone` but which returns an
    operation->operation mapping in addition to block/value mapping.
    
    MODULAR_ORIG_COMMIT_REV_ID: c3a6a1fdc07b03d9d763fe582e485d80c59cefca

--- KGEN/tools/kgen-generate/Internals.h
+++ KGEN/tools/kgen-generate/Internals.h
@@ -14,6 +14,14 @@ namespace M {
 /// from the specified library.  On error, diagnostics are emitted and the
 /// primary file isn't completely lowered.
 LogicalResult generateKernels(ModuleOp primary, ModuleOp library);
+
+/// This is like `Operation::clone`, but instead of just keeping track of the
+/// block and value mapping for the copy, it also keeps track of the
+/// operation<->operation mapping.  This matters because not all operations have
+/// results.
+Operation *cloneOperation(Operation *original, BlockAndValueMapping &mapper,
+                          DenseMap<Operation *, Operation *> &operationMap);
+
 } // namespace M
 
 #endif // INTERNALS_H
