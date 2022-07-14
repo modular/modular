9f43ae17212f5a0f28110f31847d4e936969327c
abdul dakkak <adakkak@modular.com>
2022-07-14 11:34:37 -0700

    [LLCL]: Fix atomic floating point addition in Softmax (#1564)
    The prior atomic floating point addition was not correct. This fixes that
    
    This introduces an atomic add which works on both integral and floating point values. This utility function abstracts away the lack of atomicAdd on floating point values in C++17.
    
    MODULAR_ORIG_COMMIT_REV_ID: 2bd6872cda076938b2bbd5f2a76a8299828f4109

--- LLCL/include/LLCL/Support/Atomics.h
+++ LLCL/include/LLCL/Support/Atomics.h
@@ -12,8 +12,29 @@
 #define LLCL_SUPPORT_ATOMICS_H
 
 #include <atomic>
+#include <type_traits>
 
 namespace LLCL {
+
+/// This method atomically adds 'accumValue' into 'accum'. This exists because
+/// std::atomic doesn't provide a native add operation for floating point
+/// values. It only works on arithmetic values.
+template <typename T>
+static void atomicAdd(std::atomic<T> &accumValue, const T &value) {
+  static_assert(std::is_arithmetic_v<T>,
+                "the input type T must be an arithmetic type.");
+  if constexpr (std::is_integral_v<T>) {
+    // We use the native add operation for integral values.
+    accumValue += value;
+  } else {
+    // No add operation exists in the C++17 standard so we perform the addition
+    // via a compare_exchange_weak loop.
+    T prevAccumValue = accumValue;
+    while (!accumValue.compare_exchange_weak(prevAccumValue,
+                                             prevAccumValue + value)) {
+    }
+  }
+}
 
 /// This method atomically updates 'maxValue' to 'value' if it is less than it
 /// is already.  This exists because std::atomic doesn't provide a native max
