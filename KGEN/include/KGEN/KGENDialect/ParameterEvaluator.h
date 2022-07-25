bd8e456b0af40e84c86b9ef2e21c1f4392ab8105
Chris Lattner <lattner@users.noreply.github.com>
2022-07-25 15:16:48 -0700

    [KGEN] Split ParameterEvaluator out to its own file, NFC. (#1845)
    This is generally useful for manipulating kgen parameters, pull it out
    of KernelElaborator.
    
    MODULAR_ORIG_COMMIT_REV_ID: 517f202eec41f8711a34d0c0f6b20b28237d459a

---
+++ KGEN/include/KGEN/KGENDialect/ParameterEvaluator.h
@@ -0,0 +1,83 @@
+//===- ParameterEvaluator.h -----------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef KGEN_KGENDIALECT_PARAMETEREVALUATOR_H
+#define KGEN_KGENDIALECT_PARAMETEREVALUATOR_H
+
+#include "KGEN/KGENDialect/KGENAttrs.h"
+
+namespace M {
+// TODO: Need a Support/ForwardDecls.h
+class ErrorOrSuccess;
+template <typename T>
+class ErrorOr;
+} // namespace M
+
+namespace M::KGEN {
+
+/// We expect all parameter expressions to simplify down to concrete constants,
+/// we don't want anything left as a ParamOperatorAttr or ParamDeclRefAttr.
+static inline bool isSimpleConstant(Attribute attr) {
+  return attr.isa<FloatAttr, IntegerAttr, DTypeConstantAttr>();
+}
+
+/// This typedef represents a kernel/generator declaration + a set of input
+/// parameters that provide a complete binding for something that can be
+/// resolved.
+using GeneratorAndInputParamsPair = std::pair<Operation *, ArrayAttr>;
+
+/// This class keeps a set of defined parameter values and is used to evaluate
+/// and simplify parameter expressions based on those values.
+class ParameterEvaluator {
+public:
+  ParameterEvaluator() = default;
+  ParameterEvaluator(ParameterEvaluator &&) = default;
+  ParameterEvaluator(const ParameterEvaluator &) = default;
+
+  /// Given a generator or interface declaration operation, evaluate any
+  /// constraints against inputParamValues.  If the constraints are met, return
+  /// success, otherwise return why they aren't.
+  static ErrorOrSuccess
+  evaluateConstraints(GeneratorAndInputParamsPair declAndInputParams);
+
+  /// Set a value for the specified parameter declaration to the specified
+  /// simplified value.
+  void setParameterValue(ParamDeclAttr decl, Attribute value) {
+    assert(!paramValues.count(decl.getName()) && "parameter already declared!");
+    assert(isSimpleConstant(value) && "expression isn't simplified");
+    paramValues[decl.getName()] = value;
+  }
+
+  /// Given a generic parameter expression, simplify it by folding the
+  /// expression according to known parameter values.  This returns an error if
+  /// the expression cannot be folded for one reason or another.
+  ErrorOr<Attribute> simplifyParameterExpr(Attribute expr);
+
+  /// Get the specified attribute with any nested parameter expressions
+  /// rewritten.  This can fail with incompatible IR (not due to expansion
+  /// errors).  In that case, an error is emitted at the specified location and
+  /// the attribute is returned unmodified.
+  Attribute getReboundAttribute(Attribute attr, Location loc);
+
+  /// Get the specified type with any nested parameter expressions rewritten.
+  /// This can fail with incompatible IR (not due to expansion errors).  In that
+  /// case, an error is emitted at the specified location and the type is
+  /// returned unmodified.
+  Type getReboundType(Type type, Location loc);
+
+private:
+  /// These are the bound parameter values, captured in simplified form.
+  DenseMap<StringAttr, Attribute> paramValues;
+
+  /// This caches attributes and Types with parameter references rebound, and
+  /// remembers complex attributes that don't have parameter subexprs (noted as
+  /// being rebound to themselves).
+  DenseMap<Attribute, Attribute> rewrittenAttrs;
+  DenseMap<Type, Type> rewrittenTypes;
+};
+} // namespace M::KGEN
+
+#endif // KGEN_KGENDIALECT_PARAMETEREVALUATOR_H
---
+++ KGEN/lib/KGENDialect/ParameterEvaluator.cpp
@@ -0,0 +1,222 @@
+//===- ParameterEvaluator.cpp ---------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#include "KGEN/KGENDialect/ParameterEvaluator.h"
+#include "KGEN/KGENDialect/KGENTypes.h"
+#include "Support/ErrorOr.h"
+#include "mlir/IR/BuiltinTypes.h"
+#include "mlir/IR/Diagnostics.h"
+using namespace M;
+using namespace M::KGEN;
+
+static std::string getString(Attribute attr) {
+  std::string str;
+  llvm::raw_string_ostream(str) << attr;
+  return str;
+}
+
+/// Given a generic parameter expression, simplify it by folding the
+/// expression according to known parameter values.  This returns an error if
+/// the expression cannot be folded for one reason or another.
+ErrorOr<Attribute> ParameterEvaluator::simplifyParameterExpr(Attribute expr) {
+  // Simple constants don't need simplification.
+  if (isSimpleConstant(expr))
+    return expr;
+
+  // We can directly substitute declaration references given our known table of
+  // bindings.
+  if (auto declRef = expr.dyn_cast<ParamDeclRefAttr>()) {
+    auto value = paramValues[declRef.getName()];
+    assert(value && "Verifier should check that all parameters are defined");
+    return value;
+  }
+
+  // Simplify operators by recursively simplifying their operands, then
+  // refolding the expression.
+  if (auto oper = expr.dyn_cast<ParamOperatorAttr>()) {
+    SmallVector<Attribute> simplifiedOperands;
+    for (auto value : oper.getOperands()) {
+      auto simplified = simplifyParameterExpr(value);
+      if (simplified.isError())
+        return simplified;
+      simplifiedOperands.push_back(simplified.takeValue());
+    }
+
+    // FIXME: 'index' folding should require target information to simplify
+    // things like div.
+    auto result = ParamOperatorAttr::get(oper.getOpcode(), simplifiedOperands);
+    if (!isSimpleConstant(result))
+      return Error("could not simplify operator " + getString(expr));
+    return result;
+  }
+
+  // Otherwise, we don't know how to simplify this attribute, it's an error.
+  return Error("unknown expression to fold: " + getString(expr));
+}
+
+/// Given a generator or interface declaration operation, evaluate any
+/// constraints against inputParamValues.  If the constraints are met, return
+/// success, otherwise return why they aren't.
+ErrorOrSuccess ParameterEvaluator::evaluateConstraints(
+    GeneratorAndInputParamsPair declAndInputParams) {
+  Operation *decl = declAndInputParams.first;
+
+  // If there are no constraints, we are trivially done.
+  auto constraints = getDeclConstraints(decl);
+  if (constraints.empty())
+    return success();
+
+  // Otherwise, we have constraints to evaluate.  Bind each of the input
+  // parameter names.
+  ParameterEvaluator evaluator;
+  auto inputParamDecls = getDeclParameterInfo(decl).first;
+  ArrayRef<Attribute> inputParamValues = declAndInputParams.second.getValue();
+  assert(inputParamDecls.size() == inputParamValues.size() &&
+         "incorrect number of input parameters");
+  for (auto [decl, value] : llvm::zip(inputParamDecls, inputParamValues))
+    evaluator.setParameterValue(decl.cast<ParamDeclAttr>(), value);
+
+  // Each constraint must be foldable, and must fold to true.
+  for (auto constraint : constraints) {
+    ErrorOr<Attribute> result = evaluator.simplifyParameterExpr(constraint);
+    if (failed(result))
+      return Error("constraint evaluation failure: " +
+                   Twine(result.getError()));
+    auto resultInt = (*result).dyn_cast<IntegerAttr>();
+    if (!resultInt || resultInt.getValue().getBitWidth() != 1)
+      return Error("constraint evaluation didn't return true or false");
+
+    // TODO: This isn't a very pretty printing of why the constraint failed.
+    if (resultInt.getValue().isZero())
+      return Error("constraint failed: " + getString(constraint));
+  }
+
+  // If we made it this far, then everything folded to true.
+  return success();
+}
+
+/// Get the specified attribute with any nested parameter expressions
+/// rewritten.  This can fail with incompatible IR (not due to expansion
+/// errors).  In that case, an error is emitted at the specified location and
+/// the attribute is returned unmodified.
+Attribute ParameterEvaluator::getReboundAttribute(Attribute attr,
+                                                  Location loc) {
+  // These are common leaf attributes that we know are never parameterized.
+  if (attr.isa<IntegerAttr, FloatAttr, StringAttr, SymbolRefAttr,
+               DTypeConstantAttr>())
+    return attr;
+
+  // If we've already processed this attribute, just reuse the memoized result.
+  auto iter = rewrittenAttrs.find(attr);
+  if (iter != rewrittenAttrs.end())
+    return iter->second;
+
+  // TODO(jeff): MLIR attribute should not carry types!
+  if (getReboundType(attr.getType(), loc) != attr.getType()) {
+    emitError(loc, "unsupported parameterized type in attribute ") << attr;
+    return rewrittenAttrs[attr] = attr;
+  }
+
+  // If this is a foldable parameter expression, do it.
+  Attribute result = attr;
+  if (attr.isa<ParamDeclRefAttr, ParamOperatorAttr>()) {
+    auto newVal = simplifyParameterExpr(attr);
+    if (!newVal.isError())
+      result = newVal.takeValue();
+
+  } else if (auto typeAttr = attr.dyn_cast<TypeAttr>()) {
+    // TODO: Improve SubElementTypeInterface
+    Type newType = getReboundType(typeAttr.getValue(), loc);
+    if (newType != typeAttr.getValue())
+      result = TypeAttr::get(newType);
+  } else if (auto itf = attr.dyn_cast<mlir::SubElementAttrInterface>()) {
+    SmallVector<std::pair<size_t, Attribute>> newAttrs;
+    bool changedType = false;
+    size_t attrNo = 0;
+    itf.walkImmediateSubElements(
+        [&](Attribute attr) {
+          auto newAttr = getReboundAttribute(attr, loc);
+          if (newAttr != attr)
+            newAttrs.push_back(std::make_pair(attrNo, newAttr));
+          ++attrNo;
+        },
+        [&](Type type) { changedType = type != getReboundType(type, loc); });
+    if (changedType) {
+      // TODO: Improve SubElementTypeInterface:
+      // https://github.com/llvm/llvm-project/issues/56355
+      emitError(loc, "don't know how to rebind parameterized subtypes in ")
+          << attr;
+    } else if (!newAttrs.empty()) {
+      result = itf.replaceImmediateSubAttribute(newAttrs);
+    }
+  } else {
+    emitError(loc, "unknown attribute in parameterized operation ") << attr;
+  }
+
+  return rewrittenAttrs[attr] = result;
+}
+
+/// Get the specified type with any nested parameter expressions rewritten. This
+/// can fail with incompatible IR (not due to expansion errors).  In that case,
+/// an error is emitted at the specified location and the type is returned
+/// unmodified.
+Type ParameterEvaluator::getReboundType(Type type, Location loc) {
+  // These are known leaf types that don't participate with
+  // SubElementTypeInterface and have no attributes or types within them.
+  if (type.isa<IntegerType, FloatType, NoneType, IndexType, DTypeType>())
+    return type;
+
+  // If we've already processed this type, just reuse the memoized result.
+  auto iter = rewrittenTypes.find(type);
+  if (iter != rewrittenTypes.end())
+    return iter->second;
+
+  Type result = type;
+
+  // Rebind types in aggregates that we know, but otherwise just scan the types
+  // that we don't.
+  if (auto fnType = type.dyn_cast<FunctionType>()) {
+    // Function types show up in kernel signatures.
+    SmallVector<Type> inputs, results;
+    bool changed = false;
+    for (Type input : fnType.getInputs()) {
+      inputs.push_back(getReboundType(input, loc));
+      changed |= inputs.back() != input;
+    }
+    for (Type result : fnType.getResults()) {
+      results.push_back(getReboundType(result, loc));
+      changed |= results.back() != result;
+    }
+
+    if (changed)
+      result = FunctionType::get(type.getContext(), inputs, results);
+
+  } else if (auto itf = type.dyn_cast<mlir::SubElementTypeInterface>()) {
+    SmallVector<std::pair<size_t, Attribute>> newAttrs;
+    bool changedType = false;
+    size_t attrNo = 0;
+    itf.walkImmediateSubElements(
+        [&](Attribute attr) {
+          auto newAttr = getReboundAttribute(attr, loc);
+          if (newAttr != attr)
+            newAttrs.push_back(std::make_pair(attrNo, newAttr));
+          ++attrNo;
+        },
+        [&](Type type) { changedType = type != getReboundType(type, loc); });
+    if (changedType) {
+      // TODO: Improve SubElementTypeInterface:
+      // https://github.com/llvm/llvm-project/issues/56355
+      emitError(loc, "don't know how to rebind parameterized subtypes in ")
+          << type;
+    } else if (!newAttrs.empty()) {
+      result = itf.replaceImmediateSubAttribute(newAttrs);
+    }
+  } else {
+    emitError(loc, "unknown type in parameterized operation ") << type;
+  }
+
+  return rewrittenTypes[type] = result;
+}
