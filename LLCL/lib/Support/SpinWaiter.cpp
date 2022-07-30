de22f331e4393d8599122115747d8e01ea681fb8
Chris Lattner <lattner@users.noreply.github.com>
2022-07-30 11:11:47 -0700

    [LLCL SpinWaiter] Move system call stuff out of line. NFC. (#1984)
    This logic is cold by definition and causes a significant amount of
    code bloat in things like Atomic.h and RingBuffer.
    
    MODULAR_ORIG_COMMIT_REV_ID: 98afe2ebc0bba11596fe480ae3852e0f3ad43ce7

---
+++ LLCL/lib/Support/SpinWaiter.cpp
@@ -0,0 +1,24 @@
+//===- SpinWaiter.cpp -----------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#include "LLCL/Support/SpinWaiter.h"
+
+using namespace LLCL;
+
+bool Detail::SpinWaiterBase::yieldToOS() {
+  // If that didn't work, we yield the thread back to the OS.  This is much
+  // heavier weight but can cause the OS to reschedule the problematic thread.
+  if (iterations < yieldSpins) {
+    std::this_thread::yield();
+    return true;
+  }
+
+  // Otherwise, we're in pretty serious trouble, actually go to sleep for
+  // longer times.
+  std::this_thread::sleep_for(std::chrono::microseconds(iterations / 128));
+  iterations += 128;
+  return true;
+}
