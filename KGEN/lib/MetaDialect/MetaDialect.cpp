7f6083efca8ec0ebd4ee7b892cb5a26f78b30dc1
Jeff Niu <jeff@modular.com>
2022-08-16 13:07:13 -0700

    [kgen] Move meta types to MetaTypes.cpp (NFC) (#2265)
    MODULAR_ORIG_COMMIT_REV_ID: 9944d037ec3201904f84600a10fd03a55a78aa63

--- KGEN/lib/MetaDialect/MetaDialect.cpp
+++ KGEN/lib/MetaDialect/MetaDialect.cpp
@@ -9,169 +9,13 @@
 //===----------------------------------------------------------------------===//
 
 #include "KGEN/MetaDialect/MetaDialect.h"
-#include "KGEN/KGENDialect/KGENAttrs.h"
 #include "KGEN/KGENDialect/KGENOps.h"
 #include "KGEN/KGENDialect/KGENParameters.h"
-#include "KGEN/KGENDialect/KGENTypes.h"
 #include "KGEN/MetaDialect/MetaOps.h"
-#include "KGEN/MetaDialect/MetaTypes.h"
 #include "Support/LLVMCompilerForwardDecls.h"
-#include "mlir/IR/Builders.h"
-#include "mlir/IR/DialectImplementation.h"
-#include "llvm/ADT/TypeSwitch.h"
 
 using namespace M;
 using namespace KGEN;
-
-//===----------------------------------------------------------------------===//
-// custom<ParamDTypeValue>
-//===----------------------------------------------------------------------===//
-
-static ParseResult parseParamDTypeValue(AsmParser &p,
-                                        FailureOr<TypedAttr> &result) {
-  TypedAttr retValue;
-  if (failed(parseParamValue(p, retValue, p.getBuilder().getType<DTypeType>())))
-    return failure();
-  result = retValue;
-  return success();
-}
-
-static void printParamDTypeValue(AsmPrinter &p, Attribute value) {
-  printParamValue(p, value);
-}
-
-//===----------------------------------------------------------------------===//
-// custom<OptionalParamDTypeValue>
-//===----------------------------------------------------------------------===//
-
-static ParseResult parseOptionalParamDTypeValue(AsmParser &p,
-                                                FailureOr<TypedAttr> &result) {
-  if (succeeded(p.parseOptionalQuestion())) {
-    result = TypedAttr();
-    return success();
-  }
-  return parseParamDTypeValue(p, result);
-}
-
-static void printOptionalParamDTypeValue(AsmPrinter &p, Attribute value) {
-  if (!value) {
-    p << '?';
-    return;
-  }
-  printParamDTypeValue(p, value);
-}
-
-//===----------------------------------------------------------------------===//
-// ScalarType
-//===----------------------------------------------------------------------===//
-
-LogicalResult
-ScalarType::verify(llvm::function_ref<mlir::InFlightDiagnostic()> emitError,
-                   TypedAttr dtype) {
-  if (!dtype.getType().isa<DTypeType>())
-    return emitError() << "parameter for scalar type must be a !kgen.dtype";
-  return success();
-}
-
-void ScalarType::walkImmediateSubElements(
-    function_ref<void(Attribute)> walkAttrsFn,
-    function_ref<void(Type)> walkTypesFn) const {
-  walkAttrsFn(getDType());
-}
-
-Type ScalarType::replaceImmediateSubElements(ArrayRef<Attribute> replAttrs,
-                                             ArrayRef<Type> replTypes) const {
-  assert(replAttrs.size() == 1 && replTypes.empty());
-  return ScalarType::get(replAttrs[0]);
-}
-
-//===----------------------------------------------------------------------===//
-// SIMDType
-//===----------------------------------------------------------------------===//
-
-LogicalResult
-SIMDType::verify(llvm::function_ref<mlir::InFlightDiagnostic()> emitError,
-                 TypedAttr size, TypedAttr dtype) {
-  if (!size || !dtype)
-    return emitError() << "simd type requires size and dtype";
-  if (!size.getType().isIndex())
-    return emitError() << "size parameter for simd must have type `index`";
-  if (!dtype.getType().isa<DTypeType>())
-    return emitError() << "type parameter for simd must be a !kgen.dtype";
-  return success();
-}
-
-void SIMDType::walkImmediateSubElements(
-    function_ref<void(Attribute)> walkAttrsFn,
-    function_ref<void(Type)> walkTypesFn) const {
-  walkAttrsFn(getSize());
-  walkAttrsFn(getDType());
-}
-
-Type SIMDType::replaceImmediateSubElements(ArrayRef<Attribute> replAttrs,
-                                           ArrayRef<Type> replTypes) const {
-  assert(replAttrs.size() == 2 && replTypes.empty());
-  return SIMDType::get(replAttrs[0], replAttrs[1]);
-}
-
-//===----------------------------------------------------------------------===//
-// BufferType
-//===----------------------------------------------------------------------===//
-
-LogicalResult
-BufferType::verify(llvm::function_ref<mlir::InFlightDiagnostic()> emitError,
-                   TypedAttr size, TypedAttr dtype) {
-  if (size && !size.getType().isIndex())
-    return emitError() << "size parameter for buffer must have type `index`";
-  if (dtype && !dtype.getType().isa<DTypeType>())
-    return emitError() << "type parameter for buffer must be a !kgen.dtype";
-  return success();
-}
-
-void BufferType::walkImmediateSubElements(
-    function_ref<void(Attribute)> walkAttrsFn,
-    function_ref<void(Type)> walkTypesFn) const {
-  walkAttrsFn(getSize());
-  walkAttrsFn(getDType());
-}
-
-Type BufferType::replaceImmediateSubElements(ArrayRef<Attribute> replAttrs,
-                                             ArrayRef<Type> replTypes) const {
-  assert(replAttrs.size() == 2 && replTypes.empty());
-  return BufferType::get(getContext(), replAttrs[0], replAttrs[1]);
-}
-
-//===----------------------------------------------------------------------===//
-// PointerType
-//===----------------------------------------------------------------------===//
-
-LogicalResult
-PointerType::verify(llvm::function_ref<mlir::InFlightDiagnostic()> emitError,
-                    TypedAttr dtype) {
-  if (dtype && !dtype.getType().isa<DTypeType>())
-    return emitError() << "type parameter for pointer must be a !kgen.dtype";
-  return success();
-}
-
-void PointerType::walkImmediateSubElements(
-    function_ref<void(Attribute)> walkAttrsFn,
-    function_ref<void(Type)> walkTypesFn) const {
-  walkAttrsFn(getDType());
-}
-
-Type PointerType::replaceImmediateSubElements(ArrayRef<Attribute> replAttrs,
-                                              ArrayRef<Type> replTypes) const {
-  assert(replAttrs.size() == 1 && replTypes.empty());
-  return PointerType::get(replAttrs[0]);
-}
-
-//===----------------------------------------------------------------------===//
-// Dialect Type Parsing and Printing
-//===----------------------------------------------------------------------===//
-
-// Pull in the dialect definition.
-#define GET_TYPEDEF_CLASSES
-#include "KGEN/MetaDialect/MetaTypes.cpp.inc"
 
 //===----------------------------------------------------------------------===//
 // Dialect specification.
@@ -182,10 +26,8 @@ Type PointerType::replaceImmediateSubElements(ArrayRef<Attribute> replAttrs,
 
 void MetaDialect::initialize() {
   // Register types.
-  addTypes<
-#define GET_TYPEDEF_LIST
-#include "KGEN/MetaDialect/MetaTypes.cpp.inc"
-      >();
+  registerTypes();
+
   // Register operations.
   addOperations<
 #define GET_OP_LIST
--- KGEN/lib/MetaDialect/MetaTypes.cpp
+++ KGEN/lib/MetaDialect/MetaTypes.cpp
@@ -5,11 +5,19 @@
 //===----------------------------------------------------------------------===//
 
 #include "KGEN/MetaDialect/MetaTypes.h"
+#include "KGEN/KGENDialect/KGENTypes.h"
 #include "mlir/IR/BuiltinTypes.h"
 #include "mlir/IR/Diagnostics.h"
+#include "mlir/IR/DialectImplementation.h"
+#include "llvm/ADT/TypeSwitch.h"
+#include "mlir/IR/Builders.h"
 
 using namespace M;
 using namespace KGEN;
+
+//===----------------------------------------------------------------------===//
+// Casting Between Meta and Builtin
+//===----------------------------------------------------------------------===//
 
 LogicalResult M::KGEN::checkMetaCastedTypes(
     function_ref<InFlightDiagnostic(StringRef)> emitError, Type metaTy,
@@ -48,4 +56,165 @@ LogicalResult M::KGEN::checkMetaCastedTypes(
       emitError, metaTy, standardTy, [standardTy](DTypeConstantAttr dtype) {
         return success(dtype.isCompatibleWith(standardTy));
       });
+}
+
+//===----------------------------------------------------------------------===//
+// custom<ParamDTypeValue>
+//===----------------------------------------------------------------------===//
+
+static ParseResult parseParamDTypeValue(AsmParser &p,
+                                        FailureOr<TypedAttr> &result) {
+  TypedAttr retValue;
+  if (failed(parseParamValue(p, retValue, p.getBuilder().getType<DTypeType>())))
+    return failure();
+  result = retValue;
+  return success();
+}
+
+static void printParamDTypeValue(AsmPrinter &p, Attribute value) {
+  printParamValue(p, value);
+}
+
+//===----------------------------------------------------------------------===//
+// custom<OptionalParamDTypeValue>
+//===----------------------------------------------------------------------===//
+
+static ParseResult parseOptionalParamDTypeValue(AsmParser &p,
+                                                FailureOr<TypedAttr> &result) {
+  if (succeeded(p.parseOptionalQuestion())) {
+    result = TypedAttr();
+    return success();
+  }
+  return parseParamDTypeValue(p, result);
+}
+
+static void printOptionalParamDTypeValue(AsmPrinter &p, Attribute value) {
+  if (!value) {
+    p << '?';
+    return;
+  }
+  printParamDTypeValue(p, value);
+}
+
+//===----------------------------------------------------------------------===//
+// ScalarType
+//===----------------------------------------------------------------------===//
+
+LogicalResult
+ScalarType::verify(llvm::function_ref<mlir::InFlightDiagnostic()> emitError,
+                   TypedAttr dtype) {
+  if (!dtype.getType().isa<DTypeType>())
+    return emitError() << "parameter for scalar type must be a !kgen.dtype";
+  return success();
+}
+
+void ScalarType::walkImmediateSubElements(
+    function_ref<void(Attribute)> walkAttrsFn,
+    function_ref<void(Type)> walkTypesFn) const {
+  walkAttrsFn(getDType());
+}
+
+Type ScalarType::replaceImmediateSubElements(ArrayRef<Attribute> replAttrs,
+                                             ArrayRef<Type> replTypes) const {
+  assert(replAttrs.size() == 1 && replTypes.empty());
+  return ScalarType::get(replAttrs[0]);
+}
+
+//===----------------------------------------------------------------------===//
+// SIMDType
+//===----------------------------------------------------------------------===//
+
+LogicalResult
+SIMDType::verify(llvm::function_ref<mlir::InFlightDiagnostic()> emitError,
+                 TypedAttr size, TypedAttr dtype) {
+  if (!size || !dtype)
+    return emitError() << "simd type requires size and dtype";
+  if (!size.getType().isIndex())
+    return emitError() << "size parameter for simd must have type `index`";
+  if (!dtype.getType().isa<DTypeType>())
+    return emitError() << "type parameter for simd must be a !kgen.dtype";
+  return success();
+}
+
+void SIMDType::walkImmediateSubElements(
+    function_ref<void(Attribute)> walkAttrsFn,
+    function_ref<void(Type)> walkTypesFn) const {
+  walkAttrsFn(getSize());
+  walkAttrsFn(getDType());
+}
+
+Type SIMDType::replaceImmediateSubElements(ArrayRef<Attribute> replAttrs,
+                                           ArrayRef<Type> replTypes) const {
+  assert(replAttrs.size() == 2 && replTypes.empty());
+  return SIMDType::get(replAttrs[0], replAttrs[1]);
+}
+
+//===----------------------------------------------------------------------===//
+// BufferType
+//===----------------------------------------------------------------------===//
+
+LogicalResult
+BufferType::verify(llvm::function_ref<mlir::InFlightDiagnostic()> emitError,
+                   TypedAttr size, TypedAttr dtype) {
+  if (size && !size.getType().isIndex())
+    return emitError() << "size parameter for buffer must have type `index`";
+  if (dtype && !dtype.getType().isa<DTypeType>())
+    return emitError() << "type parameter for buffer must be a !kgen.dtype";
+  return success();
+}
+
+void BufferType::walkImmediateSubElements(
+    function_ref<void(Attribute)> walkAttrsFn,
+    function_ref<void(Type)> walkTypesFn) const {
+  walkAttrsFn(getSize());
+  walkAttrsFn(getDType());
+}
+
+Type BufferType::replaceImmediateSubElements(ArrayRef<Attribute> replAttrs,
+                                             ArrayRef<Type> replTypes) const {
+  assert(replAttrs.size() == 2 && replTypes.empty());
+  return BufferType::get(getContext(), replAttrs[0], replAttrs[1]);
+}
+
+//===----------------------------------------------------------------------===//
+// PointerType
+//===----------------------------------------------------------------------===//
+
+LogicalResult
+PointerType::verify(llvm::function_ref<mlir::InFlightDiagnostic()> emitError,
+                    TypedAttr dtype) {
+  if (dtype && !dtype.getType().isa<DTypeType>())
+    return emitError() << "type parameter for pointer must be a !kgen.dtype";
+  return success();
+}
+
+void PointerType::walkImmediateSubElements(
+    function_ref<void(Attribute)> walkAttrsFn,
+    function_ref<void(Type)> walkTypesFn) const {
+  walkAttrsFn(getDType());
+}
+
+Type PointerType::replaceImmediateSubElements(ArrayRef<Attribute> replAttrs,
+                                              ArrayRef<Type> replTypes) const {
+  assert(replAttrs.size() == 1 && replTypes.empty());
+  return PointerType::get(replAttrs[0]);
+}
+
+//===----------------------------------------------------------------------===//
+// Dialect Type Parsing and Printing
+//===----------------------------------------------------------------------===//
+
+// Pull in the dialect definition.
+#define GET_TYPEDEF_CLASSES
+#include "KGEN/MetaDialect/MetaTypes.cpp.inc"
+
+//===----------------------------------------------------------------------===//
+// MetaDialect type support
+//===----------------------------------------------------------------------===//
+
+void MetaDialect::registerTypes() {
+  addTypes<
+#define GET_TYPEDEF_LIST
+#include "KGEN/MetaDialect/MetaTypes.cpp.inc"
+      >();
 }
