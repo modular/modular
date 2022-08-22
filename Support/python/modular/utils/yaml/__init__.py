d4ebfb1c0a5f6f67f07d9f9251ea105132954180
Laszlo Kindrat <laszlo@modular.ai>
2022-08-22 10:33:54 -0400

    [Python] Resolve ruamel deprecation warnings in utility lib (#2365)
    This PR reimplements the yaml utilities and tests so that they no longer rely on the deprecated `ruamel.yaml.dump` method.
    
    MODULAR_ORIG_COMMIT_REV_ID: 08acd0799ba802f3b522bd9c68c8d4c84bcb0b6d

--- Support/python/modular/utils/yaml/__init__.py
+++ Support/python/modular/utils/yaml/__init__.py
@@ -4,7 +4,7 @@
 #
 ##===----------------------------------------------------------------------===##
 
-from ._yaml import YAMLNoAliasDumper, represent_as_string, yaml
+from ._yaml import YAML, represent_as_string
 
 # Remove from the namespace so that it's not visible to users.
 del _yaml
