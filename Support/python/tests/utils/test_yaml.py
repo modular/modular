8a7db9c9db631dbff062ea814f8f40d0359a52a7
Laszlo Kindrat <laszlo@modular.ai>
2022-08-22 10:33:54 -0400

    [Python] Resolve ruamel deprecation warnings in utility lib (#2365)
    This PR reimplements the yaml utilities and tests so that they no longer rely on the deprecated `ruamel.yaml.dump` method.
    
    MODULAR_ORIG_COMMIT_REV_ID: 08acd0799ba802f3b522bd9c68c8d4c84bcb0b6d

--- Support/python/tests/utils/test_yaml.py
+++ Support/python/tests/utils/test_yaml.py
@@ -4,10 +4,14 @@
 #
 ##===----------------------------------------------------------------------===##
 
+
 from enum import Enum
 from pathlib import Path, PosixPath, WindowsPath
 
-from modular.utils.yaml import YAMLNoAliasDumper, represent_as_string, yaml
+from ruamel.yaml.compat import StringIO
+
+from modular.utils.typing import Any
+from modular.utils.yaml import YAML, represent_as_string
 
 EXPECTED_NO_ALIAS_DUMP = """a_dict:
   a_value: 123
@@ -18,13 +22,19 @@ another_dict:
 """
 
 
-def test_YAMLNoAliasDumper():
+def _dump_str(data: Any, *, sort: bool = True) -> str:
+    stream = StringIO()
+    YAML().dump(data, stream, sort=sort)
+    return stream.getvalue()
+
+
+def test_YAML_dumps():
     ref_list = ["a", "b", "c"]
     test_dict = {
         "a_dict": {"a_value": 123, "ref": ref_list},
         "another_dict": {"same_value": 123, "same_ref": ref_list},
     }
-    yaml_str = yaml.dump(test_dict, Dumper=YAMLNoAliasDumper)
+    yaml_str = _dump_str(test_dict)
     assert yaml_str == EXPECTED_NO_ALIAS_DUMP
 
 
@@ -39,5 +49,5 @@ def test_represent_as_string():
     represent_as_string([SomeData, PosixPath, WindowsPath])
 
     test_list = [Path("some/posix/path"), SomeData.FOO, SomeData.BAR]
-    yaml_str = yaml.dump(test_list)
+    yaml_str = _dump_str(test_list)
     assert yaml_str == "[some/posix/path, foo, Bar]\n"
