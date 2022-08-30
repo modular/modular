4f5a4fe6fa7575cbd4b862ede4f6c8d1bbbe8eb4
Joseph Loser <joe@modular.com>
2022-08-30 14:11:02 -0600

    [Support] Convert HMAC tests to use Google Test (#2514)
    HMAC tests currently define their own main function and it gets run as part of
    `check-all` target. We're looking to move towards leveraging a unit testing
    framework for ease going forward.
    
    This commit converts the HMAC tests to use `TEST` macros from Google Test to
    have as an example as we add more unit tests in the future for the `Support`
    library.
    
    Note (planned follow-ups):
    - Look into the `-Wno-global-constructors` issue.
    - Look into GTest CMake module and `gtest_discover_tests`.
    
    Fixes #2498.
    
    MODULAR_ORIG_COMMIT_REV_ID: 5ace71091385b25aea74acf4dbd6317780f504b6

--- Support/test/HMACTest.cpp
+++ Support/test/HMACTest.cpp
@@ -7,31 +7,28 @@
 #include "Support/HMAC.h"
 #include "llvm/ADT/StringExtras.h"
 #include "llvm/ADT/StringRef.h"
-#include "llvm/Support/raw_ostream.h"
+
+#include "gtest/gtest.h"
 
 using namespace M;
 
 /// Compute the HMAC of some test vectors provided by the wikipedia article on
 /// HMAC.
-int main() {
+TEST(HMACTest, ComputeHmacSHA256ForShortKey) {
   auto output =
       hmacSHA256("The quick brown fox jumps over the lazy dog", "key");
   auto hexStr = llvm::toHex(output);
-  if (!StringRef(hexStr).equals_insensitive(
-          "f7bc83f430538424b13298e6aa6fb143ef4d59a14946175997479dbc2d1a3cd8")) {
-    llvm::outs() << "HMAC mismatch for short key: " << hexStr;
-    return EXIT_FAILURE;
-  }
+  EXPECT_STRCASEEQ(
+      hexStr.data(),
+      "f7bc83f430538424b13298e6aa6fb143ef4d59a14946175997479dbc2d1a3cd8");
+}
 
-  output =
+TEST(HMACTest, ComputeHmacSHA256ForLongKey) {
+  auto output =
       hmacSHA256("message", "The quick brown fox jumps over the lazy dogThe "
                             "quick brown fox jumps over the lazy dog");
-  hexStr = llvm::toHex(output);
-  if (!StringRef(hexStr).equals_insensitive(
-          "5597b93a2843078cbb0c920ae41dfe20f1685e10c67e423c11ab91adfc319d12")) {
-    llvm::outs() << "HMAC mismatch for long key: " << hexStr;
-    return EXIT_FAILURE;
-  }
-
-  return EXIT_SUCCESS;
+  auto hexStr = llvm::toHex(output);
+  EXPECT_STRCASEEQ(
+      hexStr.data(),
+      "5597b93a2843078cbb0c920ae41dfe20f1685e10c67e423c11ab91adfc319d12");
 }
