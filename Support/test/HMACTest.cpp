310d17f3f9221de5e913b902eb977ac6201f8367
Joseph Loser <joeloser@fastmail.com>
2022-08-30 14:47:34 -0600

    Revert "[Support] Convert HMAC tests to use Google Test" (#2525)
    Reverts [Internal link]  CI doesn't appear to be running these tests — needs to be investigated.
    
    MODULAR_ORIG_COMMIT_REV_ID: 9e0d836d2b495619f08e7896877e70f8ce196cfe

--- Support/test/HMACTest.cpp
+++ Support/test/HMACTest.cpp
@@ -7,28 +7,31 @@
 #include "Support/HMAC.h"
 #include "llvm/ADT/StringExtras.h"
 #include "llvm/ADT/StringRef.h"
-
-#include "gtest/gtest.h"
+#include "llvm/Support/raw_ostream.h"
 
 using namespace M;
 
 /// Compute the HMAC of some test vectors provided by the wikipedia article on
 /// HMAC.
-TEST(HMACTest, ComputeHmacSHA256ForShortKey) {
+int main() {
   auto output =
       hmacSHA256("The quick brown fox jumps over the lazy dog", "key");
   auto hexStr = llvm::toHex(output);
-  EXPECT_STRCASEEQ(
-      hexStr.data(),
-      "f7bc83f430538424b13298e6aa6fb143ef4d59a14946175997479dbc2d1a3cd8");
-}
+  if (!StringRef(hexStr).equals_insensitive(
+          "f7bc83f430538424b13298e6aa6fb143ef4d59a14946175997479dbc2d1a3cd8")) {
+    llvm::outs() << "HMAC mismatch for short key: " << hexStr;
+    return EXIT_FAILURE;
+  }
 
-TEST(HMACTest, ComputeHmacSHA256ForLongKey) {
-  auto output =
+  output =
       hmacSHA256("message", "The quick brown fox jumps over the lazy dogThe "
                             "quick brown fox jumps over the lazy dog");
-  auto hexStr = llvm::toHex(output);
-  EXPECT_STRCASEEQ(
-      hexStr.data(),
-      "5597b93a2843078cbb0c920ae41dfe20f1685e10c67e423c11ab91adfc319d12");
+  hexStr = llvm::toHex(output);
+  if (!StringRef(hexStr).equals_insensitive(
+          "5597b93a2843078cbb0c920ae41dfe20f1685e10c67e423c11ab91adfc319d12")) {
+    llvm::outs() << "HMAC mismatch for long key: " << hexStr;
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
 }
