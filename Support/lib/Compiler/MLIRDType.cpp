e94d996348c27101d820b6ab8025dda8b43d0e50
Jeff Niu <jeff@modular.com>
2022-09-12 14:02:45 -0700

    [Support] Add an MCompilerSupport library (#2794)
    This library depends only on MSupportML and Core MLIR libraries. It
    provides support functionalities that link between MSupportML and MLIR.
    
    MODULAR_ORIG_COMMIT_REV_ID: c4f13b92a7ebb28871c4d34c178744b3fb42389d

---
+++ Support/lib/Compiler/MLIRDType.cpp
@@ -0,0 +1,49 @@
+//===- MLIRDType.cpp ------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#include "Support/Compiler/MLIRDType.h"
+#include "Support/ML/DType.h"
+
+using namespace M;
+
+bool M::areEquivalentFloatTypes(DType dtype, FloatType fpType) {
+  assert(dtype.isFloat() && "expected a float dtype");
+  switch (dtype.getValue()) {
+  case DType::f16:
+    return fpType.isa<Float16Type>();
+  case DType::f32:
+    return fpType.isa<Float32Type>();
+  case DType::f64:
+    return fpType.isa<Float64Type>();
+  case DType::f80:
+    return fpType.isa<Float80Type>();
+  case DType::f128:
+    return fpType.isa<Float128Type>();
+  case DType::bf16:
+    return fpType.isa<BFloat16Type>();
+  default:
+    return false;
+  }
+}
+
+FloatType M::getEquivalentFloatType(MLIRContext *ctx, DType dtype) {
+  switch (dtype.getValue()) {
+  default:
+    return nullptr;
+  case DType::f16:
+    return FloatType::getF16(ctx);
+  case DType::bf16:
+    return FloatType::getBF16(ctx);
+  case DType::f32:
+    return FloatType::getF32(ctx);
+  case DType::f64:
+    return FloatType::getF64(ctx);
+  case DType::f80:
+    return FloatType::getF80(ctx);
+  case DType::f128:
+    return FloatType::getF128(ctx);
+  }
+}
