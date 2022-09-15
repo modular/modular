f496616164bc6592b101980014a374251c2e70b4
Jeff Niu <jeff@modular.com>
2022-09-15 12:39:48 -0700

    [KGEN] Add `kgen-translate` tool (#2860)
    The tool performs only the conversion from LLVM dialect to LLVMIR.
    
    `mlir-translate` is much heavier and is meant as a testing tool for
    upstream MLIR.
    
    MODULAR_ORIG_COMMIT_REV_ID: 8da9327f305cc4fd813eff5c7de0ac8bec036ce4

---
+++ KGEN/tools/kgen-translate/kgen-translate.cpp
@@ -0,0 +1,31 @@
+//===- kgen-translate.cpp -------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#include "mlir/IR/BuiltinOps.h"
+#include "mlir/Target/LLVMIR/Dialect/LLVMIR/LLVMToLLVMIRTranslation.h"
+#include "mlir/Target/LLVMIR/Export.h"
+#include "mlir/Tools/mlir-translate/MlirTranslateMain.h"
+#include "mlir/Tools/mlir-translate/Translation.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+
+int main(int argc, char *argv[]) {
+  mlir::TranslateFromMLIRRegistration(
+      "mlir-to-llvmir",
+      [](mlir::ModuleOp module, llvm::raw_ostream &os) {
+        llvm::LLVMContext llvmContext;
+        auto llvmModule = mlir::translateModuleToLLVMIR(module, llvmContext);
+        if (!llvmModule)
+          return mlir::failure();
+
+        llvmModule->print(os, nullptr);
+        return mlir::success();
+      },
+      [](mlir::DialectRegistry &registry) {
+        mlir::registerLLVMDialectTranslation(registry);
+      });
+  return failed(mlir::mlirTranslateMain(argc, argv, "KGEN Translate Tool"));
+}
