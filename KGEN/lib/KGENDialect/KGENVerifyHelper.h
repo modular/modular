e265077fe0ceb4525bd7ad3b59cb2f97d04a98bf
Chris Lattner <lattner@users.noreply.github.com>
2022-09-17 17:14:43 -0700

    [KGEN] Refactor verifyMatchingLists out to a header, NFC. (#2924)
    This allows it to be used by multiple cpp files in KGENDialect
    
    MODULAR_ORIG_COMMIT_REV_ID: 02231e561d16203978f10429900448ac01161e9e

---
+++ KGEN/lib/KGENDialect/KGENVerifyHelper.h
@@ -0,0 +1,60 @@
+//===- KGENVerifyHelper.h -------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef KGEN_VERIFY_HELPER_H
+#define KGEN_VERIFY_HELPER_H
+
+#include "mlir/IR/Operation.h"
+
+namespace M::KGEN {
+
+/// Compare a range of values from an "originator" to a corresponding range of
+/// values from a "target".  If the two mismatch, emit an error that tries to
+/// explain the issue in a nice way.
+template <typename TargetRange, typename OriginatorRange>
+inline ParseResult verifyMatchingLists(
+    const OriginatorRange &originatorRange, const TargetRange &targetRange,
+    const char *originatorName, Operation *originator, const char *targetName,
+    Operation *target, const char *itemName, const char *propertyName) {
+  // Check that the ranges have the same size.  If not, diagnose this.
+  size_t numOriginator =
+      std::distance(originatorRange.begin(), originatorRange.end());
+  size_t numTarget = std::distance(targetRange.begin(), targetRange.end());
+  if (numOriginator != numTarget) {
+    auto diag = originator->emitOpError(originatorName)
+                << " has " << numOriginator << " " << itemName
+                << (numOriginator != 1 ? "s" : "") << " but " << targetName
+                << " expects " << numTarget;
+    diag.attachNote(target->getLoc()) << targetName << " declared here";
+    return failure();
+  }
+
+  // If they have the same sizes, diagnose any mismatches between their
+  // elements.
+
+  // NOTE: llvm::zip doesn't work with LLVM mapped iterators.
+  auto targetIt = targetRange.begin();
+  auto originatorIt = originatorRange.begin();
+  for (size_t itemNum = 0; itemNum != numTarget; ++itemNum) {
+    auto targetVal = *targetIt++;
+    auto originatorVal = *originatorIt++;
+    if (originatorVal == targetVal)
+      continue;
+
+    auto diag = originator->emitError(originatorName)
+                << ' ' << itemName << " #" << itemNum << " has " << propertyName
+                << ' ' << originatorVal << " but " << targetName << " expected "
+                << propertyName << ' ' << targetVal;
+    diag.attachNote(target->getLoc()) << targetName << " declared here";
+    return failure();
+  }
+
+  return success();
+}
+
+} // namespace M::KGEN
+
+#endif // KGEN_VERIFY_HELPER_H
