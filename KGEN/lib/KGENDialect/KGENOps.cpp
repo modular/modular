5dc932bfbecacf00edf767d2d58192a50c0d8535
Aman LaChapelle <aman.lachapelle@gmail.com>
2022-09-20 15:17:52 -0700

    [KGEN] Add `kgen.precompiled.*` ops. (#3014)
    This patch adds the `kgen.precompiled.*` ops and checks to ensure they
    can be printed/parsed correctly. These will be used soon to implement
    the phases of compilation that emit an object file.
    
    MODULAR_ORIG_COMMIT_REV_ID: f8024b0ca253fd84ef602978801fd6946550983a

--- KGEN/lib/KGENDialect/KGENOps.cpp
+++ KGEN/lib/KGENDialect/KGENOps.cpp
@@ -723,6 +723,96 @@ OpFoldResult RebindOp::fold(ArrayRef<Attribute> operands) {
 }
 
 //===----------------------------------------------------------------------===//
+// Precompiled*Op
+//===----------------------------------------------------------------------===//
+
+/// Parse a `kgen.precompiled.*` op. They all have almost exactly the same form,
+/// so we can use a single function to parse them all. This defines the form of
+/// all the `kgen.precompiled.*` ops to be:
+///
+///   kgen.precompiled.* (public|private) @symbol(...) -> (...) attributes {
+///     ...
+///   }
+///
+/// Note that none of them have bodies, but they do keep the function
+/// signatures.
+static ParseResult parsePrecompiledOp(OpAsmParser &parser,
+                                      OperationState &result) {
+  using namespace mlir::function_interface_impl;
+
+  SmallVector<OpAsmParser::Argument> entryArgs;
+  SmallVector<DictionaryAttr> resultAttrs;
+  SmallVector<Type> resultTypes;
+  auto &builder = parser.getBuilder();
+
+  // Parse visibility. If none is provided, use private by default.
+  if (failed(mlir::impl::parseOptionalVisibilityKeyword(parser,
+                                                        result.attributes)))
+    result.addAttribute(SymbolTable::getVisibilityAttrName(),
+                        parser.getBuilder().getStringAttr("private"));
+
+  // Parse the name as a symbol.
+  StringAttr nameAttr;
+  if (parser.parseSymbolName(nameAttr, SymbolTable::getSymbolAttrName(),
+                             result.attributes))
+    return failure();
+
+  bool isVariadic = false;
+  if (parseFunctionSignature(parser, /*allowVariadic=*/false, entryArgs,
+                             isVariadic, resultTypes, resultAttrs))
+    return failure();
+
+  // Get the argument types
+  SmallVector<Type> argTypes;
+  argTypes.reserve(entryArgs.size());
+  for (auto &arg : entryArgs)
+    argTypes.push_back(arg.type);
+
+  // Create and store the function type on the op.
+  Type type = builder.getFunctionType(argTypes, resultTypes);
+  result.addAttribute(getTypeAttrName(), TypeAttr::get(type));
+
+  // If function attributes are present, parse them.
+  NamedAttrList parsedAttributes;
+  if (parser.parseOptionalAttrDictWithKeyword(parsedAttributes))
+    return failure();
+
+  // Add the processed attr list to the OperationState.
+  result.attributes.append(parsedAttributes);
+
+  // Empty body region, always.
+  (void)result.addRegion();
+
+  return success();
+}
+
+/// Print a `kgen.precompiled.*` op. They all have almost exactly the same form
+/// so we use a single function to handle them all. See `parsePrecompiledOp` for
+/// an example of the form we want printed.
+static void printPrecompiledOp(OpAsmPrinter &p, Operation *op) {
+  using namespace mlir::function_interface_impl;
+  auto symOp = cast<mlir::SymbolOpInterface>(op);
+  auto funcOp = cast<mlir::FunctionOpInterface>(op);
+
+  // Print the operation and the function name.
+  p << ' ';
+
+  StringRef visibilityAttrName = SymbolTable::getVisibilityAttrName();
+  if (auto visibility = op->getAttrOfType<StringAttr>(visibilityAttrName))
+    if (visibility.getValue() != "private")
+      p << visibility.getValue() << ' ';
+  p.printSymbolName(symOp.getName());
+
+  ArrayRef<Type> argTypes = funcOp.getArgumentTypes();
+  ArrayRef<Type> resultTypes = funcOp.getResultTypes();
+  printFunctionSignature(p, op, argTypes, /*isVariadic=*/false, resultTypes);
+  printFunctionAttributes(p, op, argTypes.size(), resultTypes.size(),
+                          {mlir::SymbolTable::getSymbolAttrName(),
+                           mlir::SymbolTable::getVisibilityAttrName(),
+                           mlir::FunctionOpInterface::getTypeAttrName()});
+}
+
+//===----------------------------------------------------------------------===//
 // TableGen generated logic.
 //===----------------------------------------------------------------------===//
 
