f17528a7df10dea89456f35c876a5c8911303c4a
abdul dakkak <adakkak@modular.com>
2022-09-24 11:55:57 -0700

    [Support] Use the predefined LLVM identity function (#3114)
    This removes our definition of the identity function and uses the
    predefined LLVM function in its place
    
    MODULAR_ORIG_COMMIT_REV_ID: cceef1ce70b4400d6b6f76404fca00b457235d47

--- Support/include/Support/STLExtras.h
+++ Support/include/Support/STLExtras.h
@@ -8,29 +8,8 @@
 #define SUPPORT_STL_EXTRAS_H
 
 #include "Support/LogicalResult.h"
-#include <type_traits>
 
 namespace M {
-#if defined(__cpp_lib_type_identity)
-/// We are compiling with a compiler which knows about __cpp_lib_type_identity,
-/// so we just use it.
-template <class T>
-using type_identity = std::type_identity<T>;
-template <class T>
-using type_identity_t = std::type_identity_t<T>;
-#else  // defined(__cpp_lib_type_identity)
-/// Otherwise, we define the struct that is equivalent of C++20
-/// std::type_identity
-///
-/// TODO: This is dead code when we switch to C++20
-template <class T>
-struct type_identity {
-  using type = T;
-};
-template <class T>
-using type_identity_t = typename type_identity<T>::type;
-#endif // defined(__cpp_lib_type_identity)
-
 //===----------------------------------------------------------------------===//
 // failableInterleave
 //===----------------------------------------------------------------------===//
