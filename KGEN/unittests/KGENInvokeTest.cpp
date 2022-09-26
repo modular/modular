3dde219926fbe95b069a0b3fb6ba424ccd241542
abdul dakkak <adakkak@modular.com>
2022-09-26 16:47:14 -0700

    [KGEN] Introduce a KGEN::invoke function (#3200)
    This uses some C++ jujutsu to implement a KGEN::invoke function. The
    KGEN::invoke function takes an array ref as input and destructures it
    into a format that KGEN will take. So, if the input to the KGEN::invoke
    is `(arrayRef0, int, arrayRef1)` then the function will get invoked with
    `(arrayRef0.size(), arrayRef0.data(), dtypeOf(arrayRef0), int,
    arrayRef1.size(), arrayRef1.data(), dtypeOf(arrayRef1))`
    
    MODULAR_ORIG_COMMIT_REV_ID: 02a734beb9634310489235bb356eac35ae3839e5

---
+++ KGEN/unittests/KGENInvokeTest.cpp
@@ -0,0 +1,34 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#include "KGEN/Support/Invoke.h"
+#include "gtest/gtest.h"
+
+using namespace M;
+
+static void someKGENKernel(intptr_t, void *, uint8_t, intptr_t, void *,
+                           uint8_t) {}
+
+static float someOtherKernel(int a, intptr_t, void *, uint8_t, float b) {
+  return a + b;
+}
+
+TEST(KGENInvokeTest, testinvokeKGENFunction) {
+
+  // Test that we can call a function that returns no arguments.
+  KGEN::invoke(someKGENKernel, llvm::makeArrayRef<int32_t>(nullptr, 1),
+               llvm::makeArrayRef<int32_t>(nullptr, 1));
+
+  // Test that we can call the function with a value.
+  auto dummyArray = llvm::makeArrayRef<int32_t>(nullptr, 1);
+  KGEN::invoke(someKGENKernel, std::forward<decltype(dummyArray)>(dummyArray),
+               std::forward<decltype(dummyArray)>(dummyArray));
+
+  // Test that we can call the function with a value and get the correct result.
+  EXPECT_EQ(KGEN::invoke(someOtherKernel, 1,
+                         llvm::makeArrayRef<float>(nullptr, 1), 2.0f),
+            3.0f);
+}
