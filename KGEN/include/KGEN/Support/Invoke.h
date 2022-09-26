348ca5a57526fb95190c958e313a8d877644fd91
abdul dakkak <adakkak@modular.com>
2022-09-26 16:47:14 -0700

    [KGEN] Introduce a KGEN::invoke function (#3200)
    This uses some C++ jujutsu to implement a KGEN::invoke function. The
    KGEN::invoke function takes an array ref as input and destructures it
    into a format that KGEN will take. So, if the input to the KGEN::invoke
    is `(arrayRef0, int, arrayRef1)` then the function will get invoked with
    `(arrayRef0.size(), arrayRef0.data(), dtypeOf(arrayRef0), int,
    arrayRef1.size(), arrayRef1.data(), dtypeOf(arrayRef1))`
    
    MODULAR_ORIG_COMMIT_REV_ID: 02a734beb9634310489235bb356eac35ae3839e5

---
+++ KGEN/include/KGEN/Support/Invoke.h
@@ -0,0 +1,58 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef KGEN_SUPPORT_INVOKE_H
+#define KGEN_SUPPORT_INVOKE_H
+
+#include "Support/ML/DType.h"
+
+namespace M::KGEN {
+
+namespace detail {
+
+template <class T>
+inline constexpr bool is_arrayref_v = false;
+
+template <class T>
+inline constexpr bool is_arrayref_v<ArrayRef<T>> = true;
+
+template <class T>
+inline constexpr bool is_arrayref_v<MutableArrayRef<T>> = true;
+
+/// If the type is an ArrayRef, then destructure the ArrayRef into the form that
+/// KGEN expects (a tuple of a pointer, size, and dtype).  For all other types,
+/// just return the type as is.
+template <typename T>
+constexpr auto destructure_kgen_arguments(T &&arg) {
+  if constexpr (is_arrayref_v<T>)
+    return std::forward_as_tuple(
+        (intptr_t)arg.size(), (void *)arg.data(),
+        DTypeForCXXType<typename T::value_type>::kind.getValue());
+  else
+    return std::forward_as_tuple(std::forward<T>(arg));
+}
+
+/// Destructure the arguments (returning a tuple).
+template <typename First, typename... Rest>
+constexpr auto destructure_kgen_arguments(First &&first, Rest &&...rest) {
+  return std::tuple_cat(
+      destructure_kgen_arguments(std::forward<First>(first)),
+      destructure_kgen_arguments(std::forward<Rest>(rest)...));
+}
+} // namespace detail
+
+/// Invoke a KGEN kernel with the given arguments. This would perform
+/// destructuring so that ArrayRefs are passed as a tuple of (pointer, size,
+/// dtype). All other types are passed as is.
+template <typename F, typename... Args>
+constexpr auto invoke(F &&f, Args &&...args) {
+  return std::apply(std::forward<F>(f), detail::destructure_kgen_arguments(
+                                            std::forward<Args>(args)...));
+}
+
+} // namespace M::KGEN
+
+#endif // KGEN_SUPPORT_INVOKE_H
