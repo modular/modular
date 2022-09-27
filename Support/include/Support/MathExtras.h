80dacd7b8d72d64a70e389218f2d5a35ccb9887c
abdul dakkak <adakkak@modular.com>
2022-09-27 14:01:56 -0700

    [Faux] Introduce `faux.verify.accuracy` (#3248)
    The faux.verify.accuracy op takes two input tensors and verifies that
    they are
    within the specified tolerances.
    
    The `faux.verify.accuracy` is used to verify that the `lhs` and `rhs`
    tensors from two operations agree up to a specified tollerance. If the
    `lhs` and `rhs` are within tollerance, then the result is `true`,
    otherwise
    the result is `false`.
    
    The `lhs` and `rhs` must be of the same type and shape.
    
    An optional `absoluteTolerance` and `relativeTolerance` can be
    specified.
    If not specified, the default absolute tollerance is 10^-6 and the
    default
    relative tollerance is 10^-8.
    
    An optional `describeMismatch` flag can be specified. If specified, a
    description of the mismatched values will be printed to stdout.
    
    An optional `isNaNSensitive` flag can be specified. If specified, then
    nans
    are considered equal.
    
    Usage:
    
    ```mlir
    // Verify that the two tensors are within the default tollerances.
    %ch1, %res = faux.verify.accuracy(%ch, %lhs, %rhs) :
      (!bef.chain, tensor<10xf32>, tensor<10xf32>) -> (!bef.chain, i1)
    
    // Verify that the two tensors are within the specified absoluteTollerance.
    %ch1, %res = faux.verify.accuracy(%ch, %lhs, %rhs) {
      absoluteTolerance = 10e-6
    } : (!bef.chain, tensor<10xf32>, tensor<10xf32>) -> (!bef.chain, i1)
    
    // Verify that the two tensors are within the specified absoluteTollerance
    // and relativeTolerance.
    %ch1, %res = faux.verify.accuracy(%ch, %lhs, %rhs) {
      absoluteTolerance = 10e-6, relativeTolerance = 10e-9
    } : (!bef.chain, tensor<10xf32>, tensor<10xf32>) -> (!bef.chain, i1)
    
    // Verify that the two tensors are within the default tollerances. In the
    // event of a mismatch, print the values that mismatched as well as their
    // indices.
    %ch1, %res = faux.verify.accuracy(%ch, %lhs, %rhs) {
      describeMismatch = true
    } : (!bef.chain, tensor<10xf32>, tensor<10xf32>) -> (!bef.chain, i1)
    
    // Verify that the two tensors are within the default tollerances, but NaNs
    // are considered equal.
    %ch1, %res = faux.verify.accuracy(%ch, %lhs, %rhs) {
      isNaNSensitive = false
    } : (!bef.chain, tensor<10xf32>, tensor<10xf32>) -> (!bef.chain, i1)
    ```
    
    MODULAR_ORIG_COMMIT_REV_ID: a56ee8fd9fd1d707700f83d0bee8a1b47e99b2d5

--- Support/include/Support/MathExtras.h
+++ Support/include/Support/MathExtras.h
@@ -14,18 +14,26 @@ namespace M {
 /// When the type is integral, then equality is checked. When the type is
 /// floating point, then this checks if the two input values are numerically the
 /// close using the abs(a - b) <= max(rtol * max(abs(a), abs(b)), atol) formula.
-/// The default absolute and relative tollerances are picked from the numpy
-/// default values.
-template <typename T>
+/// The default absolute and relative tolerances are picked from the numpy
+/// default values. If IsNanSensitive is false, then two NaNs are considered
+/// equal.
+template <typename T, bool IsNanSensitive = true>
 static bool isClose(T a, T b, double absoluteTolerance = 1.0E-5,
                     double relativeTolerance = 1.0E-8) {
   static_assert(std::is_arithmetic_v<T>, "isClose requires an arithmetic type");
-  if constexpr (std::is_integral_v<T>)
+  if constexpr (std::is_integral_v<T>) {
     return a == b;
-  else
+  } else {
+    if constexpr (IsNanSensitive) {
+      if (std::isnan(a) || std::isnan(b))
+        return false;
+    } else if (std::isnan(a) && std::isnan(b)) {
+      return true;
+    }
     return std::fabs(a - b) <=
            std::max(static_cast<T>(relativeTolerance) *
                         std::max(std::fabs(a), std::fabs(b)),
                     static_cast<T>(absoluteTolerance));
+  }
 }
 } // namespace M
