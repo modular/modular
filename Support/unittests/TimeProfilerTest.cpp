af20ab14ed32a76a03314473741b1e7a7cbc08e8
Mark Shields <110413041+mbs-modular@users.noreply.github.com>
2022-09-27 11:09:22 -0700

    [Support] Copy over llvm TimeProfiler and adjust to support x-thread measurements (#3064)
    [This change started in https://reviews.llvm.org/D133083, but the switch
    to the high precision clock for cross-thread consistency is likely to be
    contentious given I don't know if it works on Windows. Bringing into the
    modular codebase while we iron this out. I suspect we'll end up redoing
    all the profiling support anyway in the long term.]
    
    This change allows a TimeProfiler profiling entry to be created on one
    thread and finished on another. This was triggered by a use case where
    we needed to trace the scheduling delay for work items ultimately
    handled by independent worker threads.
    
    The clock is changed to std::chrono::high_resolution_clock, both for its
    increased precision (our work items can be in the us range), but also
    for its cross-thread consistency (all threads see the same clock).
    
    As a pleasant side effect it is now possible to pay for the cost of
    constructing a profiling entry with a possibly expensive detail string
    outside of the critical section we wish to trace.
    
    MODULAR_ORIG_COMMIT_REV_ID: 8e8be53babd68f685ff8fc3f076fa9fd0fc44513

---
+++ Support/unittests/TimeProfilerTest.cpp
@@ -0,0 +1,93 @@
+//===- Support/unittests/TimeProfilerTest.cpp -----------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+//===----------------------------------------------------------------------===//
+//
+// File originates from:
+//   Repo:   https://github.com/llvm/llvm-project.git
+//   Commit: 271f3b91bbf80e9cf22d9e6bee738abb496fecf9
+//   Path:   llvm/unittests/Support/TimeProfilerTest.cpp
+//
+//===----------------------------------------------------------------------===//
+
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+// These are bare-minimum 'smoke' tests of the time profiler. Not tested:
+//  - multi-threading
+//  - 'Total' entries
+//  - elision of short or ill-formed entries
+//  - detail callback
+//  - no calls to now() if profiling is disabled
+//  - suppression of contributions to total entries for nested entries
+//===----------------------------------------------------------------------===//
+
+#include "llvm/Support/raw_ostream.h"
+
+#include "Support/TimeProfiler.h"
+
+#include "gtest/gtest.h"
+
+using namespace M;
+
+namespace {
+
+void setupProfiler() {
+  timeTraceProfilerInitialize(/*TimeTraceGranularity=*/0, "test");
+}
+
+std::string teardownProfiler() {
+  SmallVector<char, 1024> smallVector;
+  llvm::raw_svector_ostream os(smallVector);
+  timeTraceProfilerWrite(os);
+  timeTraceProfilerCleanup();
+  return os.str().str();
+}
+
+TEST(TimeProfiler, Scope_Smoke) {
+  setupProfiler();
+
+  { TimeTraceScope scope("event", "detail"); }
+
+  std::string json = teardownProfiler();
+  ASSERT_TRUE(json.find(R"("name":"event")") != std::string::npos);
+  ASSERT_TRUE(json.find(R"("detail":"detail")") != std::string::npos);
+}
+
+TEST(TimeProfiler, Begin_End_Smoke) {
+  setupProfiler();
+
+  timeTraceProfilerBegin("event", "detail");
+  timeTraceProfilerEnd();
+
+  std::string json = teardownProfiler();
+  ASSERT_TRUE(json.find(R"("name":"event")") != std::string::npos);
+  ASSERT_TRUE(json.find(R"("detail":"detail")") != std::string::npos);
+}
+
+TEST(TimeProfiler, Begin_End_Disabled) {
+  // Nothing should be observable here. The test is really just making sure
+  // we've not got a stray nullptr deref.
+  timeTraceProfilerBegin("event", "detail");
+  timeTraceProfilerEnd();
+}
+
+TEST(TimeProfiler, Entry_Disabled) {
+  // Only get the default entry if tracing is not setup.
+  auto entry = timeTraceProfilerBeginEntry("event", "detail");
+  entry.begin();
+  ASSERT_TRUE(entry.Name.empty());
+  ASSERT_TRUE(entry.Detail.empty());
+  ASSERT_EQ(entry.Start, TimeTraceProfilerEntry::TimePointType());
+  ASSERT_EQ(entry.End, TimeTraceProfilerEntry::TimePointType());
+  timeTraceProfilerEndEntry(std::move(entry));
+}
+
+} // namespace
