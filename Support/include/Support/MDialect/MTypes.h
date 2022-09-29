5a77f2fa8614f31968c73f64fae174dbb5126c91
Jeff Niu <jeff@modular.com>
2022-09-29 16:31:21 -0700

    [Support] Add `!M.array` type. (#3357)
    This PR adds the `M` dialect, a "builtin"-style dialect for Modular
    code. Ideally, the contents of this dialect could be upstreamed if
    necessary/desired (i.e., the dialect depends only on `MLIRIR`).
    
    And this PR adds the `ArrayType`. This is a shaped type whose rank is
    always 1 and is never dynamic. It can be used as types of elements
    attributes. This type is useful throughout the Modular codebase where we
    use ElementsAttr but restrict them to only 1 dimension.
    
    I plan to replace our uses of `RankedTensorType` when we really just
    want an array with this.
    
    MODULAR_ORIG_COMMIT_REV_ID: 929212e9708fdc00c942100253d0c3bbce424e38

---
+++ Support/include/Support/MDialect/MTypes.h
@@ -0,0 +1,21 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef SUPPORT_MDIALECT_MTYPES_H
+#define SUPPORT_MDIALECT_MTYPES_H
+
+#include "Support/LLVMCompilerForwardDecls.h"
+#include "mlir/IR/BuiltinTypeInterfaces.h"
+#include "mlir/IR/Types.h"
+
+//===----------------------------------------------------------------------===//
+// ODS-Generated Declarations
+//===----------------------------------------------------------------------===//
+
+#define GET_TYPEDEF_CLASSES
+#include "Support/MDialect/MTypes.h.inc"
+
+#endif // SUPPORT_MDIALECT_MTYPES_H
