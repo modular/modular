5ef01d1491ddc41c95cbe7f1038ca0b4fa0e58fc
abdul dakkak <adakkak@modular.com>
2022-10-03 11:13:13 -0700

    [KGEN] Reintroduce the CompilerRT library (#3387)
    Currently this is just a skeleton which includes an initialization
    function. The initialization function is needed because some compilers
    skip linking a library into an executable if no symbols are used. This
    guarantees at least one symbol is being used.
    
    MODULAR_ORIG_COMMIT_REV_ID: 5f83bbb0c585b4382afdcc89399b4577903f91e3

---
+++ KGEN/include/KGEN/CompilerRT.h
@@ -0,0 +1,12 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef KGEN_COMPILER_RT_H
+#define KGEN_COMPILER_RT_H
+
+bool KGEN_CompilerRT_Initialize();
+
+#endif // KGEN_COMPILER_RT_H
