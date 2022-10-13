08ceb2eb85be5445c3bdc0ebc80b2082a033e8e6
abdul dakkak <adakkak@modular.com>
2022-10-13 14:46:57 -0700

    [Support] Rename template params from Container to Range in MathExtras (#3642)
    Implement the suggestion in
    [Internal link] and
    add test cases while I am at it. This exposes some corner case issues.
    
    MODULAR_ORIG_COMMIT_REV_ID: 57b434116e28476df2f3dcfe748bf3e03c09a392

--- Support/unittests/MathExtras.cpp
+++ Support/unittests/MathExtras.cpp
@@ -13,12 +13,25 @@
 
 using namespace M;
 
+TEST(MathExtrasTest, Mean) {
+  auto meanValue0 = mean(std::vector<float>());
+  EXPECT_FLOAT_EQ(meanValue0, 0.0);
+
+  auto meanValue = mean(std::vector{-10.0, 1.0, 1.0, 1.0, 1.0, 20.0});
+  EXPECT_FLOAT_EQ(meanValue, 2.3333333);
+
+  // You can call mean with any container. E.g. a initializer_list.
+  auto meanInitializerList =
+      mean(std::initializer_list<float>{-10.0, 1.0, 1.0, 1.0, 1.0, 20.0});
+  EXPECT_FLOAT_EQ(meanInitializerList, 2.3333333);
+}
+
 TEST(MathExtrasTest, TrimmedMean) {
+  // You can call trimmedMean with any container. E.g. a initializer_list.
   auto trimmedMeanInitializerList = trimmedMean(
       std::initializer_list<float>{-10.0, 1.0, 1.0, 1.0, 1.0, 20.0});
   EXPECT_FLOAT_EQ(trimmedMeanInitializerList, 2.3333333);
 
-  // You can call trimmedMean with any container. E.g. a vector.
   auto trimmedMean0 = trimmedMean(std::vector{-10.0, 1.0, 1.0, 1.0, 1.0, 20.0});
   EXPECT_FLOAT_EQ(trimmedMean0, 2.3333333);
 
@@ -37,4 +50,37 @@ TEST(MathExtrasTest, TrimmedMean) {
                               10.0f, 100000.0f},
                   0);
   EXPECT_FLOAT_EQ(trimmedMean3, 9983.8);
+}
+
+TEST(MathExtrasTest, Median) {
+  auto medianValue0 = median(std::vector<float>());
+  EXPECT_FLOAT_EQ(medianValue0, 0.0);
+
+  auto medianValueOdd = median(std::vector{1.0f, 2.0f, 3.0f, 4.0f, 5.0f});
+  EXPECT_FLOAT_EQ(medianValueOdd, 3.0);
+
+  // You can call median with an even length list.
+  auto medianValue = median(std::vector{1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f});
+  EXPECT_FLOAT_EQ(medianValue, 3.5);
+}
+
+TEST(MathExtrasTest, Percentile) {
+  {
+    auto val = percentile(std::vector<float>(), 0.0);
+    EXPECT_FLOAT_EQ(val, 0.0);
+  }
+  {
+    auto val = percentile(std::vector{1.0, 2.0, 3.0}, 0.0);
+    EXPECT_FLOAT_EQ(val, 1.0);
+  }
+  {
+    auto val = percentile(
+        std::vector{0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0},
+        0.8);
+    EXPECT_FLOAT_EQ(val, 8.0);
+  }
+  {
+    auto val = percentile(std::vector{1.0, 2.0, 3.0}, 0.9);
+    EXPECT_FLOAT_EQ(val, 3.0);
+  }
 }
