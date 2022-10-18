f275b059af3b6f56a3aa8ac985baa923e9b54165
Chris Lattner <lattner@users.noreply.github.com>
2022-10-18 14:45:27 -0700

    [Lit] Completely remove NameBindingContext, NFC. (#3843)
    This merges NameBindingContext into the DeclResolver infrastructure,
    so we have one way to resolve the body of declarations.  This is a big
    step forward in terms of unifying things, but there is still a long way
    to go.
    
    MODULAR_ORIG_COMMIT_REV_ID: f67596ed26b9fa94edd85b28bff3e28669a5c5ee

--- KGEN/lib/ParseLit/LitDecls.cpp
+++ KGEN/lib/ParseLit/LitDecls.cpp
@@ -9,8 +9,12 @@
 //===----------------------------------------------------------------------===//
 
 #include "LitDecls.h"
+#include "KGEN/LITDialect/LITOps.h"
+#include "KGEN/LITDialect/LITTypes.h"
+#include "LitExprNodes.h"
+#include "LitParserBase.h"
 #include "LitScope.h"
-#include "LitSharedState.h"
+#include "llvm/ADT/TypeSwitch.h"
 
 using namespace M;
 using namespace M::KGEN::LIT;
@@ -18,6 +22,20 @@ using namespace M::KGEN::LIT;
 //===----------------------------------------------------------------------===//
 // DeclResolver
 //===----------------------------------------------------------------------===//
+
+// Declarations (e.g. module, class, function) are parsed in multiple phases to
+// increase laziness of the parse as well as make circular references possible.
+//
+// This ensures that the forward references between peer declarations are
+// handled correctly as well as circular references, for example in mutually
+// recursive functions and code like this:
+//
+//   def foo():
+//     def bar():
+//       print(x)
+//     x = 42
+//     bar()
+//   foo()
 
 DeclResolver::DeclResolver(LitSharedState &state) : sharedState(state) {}
 DeclResolver::~DeclResolver() {}
@@ -36,4 +54,75 @@ void DeclResolver::resolveAll() {
   // cause more entries to be added to this list.
   for (size_t i = 0; i != parsedDeclList.size(); ++i)
     resolve(*parsedDecls[parsedDeclList[i]]);
+}
+
+void DeclResolver::resolve(Scope &scope) {
+  // If scope is fully resolved, we're done.
+  if (scope.getIsResolved())
+    return;
+
+  Operation *decl = scope.getDecl();
+
+  // If we are currently name binding this operation, we found a cycle, reject
+  // it with an error.
+  if (!declsCurrentlyProcessing.insert(decl).second) {
+    assert(0 &&
+           "FIXME: Diagnose cyclic reference when it is possible to happen");
+  }
+
+  // Handle each operation that can be name bound.
+  TypeSwitch<Operation *>(decl)
+      .Case<LITFuncOp>([&](auto op) { resolveBody(op, scope); })
+      .Case<VarDeclOp>([&](auto op) { resolveSignature(op, scope); })
+      .Default([&](auto attr) {
+        decl->emitError("do not know how to perform name binding on this op!");
+      });
+
+  declsCurrentlyProcessing.erase(decl);
+  scope.setIsResolved();
+}
+
+/// Given a cursor location for a type expression that correctly parsed in the
+/// first pass, reparse it into an expression and resolve it into a type by
+/// performing name lookup and other resolution.  This can produce errors, but
+/// always returns a non-null type.
+Type DeclResolver::resolveType(LitLexerCursor cursor, Scope &scope,
+                               LitParserBase &parser) {
+  // FIXME: This is the wrong design, shouldn't be reparsing.
+
+  // Move the cursor to the specified location.
+  cursor.restore(parser.getLexer());
+  // Re-parse the expression at that location.
+  ExprParser exprParser(parser);
+  ExprNode *typeExpr = exprParser.parseExpression();
+  assert(typeExpr && "We know expr parsing will work");
+
+  auto emitError = [&](const Twine &message) -> Type {
+    parser.emitError(typeExpr->getLoc(), message);
+    return UnresolvedType::get(parser.getContext());
+  };
+
+  // TODO: Make this a recursive walk when we have more interesting types.
+  if (auto dre = dyn_cast<DeclRefNode>(typeExpr)) {
+    // TODO(types): This is a hack to unblock tests in the interim.
+    if (dre->spelling == "index")
+      return IndexType::get(parser.getContext());
+
+    // Lookup the identifier.
+    Optional<Scope::ScopeValue> lookup = scope.lookup(dre->spelling);
+    if (!lookup)
+      return emitError("unknown type name '" + dre->spelling + "'");
+    if (std::holds_alternative<VarDeclOp>(*lookup))
+      return emitError("'" + dre->spelling + "' names a value, not a type");
+    auto attr = dyn_cast<SymbolConstantAttr>(
+        std::get<Scope::MetaParameterValue>(*lookup).getAttr());
+    if (!attr || !isa<MLIRTypeType>(attr.getType()))
+      return emitError("'" + dre->spelling + "' names a value, not a type");
+
+    // TODO: Handle type parameters!
+    return RefType::get(attr.getSymbol(),
+                        ParamBindArrayAttr::get(parser.getContext(), {}));
+  }
+
+  return emitError("FIXME: Unsupported type kind!");
 }
