91eb7ae4ce601979072840b750a309c4bcdcbd01
Laszlo Kindrat <laszlo@modular.ai>
2022-10-20 12:11:22 -0400

    [Utils][Python] Add `modular.utils.misc` for miscellaneous utilites (#3937)
    Currently this module includes the `set_env_var` context manager only,
    but the module should serve as a place for other utilities that don't
    fit into (or deserve a new) module.
    
    Co-authored-by: Laszlo Kindrat <laszlo@modular.com>
    MODULAR_ORIG_COMMIT_REV_ID: e0ec694489bf86a22e907f37e81431359858c085

---
+++ Support/python/modular/utils/misc/__init__.py
@@ -0,0 +1,10 @@
+# ===----------------------------------------------------------------------=== #
+#
+# This file is Modular Inc proprietary.
+#
+# ===----------------------------------------------------------------------=== #
+
+from ._misc import set_env_var
+
+# Remove from the namespace so that it's not visible to users.
+del _misc  # noqa: F821
