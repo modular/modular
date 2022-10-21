2ded430e5138961c6e7ad8ed8ddda4974d7f3de0
abdul dakkak <adakkak@modular.com>
2022-10-21 09:56:34 -0700

    [KGEN] Remove destructuring of Tensor in KGEN::invoke (#3998)
    Without some monumental effort, there is no way to get this to work with
    our current NDBuffer representation. This also brings in LLCL and other
    dependencies which are a bit high-cost given what we are doing.
    
    MODULAR_ORIG_COMMIT_REV_ID: 0bba9e9ffe2088aff8a7cd7e422170acb6ad2389

--- KGEN/include/KGEN/Support/Invoke.h
+++ KGEN/include/KGEN/Support/Invoke.h
@@ -7,8 +7,8 @@
 #ifndef KGEN_SUPPORT_INVOKE_H
 #define KGEN_SUPPORT_INVOKE_H
 
-#include "GenericML/Support/Tensor.h"
 #include "Support/ML/DType.h"
+#include "llvm/ADT/ArrayRef.h"
 
 namespace M::KGEN {
 
@@ -28,36 +28,7 @@ inline constexpr bool is_arrayref_v<MutableArrayRef<T>> = true;
 /// just return the type as is.
 template <typename T>
 constexpr auto destructure_kgen_arguments(T &&arg) {
-  if constexpr (std::is_same_v<T, Tensor>) {
-    ssize_t rank = arg.getShape().getRank();
-    assert(rank <= 5 && "KGEN only supports tensors of rank <= 5");
-    auto tensorShape = arg.getShape();
-    ssize_t shape0 = 0, shape1 = 0, shape2 = 0, shape3 = 0, shape4 = 0;
-    switch (rank) {
-    case 5:
-      shape4 = tensorShape[4];
-      [[fallthrough]];
-    case 4:
-      shape3 = tensorShape[3];
-      [[fallthrough]];
-    case 3:
-      shape2 = tensorShape[2];
-      [[fallthrough]];
-    case 2:
-      shape1 = tensorShape[1];
-      [[fallthrough]];
-    case 1:
-      shape0 = tensorShape[0];
-    }
-    return std::tuple{(void *)arg.getPointer(),
-                      rank,
-                      shape0,
-                      shape1,
-                      shape2,
-                      shape3,
-                      shape4,
-                      arg.getEltType().getValue()};
-  } else if constexpr (is_arrayref_v<T>) {
+  if constexpr (is_arrayref_v<T>) {
     return std::tuple{(void *)arg.data(), (ssize_t)arg.size(),
                       DTypeForCXXType<typename T::value_type>::kind.getValue()};
   } else {
