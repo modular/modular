4df42c5aedba2106bed31dcfbb1bab785055099e
Jeff Niu <jeff@modular.com>
2022-10-21 18:01:47 -0700

    [KGEN] Fix lowering of nested KGEN structs (#4025)
    This is a workaround LLVMStructType not implementing
    `replaceImmediateSubElements` (fixed on next LLVM integrate), which
    causes null types to appear when substitutiing references.
    
    MODULAR_ORIG_COMMIT_REV_ID: 05ceff7f41e632639b495937c65122ca278c0005

--- KGEN/lib/KGENToLLVM/LowerKGENToLLVM.cpp
+++ KGEN/lib/KGENToLLVM/LowerKGENToLLVM.cpp
@@ -353,34 +353,26 @@ static void populateKGENToLLVMPatterns(mlir::LLVMTypeConverter &typeConverter,
 // Type Lowering
 //===----------------------------------------------------------------------===//
 
-/// Lower a concrete struct declaration to an LLVM struct. Struct types should
-/// only appear in KGEN dialect operations.
-static void configureTypeConverter(POPToLLVMTypeConverter &typeConverter,
-                                   StructDeclarations &structDecls) {
-  typeConverter.addConversion([&](RefType typeDef) -> Optional<Type> {
-    auto it = structDecls.fieldTypes.find(typeDef.getName().getAttr());
-    if (it == structDecls.fieldTypes.end()) {
-      typeConverter.emitError("could not find struct declaration ")
-          << typeDef.getName();
-      return {};
-    }
-    // Substitute parameters into the field types.
-    ParameterEvaluator evaluator;
-    for (ParamBindAttr bind : typeDef.getParamValues())
-      evaluator.setParameterValue(bind.getDecl(), bind.getValue());
+/// Replace a KGEN struct with an LLVM struct.
+static LLVM::LLVMStructType
+substituteStructDecl(const StructDeclarations &structDecls, RefType typeDef,
+                     function_ref<Type(Type)> transformElement) {
+  auto it = structDecls.fieldTypes.find(typeDef.getName().getAttr());
+  if (it == structDecls.fieldTypes.end())
+    return {};
+  // Substitute parameters into the field types.
+  ParameterEvaluator evaluator;
+  for (ParamBindAttr bind : typeDef.getParamValues())
+    evaluator.setParameterValue(bind.getDecl(), bind.getValue());
 
-    SmallVector<Type> elementTypes;
-    for (Type type : it->second) {
-      Type elementType =
-          typeConverter.convertType(evaluator.getReboundType(type));
-      if (!elementType) {
-        typeConverter.emitError("failed to convert element type ") << type;
-        return {};
-      }
-      elementTypes.push_back(elementType);
-    }
-    return LLVM::LLVMStructType::getLiteral(typeDef.getContext(), elementTypes);
-  });
+  SmallVector<Type> elementTypes;
+  for (Type type : it->second) {
+    Type elementType = transformElement(evaluator.getReboundType(type));
+    if (!elementType)
+      return {};
+    elementTypes.push_back(elementType);
+  }
+  return LLVM::LLVMStructType::getLiteral(typeDef.getContext(), elementTypes);
 }
 
 //===----------------------------------------------------------------------===//
@@ -801,7 +793,11 @@ void LowerKGENToLLVMPass::runOnOperation() {
 
   // Configure the type converter.
   POPToLLVMTypeConverter typeConverter(theModule->getLoc(), options);
-  configureTypeConverter(typeConverter, structDecls);
+  typeConverter.addConversion([&](RefType typeDef) -> Optional<Type> {
+    return substituteStructDecl(structDecls, typeDef, [&](Type elType) {
+      return typeConverter.convertType(elType);
+    });
+  });
 
   // Populate patterns and run the conversion.
   mlir::RewritePatternSet patterns(&getContext());
@@ -817,30 +813,45 @@ void LowerKGENToLLVMPass::runOnOperation() {
     signalPassFailure();
 
   // Type references can be used in nested types. Walk through all the types and
-  // rewrite them in-place to use the lowered types.
-  auto rewriteType = [&](Type type) -> Type {
+  // rewrite them in-place to use the lowered types. We need to manually walk
+  // the nested types to handle LLVM structs, which don't support
+  // `replaceSubElements`.
+  std::function<Type(Type)> substituteRefs = [&](Type type) -> Type {
     if (auto ref = dyn_cast<RefType>(type))
-      return typeConverter.convertType(ref);
+      return substituteStructDecl(structDecls, ref, substituteRefs);
+    // LLVMPointerType::replaceImmediateSubElements is broken for opaque
+    // pointers.
+    if (auto ptr = dyn_cast<LLVM::LLVMPointerType>(type))
+      if (ptr.isOpaque())
+        return ptr;
     auto itf = dyn_cast<mlir::SubElementTypeInterface>(type);
     if (!itf)
       return type;
-
-    return itf.replaceSubElements([&](Type type) -> Type {
-      auto ref = dyn_cast<RefType>(type);
-      if (!ref)
-        return type;
-      return typeConverter.convertType(ref);
-    });
+    SmallVector<Attribute> attrs;
+    SmallVector<Type> types;
+    itf.walkImmediateSubElements(
+        [&](Attribute attr) {
+          if (auto typeCst = dyn_cast<ConcreteTypeConstantAttr>(attr))
+            attrs.push_back(ConcreteTypeConstantAttr::get(
+                substituteRefs(typeCst.getValue())));
+          else
+            attrs.push_back(attr);
+        },
+        [&](Type type) { types.push_back(substituteRefs(type)); });
+    if (auto structTy = dyn_cast<LLVM::LLVMStructType>(type))
+      return LLVM::LLVMStructType::getLiteral(&getContext(), types,
+                                              structTy.isPacked());
+    return itf.replaceImmediateSubElements(attrs, types);
   };
   WalkResult result = getOperation()->walk([&](Operation *op) {
     // Substitute any references in attributes.
     op->setAttrs(op->getAttrDictionary()
-                     .replaceSubElements(rewriteType)
+                     .replaceSubElements(substituteRefs)
                      .cast<DictionaryAttr>());
 
     // Substitute the result types.
     for (OpResult result : op->getOpResults()) {
-      Type replType = rewriteType(result.getType());
+      Type replType = substituteRefs(result.getType());
       if (!replType) {
         op->emitError("failed to substitute result type #")
             << result.getResultNumber();
@@ -853,7 +864,7 @@ void LowerKGENToLLVMPass::runOnOperation() {
     for (Region &region : op->getRegions()) {
       for (Block &block : region) {
         for (BlockArgument arg : block.getArguments()) {
-          Type replType = rewriteType(arg.getType());
+          Type replType = substituteRefs(arg.getType());
           if (!replType) {
             op->emitError("failed to substitute block argument type ")
                 << arg.getType();
