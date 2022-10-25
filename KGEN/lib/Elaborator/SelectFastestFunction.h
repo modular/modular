1ed61d467a70e164764be29f2a64eba41da13b88
Jeff Niu <jeff@modular.com>
2022-10-25 12:25:40 -0700

    [KGEN] Delete EvalConfigurationAttr (#4117)
    This attribute and all the machinery that interacts with it (the old
    hard-coded search algorithm) can be deleted now that benchmarking is
    done in KGEN and not in C++.
    
    MODULAR_ORIG_COMMIT_REV_ID: 60f2873ad56699470d04146715b6abd2a10c13d9

--- KGEN/lib/Elaborator/SelectFastestFunction.h
+++ KGEN/lib/Elaborator/SelectFastestFunction.h
@@ -9,16 +9,8 @@
 
 #include "KGEN/KGENDialect/KGENOps.h"
 #include "Support/ForwardDecls.h"
-#include "mlir/IR/BuiltinOps.h"
 
 namespace M::KGEN {
-/// Search for the fastest specialization of the given interface. On success,
-/// returns an index into the `specializations` vector - that is the fastest
-/// implementation.
-ErrorOr<size_t> selectFastestFunction(GeneratorInterfaceOp itf,
-                                      SymbolTable &symtab,
-                                      ArrayRef<FuncOp> specializations);
-
 /// Given a list of valid specializations for an interface, select the best
 /// specialization according to a user-defined evaluator function.
 ErrorOr<size_t> evaluateSpecializations(FuncOp evaluator, SymbolTable &symtab,
