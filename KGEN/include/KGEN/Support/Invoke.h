c29d8f9630a4575de86d56881a213f6375e9ce04
abdul dakkak <adakkak@modular.com>
2022-10-27 11:16:27 -0700

    [KGEN] Reteach KGEN::invoke about NDBuffer (#4229)
    This resurrects [Internal link] . It
    reteaches kgen::invoke how to call kgen kernels given a tensor. The
    reason why #3842 did not work is because it was leaking stack
    allocations, this instead stores the data in heap allocated SmallVector.
    
    MODULAR_ORIG_COMMIT_REV_ID: ac950188a3b38b9119c2a95290373d83a006700d

--- KGEN/include/KGEN/Support/Invoke.h
+++ KGEN/include/KGEN/Support/Invoke.h
@@ -7,12 +7,16 @@
 #ifndef KGEN_SUPPORT_INVOKE_H
 #define KGEN_SUPPORT_INVOKE_H
 
-#include "Support/ML/DType.h"
-#include "llvm/ADT/ArrayRef.h"
+#include "GenericML/Support/Tensor.h"
+#include "llvm/Support/Compiler.h"
 
 namespace M::KGEN {
 
 namespace detail {
+
+inline constexpr ssize_t MaximumTensorRank = 5;
+using TensorShapeArrayType = std::array<ssize_t, MaximumTensorRank>;
+using TensorShapeStorageType = SmallVector<TensorShapeArrayType, 4>;
 
 template <class T>
 inline constexpr bool is_arrayref_v = false;
@@ -27,8 +31,20 @@ inline constexpr bool is_arrayref_v<MutableArrayRef<T>> = true;
 /// KGEN expects (a tuple of a pointer, size, and dtype).  For all other types,
 /// just return the type as is.
 template <typename T>
-constexpr auto destructure_kgen_arguments(T &&arg) {
-  if constexpr (is_arrayref_v<T>) {
+constexpr auto destructure_kgen_arguments(TensorShapeStorageType &shapeStorage,
+                                          T &&arg) {
+  if constexpr (std::is_same_v<T, Tensor>) {
+    ssize_t rank = arg.getShape().getRank();
+    assert(rank <= MaximumTensorRank &&
+           "KGEN only supports tensors of rank <= 5");
+    auto tensorShape = arg.getShape();
+    TensorShapeArrayType shape{0};
+    std::copy(tensorShape.begin(), tensorShape.begin() + rank, shape.begin());
+    shapeStorage.emplace_back(shape);
+    return std::tuple{arg.getPointer(), rank,
+                      reinterpret_cast<ssize_t *>(shapeStorage.back().data()),
+                      arg.getEltType().getValue()};
+  } else if constexpr (is_arrayref_v<T>) {
     return std::tuple{(void *)arg.data(), (ssize_t)arg.size(),
                       DTypeForCXXType<typename T::value_type>::kind.getValue()};
   } else {
@@ -38,10 +54,11 @@ constexpr auto destructure_kgen_arguments(T &&arg) {
 
 /// Destructure the arguments (returning a tuple).
 template <typename First, typename... Rest>
-constexpr auto destructure_kgen_arguments(First &&first, Rest &&...rest) {
+constexpr auto destructure_kgen_arguments(TensorShapeStorageType &shapeStorage,
+                                          First &&first, Rest &&...rest) {
   return std::tuple_cat(
-      destructure_kgen_arguments(std::forward<First>(first)),
-      destructure_kgen_arguments(std::forward<Rest>(rest)...));
+      destructure_kgen_arguments(shapeStorage, std::forward<First>(first)),
+      destructure_kgen_arguments(shapeStorage, std::forward<Rest>(rest)...));
 }
 } // namespace detail
 
@@ -49,9 +66,11 @@ constexpr auto destructure_kgen_arguments(First &&first, Rest &&...rest) {
 /// destructuring so that ArrayRefs are passed as a tuple of (pointer, size,
 /// dtype). All other types are passed as is.
 template <typename F, typename... Args>
-constexpr auto invoke(F &&f, Args &&...args) {
-  return std::apply(std::forward<F>(f), detail::destructure_kgen_arguments(
-                                            std::forward<Args>(args)...));
+inline auto invoke(F &&f, Args &&...args) {
+  detail::TensorShapeStorageType shapeStorage;
+  return std::apply(std::forward<F>(f),
+                    detail::destructure_kgen_arguments(
+                        shapeStorage, std::forward<Args>(args)...));
 }
 
 } // namespace M::KGEN
