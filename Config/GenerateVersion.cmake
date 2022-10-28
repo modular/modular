5269e04830fdc3874c0c3b85541946ba887e4c99
River Riddle <riddleriver@gmail.com>
2022-10-28 10:24:07 -0700

    [All] Add initial cmake structure for defining modular versions (#4273)
    We've increasingly grown a need to have some type of versioning for
    the code base, so that we can detect when things change. This PR adds
    in a very basic structure incorporating Major/Minor/Patch/Revision, where
    the first 3 are currently stubbed to 0 (as we haven't defined any kind of
    release schedule), and the latter is defined as the current git revision.
    
    We will want to refine this structure over time, and for customer releases
    generally won't want to include the revision number, but this should give
    us enough to start working with.
    
    MODULAR_ORIG_COMMIT_REV_ID: 0803040045cccd908cf18e6015027a876625451b

---
+++ Config/GenerateVersion.cmake
@@ -0,0 +1,42 @@
+##===----------------------------------------------------------------------===##
+#
+# This file is Modular Inc proprietary.
+#
+##===----------------------------------------------------------------------===##
+#
+# This file contains the recipe for generating the Modular version file.
+#
+##===----------------------------------------------------------------------===##
+
+find_package(Git REQUIRED)
+
+execute_process (
+    COMMAND ${GIT_EXECUTABLE} rev-parse --short=8 HEAD
+    WORKING_DIRECTORY ${MODULAR_SOURCE_DIR}
+    OUTPUT_VARIABLE MODULAR_VERSION_REVISION
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+
+set(version_inc "${MODULAR_BINARY_DIR}/Config/include/Config/Version.h.inc")
+
+file(APPEND "${version_inc}.tmp"
+  "/* Major version */\n"
+  "#define MODULAR_VERSION_MAJOR ${MODULAR_VERSION_MAJOR}\n\n"
+  
+  "/* Minor version */\n"
+  "#define MODULAR_VERSION_MINOR ${MODULAR_VERSION_MINOR}\n\n"
+  
+  "/* Patch version */\n"
+  "#define MODULAR_VERSION_MAJOR ${MODULAR_VERSION_MAJOR}\n\n"
+  
+  "/* Revision sha */\n"
+  "#define MODULAR_VERSION_REVISION \"${MODULAR_VERSION_REVISION}\"\n\n"
+  
+  "/* Version string */\n"
+  "#define MODULAR_VERSION_STRING \"${MODULAR_VERSION_MAJOR}.${MODULAR_VERSION_MINOR}.${MODULAR_VERSION_PATCH}-${MODULAR_VERSION_REVISION}\"\n"
+)
+
+# Copy the file only if it has changed.
+execute_process(COMMAND ${CMAKE_COMMAND} -E copy_if_different
+  "${version_inc}.tmp" "${version_inc}")
+file(REMOVE "${version_inc}.tmp")
