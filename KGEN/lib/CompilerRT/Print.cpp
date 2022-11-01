41680fbe670e8589ca6f078cb102417c82c59c01
Jeff Niu <jeff@modular.com>
2022-11-01 11:58:41 -0700

    [KGEN] Make `zap.print` flush the output stream (#4393)
    Fixes #3253
    
    MODULAR_ORIG_COMMIT_REV_ID: 90529ca89a68e769417a717d11f7610cdbe977b2

--- KGEN/lib/CompilerRT/Print.cpp
+++ KGEN/lib/CompilerRT/Print.cpp
@@ -10,3 +10,11 @@ extern "C" void KGEN_CompilerRT_PrintToStdErr(const char *data, ssize_t size) {
   llvm::errs() << llvm::StringRef(data, size);
   llvm::errs().flush();
 }
+
+extern "C" void KGEN_CompilerRT_PrintFormat(const char *fmt, ...) {
+  va_list args;
+  va_start(args, fmt);
+  vfprintf(stdout, fmt, args);
+  fflush(stdout);
+  va_end(args);
+}
