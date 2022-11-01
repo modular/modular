d7ca09a6d4411097d5c08547a6e058d745991f20
Jeff Niu <jeff@modular.com>
2022-11-01 12:04:21 -0700

    [KGEN] Fix GCC build by adding missing header for va_list (#4429)
    In Print.cpp
    
    MODULAR_ORIG_COMMIT_REV_ID: 6dc477c96593f5ba8e05ea9152b70d525e568f46

--- KGEN/lib/CompilerRT/Print.cpp
+++ KGEN/lib/CompilerRT/Print.cpp
@@ -5,6 +5,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "llvm/Support/raw_ostream.h"
+#include <cstdarg>
 
 extern "C" void KGEN_CompilerRT_PrintToStdErr(const char *data, ssize_t size) {
   llvm::errs() << llvm::StringRef(data, size);
