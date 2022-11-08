99489a3bce86a0d8e3b1ad6d7b69cbea84e1ef78
Jeff Niu <jeff@modular.com>
2022-11-07 18:55:58 -0800

    [Support] Add boilerplate for HLCF Dialect (#4694)
    This PR adds the boilerplate for the HLCF dialect. This dialect will be
    placed in the Support directory despite being created specifically for
    KGEN/Lightning because it is type system agnostic -- it can be used with
    any generic MLIR code. This does not imply a desire to upstream the
    dialect, however.
    
    MODULAR_ORIG_COMMIT_REV_ID: 7410ca73d0cea56503c24282f414e18a25852872

---
+++ Support/include/Support/HLCFDialect/HLCFDialect.h
@@ -0,0 +1,18 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef SUPPORT_HLCFDIALECT_HLCFDIALECT_H
+#define SUPPORT_HLCFDIALECT_HLCFDIALECT_H
+
+#include "mlir/IR/Dialect.h"
+
+//===----------------------------------------------------------------------===//
+// ODS-Generated Declarations
+//===----------------------------------------------------------------------===//
+
+#include "Support/HLCFDialect/HLCFDialect.h.inc"
+
+#endif // SUPPORT_HLCFDIALECT_HLCFDIALECT_H
---
+++ Support/include/Support/HLCFDialect/HLCFOps.h
@@ -0,0 +1,17 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef SUPPORT_HLCFDIALECT_HLCFOPS_H
+#define SUPPORT_HLCFDIALECT_HLCFOPS_H
+
+//===----------------------------------------------------------------------===//
+// ODS-Generated Declarations
+//===----------------------------------------------------------------------===//
+
+#define GET_OP_CLASSES
+#include "Support/HLCFDialect/HLCF.h.inc"
+
+#endif // SUPPORT_HLCFDIALECT_HLCFOPS_H
