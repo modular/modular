8d1a842356d228746426f6e070e60ed08c52c89a
Aman LaChapelle <aman.lachapelle@gmail.com>
2022-11-16 10:37:14 -0800

    [Support] Add inflate/deflate capabilities from/to `cache.symbol`. (#4978)
    This patch adds support for inflating a symbol that's been cached and represented as a `cache.symbol`, as well as support for deflating a symbol and replacing it with a `cache.symbol`. This patch also adds tests for deflating, and roundtrip support.
    
    MODULAR_ORIG_COMMIT_REV_ID: 0f1f7eb6b90082b205f3367dfbcef27368846328

--- Support/include/Support/CacheDialect/CacheOps.h
+++ Support/include/Support/CacheDialect/CacheOps.h
@@ -7,11 +7,35 @@
 #ifndef SUPPORT_CACHEDIALECT_CACHEOPS_H
 #define SUPPORT_CACHEDIALECT_CACHEOPS_H
 
+#include "Support/BlobCache.h"
 #include "Support/CacheDialect/CacheAttrs.h"
 #include "Support/LLVMCompilerForwardDecls.h"
 #include "Support/LogicalResult.h"
 #include "mlir/IR/OpDefinition.h"
 #include "mlir/IR/SymbolTable.h"
+
+namespace M::Cache {
+// Forward declare SymbolOp so we can use it here.
+class SymbolOp;
+
+/// The Cache dialect stores the region of an op - this defines the cache key.
+/// It can be a region (indicating that we need to hash the region) or a string
+/// (indicating that we already know the hash).
+struct RegionCacheKey {
+  using KeyTy = std::variant<Region *, StringRef>;
+  static std::string hashKey(KeyTy key);
+};
+
+/// This function allows the user to deflate a symbol op, store its body
+/// in the cache, and replace it with a `cache.symbol`.
+FailureOr<SymbolOp> deflateSymbol(Operation *symbol, SymbolTable &symtab,
+                                  BlobCache<RegionCacheKey> &cache);
+
+/// This function allows the user to inflate a cached symbol into its original
+/// operation.
+FailureOr<Operation *> inflateSymbol(SymbolOp cached, SymbolTable &symtab,
+                                     BlobCache<RegionCacheKey> &cache);
+} // namespace M::Cache
 
 //===----------------------------------------------------------------------===//
 // ODS-Generated Declarations
