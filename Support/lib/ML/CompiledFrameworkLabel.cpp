82cf1dbb415df2b57d4d330cc6777e2a4012ddef
Mark Shields <110413041+mbs-modular@users.noreply.github.com>
2022-11-15 16:12:33 -0800

    [BEF] Move BEFContents down into Support/ML/CompiledFrameworkLabel (#4963)
    [Fixes #4896]
    
    In the switch to mop.model I needed a way to validate and decode the
    'frameworkName' attribute and BEFContents is 1:1 with that notion. To
    avoid the weirdness of a dialect depending on BEF directly, push that
    notion down into Support/ML as the way we differentiate the 'framework'
    flavor needed by the Modular runtime.
    
    MODULAR_ORIG_COMMIT_REV_ID: b8d15276ed00acf490ee319ec55b281f6eb17657

---
+++ Support/lib/ML/CompiledFrameworkLabel.cpp
@@ -0,0 +1,68 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#include "Support/ML/CompiledFrameworkLabel.h"
+
+using namespace M;
+
+const char *CompiledFrameworkLabel::getAsOpNameOrNull() const {
+  switch (value) {
+  case kTensorFlowModel:
+    return "tfp.model";
+  case kTFLiteModel:
+    return "mop.model";
+  case kFauxModel:
+    return "faux.testcase";
+  default:
+    return nullptr;
+  }
+}
+
+const char *CompiledFrameworkLabel::getAsFrameworkNameOrNull() const {
+  switch (value) {
+  case kTFLiteModel:
+    return "tfl";
+  default:
+    return nullptr;
+  }
+}
+
+bool CompiledFrameworkLabel::isValidOpName(StringRef opName) {
+  return opName == "tfp.model" || opName == "mop.model" ||
+         opName == "faux.testcase";
+}
+
+bool CompiledFrameworkLabel::isValidFrameworkName(StringRef frameworkName) {
+  return frameworkName == "tfl";
+}
+
+CompiledFrameworkLabel
+CompiledFrameworkLabel::getLabelForOpName(StringRef opName,
+                                          StringRef frameworkName) {
+  if (opName == "tfp.model")
+    return CompiledFrameworkLabel{kTensorFlowModel};
+  if (opName == "faux.testcase")
+    return CompiledFrameworkLabel{kFauxModel};
+  if (opName == "mop.model") {
+    if (frameworkName == "tfl")
+      return CompiledFrameworkLabel{kTFLiteModel};
+  }
+  return CompiledFrameworkLabel{kUnknown};
+}
+
+const char *CompiledFrameworkLabel::getAsString() const {
+  switch (value) {
+  case kUnknown:
+    return "unknown";
+  case kTensorFlowModel:
+    return "compiled TensorFlow model";
+  case kTFLiteModel:
+    return "compiled TFLite model";
+  case kFauxModel:
+    return "compiled Faux model";
+  }
+  llvm::report_fatal_error("invalid CompiledFrameworkLabel value");
+};
