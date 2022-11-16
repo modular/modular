bdb6a899842280fe1debffc2b80a1c4b4b7a20e4
Mark Shields <110413041+mbs-modular@users.noreply.github.com>
2022-11-15 16:12:33 -0800

    [BEF] Move BEFContents down into Support/ML/CompiledFrameworkLabel (#4963)
    [Fixes #4896]
    
    In the switch to mop.model I needed a way to validate and decode the
    'frameworkName' attribute and BEFContents is 1:1 with that notion. To
    avoid the weirdness of a dialect depending on BEF directly, push that
    notion down into Support/ML as the way we differentiate the 'framework'
    flavor needed by the Modular runtime.
    
    MODULAR_ORIG_COMMIT_REV_ID: b8d15276ed00acf490ee319ec55b281f6eb17657

---
+++ Support/include/Support/ML/CompiledFrameworkLabel.h
@@ -0,0 +1,86 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef SUPPORT_ML_COMPILEDFRAMEWORKLABEL_H
+#define SUPPORT_ML_COMPILEDFRAMEWORKLABEL_H
+
+#include "Support/LLVMForwardDecls.h"
+#include "llvm/ADT/StringRef.h"
+
+#include <cassert>
+
+namespace M {
+
+/// Indicates the 'framework' needed to execute a compiled model.
+///
+/// This is an enum coupled with helpers needed at compile-time, at
+/// runtime, and within tools to indicate which overall framework
+/// should be used to execute a compiled model. The framework influences:
+///  - how the model's inputs and outputs need to be adjusted to mediate
+///    between our internal implementation and the frameworks API.
+///  - which additional shared libraries need to be loaded to support
+///    execution.
+///
+/// Note that this cannot be used to represent source-level model types
+/// since most frameworks have multiple source representations.
+///
+/// In MLIR the top-level 'model' op determines the framework, currently by
+/// a mixture of op name and, for mop.model, 'frameworkName' string attribute.
+///
+/// In BEF the top-level invokable can be decoded to recover the op name
+/// and (if needed) frameworkName attribute to match MLIR.
+///
+/// Some tools need to 'sniff' formats from textual files. Some of the op
+/// name and framework name conventions encoded here can help with that.
+///
+/// TODO(#4991): Hoist up into GenericML once BEFFile supports it.
+class CompiledFrameworkLabel {
+public:
+  enum Cases {
+    /// Framework is unknown.
+    kUnknown,
+    /// A compiled TensorFlow model.
+    kTensorFlowModel,
+    /// A compiled TFLite model.
+    kTFLiteModel,
+    /// A compiled Faux test case.
+    kFauxModel,
+  } value;
+
+  /// Returns the top-level operator name representing this framework, or
+  /// null if no such representation is possible.
+  const char *getAsOpNameOrNull() const;
+
+  /// Returns the frameworkName representing this framework, or null
+  /// if no such representation is possible.
+  const char *getAsFrameworkNameOrNull() const;
+
+  /// Returns true if opName is a valid top-level operator name.
+  static bool isValidOpName(StringRef opName);
+
+  /// Returns true if opName is a valid framework name.
+  static bool isValidFrameworkName(StringRef frameworkName);
+
+  /// Returns the framework describing the framework appropriate for the
+  /// given top-level opName with an optional frameworkName if it is known.
+  /// Returns unknown if framework cannot be determined.
+  static CompiledFrameworkLabel getLabelForOpName(StringRef opName,
+                                                  StringRef frameworkName = {});
+
+  /// Return a printable string identifying this framework label.
+  const char *getAsString() const;
+
+  constexpr bool operator==(CompiledFrameworkLabel other) const {
+    return value == other.value;
+  }
+  constexpr bool operator!=(CompiledFrameworkLabel other) const {
+    return !(*this == other);
+  }
+};
+
+} // namespace M
+
+#endif // SUPPORT_ML_COMPILEDFRAMEWORKLABEL_H
