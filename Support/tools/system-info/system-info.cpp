94d7cd630762329436046ac79adbce641a10a911
abdul dakkak <adakkak@modular.com>
2022-11-16 11:04:34 -0800

    [Support] Split the cache query from system-info (#4980)
    This lifts the cache query function from system-info to the MSupport
    library. This allows other parts of the code to use this function.
    
    MODULAR_ORIG_COMMIT_REV_ID: 8659d4aa82ad7ac6a364f47b92f9b70b171878e6

--- Support/tools/system-info/system-info.cpp
+++ Support/tools/system-info/system-info.cpp
@@ -5,20 +5,12 @@
 //===----------------------------------------------------------------------===//
 
 #include "Support/CommandLine.h"
-#include "Support/ErrorOr.h"
+#include "Support/Host.h"
 #include "Support/SIMD.h"
-#include "mlir/Support/FileUtilities.h"
 #include "llvm/ADT/STLExtras.h"
 #include "llvm/ADT/StringMap.h"
 #include "llvm/Support/Host.h"
-#include "llvm/Support/MemoryBuffer.h"
 #include "llvm/Support/raw_ostream.h"
-
-#include <errno.h>
-
-#ifdef __APPLE__
-#include <sys/sysctl.h>
-#endif // __APPLE__
 
 using namespace M;
 using namespace llvm;
@@ -62,68 +54,6 @@ struct SystemInfoCLIOptions {
 };
 } // namespace
 
-template <int CacheLevel>
-static M::ErrorOr<size_t> cacheSize() {
-#if defined(__APPLE__)
-  size_t result;
-  size_t len = sizeof(result);
-  switch (CacheLevel) {
-  case 1:
-    if (sysctlbyname("hw.l1dcachesize", &result, &len, nullptr, 0))
-      return M::Error("unable to query the hw.l1dcachesize");
-    return result;
-  case 2:
-    if (sysctlbyname("hw.l2cachesize", &result, &len, nullptr, 0))
-      return M::Error("unable to query the hw.l3dcachesize");
-    return result;
-  default:
-    return 0;
-  }
-#elif defined(__linux__)
-  std::string path = "/sys/devices/system/cpu/cpu0/cache/index" +
-                     std::to_string(CacheLevel - 1) + "/size";
-  auto file = llvm::MemoryBuffer::getFileOrSTDIN(path);
-  if (std::error_code error = file.getError())
-    return M::Error("unable to open '" + path + "': " + error.message());
-
-  std::string buffer = (*file)->getBuffer().str();
-  const char *line = buffer.c_str();
-
-  char *suffix;
-  size_t quantity = std::strtoull(line, &suffix, 0);
-  if (errno)
-    return M::Error(Twine("Unable to parse the ") + buffer + " from the '" +
-                    path + "' file: " + strerror(errno));
-
-  size_t multiplier = 1;
-  switch (*suffix) {
-  case 'g':
-    [[fallthrough]];
-  case 'G':
-    multiplier = 1024;
-    [[fallthrough]];
-  case 'm':
-    [[fallthrough]];
-  case 'M':
-    multiplier *= 1024;
-    [[fallthrough]];
-  case 'k':
-    [[fallthrough]];
-  case 'K':
-    multiplier *= 1024;
-  default:
-    break;
-  }
-  return quantity * multiplier;
-#elif defined(_WIN32)
-  // TODO: Figure out later, but this needs to use the
-  // GetLogicalProcessorInformation API.
-  return 0;
-#else
-  llvm_unreachable("unsupported platform");
-#endif
-}
-
 int main(int argc, char **argv) {
   SystemInfoCLIOptions cli;
 
@@ -154,23 +84,39 @@ int main(int argc, char **argv) {
     os << kPreferredSIMDBitWidth;
     break;
   case QuerySystemProperty::L1CacheSize: {
-    if (auto val = cacheSize<1>(); succeeded(val))
-      os << *val;
+    auto val = getHostCPUCacheSize(1);
+    if (val.isError()) {
+      os << "Error: " << val.getError();
+      return EXIT_SUCCESS;
+    }
+    os << *val;
     break;
   }
   case QuerySystemProperty::L2CacheSize: {
-    if (auto val = cacheSize<2>(); succeeded(val))
-      os << *val;
+    auto val = getHostCPUCacheSize(1);
+    if (val.isError()) {
+      os << "Error: " << val.getError();
+      return EXIT_SUCCESS;
+    }
+    os << *val;
     break;
   }
   case QuerySystemProperty::L3CacheSize: {
-    if (auto val = cacheSize<3>(); succeeded(val))
-      os << *val;
+    auto val = getHostCPUCacheSize(3);
+    if (val.isError()) {
+      os << "Error: " << val.getError();
+      return EXIT_SUCCESS;
+    }
+    os << *val;
     break;
   }
   case QuerySystemProperty::L4CacheSize: {
-    if (auto val = cacheSize<4>(); succeeded(val))
-      os << *val;
+    auto val = getHostCPUCacheSize(4);
+    if (val.isError()) {
+      os << "Error: " << val.getError();
+      return EXIT_SUCCESS;
+    }
+    os << *val;
     break;
   }
   }
