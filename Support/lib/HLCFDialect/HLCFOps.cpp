025985534c15cd1ab253d047026f80363d8207c9
Jeff Niu <jeff@modular.com>
2022-11-19 06:13:43 +0900

    [Support] Inline some HLCF builders (#5128)
    Post-commit review of #4836
    
    MODULAR_ORIG_COMMIT_REV_ID: 6619b7c84eac97ccf92073def12c5160bb9e28d6

--- Support/lib/HLCFDialect/HLCFOps.cpp
+++ Support/lib/HLCFDialect/HLCFOps.cpp
@@ -237,26 +237,6 @@ LogicalResult LoopOp::verify() {
   return success();
 }
 
-void LoopOp::build(OpBuilder &b, OperationState &state) {
-  build(b, state, {}, {});
-}
-
-//===----------------------------------------------------------------------===//
-// IfOp
-//===----------------------------------------------------------------------===//
-
-void IfOp::build(OpBuilder &b, OperationState &state, Value cond) {
-  build(b, state, {}, cond);
-}
-
-//===----------------------------------------------------------------------===//
-// ContinueOp
-//===----------------------------------------------------------------------===//
-
-void ContinueOp::build(OpBuilder &b, OperationState &state) {
-  build(b, state, {});
-}
-
 //===----------------------------------------------------------------------===//
 // BreakOp
 //===----------------------------------------------------------------------===//
@@ -271,18 +251,6 @@ mlir::Speculation::Speculatability BreakOp::getSpeculatability() {
   return isa<LoopOp>((*this)->getParentOp())
              ? mlir::Speculation::Speculatable
              : mlir::Speculation::NotSpeculatable;
-}
-
-void BreakOp::build(OpBuilder &b, OperationState &state) {
-  build(b, state, {});
-}
-
-//===----------------------------------------------------------------------===//
-// YieldOp
-//===----------------------------------------------------------------------===//
-
-void YieldOp::build(OpBuilder &b, OperationState &state) {
-  build(b, state, {});
 }
 
 //===----------------------------------------------------------------------===//
