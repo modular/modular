37f723df37ec71407ef09a0dc9f23233a19f6a0c
River Riddle <riddleriver@gmail.com>
2022-12-13 17:50:37 -0800

    [DebugInfo] Create a DIBuilder class to simplify generation (#6025)
    This is just a simple helper class that makes it easier to build
    constructs, and manage scopes. This will be more useful when
    plumbing debug info into lit (which is a bit more adhoc than
    the synthetic debug info builder).
    
    MODULAR_ORIG_COMMIT_REV_ID: e205ef749818acfcb2cdd4bba82476e23054b6be

---
+++ Support/lib/DebugInfoDialect/IR/DIBuilder.cpp
@@ -0,0 +1,82 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#include "Support/DebugInfoDialect/IR/DIBuilder.h"
+
+using namespace M;
+using namespace M::DebugInfo;
+
+//===----------------------------------------------------------------------===//
+// DIBuilder
+//===----------------------------------------------------------------------===//
+
+DICompileUnitAttr DIBuilder::initializeCompileUnit(unsigned sourceLanguage,
+                                                   DIFileAttr file,
+                                                   StringRef producer,
+                                                   bool isOptimized,
+                                                   EmissionKind emissionKind) {
+  assert(!compileUnit && "compile unit already initialized");
+  compileUnit = DICompileUnitAttr::get(sourceLanguage, file, producer,
+                                       isOptimized, emissionKind);
+  return compileUnit;
+}
+
+//===----------------------------------------------------------------------===//
+// Scopes
+
+void DIBuilder::pushScope(DIScopeAttr scope) { scopes.push_back(scope); }
+
+void DIBuilder::popScope() {
+  assert(!scopes.empty() && "Cannot pop the compile unit scope!");
+  scopes.pop_back();
+}
+
+Location DIBuilder::createScopedLoc(Location loc) {
+  if (scopes.empty())
+    return loc;
+
+  // Check if this is already a scoped location with the expected scope.
+  if (auto scopedLoc = dyn_cast<mlir::FusedLocWith<DIScopeAttr>>(loc))
+    if (scopedLoc.getMetadata() == scopes.back())
+      return loc;
+  return FusedLoc::get(loc.getContext(), loc, scopes.back());
+}
+
+//===----------------------------------------------------------------------===//
+// Creation
+
+DILexicalBlockAttr DIBuilder::createLexicalBlock(DIFileAttr file, unsigned line,
+                                                 unsigned column) {
+  auto scope = cast<DILocalScopeAttr>(scopes.back());
+  return DILexicalBlockAttr::get(scope, file, line, column);
+}
+
+DISubprogramAttr DIBuilder::createSubprogram(StringRef name,
+                                             StringRef linkageName,
+                                             DIFileAttr file, unsigned int line,
+                                             unsigned int scopeLine,
+                                             SubprogramFlags subprogramFlags,
+                                             DISubroutineType type) {
+  return DISubprogramAttr::get(compileUnit, scopes.back(), name, linkageName,
+                               file, line, scopeLine, subprogramFlags, type);
+}
+
+DIFileAttr DIBuilder::createFile(StringRef name, StringRef directory) {
+  return DIFileAttr::get(context, name, directory);
+}
+DIFileAttr DIBuilder::createFile(FileLineColLoc loc) {
+  return DIFileAttr::get(context, loc.getFilename(), "/");
+}
+
+DILocalVariableAttr DIBuilder::createLocalVariable(StringRef name,
+                                                   DIFileAttr file,
+                                                   unsigned line, unsigned arg,
+                                                   unsigned alignInBits,
+                                                   DIType type) {
+  auto scope = cast<DILocalScopeAttr>(scopes.back());
+  return DILocalVariableAttr::get(scope, name, file, line, arg, alignInBits,
+                                  type);
+}
