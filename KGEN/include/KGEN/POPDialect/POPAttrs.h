9bb399cfc72461178a336ef6fe11e1951d3def04
Jeff Niu <jeff@modular.com>
2022-12-14 12:13:59 -0800

    [KGEN] Add a `POP::SIMDAttr` (#6010)
    This attribute represents compile-time constants of SIMDType. This
    attribute makes an effort to accurately roundtrip floating point values by
    disallowing conversions and using the right APFloat methods to convert
    losslessly between textual IR.
    
    MODULAR_ORIG_COMMIT_REV_ID: 630952cba68d3ec3e0c31d4ffa1febc7facc34e0

--- KGEN/include/KGEN/POPDialect/POPAttrs.h
+++ KGEN/include/KGEN/POPDialect/POPAttrs.h
@@ -7,8 +7,65 @@
 #ifndef KGEN_POPDIALECT_POPATTRS_H
 #define KGEN_POPDIALECT_POPATTRS_H
 
+#include "KGEN/POPDialect/POPTypes.h"
 #include "mlir/IR/Attributes.h"
+#include "mlir/IR/BuiltinAttributeInterfaces.h"
 #include "mlir/IR/SubElementInterfaces.h"
+#include "llvm/ADT/APSInt.h"
+
+//===----------------------------------------------------------------------===//
+// DTypeValue
+//===----------------------------------------------------------------------===//
+
+namespace M::KGEN::POP {
+/// This class stores a value of a particular dtype. It supports containing
+/// integer, float, and bool dtype values only.
+class DTypeValue {
+public:
+  /// Get an integer value.
+  DTypeValue(APSInt value, DType dtype);
+
+  /// Get a floating point value.
+  DTypeValue(APFloat value, DType dtype);
+
+  /// Get a bool value.
+  DTypeValue(bool value, DType dtype);
+
+  /// Compare two dtype values.
+  bool operator==(const DTypeValue &rhs) const {
+    return std::tie(dtype, data) == std::tie(rhs.dtype, rhs.data);
+  };
+
+  /// Get the underlying data.
+  const APInt &getData() const { return data; }
+
+  /// Get the dtype.
+  DType getDType() const { return dtype; }
+
+  /// Get the value as an integer.
+  APSInt getIntVal() const;
+
+  /// Get the value as a float.
+  APFloat getFloatVal() const;
+
+  /// Get the value as a bool
+  bool getBoolVal() const;
+
+private:
+  /// Private constructor.
+  DTypeValue(APInt data, DType dtype) : data(std::move(data)), dtype(dtype) {}
+
+  /// All values are stored as `APInt`s.
+  APInt data;
+
+  /// The dtype of the value. This indicates how to interpret `data`.
+  DType dtype;
+};
+} // namespace M::KGEN::POP
+
+//===----------------------------------------------------------------------===//
+// ODS-Generated Definitions
+//===----------------------------------------------------------------------===//
 
 #include "KGEN/POPDialect/POPEnums.h.inc"
 
--- KGEN/lib/POPDialect/POPAttrs.cpp
+++ KGEN/lib/POPDialect/POPAttrs.cpp
@@ -8,18 +8,220 @@
 #include "KGEN/POPDialect/POPDialect.h"
 #include "Support/LLVMCompilerForwardDecls.h"
 #include "Support/LLVMForwardDecls.h"
+#include "Support/STLExtras.h"
 #include "mlir/IR/Builders.h"
 #include "mlir/IR/DialectImplementation.h"
 #include "llvm/ADT/TypeSwitch.h"
+#include "llvm/Support/Error.h"
 
-#include "KGEN/POPDialect/POPEnums.cpp.inc"
+using namespace M;
+using namespace KGEN;
+using namespace POP;
 
-#define GET_ATTRDEF_CLASSES
-#include "KGEN/POPDialect/POPAttrs.cpp.inc"
+//===----------------------------------------------------------------------===//
+// POPDialect
+//===----------------------------------------------------------------------===//
 
-void M::KGEN::POP::POPDialect::registerAttributes() {
+void POPDialect::registerAttributes() {
   addAttributes<
 #define GET_ATTRDEF_LIST
 #include "KGEN/POPDialect/POPAttrs.cpp.inc"
       >();
 }
+
+//===----------------------------------------------------------------------===//
+// DTypeValue
+//===----------------------------------------------------------------------===//
+
+/// Returns true if this is a supported float dtype.
+static bool isValidFloatDType(DType dtype) {
+  return dtype.isFloat() &&
+         !(dtype == DType::f8 || dtype == DType::f24 || dtype == DType::tf32);
+}
+
+/// Returns the floating semantics for the given dtype.
+static const llvm::fltSemantics &getFloatSemantics(DType dtype) {
+  switch (dtype.getValue()) {
+  case DType::f16:
+    return APFloat::IEEEhalf();
+  case DType::f32:
+    return APFloat::IEEEsingle();
+  case DType::f64:
+    return APFloat::IEEEdouble();
+  case DType::f128:
+    return APFloat::IEEEquad();
+  case DType::bf16:
+    return APFloat::BFloat();
+  case DType::f80:
+    return APFloat::x87DoubleExtended();
+
+  case DType::f8:
+  case DType::f24:
+  case DType::tf32:
+  default:
+    llvm_unreachable("unknown float dtype");
+  }
+}
+
+DTypeValue::DTypeValue(APSInt value, DType dtype)
+    : DTypeValue(APInt(value), dtype) {
+  assert(dtype.isInt() && value.getBitWidth() == dtype.getIntegerWidthInBits());
+}
+
+DTypeValue::DTypeValue(APFloat value, DType dtype)
+    : DTypeValue(value.bitcastToAPInt(), dtype) {
+  assert(isValidFloatDType(dtype));
+}
+
+DTypeValue::DTypeValue(bool value, DType dtype)
+    : DTypeValue(APInt(1, value), dtype) {
+  assert(dtype.isBool());
+}
+
+APSInt DTypeValue::getIntVal() const {
+  assert(dtype.isInt());
+  return APSInt(data, /*isUnsigned=*/dtype.isUInt());
+}
+
+APFloat DTypeValue::getFloatVal() const {
+  assert(isValidFloatDType(dtype));
+  return APFloat(getFloatSemantics(dtype), data);
+}
+
+bool DTypeValue::getBoolVal() const {
+  assert(dtype.isBool());
+  return data.isOne();
+}
+
+namespace M::KGEN::POP {
+/// Provide the ability to hash values for attribute uniquing.
+inline llvm::hash_code hash_value(const DTypeValue &value) {
+  return hash_combine(value.getData(), value.getDType().getValue());
+}
+} // namespace M::KGEN::POP
+
+/// Parse a value of a particular DType.
+static FailureOr<DTypeValue> parseDTypeValue(AsmParser &p, DType dtype) {
+  llvm::SMLoc loc = p.getCurrentLocation();
+
+  // Handle integers.
+  if (dtype.isInt()) {
+    APInt apInt;
+    if (p.parseInteger(apInt))
+      return failure();
+    APSInt apsInt(apInt, /*isUnsigned=*/dtype.isUInt());
+    APSInt fitted = apsInt.extOrTrunc(dtype.getIntegerWidthInBits());
+    if (fitted.extOrTrunc(apsInt.getBitWidth()) != apsInt) {
+      SmallVector<char, 256> strVal;
+      apsInt.toString(strVal);
+      return p.emitError(loc, "integer value doesn't fit into ")
+             << dtype.getIntegerWidthInBits()
+             << " bits: " << StringRef(strVal.data(), strVal.size());
+    }
+    return DTypeValue(fitted, dtype);
+  }
+
+  // Handle floats.
+  if (dtype.isFloat()) {
+    // Parse the float value as a string. MLIR doesn't expose access to raw
+    // float literals. Make sure we can parse and print arbitrary precision
+    // floats losslessly.
+    std::string strVal;
+    if (p.parseString(&strVal))
+      return failure();
+    APFloat apFp(getFloatSemantics(dtype));
+    llvm::Expected<APFloat::opStatus> status =
+        apFp.convertFromString(strVal, APFloat::rmNearestTiesToEven);
+    if (llvm::errorToBool(status.takeError()))
+      return p.emitError(loc, "failed to parse floating point value");
+    if (*status != APFloat::opOK)
+      return p.emitError(loc, "cannot convert ")
+             << strVal << " to " << dtype.getAsString();
+    return DTypeValue(apFp, dtype);
+  }
+
+  // Handle bools.
+  assert(dtype.isBool());
+  if (succeeded(p.parseOptionalKeyword("true")))
+    return {{true, dtype}};
+  if (succeeded(p.parseOptionalKeyword("false")))
+    return {{false, dtype}};
+  return p.emitError(loc, "expected 'true' or 'false' for bool literal");
+}
+
+LogicalResult SIMDAttr::verify(function_ref<InFlightDiagnostic()> emitError,
+                               ArrayRef<DTypeValue> values, SIMDType type) {
+  Optional<KGENDType> dtype = type.getResolvedDType();
+  Optional<int64_t> size = type.getResolvedSize();
+  if (!dtype || !size)
+    return emitError() << "SIMD attribute requires fully-resolved SIMD type";
+  if (static_cast<int64_t>(values.size()) != *size)
+    return emitError() << "wrong number of elements, got " << values.size()
+                       << " but expected " << *size;
+  if (!llvm::all_of(values, [&](const DTypeValue &value) {
+        return value.getDType() == *dtype;
+      }))
+    return emitError() << "all elements must have dtype "
+                       << dtype->getAsString();
+  return success();
+}
+
+/// Value is a constant by definition.
+bool SIMDAttr::isConstant() const { return true; }
+
+//===----------------------------------------------------------------------===//
+// custom<DTypeValues>
+//===----------------------------------------------------------------------===//
+
+static ParseResult parseDTypeValues(AsmParser &p,
+                                    FailureOr<SmallVector<DTypeValue>> &values,
+                                    SIMDType type) {
+  Optional<KGENDType> dtype = type.getResolvedDType();
+  Optional<int64_t> size = type.getResolvedSize();
+  if (!dtype || !size) {
+    return p.emitError(p.getCurrentLocation(),
+                       "SIMD constant requires a concrete type");
+  }
+  if (!dtype->isInt() && !isValidFloatDType(*dtype) && !dtype->isBool()) {
+    return p.emitError(
+        p.getCurrentLocation(),
+        "only integer, float, and bool dtype constants can be parsed");
+  }
+
+  values.emplace();
+  auto parseElt = [&](int64_t) -> ParseResult {
+    FailureOr<DTypeValue> value = parseDTypeValue(p, *dtype);
+    if (failed(value))
+      return failure();
+    values->push_back(*value);
+    return success();
+  };
+  return failableInterleave(llvm::seq<int64_t>(0, *size), parseElt,
+                            [&] { return p.parseComma(); });
+}
+
+static void printDTypeValues(AsmPrinter &p, ArrayRef<DTypeValue> values,
+                             SIMDType type) {
+  DType dtype = *type.getResolvedDType();
+  auto printElt = [&](const DTypeValue &value) {
+    if (dtype.isInt()) {
+      p << APInt(value.getIntVal());
+    } else if (dtype.isFloat()) {
+      SmallVector<char, 256> strVal;
+      value.getFloatVal().toString(strVal);
+      p << '"' << StringRef(strVal.data(), strVal.size()) << '"';
+    } else {
+      p << (value.getBoolVal() ? "true" : "false");
+    }
+  };
+  llvm::interleaveComma(values, p, printElt);
+}
+
+//===----------------------------------------------------------------------===//
+// ODS-Generated Declarations
+//===----------------------------------------------------------------------===//
+
+#include "KGEN/POPDialect/POPEnums.cpp.inc"
+
+#define GET_ATTRDEF_CLASSES
+#include "KGEN/POPDialect/POPAttrs.cpp.inc"
