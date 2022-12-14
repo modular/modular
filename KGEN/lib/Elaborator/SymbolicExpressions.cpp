799fe8c8d0446205f0775314667e1d7f4fbe5d55
Jeff Niu <jeff@modular.com>
2022-12-14 12:14:59 -0800

    [KGEN] Move ErrorTree into its own file (NFC) (#6048)
    MODULAR_ORIG_COMMIT_REV_ID: ab044f0c5fd9e3ae5308a13a2bb78790ec5e49a8

--- KGEN/lib/Elaborator/SymbolicExpressions.cpp
+++ KGEN/lib/Elaborator/SymbolicExpressions.cpp
@@ -182,51 +182,8 @@ IREvaluator::evaluateFunction(FuncOp func, ArrayRef<TypedAttr> inputs) {
                            " result is not a parameter expression")));
 }
 
-//===----------------------------------------------------------------------===//
-// ErrorTree
-//===----------------------------------------------------------------------===//
-
-ErrorTree::ErrorTree(Location loc, Error error, ErrorTree causes)
-    : loc(loc), error(std::move(error)) {
-  addCause(std::move(causes));
-}
-
-ErrorTree::ErrorTree(Location loc, Error error,
-                     MutableArrayRef<ErrorTree> causes)
-    : loc(loc), error(std::move(error)) {
-  addCauses(causes);
-}
-
-ErrorTree ErrorTree::copy() const {
-  ErrorTree copy(loc, error.copy());
-  copy.causes.reserve(causes.size());
-  for (const ErrorTree &cause : causes)
-    copy.causes.push_back(cause.copy());
-  return copy;
-}
-
-void ErrorTree::emit(
-    function_ref<InFlightDiagnostic(Location)> emitError) const {
-  // Emit the main error.
-  InFlightDiagnostic diag = emitError(loc) << getMessage();
-  // Emit the causes
-  emit(diag, causes, /*indentDepth=*/2);
-}
-
-void ErrorTree::emit(InFlightDiagnostic &diag, ArrayRef<ErrorTree> errors,
-                     unsigned indentDepth) {
-  if (errors.empty())
-    return;
-
-  std::string spaces(indentDepth, ' ');
-  for (const ErrorTree &err : errors) {
-    diag.attachNote(err.loc) << spaces << err.getMessage();
-    emit(diag, err.causes, indentDepth + 2);
-  }
-}
-
 //=----------------------------------------------------------------------===//
-// R Evaluator
+// IR Evaluator
 //===----------------------------------------------------------------------===//
 
 FailureOr<TypedAttr>
