e169eed6753c43d1e112bbd4bd6bdff7d1440322
Chris Lattner <lattner@users.noreply.github.com>
2022-12-21 11:26:02 -0800

    [Lit] Fix a bug in identifier lexing. (#6276)
    minus is not an identifier character, for example `a-b` is three
    tokens, not a 3-character long identifier.
    
    MODULAR_ORIG_COMMIT_REV_ID: cbe1333bd9137c06af7c6e8671f4e37b18f32d7e

--- KGEN/lib/ParseLit/LitLexer.cpp
+++ KGEN/lib/ParseLit/LitLexer.cpp
@@ -311,9 +311,9 @@ LitToken LitLexer::lexTokenImpl() {
 ///
 LitToken LitLexer::lexIdentifierOrKeyword(const char *tokStart,
                                           ssize_t indentation) {
-  // Match the rest of the identifier regex: [0-9a-zA-Z_$-]*
+  // Match the rest of the identifier regex: [0-9a-zA-Z_$]*
   while (llvm::isAlpha(*curPtr) || llvm::isDigit(*curPtr) || *curPtr == '_' ||
-         *curPtr == '$' || *curPtr == '-')
+         *curPtr == '$')
     ++curPtr;
 
   StringRef spelling(tokStart, curPtr - tokStart);
