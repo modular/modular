ab409cca313b18dbf187b9db519f410f8c9c92cd
Jeff Niu <jeff@modular.com>
2022-12-22 21:51:36 -0800

    [Support] Use external models for DataLayoutInterface on builtin types (#6383)
    MODULAR_ORIG_COMMIT_REV_ID: f4f7866bf1f21b36c6b791868a6d3c9ccecbd392

--- Support/lib/MDialect/MDialect.cpp
+++ Support/lib/MDialect/MDialect.cpp
@@ -15,6 +15,7 @@ using namespace M;
 void MDialect::initialize() {
   registerAttributes();
   registerTypes();
+  injectAttrInterfaces();
 }
 
 //===----------------------------------------------------------------------===//
