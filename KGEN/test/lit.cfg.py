5ed59c09d1f84f099bb1d261b95af5a70c5ebcd5
abdul dakkak <adakkak@modular.com>
2022-12-28 11:10:49 -0800

    [Lit] Switch tests to import stdlib, NFC (#6483)
    This switches the lit tests to import stdlib rather than include it.
    This changes the mangling of the names, which have been applied. The
    change makes stdlib a lot more flexible.
    
    MODULAR_ORIG_COMMIT_REV_ID: 678e1413de0e5b71c30304bfb27f72e90daeba9f

--- KGEN/test/lit.cfg.py
+++ KGEN/test/lit.cfg.py
@@ -22,6 +22,13 @@ config.test_source_root = os.path.dirname(__file__)
 # test_exec_root: The root path where tests should be run.
 config.test_exec_root = os.path.join(config.modular_obj_root, "KGEN", "test")
 
+config.substitutions.append(
+    (
+        "%stdlibdir",
+        os.path.join(config.modular_src_root, "Kernels", "lit-stdlib"),
+    )
+)
+
 tool_dirs = [
     config.modular_tools_dir,
     config.mlir_tools_dir,
