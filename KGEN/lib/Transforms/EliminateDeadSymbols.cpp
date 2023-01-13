db62deb46b1cea8848730a57ac366e5f2d9a9927
Aman LaChapelle <aman.lachapelle@gmail.com>
2023-01-12 18:14:42 -0800

    [KGEN] Add our own SymbolDCE pass. (#6894)
    This pass is meant to run post-elaboration and takes into account our visibility characteristics. It erases symbols that aren't used by an exported symbol or directly exported themselves.
    
    MODULAR_ORIG_COMMIT_REV_ID: fb20343eb16b9b02ad203789bb136f5456cb305e

---
+++ KGEN/lib/Transforms/EliminateDeadSymbols.cpp
@@ -0,0 +1,69 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#include "KGEN/KGENDialect/KGENOps.h"
+#include "KGEN/KGENPasses.h"
+#include "Support/Compiler/SymbolTableAnalysis.h"
+
+using namespace M;
+using namespace KGEN;
+
+namespace M::KGEN {
+#define GEN_PASS_DEF_ELIMINATEDEADSYMBOLS
+#include "KGEN/KGENPasses.h.inc"
+} // namespace M::KGEN
+
+namespace {
+struct EliminateDeadSymbolsPass
+    : M::KGEN::impl::EliminateDeadSymbolsBase<EliminateDeadSymbolsPass> {
+  void runOnOperation() override;
+};
+} // namespace
+
+void EliminateDeadSymbolsPass::runOnOperation() {
+  ModuleOp theModule = getOperation();
+  if (!theModule.getOps<GeneratorOp>().empty()) {
+    mlir::emitError(theModule.getLoc())
+        << "cannot run EliminateDeadSymbols before elaboration";
+    return signalPassFailure();
+  }
+
+  auto &analysis = getAnalysis<SymbolTableAnalysis>();
+
+  DenseSet<StringAttr> usedSymbols;
+  // The base of the export set is the used symbols.
+  theModule.walk([&](ExportOp exportOp) {
+    for (auto e : exportOp.getExports().getAsRange<SymbolRefAttr>())
+      usedSymbols.insert(e.getRootReference());
+  });
+
+  // Now walk the used symbols and find symbols that they use.
+  llvm::SetVector<StringAttr> worklist = {usedSymbols.begin(),
+                                          usedSymbols.end()};
+  while (!worklist.empty()) {
+    StringAttr symbolRef = worklist.pop_back_val();
+    auto callee = analysis.getTopLevelSymbolTable().lookup<FuncOp>(symbolRef);
+    if (!callee)
+      continue;
+    // Walk the callee and add any symbol uses to the worklist as long as
+    // we haven't already seen them.
+    callee.walk([&](Operation *op) {
+      op->getAttrDictionary().walkSubAttrs([&](Attribute attr) {
+        if (auto symbolRef = dyn_cast<SymbolRefAttr>(attr)) {
+          if (usedSymbols.insert(symbolRef.getRootReference()).second)
+            worklist.insert(symbolRef.getRootReference());
+        }
+      });
+    });
+  }
+
+  // OK, we have all the used symbols. Now, just erase ones that aren't in
+  // there.
+  for (auto sym :
+       llvm::make_early_inc_range(theModule.getOps<mlir::SymbolOpInterface>()))
+    if (!usedSymbols.contains(sym.getNameAttr()))
+      analysis.getTopLevelSymbolTable().erase(sym);
+}
