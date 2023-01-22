e845d45cc819b28c3c207d1718b2dc66a4bc2cd7
Jeff Niu <jeff@modular.com>
2023-01-21 20:31:28 -0800

    [KGEN] Add a `lift-mlir-ops` pass (#7324)
    This PR adds a simple pass that lifts all `#kgen.param.mlir_op`
    attributes into KGEN generators, while deduplicating them where
    possible. This is necessary to avoid duplicating all MLIR infrastructure
    around handling ops, especially op lowering. Importantly, all MLIR ops
    described by these attributes will have to be materialized as IR
    eventually anyways, and by doing so globally, we can have better error
    reporting (verifier failures, for example), and less IR churn by materializing
    each unique op only once.
    
    MODULAR_ORIG_COMMIT_REV_ID: 21c2cf4187d7384310f3b097107a7870cc9598f9

---
+++ KGEN/lib/Transforms/LiftMLIROperations.cpp
@@ -0,0 +1,85 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#include "KGEN/KGENDialect/KGENAttrs.h"
+#include "KGEN/KGENDialect/KGENOps.h"
+#include "KGEN/KGENPasses.h"
+#include "mlir/Analysis/SymbolTableAnalysis.h"
+#include "mlir/IR/ImplicitLocOpBuilder.h"
+
+using namespace M;
+using namespace KGEN;
+
+namespace M::KGEN {
+#define GEN_PASS_DEF_LIFTMLIROPERATIONS
+#include "KGEN/KGENPasses.h.inc"
+} // namespace M::KGEN
+
+namespace {
+struct LiftMLIROperationsPass
+    : impl::LiftMLIROperationsBase<LiftMLIROperationsPass> {
+  using LiftMLIROperationsBase::LiftMLIROperationsBase;
+
+  void runOnOperation() override;
+};
+} // namespace
+
+static SymbolConstantAttr materializeOperation(Location loc,
+                                               SymbolTable &symtab,
+                                               unsigned &counter,
+                                               MLIROpAttr opExpr) {
+  ImplicitLocOpBuilder b(loc, OpBuilder(loc.getContext()));
+  StringAttr name =
+      b.getStringAttr(opExpr.getName().getValue() + Twine(counter++));
+  auto gen = b.create<GeneratorOp>(name, opExpr.getType());
+  Block *body = b.createBlock(&gen.getBodyRegion());
+  SmallVector<Value> operands;
+  for (Type type : opExpr.getType().getValueInputs())
+    operands.push_back(body->addArgument(type, loc));
+
+  // "Bind" any remaining parameters by making them parameter reference
+  // attributes.
+  OperationState state(loc, opExpr.getName(), operands,
+                       opExpr.getType().getValueResults(),
+                       opExpr.getAttrs().getValue());
+  for (ParamDeclAttr param : opExpr.getType().getInputParams())
+    state.attributes.set(
+        param.getName(),
+        ParamDeclRefAttr::get(param.getName(), param.getType()));
+
+  Operation *op = b.create(state);
+  b.create<ReturnOp>(ArrayRef<TypedAttr>(), op->getResults());
+
+  StringAttr newName =
+      symtab.insert(gen, cast<ModuleOp>(symtab.getOp()).getBody()->begin());
+  if (newName != name)
+    gen.setSymNameAttr(newName);
+
+  return SymbolConstantAttr::get(FlatSymbolRefAttr::get(newName),
+                                 opExpr.getType());
+}
+
+void LiftMLIROperationsPass::runOnOperation() {
+  SymbolTable &symtab =
+      getAnalysis<mlir::SymbolTableAnalysis>().getTopLevelSymbolTable();
+  // Deduplicate identical operations.
+  DenseMap<MLIROpAttr, SymbolConstantAttr> opExprMap;
+  // Reduce the odds of name collision by incrementing a counter.
+  unsigned counter = 0;
+
+  // Collect all operations to materialize.
+  getOperation()->walk([&](Operation *op) {
+    mlir::AttrTypeReplacer replacer;
+    replacer.addReplacement([&](MLIROpAttr opExpr) -> Attribute {
+      SymbolConstantAttr &symbol = opExprMap[opExpr];
+      if (!symbol)
+        symbol = materializeOperation(op->getLoc(), symtab, counter, opExpr);
+      return symbol;
+    });
+    replacer.replaceElementsIn(op, /*replaceAttrs=*/true, /*replaceLocs=*/false,
+                               /*replaceTypes=*/true);
+  });
+}
