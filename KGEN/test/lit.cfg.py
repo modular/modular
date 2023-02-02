cbb92bde67fc1fbbd6a9abadc2e39879f987f41f
Jeff Niu <jeff@modular.com>
2023-02-02 14:06:35 -0800

    [lit-lang] Add `lit` tool to test dependencies (#8130)
    MODULAR_ORIG_COMMIT_REV_ID: 2ea8343d9af470f9097f684d51b59c5ead0d7f46

--- KGEN/test/lit.cfg.py
+++ KGEN/test/lit.cfg.py
@@ -34,6 +34,13 @@ tool_dirs = [
     config.mlir_tools_dir,
     config.llvm_tools_dir,
 ]
-tools = ["llvm-objdump", "kgen", "kgen-opt", "kgen-execute", "kgen-translate"]
+tools = [
+    "llvm-objdump",
+    "kgen",
+    "kgen-opt",
+    "kgen-execute",
+    "kgen-translate",
+    "lit",
+]
 
 llvm_config.add_tool_substitutions(tools, tool_dirs)
