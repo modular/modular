ac9d52774ea594de012029f0efe304bd9b42d4cd
abdul dakkak <adakkak@modular.com>
2023-02-07 07:06:32 -0800

    [Support] Extend the functionality of `system-info` (#8306)
    Now the system-info command will print out all the query values if
    none are specified. E.g.
    
    ```
    $ system-info
    target-triple: arm64-apple-darwin22.1.0
    arch: apple-m1
    features:
    core-count: 10
    simd-bitwidth: 128
    l1-cache-size: 65536
    l2-cache-size: 4194304
    l3-cache-size: 0
    l4-cache-size: 0
    ```
    
    This also allows one to specify multiple queries. E.g.
    
    ```
    $ system-info --query=target-triple,simd-bitwidth
    arm64-apple-darwin22.1.0
    128
    ```
    
    Closes #8297
    MODULAR_ORIG_COMMIT_REV_ID: 0eea8c586ec1212414faa3fe7f127230d5351de4

--- Support/tools/system-info/system-info.cpp
+++ Support/tools/system-info/system-info.cpp
@@ -32,7 +32,7 @@ enum class QuerySystemProperty {
 };
 
 struct SystemInfoCLIOptions {
-  M::cl::opt<QuerySystemProperty> QueryProperty{
+  llvm::cl::list<QuerySystemProperty> QueryProperty{
       "query", M::cl::desc("Available Queries:"),
       M::cl::values(
           clEnumValN(QuerySystemProperty::TargetTriple, "target-triple",
@@ -44,7 +44,7 @@ struct SystemInfoCLIOptions {
                      "Host CPU features printed as comma-separated values"),
           clEnumValN(QuerySystemProperty::CoreCount, "core-count",
                      "Host number of cores"),
-          clEnumValN(QuerySystemProperty::SIMDBitWidth, "simd-width",
+          clEnumValN(QuerySystemProperty::SIMDBitWidth, "simd-bitwidth",
                      "Host SIMD bitwidth"),
           clEnumValN(QuerySystemProperty::L1CacheSize, "l1-cache-size",
                      "Host L1 DCache size"),
@@ -54,9 +54,43 @@ struct SystemInfoCLIOptions {
                      "Host L3 DCache size"),
           clEnumValN(QuerySystemProperty::L4CacheSize, "l4-cache-size",
                      "Host L4 DCache size")),
-      llvm::cl::Required};
+      llvm::cl::ZeroOrMore, llvm::cl::CommaSeparated};
 };
 } // namespace
+
+static void dumpTargetTriple(raw_ostream &os) {
+  os << sys::getDefaultTargetTriple();
+}
+
+static void dumpOS(raw_ostream &os) {
+  os << llvm::Triple::getOSTypeName(
+      llvm::Triple(sys::getDefaultTargetTriple()).getOS());
+}
+
+static void dumpArch(raw_ostream &os) { os << sys::getHostCPUName(); }
+
+static void dumpFeatures(raw_ostream &os) {
+  StringMap<bool> features;
+  if (sys::getHostCPUFeatures(features)) {
+    llvm::interleaveComma(
+        llvm::make_filter_range(
+            features, [](const auto &feature) { return feature.getValue(); }),
+        os, [&](const auto &feature) { os << feature.getKey(); });
+  }
+}
+
+static void dumpCoreCount(raw_ostream &os) { os << get_physical_cores(); }
+
+static void dumpSIMDBitWidth(raw_ostream &os) { os << kPreferredSIMDBitWidth; }
+
+static void dumpCacheSize(raw_ostream &os, size_t cacheLevel) {
+  auto val = getHostCPUCacheSize(cacheLevel);
+  if (val.isError()) {
+    os << "Error: " << val.getError();
+    return;
+  }
+  os << *val;
+}
 
 int main(int argc, char **argv) {
   SystemInfoCLIOptions cli;
@@ -64,70 +98,63 @@ int main(int argc, char **argv) {
   llvm::cl::ParseCommandLineOptions(argc, argv, "Modular System Info Tool");
 
   raw_ostream &os(outs());
-  switch (cli.QueryProperty) {
-  case QuerySystemProperty::TargetTriple:
-    os << sys::getDefaultTargetTriple();
-    break;
-  case QuerySystemProperty::OS:
-    os << llvm::Triple::getOSTypeName(
-        llvm::Triple(sys::getDefaultTargetTriple()).getOS());
-    break;
-  case QuerySystemProperty::Arch:
-    os << sys::getHostCPUName();
-    break;
-  case QuerySystemProperty::Features: {
-    StringMap<bool> features;
-    if (sys::getHostCPUFeatures(features)) {
-      llvm::interleaveComma(
-          llvm::make_filter_range(
-              features, [](const auto &feature) { return feature.getValue(); }),
-          os, [&](const auto &feature) { os << feature.getKey(); });
+
+  if (cli.QueryProperty.empty()) {
+    os << "target-triple: ";
+    dumpTargetTriple(os);
+    os << "\narch: ";
+    dumpArch(os);
+    os << "\nfeatures: ";
+    dumpFeatures(os);
+    os << "\ncore-count: ";
+    dumpCoreCount(os);
+    os << "\nsimd-bitwidth: ";
+    dumpSIMDBitWidth(os);
+    os << "\nl1-cache-size: ";
+    dumpCacheSize(os, 1);
+    os << "\nl2-cache-size: ";
+    dumpCacheSize(os, 2);
+    os << "\nl3-cache-size: ";
+    dumpCacheSize(os, 3);
+    os << "\nl4-cache-size: ";
+    dumpCacheSize(os, 4);
+    os << "\n";
+    return EXIT_SUCCESS;
+  }
+
+  for (auto query : cli.QueryProperty) {
+    switch (query) {
+    case QuerySystemProperty::TargetTriple:
+      dumpTargetTriple(os);
+      break;
+    case QuerySystemProperty::OS:
+      dumpOS(os);
+      break;
+    case QuerySystemProperty::Arch:
+      dumpArch(os);
+      break;
+    case QuerySystemProperty::Features:
+      dumpFeatures(os);
+      break;
+    case QuerySystemProperty::CoreCount:
+      dumpCoreCount(os);
+      break;
+    case QuerySystemProperty::SIMDBitWidth:
+      dumpSIMDBitWidth(os);
+      break;
+    case QuerySystemProperty::L1CacheSize:
+      dumpCacheSize(os, 1);
+      break;
+    case QuerySystemProperty::L2CacheSize:
+      dumpCacheSize(os, 2);
+      break;
+    case QuerySystemProperty::L3CacheSize:
+      dumpCacheSize(os, 3);
+      break;
+    case QuerySystemProperty::L4CacheSize:
+      dumpCacheSize(os, 4);
     }
-    break;
+    os << "\n";
   }
-  case QuerySystemProperty::CoreCount:
-    os << get_physical_cores();
-    break;
-  case QuerySystemProperty::SIMDBitWidth:
-    os << kPreferredSIMDBitWidth;
-    break;
-  case QuerySystemProperty::L1CacheSize: {
-    auto val = getHostCPUCacheSize(1);
-    if (val.isError()) {
-      os << "Error: " << val.getError();
-      return EXIT_SUCCESS;
-    }
-    os << *val;
-    break;
-  }
-  case QuerySystemProperty::L2CacheSize: {
-    auto val = getHostCPUCacheSize(2);
-    if (val.isError()) {
-      os << "Error: " << val.getError();
-      return EXIT_SUCCESS;
-    }
-    os << *val;
-    break;
-  }
-  case QuerySystemProperty::L3CacheSize: {
-    auto val = getHostCPUCacheSize(3);
-    if (val.isError()) {
-      os << "Error: " << val.getError();
-      return EXIT_SUCCESS;
-    }
-    os << *val;
-    break;
-  }
-  case QuerySystemProperty::L4CacheSize: {
-    auto val = getHostCPUCacheSize(4);
-    if (val.isError()) {
-      os << "Error: " << val.getError();
-      return EXIT_SUCCESS;
-    }
-    os << *val;
-    break;
-  }
-  }
-  os << "\n";
   return EXIT_SUCCESS;
 }
