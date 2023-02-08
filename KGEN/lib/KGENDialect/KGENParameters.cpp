dff327bf66d7b197b80284b958e3a3d031200232
Jeff Niu <jeff@modular.com>
2023-02-08 10:42:14 -0800

    [KGEN] Delete MLIROpAttr and LiftMLIROperations (#8410)
    These are not needed by the LIT frontend and don't really serve a
    purpose.
    
    Fixes [Internal link]
    Fixes [Internal link]
    
    MODULAR_ORIG_COMMIT_REV_ID: 77f780265da0a41e13beb8e99ec9b6dd65ffc9ef

--- KGEN/lib/KGENDialect/KGENParameters.cpp
+++ KGEN/lib/KGENDialect/KGENParameters.cpp
@@ -106,11 +106,6 @@ void ParameterCollector::collectUsesFromAttr(
   if (auto symbolConstant = dyn_cast<SymbolConstantAttr>(attr))
     verifySymbolConstantAttr(symbolConstant);
 
-  // Expression functions and MLIR operation expressions are isolated from
-  // above. They are verified to be self-contained on their own.
-  if (isa<MLIROpAttr>(attr))
-    return;
-
   // Save the number of nested parameters before recursing and check whether the
   // attribute has a nested constant expression.
   size_t oldSize = uses.size();
@@ -193,47 +188,6 @@ void ParameterCollector::collectUsesFromTypesImpl(
     parameterLessTypes.try_emplace(type, hasNestedConstExpr);
     hasConstExpr |= hasNestedConstExpr;
   }
-}
-
-//===----------------------------------------------------------------------===//
-// MLIROpAttr Verification
-//===----------------------------------------------------------------------===//
-
-template <typename ElementT>
-static LogicalResult
-checkParameterUsesIn(function_ref<InFlightDiagnostic()> emitError, ElementT el,
-                     DenseMap<StringAttr, Type> &paramsMap, StringRef name) {
-  ParameterCollector collector;
-  SmallVector<ParamDeclRefAttr> uses;
-  bool hasConstExpr;
-  if constexpr (std::is_same_v<ElementT, Type>)
-    collector.collectUsesFromType(el, uses, hasConstExpr);
-  else
-    collector.collectUsesFromAttr(el, uses, hasConstExpr);
-  for (ParamDeclRefAttr use : uses) {
-    Type &entry = paramsMap[use.getName()];
-    if (!entry)
-      return emitError() << use.getName() << " parameter not defined in "
-                         << name;
-    if (entry != use.getType())
-      return emitError() << "use of " << use.getName()
-                         << " with incorrect type in " << name;
-  }
-  return success();
-}
-
-LogicalResult
-MLIROpAttr::checkSelfContained(function_ref<InFlightDiagnostic()> emitError,
-                               SignatureType type) {
-  DenseMap<StringAttr, Type> paramsMap;
-  for (ParamDeclAttr decl : type.getInputParams())
-    paramsMap.try_emplace(decl.getName(), decl.getType());
-
-  for (Type type :
-       llvm::concat<const Type>(type.getValueInputs(), type.getValueResults()))
-    if (failed(checkParameterUsesIn(emitError, type, paramsMap, "signature")))
-      return failure();
-  return success();
 }
 
 //===----------------------------------------------------------------------===//
