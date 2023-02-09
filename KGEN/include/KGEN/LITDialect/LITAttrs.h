2c5271044f385d402945e6dc756e13cc1d257950
Jeff Niu <jeff@modular.com>
2023-02-08 18:19:36 -0800

    [KGEN] Implement DeclRefAttr verifier hook on `LIT::StructAttr` (#8445)
    Actually enforce that the fields match the declaration.
    
    Fixes [Internal link]
    
    MODULAR_ORIG_COMMIT_REV_ID: 36efd9ba62a8d49a4be13298d582b5431bdf5e66

--- KGEN/include/KGEN/LITDialect/LITAttrs.h
+++ KGEN/include/KGEN/LITDialect/LITAttrs.h
@@ -7,16 +7,16 @@
 #ifndef KGEN_LITDIALECT_LITATTRS_H
 #define KGEN_LITDIALECT_LITATTRS_H
 
+#include "KGEN/KGENDialect/KGENAttrInterfaces.h"
 #include "Support/LLVMCompilerForwardDecls.h"
 #include "mlir/IR/BuiltinAttributes.h"
 
 namespace M::KGEN {
 class DeclRefType;
-} // namespace M::KGEN
-
-namespace M::KGEN::LIT {
+namespace LIT {
 class StructFieldOp;
-}
+} // namespace LIT
+} // namespace M::KGEN
 
 #define GET_ATTRDEF_CLASSES
 #include "KGEN/LITDialect/LITAttrs.h.inc"
