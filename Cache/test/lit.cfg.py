6cdd4bbd78aaed9a55c056f2f3bec507066da252
Aman LaChapelle <aman.lachapelle@gmail.com>
2023-03-08 09:52:57 -0800

    [Cache] Add `cache-mgr` tool to manually upload and download objects from the cache. (#9944)
    This patch adds a tool that allows the user to manually put objects into, or take objects from, the CAS. One or more objects are specified on the command line, and based on context, the tool understands whether to store them or retrieve them.
    
    MODULAR_ORIG_COMMIT_REV_ID: b4be41ef98905edbf030fc065a56bb08a0f49493

--- Cache/test/lit.cfg.py
+++ Cache/test/lit.cfg.py
@@ -14,7 +14,7 @@ from lit.llvm import llvm_config
 config.name = "Cache"
 
 # suffixes: A list of file extensions to treat as test files.
-config.suffixes = [".mlir"]
+config.suffixes = [".mlir", ".test"]
 
 # test_source_root: The root path where tests are located.
 config.test_source_root = os.path.dirname(__file__)
@@ -22,12 +22,11 @@ config.test_source_root = os.path.dirname(__file__)
 # test_exec_root: The root path where tests should be run.
 config.test_exec_root = os.path.join(config.modular_obj_root, "Cache", "test")
 
-
 tool_dirs = [
     config.modular_tools_dir,
     config.mlir_tools_dir,
     config.llvm_tools_dir,
 ]
-tools = ["cache-opt"]
+tools = ["cache-opt", "cache-mgr"]
 
 llvm_config.add_tool_substitutions(tools, tool_dirs)
