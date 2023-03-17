79113c2690456145c2e167a7f57bcd4bc16b5f26
Arjun Surendran <arjun@modular.com>
2023-03-17 16:41:35 -0700

    [API][Cache] Introduce a Host Aware Key type. (#10643)
    Introduce a new key generator that attaches host machine information.
    This is useful in cases where we need to derive a new key from another
    key, but the output needs to be host specific.
    
    Use this to generate key for MEF. MEF is not stored in CAS yet.
    
    MODULAR_ORIG_COMMIT_REV_ID: cda86c53a82712c06d8222162f93f1b19e591548

--- Cache/include/Cache/Support/Keys.h
+++ Cache/include/Cache/Support/Keys.h
@@ -13,6 +13,7 @@
 #define CACHE_SUPPORT_KEYS_H
 
 #include "Cache/Buffer.h"
+#include "Support/Host.h"
 #include "llvm/ADT/StringRef.h"
 #include "llvm/Support/BLAKE3.h"
 #include "llvm/Support/Casting.h"
@@ -36,6 +37,7 @@ template <>
 struct TypeKey<llvm::ArrayRef<uint8_t>> {
   using KeyTy = llvm::ArrayRef<uint8_t>;
   static std::string hashKey(KeyTy key) {
+
     llvm::BLAKE3 hashState{};
     hashState.update(key);
 
@@ -80,6 +82,58 @@ private:
   }
 };
 
+struct HostInfoWrapper {
+  static std::string wrapKey(const std::string &keyToBeWrapped) {
+    std::string hostInfo;
+    llvm::raw_string_ostream os(hostInfo);
+    auto machineInfoOr = M::getHostMachineInfo();
+    if (machineInfoOr.isError())
+      return "";
+    HostMachineInfo machineInfo = machineInfoOr.takeValue();
+    machineInfo.print(os);
+    return keyToBeWrapped + hostInfo;
+  }
+};
+
+/// Wrap a given key generator with one or more wrappers. Wrappers need to
+/// implement a static function wrapKey which takes a string and returns a
+/// string back. Wrapping works like this.
+///        1. Generate key with key generator
+///        2. Call Wrappers::wrapKey(...) on generated key.
+///        4. Repeat for all wrappers with previous result.
+///        5. Once again hash the final accumulated string.
+/// Wrapping takes place in the order in which it is defined
+template <typename KeyGen, typename... Wrappers>
+class WrappedKey {
+  using KeyTy = typename KeyGen::KeyTy;
+
+public:
+  static std::string hashKey(KeyTy key) {
+    std::string hashedKey = KeyGen::hashKey(std::forward<KeyTy>(key));
+    // Apply all the wrappers.
+    ([&]() mutable {
+      hashedKey = Wrappers::wrapKey(hashedKey);
+      return true;
+    }() &&
+     ...);
+    llvm::BLAKE3 hashState{};
+    hashState.update(hashedKey);
+    auto hash = hashState.final();
+    return {hash.begin(), hash.end()};
+  }
+};
+
+/// A Key Generator that qualifies a given key with host machine info. This is
+/// useful in cases where there is one common parent cache object and this
+/// produces target specific cache artifacts. This key can be used to lookup
+/// and add objects to cache related to the parent but is specific to current
+/// host machine. Note that this key doesn't have semantic understanding. So
+/// if 2 machines have same cpu features but are ordered differently the hash
+/// key will be different. That being said, the machinery used underneath to
+/// generate host info sorts the features alphabetically to try to maintain
+/// consistency.
+template <typename TyKey>
+using KeyWithHostInfo = WrappedKey<TyKey, HostInfoWrapper>;
 /// Provide a key that doesn't do any hashing - we only want to read things from
 /// keys provided to this.
 using ReadOnlyKey = TypeKey<llvm::StringRef>;
