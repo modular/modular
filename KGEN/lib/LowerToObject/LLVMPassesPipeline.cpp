modular/modular

commit 37ee90e042933240454b8f9b7eebe59fb3103444

[KGEN] Guard LoopVectorizer in the LLVM pass pipeline with an envvar. (#10357)
Removing it causes a performance degradation on gemm.naive, which needs
to be resolved first.

To reproduce the regression:
```console
 # Benchmark with loop-vectorizer disabled
rm -rf .derived/.kgen_cache/transform
touch Kernels/lib/LitKernels/MatmulKernels.lit
MODULAR_ENABLE_LLVM_LOOPVEC=OFF build
time kernels-benchmark --filter=Lit.gemm.naive --shapes=Faux/utils/shapes/pow2_ranges.yaml  --warmup-iterations=2

 # Benchmark with loop-vectorizer enabled
rm -rf .derived/.kgen_cache/transform
touch Kernels/lib/LitKernels/MatmulKernels.lit
MODULAR_ENABLE_LLVM_LOOPVEC=ON build
time kernels-benchmark --filter=Lit.gemm.naive --shapes=Faux/utils/shapes/pow2_ranges.yaml  --warmup-iterations=2
```

The regression occurs on a single case, when matrix B is transposed:
```
Kernel Name    Args                                                           LHS      RHS    % diff
-------------  ---------------------------------------------------------  -------  -------  --------
gemm.naive     A=512x128;B=1024x128;C=512x1024;transposeA=0;transposeB=1    2.874    1.795     -37.5
```

Seemingly, to recover performance we need to vectorize the inner loop in
`naive_matmul`:
```lit
  for k in range(gemm_shape.K):
      let a_val = matrix_a[m, k].cast[accum_type]()
      let b_val = matrix_b[k, n].cast[accum_type]()
      c_val += a_val * b_val
```

MODULAR_ORIG_COMMIT_REV_ID: 9536a2fe1eed34fa0a7da6d4b5cf1bd5c111fdf9

KGEN/lib/LowerToObject/LLVMPassesPipeline.cpp
@@ -277,8 +277,12 @@ static ModuleInlinerWrapperPass buildInlinerPipeline() {
 }
 
 static void addVectorPasses(FunctionPassManager &FPM) {
-  FPM.addPass(LoopVectorizePass(LoopVectorizeOptions(
-      /*InterleaveOnlyWhenForced*/ false, /*VectorizeOnlyWhenForced*/ false)));
+  if (llvm::sys::Process::GetEnv("MODULAR_ENABLE_LLVM_LOOPVEC")
+          .value_or("ON") == "ON") {
+    FPM.addPass(LoopVectorizePass(LoopVectorizeOptions(
+        /*InterleaveOnlyWhenForced*/ false,
+        /*VectorizeOnlyWhenForced*/ false)));
+  }
 
   // Eliminate loads by forwarding stores from the previous iteration to loads
   // of the current iteration.

commit 6f72234e42f5296dfaa5e053a3fda69a51ca1be1

[KGEN] Delete loop vectorizer from LLVM pipeline. (#10733)
Per discussion with @abduld, we are ok with `gemm.naive` regression from
the vectorizer removal (more data on the regression is in #10357).

MODULAR_ORIG_COMMIT_REV_ID: cfb1582f170f8d7947cf76bb92624a84e3e38ab7

KGEN/lib/LowerToObject/LLVMPassesPipeline.cpp
@@ -75,7 +75,6 @@
 #include "llvm/Transforms/Utils/Mem2Reg.h"
 #include "llvm/Transforms/Utils/RelLookupTableConverter.h"
 #include "llvm/Transforms/Utils/SimplifyCFGOptions.h"
-#include "llvm/Transforms/Vectorize/LoopVectorize.h"
 #include "llvm/Transforms/Vectorize/SLPVectorizer.h"
 #include "llvm/Transforms/Vectorize/VectorCombine.h"
 
@@ -277,13 +276,6 @@ static ModuleInlinerWrapperPass buildInlinerPipeline() {
 }
 
 static void addVectorPasses(FunctionPassManager &FPM) {
-  if (llvm::sys::Process::GetEnv("MODULAR_ENABLE_LLVM_LOOPVEC")
-          .value_or("ON") == "ON") {
-    FPM.addPass(LoopVectorizePass(LoopVectorizeOptions(
-        /*InterleaveOnlyWhenForced*/ false,
-        /*VectorizeOnlyWhenForced*/ false)));
-  }
-
   // Eliminate loads by forwarding stores from the previous iteration to loads
   // of the current iteration.
   FPM.addPass(LoopLoadEliminationPass());