a59e8c25bcbbf99cd5cd9edde601a6e40fb05abf
Jeff Niu <jeff@modular.com>
2023-03-28 21:16:19 +0200

    [KGEN] Thread-local parameter collector cache, and consolidation (#11268)
    This PR gives each thread in `VerifyParameters.cpp` a copy of the
    parameter collector cache, rather than each work item. This reduces time
    spent copying the cache to once per thread instead of once per function.
    In addition, actually consolidate the thread-local caches at the end of
    the parallel run, because in reality the original cache has always ended
    up being empty. However, this is only profitable to do when the cache is
    completely empty: it does not grow much in subsequent runs.
    
    MODULAR_ORIG_COMMIT_REV_ID: cbd3825c93226dbdf5810047156b2b017c164ed7

--- KGEN/lib/Transforms/VerifyParameters.cpp
+++ KGEN/lib/Transforms/VerifyParameters.cpp
@@ -10,6 +10,7 @@
 #include "mlir/IR/Threading.h"
 #include "mlir/IR/Verifier.h"
 #include "mlir/Pass/Pass.h"
+#include "llvm/Support/Mutex.h"
 
 using namespace M;
 using namespace KGEN;
@@ -28,18 +29,49 @@ struct VerifyParametersPass : impl::VerifyParametersBase<VerifyParametersPass> {
     mlir::LockedSymbolTableCollection sharedSymtabs(analysis.getSymbolTables());
     auto &paramCache = getAnalysis<ParameterCollector::Analysis>();
 
+    // Give each thread a copy of the parameter cache, rather than each work
+    // item.
+    DenseMap<uint64_t, ParameterCollector::Analysis> threadCaches;
+    threadCaches.reserve(getContext().getThreadPool().getThreadCount());
+    llvm::sys::SmartRWMutex<true> mutex;
+
     std::vector<Region *> declRegions;
     for (auto decl : getOperation().getOps<DeclInterface>())
       for (Region &region : decl->getRegions())
         declRegions.push_back(&region);
-    auto workFunc = [&sharedSymtabs, &paramCache](Region *declRegion) {
+    auto workFunc = [&sharedSymtabs, &paramCache, &mutex,
+                     &threadCaches](Region *declRegion) {
+      // Get the thread-local cache.
+      ParameterCollector::Analysis *cache = nullptr;
+      uint64_t threadId = llvm::get_threadid();
+      {
+        llvm::sys::SmartScopedReader<true> lock(mutex);
+        auto it = threadCaches.find(threadId);
+        if (it != threadCaches.end())
+          cache = &it->second;
+      }
+      if (!cache) {
+        llvm::sys::SmartScopedWriter<true> lock(mutex);
+        // Each thread gets a copy of the saved cache.
+        cache = &threadCaches.try_emplace(threadId, paramCache).first->second;
+      }
+
       ParameterUseDefGraph graph(*declRegion);
-      ParameterCollector::Analysis cache = paramCache;
-      return graph.verify(sharedSymtabs, cache);
+      return graph.verify(sharedSymtabs, *cache);
     };
     if (failed(mlir::failableParallelForEach(&getContext(), declRegions,
                                              workFunc)))
       return signalPassFailure();
+
+    // Consolidate the caches, but only when the original cache is empty. In
+    // reality, the cache does not grow much after the first run of this pass on
+    // an input IR, so consolidation is only worthwhile on the first run of the
+    // pass, when the cache is empty.
+    if (paramCache.parameterLess.empty()) {
+      for (auto &[_, threadCache] : threadCaches)
+        paramCache.parameterLess.insert(threadCache.parameterLess.begin(),
+                                        threadCache.parameterLess.end());
+    }
 
     // This pass does not modify any IR, so mark all analyses as preserved. In
     // addition, this signals the pass manager that the MLIR verifier need not
