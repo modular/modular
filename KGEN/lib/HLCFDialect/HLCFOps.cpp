94e385b5db356f73a5841efe46b19d93d1702217
Mikhail Zolotukhin <mvz@modular.com>
2023-03-28 16:47:24 -0700

    [KGEN] Add a constant condition simplifier for `HLCF::IfOp`. (#11213)
    More simplifications will follow, but this one already improves
    compilation time on Matmul kernels by ~10% (12.2s -> 11.2s).
    
    MODULAR_ORIG_COMMIT_REV_ID: 7442f45f8c30eb6016139112d16d45702c6ea634

--- KGEN/lib/HLCFDialect/HLCFOps.cpp
+++ KGEN/lib/HLCFDialect/HLCFOps.cpp
@@ -5,10 +5,13 @@
 //===----------------------------------------------------------------------===//
 
 #include "KGEN/HLCFDialect/HLCFOps.h"
+#include "KGEN/KGENDialect/KGENOps.h"
 #include "mlir/IR/Builders.h"
 #include "mlir/IR/BuiltinTypes.h"
 #include "mlir/IR/FunctionInterfaces.h"
+#include "mlir/IR/Matchers.h"
 #include "mlir/IR/OpImplementation.h"
+#include "mlir/IR/PatternMatch.h"
 
 using namespace M;
 using namespace HLCF;
@@ -132,6 +135,73 @@ OpBuilder IfOp::getThenBodyBuilder() {
 OpBuilder IfOp::getElseBodyBuilder() {
   assert(!getElseRegion().empty() && "Need an else block");
   return OpBuilder::atBlockEnd(&getElseRegion().front());
+}
+
+/// Erase all operations following the given OP in its parent region. The OP
+/// itself does not get deleted.
+static void eraseOpsAfter(PatternRewriter &rewriter, Operation *op) {
+  auto range =
+      llvm::make_range(op->getNextNode()->getIterator(),
+                       op->getParentRegion()->front().getOperations().end());
+
+  // We have to delete ops in reverse order to avoid dealing with value uses.
+  SmallVector<Operation *> worklist;
+  for (Operation &op : range)
+    worklist.push_back(&op);
+
+  while (!worklist.empty())
+    rewriter.eraseOp(worklist.pop_back_val());
+}
+
+/// Replace the given op with a region. If the region ends with YieldOp then
+/// uses of the results of the original op will be replaced with the
+/// corresponding yielded values. Otherwise, the region must be ending with a
+/// Return or a similar terminator - in that case we erase all the ops after the
+/// original op as dead code.
+static void replaceOpWithRegion(PatternRewriter &rewriter, Operation *op,
+                                Region &region, ValueRange blockArgs = {}) {
+  assert(llvm::hasSingleElement(region) && "expected single-block region");
+  Block *block = &region.front();
+  Operation *terminator = block->getTerminator();
+  rewriter.inlineBlockBefore(block, op, blockArgs);
+  if (isa<YieldOp>(terminator)) {
+    // If the op block ends with yield, we rewire the values in the remaining of
+    // the parent block to use the yielded values.
+    rewriter.replaceOp(op, terminator->getOperands());
+    rewriter.eraseOp(terminator);
+  } else {
+    // Delete all ops after the op - the block in the op ends with a terminator
+    // that renders the remaining of the parent block dead.
+    eraseOpsAfter(rewriter, op);
+    rewriter.eraseOp(op);
+  }
+}
+
+namespace {
+/// If the IfOp condition is known at compile time, replace the IfOp with the
+/// contents of the corresponding branch. If the block we're inserting doesn't
+/// end with YieldOp, operations following the original IfOp will be discarded.
+struct RemoveStaticCondition : public OpRewritePattern<IfOp> {
+  using OpRewritePattern<IfOp>::OpRewritePattern;
+
+  LogicalResult matchAndRewrite(IfOp op,
+                                PatternRewriter &rewriter) const override {
+    BoolAttr condition;
+    if (!matchPattern(op.getCond(), m_Constant(&condition)))
+      return failure();
+
+    Region &active =
+        condition.getValue() ? op.getThenRegion() : op.getElseRegion();
+    replaceOpWithRegion(rewriter, op, active);
+
+    return success();
+  }
+};
+} // namespace
+
+void IfOp::getCanonicalizationPatterns(RewritePatternSet &results,
+                                       MLIRContext *context) {
+  results.add<RemoveStaticCondition>(context);
 }
 
 //===----------------------------------------------------------------------===//
--- KGEN/test/hlcf-ir/hlcf-canonicalize.mlir
+++ KGEN/test/hlcf-ir/hlcf-canonicalize.mlir
@@ -38,3 +38,34 @@ func.func @terminators_conditionally_pure(%arg0: i1) {
   }
   return
 }
+
+// CHECK-LABEL: @fold_if_return
+kgen.func @fold_if_return(%arg0 : index, %arg1: index, %arg2: index) -> index {
+  // CHECK-NOT: hlcf.if
+  // CHECK-NEXT: kgen.return %arg0
+  // CHECK-NOT: kgen.return
+  %cond = kgen.param.constant: i1 = <1>
+  hlcf.if %cond {
+    kgen.return %arg0: index
+  } else {
+    kgen.return %arg1: index
+  }
+  kgen.return %arg2: index
+}
+
+// CHECK-LABEL: @fold_if_yield
+kgen.func @fold_if_yield(%arg0 : index, %arg1: index) -> index {
+  // CHECK-NOT: hlcf.if
+  // CHECK-NEXT: %[[TEN:.*]] = index.constant 10
+  // CHECK-NEXT: %[[RES:.*]] = index.add %arg1, %[[TEN]]
+  // CHECK-NEXT: kgen.return %[[RES]]
+  %cond = kgen.param.constant: i1 = <0>
+  %z = hlcf.if %cond -> index {
+    hlcf.yield %arg0: index
+  } else {
+    hlcf.yield %arg1: index
+  }
+  %ten = index.constant 10
+  %r = index.add %z, %ten
+  kgen.return %r: index
+}
