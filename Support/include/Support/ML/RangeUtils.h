e0909a29de106d36c506d89e0b33d7c676d97f59
Laszlo Kindrat <laszlo@modular.ai>
2023-03-29 15:13:52 -0400

    [Support][MO] Fix runtime shape function for `mo.range` (#11364)
    This was using floordiv instead of ceildiv, leading to incorrect shapes in some cases. The fix involves refactoring an existing helper from the `mo.range` implementation into `Support`.
    
    MODULAR_ORIG_COMMIT_REV_ID: 15fb137300f5ebaa8678d3bf55135d3f4ba2016d

---
+++ Support/include/Support/ML/RangeUtils.h
@@ -0,0 +1,25 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+//
+// Utilities for working with integer ranges, both at compile time and runtime.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef SUPPORT_ML_RANGEUTILS_H
+#define SUPPORT_ML_RANGEUTILS_H
+
+#include "Support/ErrorOr.h"
+
+namespace M {
+
+/// Return the number of results given the start/limit/step values. Returns an
+/// error if the given values are invalid.
+ErrorOr<int64_t> getRangeNumElements(int64_t start, int64_t limit,
+                                     int64_t step);
+
+} // namespace M
+
+#endif // SUPPORT_ML_RANGEUTILS_H
---
+++ Support/lib/ML/RangeUtils.cpp
@@ -0,0 +1,27 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#include "Support/ML/RangeUtils.h"
+#include "llvm/ADT/Twine.h"
+#include "llvm/Support/MathExtras.h"
+
+using namespace M;
+
+ErrorOr<int64_t> M::getRangeNumElements(int64_t start, int64_t end,
+                                        int64_t step) {
+  if (step == 0)
+    return Error("step must not be zero");
+
+  bool stepSign = step > 0;
+  int64_t intervalLen = (stepSign ? 1 : -1) * (end - start);
+  if (intervalLen < 0) {
+    return Error(Twine("limit must be ") + (stepSign ? "greater" : "less") +
+                 " than or equal to start when step is " +
+                 (stepSign ? "positive" : "negative"));
+  }
+
+  return llvm::divideCeil(intervalLen, std::abs(step));
+}
