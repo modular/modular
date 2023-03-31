302c5d37e0093d2747c886d076cc5d49ae25bcf3
Mikhail Zolotukhin <mvz@modular.com>
2023-03-30 20:04:03 -0700

    [KGEN] Move helper functions to HLCFUtils, NFC. (#11557)
    MODULAR_ORIG_COMMIT_REV_ID: bb594ad57231e85d54ddd4ff1b243a76074b8165

---
+++ KGEN/include/KGEN/HLCFDialect/HLCFUtils.h
@@ -0,0 +1,24 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef KGEN_HLCFDIALECT_HLCFUTILS_H
+#define KGEN_HLCFDIALECT_HLCFUTILS_H
+
+#include "KGEN/HLCFDialect/HLCFOps.h"
+
+namespace M::HLCF {
+/// Return true if the operation is a loop and has a matching label.
+bool isMatchingLoop(Operation *op, StringAttr label);
+
+/// Return the nearest enclosing matching loop. This runs on valid IR, so it
+/// must find a matching loop.
+LoopOp getParentLoop(Operation *op, StringAttr label);
+
+/// Check if the child loop is nested in the parentToCheck loop.
+bool isParentLoop(LoopOp child, LoopOp parentToCheck);
+} // namespace M::HLCF
+
+#endif // KGEN_HLCFDIALECT_HLCFUTILS_H
