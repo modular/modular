c8d3e242c5c74934bb188c97149a8c6389f43fee
Jeff Niu <jeff@modular.com>
2023-04-01 16:29:34 -0700

    [KGEN] Inline leaf-to-root in parametric inliner (#11616)
    This partially addresses
    [Internal link]
    
    This PR introduces an InliningGraph datastructure to Inlining.cpp that
    computes a leaf-to-root order for inlining. This will temporarily
    regress compile times because it is not parallel. That will come in the
    next PR.
    
    This PR also fixes yet another bug with the mangling scheme exposed by
    the reverse order of the inlining (the inline-ees tend to be much
    bigger), by reworking the name uniquing scheme entirely to use a max
    subscript.
    
    MODULAR_ORIG_COMMIT_REV_ID: 909fdea9a9f6da32d5aea393fa4390e4d09487a2

--- KGEN/include/KGEN/Inlining.h
+++ KGEN/include/KGEN/Inlining.h
@@ -7,23 +7,23 @@
 #ifndef KGEN_INLINING_H
 #define KGEN_INLINING_H
 
-#include "KGEN/KGENDialect/KGENOps.h"
 #include "KGEN/KGENDialect/KGENParameters.h"
-#include "Support/ADT/SmartVariant.h"
 #include "Support/LLVMCompilerForwardDecls.h"
 
 namespace M::KGEN {
+class CallOp;
+class GeneratorOp;
+
 /// Given a call (to a GeneratorOp) and a ParameterUseDefGraph that describes
 /// the parameter graph of the call's parent DeclInterface, inline the callee
 /// into the call's context. This will mangle input parameters as necessary to
 /// ensure that there are no conflicts.
-LogicalResult inlineGeneratorCall(
-    KGENCallOpInterface topCall, ParameterUseDefGraph &topLevelGraph,
-    ParameterCollector::Analysis &paramCache,
-    function_ref<ParameterUseDefGraph &(ParameterCollector::Analysis &,
-                                        GeneratorOp)>
-        getGraph,
-    function_ref<GeneratorOp(KGENCallOpInterface)> lookupCallee);
+void inlineGeneratorCall(CallOp call, GeneratorOp callee,
+                         ParameterUseDefGraph &topLevelGraph,
+                         const ParameterUseDefGraph &calleeParams,
+                         const llvm::SetVector<StringAttr> &calleeDecls,
+                         DenseSet<const void *> &manglerCache);
+
 } // namespace M::KGEN
 
 #endif // KGEN_INLINING_H
