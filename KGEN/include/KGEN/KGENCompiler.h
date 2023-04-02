0faf1499f61624612d4c40df5af45274a0500691
Jeff Niu <jeff@modular.com>
2023-04-01 17:48:14 -0700

    [KGEN] Re-parallelize the (parametric) inliner (#11630)
    With this change, we achieve recover most of the performance of the
    previous version of the parametric inliner. The new version is still
    slightly slower (~10 ms on Elementwise.lit), due to a combination of
    higher multithreading overhead (not trivial parallelFor anymore), and
    more expensive ParameterUseDefGraph computations on larger functions. To
    elaborate, previously, when inlining root to leaf, the individual
    functions being inlined were smaller. Now in the reverse direction, each
    inlined function is much larger, and this slows down
    ParameterUseDefGraph due to nested scopes.
    
    This doesn't matter as much since in a subsequent PR, this pass will be
    changed to only inline `always_inline("nodebug")` functions.
    
    MODULAR_ORIG_COMMIT_REV_ID: 9c935f88a403e2906c0251ee9ae6a1b912730f77

--- KGEN/include/KGEN/KGENCompiler.h
+++ KGEN/include/KGEN/KGENCompiler.h
@@ -23,7 +23,8 @@ namespace M::KGEN {
 /// This populates the pre-elaboration phase passes of the KGEN compiler. The
 /// distribution format of a KGEN library is essentially what comes just before
 /// elaboration because the parameter system allows significant extension.
-void populateGenerateLibraryFilePasses(mlir::PassManager &pm);
+void populateGenerateLibraryFilePasses(mlir::PassManager &pm,
+                                       LLCL::Runtime &runtime);
 
 /// This populates the passes to produce a fully concrete KGEN module. That
 /// means it runs pre-elaboration, elaboration, and then the post-elaboration
