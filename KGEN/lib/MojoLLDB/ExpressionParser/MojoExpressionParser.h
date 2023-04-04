6c8b270eefd48bc595537e310ab0415bc333772c
River Riddle <riddleriver@gmail.com>
2023-04-03 23:45:23 -0700

    [Mojo][LLDB] Add initial support for expression evaluation (#11485)
    This adds the minimum amount needed to execute Mojo expressions.
    This doesn't yet provide support for persistent state, printing
    the output of the expression, etc. It purely defines the structure for
    executing code in general (e.g. you can call print!).
    
    This effectively provides the minimum necessary for the REPL to be
    functional, and thus enough to start building a jupyter kernel.
    
    MODULAR_ORIG_COMMIT_REV_ID: b7977b61c5faa0f1d5755f5590d52d69289548c2

---
+++ KGEN/lib/MojoLLDB/ExpressionParser/MojoExpressionParser.h
@@ -0,0 +1,47 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef KGEN_LIB_MOJOLLDB_EXPRESSIONPARSER_MOJOEXPRESSIONPARSER_H
+#define KGEN_LIB_MOJOLLDB_EXPRESSIONPARSER_MOJOEXPRESSIONPARSER_H
+
+#include "Support/LLVMCompilerForwardDecls.h"
+#include "lldb/Expression/ExpressionParser.h"
+
+namespace M::KGEN::Mojo {
+class MojoExpressionParser : public lldb_private::ExpressionParser {
+public:
+  MojoExpressionParser(lldb_private::ExecutionContextScope *exeScope,
+                       lldb_private::Expression &expr,
+                       const lldb_private::EvaluateExpressionOptions &options);
+  ~MojoExpressionParser() override;
+
+  /// Attempt to find possible command line completions for the given
+  /// expression.
+  bool Complete(lldb_private::CompletionRequest &request, unsigned line,
+                unsigned pos, unsigned typedPos) override {
+    return false;
+  }
+
+  /// Parse a single expression and convert it to IR.
+  LogicalResult parse();
+
+  /// Ready an already-parsed expression for execution, possibly evaluating it
+  /// statically.
+  lldb_private::Status
+  PrepareForExecution(lldb::addr_t &funcAddr, lldb::addr_t &funcEnd,
+                      lldb::IRExecutionUnitSP &executionUnit,
+                      lldb_private::ExecutionContext &exeCtx,
+                      bool &canInterpret,
+                      lldb_private::ExecutionPolicy executionPolicy) override;
+
+private:
+  struct Impl;
+
+  std::unique_ptr<Impl> impl;
+};
+} // namespace M::KGEN::Mojo
+
+#endif // KGEN_LIB_MOJOLLDB_EXPRESSIONPARSER_MOJOEXPRESSIONPARSER_H
