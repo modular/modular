1538c3b79b4dc3e2ebfc4fcf4a77516c2382106b
akirchhoff-modular <akirchhoff@modular.com>
2023-04-05 11:38:17 -0700

    [Support] Make cache size detection aware of cache types (#11849)
    There was an assumption on Linux that indices corresponded to levels,
    but Mark Shields discovered that's not necessarily true -- sometimes
    cache types differ, which can cause multiple consecutive indices to have
    the same level. Windows had a similar bug, also assuming there is one
    type of cache. This PR makes getHostCPUCacheSize consistently return the
    size of the data or unified cache and always ignore instruction cache.
    
    MODULAR_ORIG_COMMIT_REV_ID: b1c68509eeb165c42eda21bb90649d50ce476435

--- Support/include/Support/Host.h
+++ Support/include/Support/Host.h
@@ -109,6 +109,9 @@ ErrorOrSuccess runWithThreadAffinity(size_t cpuID,
 // Cache sizes
 //===----------------------------------------------------------------------===//
 
+// Get the D$ or unified cache size in bytes at a 1-based cache level index.
+// An error is returned if there is an OS error in finding the cache level.  If
+// the cache level does not exist, 0 is returned.
 ErrorOr<size_t> getHostCPUCacheSize(size_t cacheLevel);
 
 //===----------------------------------------------------------------------===//
@@ -139,6 +142,9 @@ struct HostMachineInfo {
   size_t numPhysicalCores;
   size_t simdBitWidth;
   size_t preferredMemoryAlignment;
+  // These represent either data or unified cache size -- they do not include
+  // instruction-only caches, but may include cache size that are shared
+  // between instruction and data.
   size_t l1CacheSize;
   size_t l2CacheSize;
   size_t l3CacheSize;
