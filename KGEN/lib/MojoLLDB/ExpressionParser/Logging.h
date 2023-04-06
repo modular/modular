5e1064a3a943f729281289b36d2ca6f97d2a8449
Aman LaChapelle <aman.lachapelle@gmail.com>
2023-04-06 16:23:23 -0700

    [mojo-lldb] Clean up REPL logging inside the ExpressionParser. (#12040)
    This patch cleans up the logging by:
    - Default enable logging to a logfile
      - Defaults to .derived/.repl/debug.log, but can be set with a CMake flag `-DMOJO_REPL_DEBUG_LOG_FILE=/path/to/file`
    - Adds some more logging, including significant intermediate IR steps (ones that are useful for debugging problems)
    
    MODULAR_ORIG_COMMIT_REV_ID: 3f66e06d44db671c41908bb4574f4b43f58f1d94

---
+++ KGEN/lib/MojoLLDB/ExpressionParser/Logging.h
@@ -0,0 +1,16 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef KGEN_LIB_MOJOLLDB_EXPRESSIONPARSER_H
+#define KGEN_LIB_MOJOLLDB_EXPRESSIONPARSER_H
+
+#define MOJO_EXPR_LOG(...)                                                     \
+  do {                                                                         \
+    Log *__logPtr = GetLog(LLDBLog::Expressions);                              \
+    LLDB_LOG(__logPtr, "[mojo] " __VA_ARGS__);                                  \
+  } while (0)
+
+#endif // KGEN_LIB_MOJOLLDB_EXPRESSIONPARSER_H
