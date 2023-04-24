fe4c274698701f77a8b42c306ba6282e7164f519
Walter Erquinigo <walter@modular.com>
2023-04-24 13:38:21 -0500

    [mojo-lldb] Support tests with asan (#13219)
    This was easier than expected. I created a global llvm-lit config that
    holds the env var needed to inject the asan runtime in lldb. A similar
    logic is being used locally in a couple of places in the repo, so I
    think it's time to have a global location for this.
    
    However, just setting the env var doesn't work for jupyter cli tests.
    I'm tired of dealing with them so I'm going to swith those tests to use
    our own executor.
    
    Closes [Internal link]
    MODULAR_ORIG_COMMIT_REV_ID: d843c99f06f8eb50b4b83cf98b32a1d0fd0cdb2c

--- KGEN/test/lit.cfg.py
+++ KGEN/test/lit.cfg.py
@@ -23,11 +23,9 @@ config.test_source_root = os.path.dirname(__file__)
 # test_exec_root: The root path where tests should be run.
 config.test_exec_root = os.path.join(config.modular_obj_root, "KGEN", "test")
 
+
 config.substitutions.append(
-    (
-        "%lldb",
-        which("lldb"),
-    ),
+    ("%lldb", config.asan_lib_inject_env + " " + which("lldb")),
 )
 
 tool_dirs = [
