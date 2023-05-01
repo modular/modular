02e0c9764a11417284c91c8abc4a6519fd44cced
Jeff Niu <jeff@modular.com>
2023-05-01 16:46:17 -0700

    [KGEN] Merge LowerAsyncFunctions.cpp and RuntimeClosures.cpp (NFC) (#13861)
    I will be merging these two passes together, since they do the same-ish
    thing.
    
    MODULAR_ORIG_COMMIT_REV_ID: e01a011eb127154663f5e18ac1b72348f8f4e47f

--- KGEN/lib/LowerLIT/LowerClosures.cpp
+++ KGEN/lib/LowerLIT/LowerClosures.cpp
@@ -11,7 +11,9 @@
 #include "KGEN/POPDialect/POPTypes.h"
 #include "Support/Compiler/OperationUtils.h"
 #include "mlir/Analysis/SymbolTableAnalysis.h"
+#include "mlir/IR/IRMapping.h"
 #include "mlir/IR/ImplicitLocOpBuilder.h"
+#include "mlir/IR/PatternMatch.h"
 #include "mlir/IR/Threading.h"
 #include "mlir/Pass/Pass.h"
 #include "mlir/Transforms/RegionUtils.h"
@@ -177,6 +179,8 @@ static LogicalResult lowerAsyncFunction(FuncOp func,
 namespace M::KGEN {
 #define GEN_PASS_DEF_LOWERASYNCFUNCTIONS
 #include "KGEN/KGENPasses.h.inc"
+#define GEN_PASS_DEF_RUNTIMECLOSURES
+#include "KGEN/KGENPasses.h.inc"
 } // namespace M::KGEN
 
 namespace {
@@ -199,3 +203,92 @@ struct LowerAsyncFunctionsPass
   }
 };
 } // namespace
+
+namespace {
+struct RuntimeClosuresPass : impl::RuntimeClosuresBase<RuntimeClosuresPass> {
+  void runOnOperation() override;
+};
+} // namespace
+
+static SymbolConstantAttr callSymbolOfLiftedRegion(StageClosureOp opWithRegion,
+                                                   SmallVector<Value> &captures,
+                                                   SymbolTable &symtab) {
+  assert(opWithRegion->getNumRegions() == 1);
+  OpBuilder builder(opWithRegion->getParentOfType<ModuleOp>());
+
+  llvm::SetVector<Value> captureSet;
+  Region &sourceRegion = opWithRegion->getRegion(0);
+  operationIsIsolatedFromAbove(opWithRegion, &captureSet);
+  for (Value capture : captureSet) {
+    Operation *capturingOp = capture.getDefiningOp();
+    // Clone ConstantLike operations into the region.
+    if (capturingOp && capturingOp->hasTrait<OpTrait::ConstantLike>()) {
+      ImplicitLocOpBuilder b(capturingOp->getLoc(),
+                             OpBuilder::atBlockBegin(&sourceRegion.front()));
+      Operation *cloned = b.clone(*capturingOp);
+      for (auto [orig, replacement] :
+           llvm::zip(capturingOp->getResults(), cloned->getResults()))
+        replaceAllUsesInRegionWith(orig, replacement, sourceRegion);
+    } else {
+      // Otherwise these are captured variables and we need to pass them as
+      // arguments to the block body.
+      captures.emplace_back(capture);
+    }
+  }
+
+  SignatureType signatureType = opWithRegion.getType();
+  // Lift the body by making source region isolated from above
+  // add captures in reverse so they appear the same order in
+  // the parameter list as they do in the capture order
+  for (int i = captures.size() - 1; i >= 0; --i) {
+    Value from = captures[i];
+    BlockArgument newArg =
+        sourceRegion.insertArgument((unsigned)0, from.getType(), from.getLoc());
+    replaceAllUsesInRegionWith(from, newArg, sourceRegion);
+  }
+  auto liftedValueSignature =
+      FunctionType::get(builder.getContext(), sourceRegion.getArgumentTypes(),
+                        signatureType.getValueResults());
+
+  auto noTypes = TypeArrayAttr::get(signatureType.getContext(), {});
+  auto liftedSignature = SignatureType::get(
+      noTypes, noTypes, liftedValueSignature,
+      MetadataAttr::get(signatureType.getContext(),
+                        sourceRegion.getNumArguments(), FnEffects::Capturing));
+  builder.setInsertionPoint(opWithRegion->getParentOfType<FuncOp>());
+
+  std::string name = "stage_closure";
+  if (opWithRegion->hasAttr("name")) {
+    auto nameMaybe =
+        dyn_cast_or_null<StringAttr>(opWithRegion->getAttr("name"));
+    if (nameMaybe)
+      name = nameMaybe.str();
+  }
+
+  auto lifted = builder.create<FuncOp>(
+      opWithRegion->getLoc(), StringAttr::get(builder.getContext(), name),
+      liftedSignature, AlwaysInlineLevel::Disabled);
+  symtab.insert(lifted);
+  auto liftedSymbol = SymbolConstantAttr::get(
+      SymbolRefAttr::get(lifted.getSymNameAttr()), liftedSignature);
+  IRMapping mapper;
+  sourceRegion.cloneInto(&lifted.getBodyRegion(), mapper);
+  return liftedSymbol;
+}
+
+void RuntimeClosuresPass::runOnOperation() {
+  ModuleOp theModule = getOperation();
+  SymbolTable &symtab =
+      getAnalysis<mlir::SymbolTableAnalysis>().getTopLevelSymbolTable();
+
+  // Lift nested functions
+  mlir::IRRewriter rewriter{OpBuilder(theModule)};
+  theModule->walk([&](StageClosureOp stageClosure) {
+    SmallVector<Value> captures;
+    SymbolConstantAttr symbol =
+        callSymbolOfLiftedRegion(stageClosure, captures, symtab);
+    rewriter.setInsertionPoint(stageClosure);
+    rewriter.replaceOpWithNewOp<CreateClosureOp>(
+        stageClosure, stageClosure.getType(), symbol, captures);
+  });
+}
