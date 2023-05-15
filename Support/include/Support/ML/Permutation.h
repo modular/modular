98e90c11c08d896a8fe7cd767b640464d9c62cb0
Xiangxi Guo (Ryan) <ryanguo@modular.com>
2023-05-15 14:19:49 -0700

    [MO] Move permutation-related helper to a `Support/ML/Permutation.h` file (#14348)
    This unifies the permutation helpers used in Graph Compiler and ONNX
    lowering, and enables more potential reuse in other frontends.
    
    Closes #14330.
    MODULAR_ORIG_COMMIT_REV_ID: 2ce94ce63109a0ca8ca5b26b69c4b905a4b0654a

---
+++ Support/include/Support/ML/Permutation.h
@@ -0,0 +1,35 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+//
+// Utilities for working with permutations.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef SUPPORT_ML_PERMUTATION_H
+#define SUPPORT_ML_PERMUTATION_H
+
+#include "Support/LLVMCompilerForwardDecls.h"
+#include "llvm/ADT/ArrayRef.h"
+
+namespace M {
+
+/// Returns a vector with the contents of the data vector rearranged according
+/// to the permutation vector and stride.
+template <typename T>
+static SmallVector<T> permute(ArrayRef<T> data, ArrayRef<int64_t> permutation,
+                              int64_t stride = 1) {
+  SmallVector<T> output;
+
+  for (int64_t permIdx : permutation)
+    for (int64_t j = 0; j < stride; ++j)
+      output.emplace_back(data[permIdx * stride + j]);
+
+  return output;
+}
+
+} // namespace M
+
+#endif // SUPPORT_ML_PERMUTATION_H
