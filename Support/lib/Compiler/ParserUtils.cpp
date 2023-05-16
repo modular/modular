90c29edcca6edeb2797ab09732bb83e9fdda6aec
Xiangxi Guo (Ryan) <ryanguo@modular.com>
2023-05-16 11:21:47 -0700

    [Support] Extend `parseParenOperandList` to allow optional default type (#14445)
    This refactors similar logic introduced in a previous patch.
    
    MODULAR_ORIG_COMMIT_REV_ID: 081f3e728b4c060cf6bde29ce33498ca7b40c2ea

--- Support/lib/Compiler/ParserUtils.cpp
+++ Support/lib/Compiler/ParserUtils.cpp
@@ -12,11 +12,29 @@ using namespace M;
 /// `(%a: i32, %b: f32)`.
 ParseResult
 M::parseParenOperandList(OpAsmParser &parser, OperationState &result,
-                         SmallVectorImpl<OpAsmParser::Argument> &argumentInfo) {
+                         SmallVectorImpl<OpAsmParser::Argument> &argumentInfo,
+                         Type optDefaultType) {
   auto parseOperandFn = [&]() -> ParseResult {
     OpAsmParser::Argument arg;
-    if (parser.parseArgument(arg, /*allowType=*/true) ||
-        parser.resolveOperand(arg.ssaName, arg.type, result.operands))
+
+    // parse argument name
+    NamedAttrList attrs;
+    if (failed(parser.parseOperand(arg.ssaName, /*allowResultNumber=*/false)))
+      return failure();
+
+    // parse optional type annotation
+    arg.type = optDefaultType;
+    if (succeeded(parser.parseOptionalColon())) {
+      if (failed(parser.parseType(arg.type)))
+        return failure();
+    } else if (!optDefaultType) {
+      return parser.emitError(parser.getCurrentLocation(),
+                              "expected type annotation");
+    }
+    arg.attrs = attrs.getDictionary(parser.getContext());
+
+    // resolve operand
+    if (failed(parser.resolveOperand(arg.ssaName, arg.type, result.operands)))
       return failure();
     argumentInfo.push_back(arg);
     return success();
