369b90b141cdcd51dbfe779f280e5a7f3f5076f4
Brian Gesiak <brian@modular.com>
2023-06-05 12:47:17 -0400

    [CI][docs] Print all `mojo -doc-validate` errors (#14959)
    Closes #14952: instead of just printing that warnings originated in the
    file being `mojo -doc-validate`'d, print all the warnings that occurred.
    
    MODULAR_ORIG_COMMIT_REV_ID: 781a47646b8fd799e91235dbe148eb833e079296

--- KGEN/CheckPatternInFile.cmake
+++ KGEN/CheckPatternInFile.cmake
@@ -5,11 +5,18 @@
 ##===----------------------------------------------------------------------===##
 
 # A CMake script that raises a fatal error if the regular expression `PATTERN`
-# occurs within the contents of the file at `PATH`.
+# occurs within the contents of the file at `PATH`. The error message is
+# `FAILURE_MESSAGE`, followed by each match of the regular expression.
 
 file(READ "${PATH}" contents)
-string(REGEX MATCH "${PATTERN}" result "${contents}")
+string(REGEX MATCHALL "${PATTERN}" results "${contents}")
 
-if(NOT ${result} EQUAL -1)
-  message(FATAL_ERROR "Pattern \"${PATTERN}\" appears in \"${PATH}\"")
+message(VERBOSE "Searching for pattern \"${PATTERN}\" in \"${PATH}\"...")
+
+if(results)
+  foreach(result ${results})
+    string(CONCAT FAILURE_MESSAGE "${FAILURE_MESSAGE}" "\n${result}\n")
+  endforeach()
+
+  message(FATAL_ERROR "${FAILURE_MESSAGE}")
 endif()
