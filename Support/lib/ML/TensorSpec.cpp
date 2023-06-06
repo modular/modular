7b9270feaec7b839d32993d3fe76cd35fbd64a3e
Fabian Tschopp <fabian.tschopp@modular.com>
2023-06-06 17:23:36 +0200

    [GenericML] Add parsing and command line option for `TensorSpec`. (#15014)
    This adds a parser to TensorSpec akin to TensorShape. Additionally, the
    command line options for the driver implementations now support
    `--input-spec` in addition to `--input-shape`, which will be required
    for the PyTorch integration, where model inputs lack both shape and
    tensor type.
    Going forward, `--input-spec` can also specify dynamic shapes, which can
    then be refined to static shapes using `--input-shape`.
    
    MODULAR_ORIG_COMMIT_REV_ID: 65372f8d5e0116ae002863dbc128dc031331742f

--- Support/lib/ML/TensorSpec.cpp
+++ Support/lib/ML/TensorSpec.cpp
@@ -11,6 +11,40 @@
 using namespace M;
 
 //===----------------------------------------------------------------------===//
+// Parsing methods
+//===----------------------------------------------------------------------===//
+
+/// Parses a string of the form dim0xdim1x...xDType into a TensorSpec.
+ErrorOr<TensorSpec> TensorSpec::parseFromString(StringRef str) {
+  // dtype is the portion after the last 'x'.  Shape is the portion before
+  // that.  If there is no 'x', the whole string is the dtype.  (rsplit almost
+  // does this, but the no-'x' case would put the string in shape instead of
+  // dtype.)
+  auto lastXIndex = str.rfind('x');
+  StringRef shapeStr, dtypeStr;
+  if (lastXIndex == StringRef::npos) {
+    shapeStr = "";
+    dtypeStr = str;
+  } else {
+    shapeStr = str.slice(0, lastXIndex);
+    dtypeStr = str.slice(lastXIndex + 1, StringRef::npos);
+  }
+
+  auto shape = TensorShape::parseFromString(shapeStr);
+  if (failed(shape))
+    return Error(Twine("could not parse shape from string: ") + str + ": " +
+                 shape.getError());
+
+  auto dtype = DType::getFromString(dtypeStr);
+  if (failed(dtype))
+    return Error(Twine("could not parse dtype from string: ") + str +
+                 " because " + dtypeStr + " is not a valid DType");
+
+  // Create the tensor spec from the shape and dtype information.
+  return TensorSpec(*shape, *dtype);
+}
+
+//===----------------------------------------------------------------------===//
 // Stringification and printing methods
 //===----------------------------------------------------------------------===//
 
