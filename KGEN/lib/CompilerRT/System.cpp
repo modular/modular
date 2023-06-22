d351e60c590576393bf92df20941d01aa3028d81
River Riddle <riddleriver@gmail.com>
2023-06-21 22:15:53 -0700

    [Mojo] Add initial sys.argv support and plumbing into mojo exe (#15807)
    We don't have global variables at all right now, so we need to
    implement this as a list stored in KGEN_CompilerRT and a function
    named `argv` instead of a variable. After this PR, one can execute
    mojo with additional arguments: `mojo my_file.mojo --mojo_arg`.
    
    MODULAR_ORIG_COMMIT_REV_ID: 03f04a91bbe396d5c026ac0a29b8d31a9fd8ebf5

--- KGEN/lib/CompilerRT/System.cpp
+++ KGEN/lib/CompilerRT/System.cpp
@@ -11,11 +11,19 @@
 #include <stdio.h>
 #include <thread>
 
+//===----------------------------------------------------------------------===//
+// CPU Information
+//===----------------------------------------------------------------------===//
+
 /// Returns the number of cores on the system.
 COMPILERRT_EXPORT COMPILERRT_VISIBILITY_EXPORT size_t
 KGEN_CompilerRT_CoreCount() {
   return std::thread::hardware_concurrency();
 }
+
+//===----------------------------------------------------------------------===//
+// Printing
+//===----------------------------------------------------------------------===//
 
 COMPILERRT_EXPORT COMPILERRT_VISIBILITY_EXPORT int
 KGEN_CompilerRT_fprintf(FILE *stream, const char *format, ...) {
@@ -26,10 +34,63 @@ KGEN_CompilerRT_fprintf(FILE *stream, const char *format, ...) {
   return result;
 }
 
+//===----------------------------------------------------------------------===//
+// Arguments
+//===----------------------------------------------------------------------===//
+
+namespace {
+/// This class represents the set of argv values passed to the current mojo
+/// program.
+struct ArgVList {
+  /// The raw list of arguments, used when communicating with Mojo.
+  struct RawList {
+    llvm::StringRef *args;
+    size_t size;
+  };
+
+  /// Return the global argv instance.
+  static ArgVList &get() {
+    static ArgVList argVList;
+    return argVList;
+  }
+
+  /// Return the raw list of arguments.
+  RawList getRawList() { return {args.data(), args.size()}; }
+
+  /// Allow argv[0] to be empty by default, matching python behavior when no
+  /// script name is passed.
+  std::vector<llvm::StringRef> args{""};
+  std::vector<std::string> argStrings;
+};
+} // namespace
+
+COMPILERRT_EXPORT
+COMPILERRT_VISIBILITY_EXPORT ArgVList::RawList KGEN_CompilerRT_GetArgV() {
+  return ArgVList::get().getRawList();
+}
+
+COMPILERRT_EXPORT
+COMPILERRT_VISIBILITY_EXPORT void KGEN_CompilerRT_SetArgV(size_t argc,
+                                                          char **argv) {
+  ArgVList &argVList = ArgVList::get();
+  argVList.args.resize(argc);
+  argVList.argStrings.resize(argc);
+  for (size_t i = 0; i < argc; ++i)
+    argVList.args[i] = argVList.argStrings[i] = argv[i];
+}
+
+//===----------------------------------------------------------------------===//
+// CompilerRT Registration
+//===----------------------------------------------------------------------===//
+
 void M::KGEN::registerSystem(
     std::vector<std::pair<llvm::StringLiteral, void *>> &funcs) {
   funcs.push_back(
       {"KGEN_CompilerRT_fprintf", (void *)&KGEN_CompilerRT_fprintf});
   funcs.push_back(
       {"KGEN_CompilerRT_CoreCount", (void *)&KGEN_CompilerRT_CoreCount});
+  funcs.push_back(
+      {"KGEN_CompilerRT_GetArgV", (void *)&KGEN_CompilerRT_GetArgV});
+  funcs.push_back(
+      {"KGEN_CompilerRT_SetArgV", (void *)&KGEN_CompilerRT_SetArgV});
 }
---
+++ KGEN/test/mojo/exec-argv.mojo
@@ -0,0 +1,24 @@
+# ===----------------------------------------------------------------------=== #
+#
+# This file is Modular Inc proprietary.
+#
+# ===----------------------------------------------------------------------=== #
+
+# RUN: mojo %s | FileCheck %s --check-prefix=NO_ARGS
+# RUN: mojo %s --arg1 --arg2=10 --arg3="arg3" | FileCheck %s --check-prefix=ARGS
+
+from IO import print
+from Range import range
+from Sys import argv
+
+
+def main() -> None:
+    # NO_ARGS: exec-argv.mojo
+
+    # ARGS: exec-argv.mojo
+    # ARGS: --arg1
+    # ARGS: --arg2=10
+    # ARGS: --arg3=arg3
+
+    for i in range(argv().__len__()):
+        print(argv()[i])
