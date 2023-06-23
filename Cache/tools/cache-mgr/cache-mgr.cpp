0d53553a13354766b019e7f5ba5d289978729370
Aman LaChapelle <aman.lachapelle@gmail.com>
2023-06-23 11:09:41 -0700

    [Cache] Simplify cache-mgr. (#15889)
    So far we don't have any use cases that require more than a single input
    for `cache-mgr`, so simplify the logic by only allowing one input and
    one output. The CMake command still allows multiple inputs and outputs,
    but it handles it in CMake rather than attempting to do the odd argument
    parsing we were doing before.
    
    MODULAR_ORIG_COMMIT_REV_ID: 85fdeaeb05cbd2dfff13cfffda5feb336db9ce58

--- Cache/tools/cache-mgr/cache-mgr.cpp
+++ Cache/tools/cache-mgr/cache-mgr.cpp
@@ -28,54 +28,19 @@ namespace {
 /// this tool, and it should be simple to read/write.
 using BinaryBlobCacheKey = Keys::VariantTypeKey<Cache::BufferRef, StringRef>;
 
-/// Describes an input file, or a cached object request. An input file is simply
-/// a path, while a cached object request is `<hash> > <output-path>` or
-/// `key:<key value> > <output-path>`. First format is useful if we are directly
-/// specifying the hash, whereas the later is for cases where we need to derive
-/// key after some transformation. This format could easily be extended to
-/// include things like extra data to be included in the hash. The output path
-/// can be `-`, in which case the output is written to `-o` (which itself could
-/// be stdout or a file) with a newline after each object retrieved.
-struct FileOrCachedObjectRequest {
-  std::string hashOrFilename;
-  std::string outFileName;
-
-  std::filesystem::path getInputFilename() const { return {hashOrFilename}; }
-  StringRef getHash() const { return hashOrFilename; }
-  std::filesystem::path getOutputFilename() const { return {outFileName}; }
-  bool isaGet() const {
-    // When we don't have an output file name, it must be a PUT.
-    return !outFileName.empty();
-  }
-};
-
-/// Provide a parser for the FileOrCachedObjectRequest object.
-class FileOrCachedObjectRequestParser
-    : public llvm::cl::parser<FileOrCachedObjectRequest> {
-public:
-  using llvm::cl::parser<FileOrCachedObjectRequest>::parser;
-
-  bool parse(llvm::cl::Option &o, StringRef argName, StringRef argValue,
-             FileOrCachedObjectRequest &val);
-};
-
 /// Provides the CLOptions for this tool.
 class CLOptions : public CLOptionsBase {
 public:
   using CLOptionsBase::CLOptionsBase;
 
   /// Specify the input file or cached object.
-  cl::list<FileOrCachedObjectRequest, bool, FileOrCachedObjectRequestParser>
-      inputs{"input", cl::desc("<input file or CAS reference>"),
-             llvm::cl::OneOrMore};
+  cl::opt<std::string> input{"input",
+                             cl::desc("<input file or CAS reference>")};
 
-  cl::list<std::string> keys{
+  cl::opt<std::string> key{
       "key",
-      cl::desc(
-          "Explicitly Specify key. In this case instead of binary hash, "
-          "this key will be used for adding object to cache. Only valid for "
-          "PUT operation. For get specify the key with --input option with a "
-          "prefix 'key:'. For eg: key:my-key.")};
+      cl::desc("Explicitly Specify key. In this case instead of binary hash, "
+               "this key will be used for adding object to cache.")};
 
   cl::opt<std::string> target{
       "target",
@@ -100,10 +65,35 @@ public:
       "o", cl::desc("File path to use for program outputs."),
       llvm::cl::init("-")};
 
+  cl::opt<bool> append{"append",
+                       cl::desc("Append to the output file."),
+                       cl::init(false)};
+
   /// Get the filesystem path for the CAS. Defaults to a temporary directory.
   std::filesystem::path getFsPath() const;
 };
 } // namespace
+
+/// Attempt to decode a hash from the encoded string. If the incoming string is
+/// hex or base64 this will return the raw bytes, otherwise it will return
+/// std::nullopt.
+static std::optional<std::string> decodeHash(StringRef encoded) {
+  std::string hash;
+  if (!llvm::tryGetFromHex(encoded, hash)) {
+    std::vector<char> ref;
+    ref.reserve(encoded.size());
+    if (auto err = llvm::decodeBase64(encoded, ref)) {
+      (void)llvm::toString(
+          std::move(err)); // Consume the error without doing anything.
+      return std::nullopt;
+    }
+
+    hash = std::string(ref.begin(), ref.end());
+    hash.shrink_to_fit();
+    return hash;
+  }
+  return hash;
+}
 
 static bool isCustomTargetValid(const llvm::StringRef &customTarget) {
   using namespace M::Cache::Keys;
@@ -136,65 +126,6 @@ static std::string wrapKey(BinaryBlobCacheKey::KeyTy key,
 }
 
 //===----------------------------------------------------------------------===//
-// FileOrCachedObjectRequestParser::parse
-//===----------------------------------------------------------------------===//
-
-bool FileOrCachedObjectRequestParser::parse(llvm::cl::Option &o,
-                                            StringRef argName,
-                                            StringRef argValue,
-                                            FileOrCachedObjectRequest &val) {
-  // Check if the value contains the name and signature.
-  SmallVector<StringRef> split;
-  argValue.split(split, '>');
-  // Trim whitespace from the ends of the string.
-  llvm::for_each(split, [](StringRef &s) { s = s.ltrim().rtrim(); });
-  if (split.size() == 2) {
-
-    SmallVector<StringRef> tagSplit;
-
-    split.front().split(tagSplit, ':');
-    if (tagSplit.size() == 2) {
-      // This is of the form tag:value
-      val.hashOrFilename = tagSplit.back();
-      val.hashOrFilename.shrink_to_fit();
-    } else { // First try to get it as a hex string. If that fails, try base64.
-      if (!llvm::tryGetFromHex(split[0], val.hashOrFilename)) {
-        std::vector<char> ref;
-        ref.reserve(split[0].size());
-        if (auto err = llvm::decodeBase64(split[0], ref)) {
-          o.error(toString(std::move(err)));
-          return true;
-        }
-        val.hashOrFilename = std::string(ref.begin(), ref.end());
-        val.hashOrFilename.shrink_to_fit();
-      }
-      if (val.hashOrFilename.empty() || val.hashOrFilename.size() != 32) {
-        o.error("parsed hash could not be decoded into 32 bytes");
-        return true;
-      }
-    }
-    val.outFileName = split.back();
-    return false;
-  }
-
-  // Make sure the arg value is not a directory.
-  std::error_code ec;
-  if (std::filesystem::is_directory(argValue.str(), ec)) {
-    o.error(argValue + " is a directory, please redirect to a file");
-    return true;
-  }
-  if (ec) {
-    o.error(ec.message());
-    return true;
-  }
-
-  // Otherwise, if it's not a hash, the value is the name.
-  val.hashOrFilename = argValue;
-  val.outFileName = "";
-  return false;
-}
-
-//===----------------------------------------------------------------------===//
 // CLOptions::getFsPath
 //===----------------------------------------------------------------------===//
 
@@ -222,19 +153,16 @@ std::filesystem::path CLOptions::getFsPath() const {
   return out;
 }
 
-static AsyncValueRef<std::string>
-putObjectsIntoCache(BinaryBlobCacheKey::KeyTy key, Cache::BufferRef value,
-                    const FileOrCachedObjectRequest &input,
-                    RCRef<BlobCache<BinaryBlobCacheKey>> &cache,
-                    LLCL::Runtime &runtime) {
+static AsyncValueRef<std::string> putObjectsIntoCache(
+    BinaryBlobCacheKey::KeyTy key, Cache::BufferRef value, StringRef input,
+    RCRef<BlobCache<BinaryBlobCacheKey>> &cache, LLCL::Runtime &runtime) {
 
   AsyncValueRef<std::string> insert =
       cache->insert(std::move(key), std::move(value));
   auto outCh = AsyncValueRef<std::string>::allocate(runtime);
   std::move(insert).andThenSync(
       [outCh = outCh.copy(),
-       input = Buffer::get(input.getInputFilename().string())](
-          AsyncValueRef<std::string> &&hash) mutable {
+       input = Buffer::get(input)](AsyncValueRef<std::string> &&hash) mutable {
         // If we have an error, report it.
         if (hash.isError())
           return std::move(outCh).setToError(hash.takeDiagnostic());
@@ -261,96 +189,90 @@ int main(int argc, char **argv) {
   auto cache =
       RCRef<BlobCache<BinaryBlobCacheKey>>::create(std::move(*backendChainOr));
 
-  SmallVector<AnyAsyncValueRef> results;
-  if (!clOptions.keys.empty()) {
-    if (clOptions.inputs.size() != clOptions.keys.size())
-      return clOptions.reportError(
-          "Number of inputs should match number of keys.");
+  // The result of the operation will always be either a buffer (raw contents
+  // from a GET), string (hash from a PUT), or an error.
+  SmartVariant<BufferRef, std::string, EncodedDiagnostic> operationResult;
 
-    for (const auto &[key, input] :
-         llvm::zip(clOptions.keys, clOptions.inputs)) {
-      auto bufOr = Cache::Buffer::getFile(input.getInputFilename());
-      if (bufOr.isError())
-        return clOptions.reportError(bufOr.getError());
-      AsyncValueRef<std::string> outCh =
-          putObjectsIntoCache(wrapKey(key, clOptions.target), (*bufOr).copy(),
-                              input, cache, runtime);
-      results.push_back(std::move(outCh));
+  // If we were able to decode the hash, then it's a GET - otherwise it's a
+  // PUT.
+  auto hash = decodeHash(clOptions.input);
+  std::string key = clOptions.key;
+  if (hash || !key.empty()) {
+    // Attempt to find the value in the cache, if it exists, write it out.
+    // If the key is specified, use it directly.
+    std::string keyToFind =
+        key.empty() ? wrapKey(*hash, clOptions.target) : key;
+    auto result = cache->find(keyToFind);
+    auto outCh = AsyncValueRef<BufferRef>::allocate(runtime);
+    std::move(result).andThenSync(
+        [outCh = outCh.copy(), input = Buffer::get(*hash)](
+            AsyncValueRef<std::optional<BufferRef>> &&found) mutable {
+          // If there was an error, produce that diagnostic.
+          if (found.isError())
+            return std::move(outCh).setToError(found.takeDiagnostic());
+
+          // No value, emit an error.
+          if (!found->has_value()) {
+            return std::move(outCh).setToError(
+                UnknownLocationDecoder::getDiagnostic(
+                    Twine(llvm::encodeBase64(input->getBuffer())) +
+                    ": value not found in the cache"));
+          }
+
+          // Has a value, write the value.
+          BufferRef buf = std::move(**found);
+          std::move(outCh).emplace(std::move(buf));
+        });
+    await(outCh);
+    if (outCh.isError()) {
+      operationResult.getUnderlyingStorage().emplace<EncodedDiagnostic>(
+          outCh.takeDiagnostic());
+    } else {
+      operationResult.getUnderlyingStorage().emplace<BufferRef>(
+          std::move(*outCh));
+    }
+  } else {
+    auto bufOr = Cache::Buffer::getFile(clOptions.input.getValue());
+    if (bufOr.isError())
+      return clOptions.reportError(bufOr.getError());
+    std::string keyToWrite =
+        key.empty() ? wrapKey((*bufOr).copy(), clOptions.target) : key;
+    AsyncValueRef<std::string> outCh = putObjectsIntoCache(
+        keyToWrite, (*bufOr).copy(), clOptions.input, cache, runtime);
+    await(outCh);
+    if (outCh.isError()) {
+      operationResult.getUnderlyingStorage().emplace<EncodedDiagnostic>(
+          outCh.takeDiagnostic());
+    } else {
+      operationResult.getUnderlyingStorage().emplace<std::string>(
+          std::move(*outCh));
     }
   }
 
-  for (const FileOrCachedObjectRequest &input : clOptions.inputs) {
-    // If it's a PUT, then hash the input and write it to the CAS.
-    if (input.isaGet()) {
-      // Attempt to find the value in the cache, if it exists, write it out.
-      auto result = cache->find(wrapKey(input.getHash(), clOptions.target));
-      auto outCh = AsyncValueRef<BufferRef>::allocate(runtime);
-      std::move(result).andThenSync(
-          [outCh = outCh.copy(), input = Buffer::get(input.getHash())](
-              AsyncValueRef<std::optional<BufferRef>> &&found) mutable {
-            // If there was an error, produce that diagnostic.
-            if (found.isError())
-              return std::move(outCh).setToError(found.takeDiagnostic());
-
-            // No value, emit an error.
-            if (!found->has_value()) {
-              return std::move(outCh).setToError(
-                  UnknownLocationDecoder::getDiagnostic(
-                      Twine(llvm::encodeBase64(input->getBuffer())) +
-                      ": value not found in the cache"));
-            }
-
-            // Has a value, write the value.
-            BufferRef buf = std::move(**found);
-            std::move(outCh).emplace(std::move(buf));
-          });
-      results.push_back(std::move(outCh));
-    } else if (clOptions.keys.empty()) {
-      auto bufOr = Cache::Buffer::getFile(input.getInputFilename());
-      if (bufOr.isError())
-        return clOptions.reportError(bufOr.getError());
-      std::string key = wrapKey((*bufOr).copy(), clOptions.target);
-      AsyncValueRef<std::string> outCh =
-          putObjectsIntoCache(key, (*bufOr).copy(), input, cache, runtime);
-      results.push_back(std::move(outCh));
-    }
-  }
-
-  // Await for all the results to quiesce.
-  await(results);
-
-  std::string errMsg;
-  std::unique_ptr<llvm::ToolOutputFile> outFile =
-      mlir::openOutputFile(clOptions.outFile, &errMsg);
-  if (!outFile)
-    return clOptions.reportError(errMsg);
+  std::error_code ec;
+  llvm::ToolOutputFile outFile(clOptions.outFile, ec,
+                               clOptions.append ? llvm::sys::fs::OF_Append
+                                                : llvm::sys::fs::OF_None);
+  if (ec)
+    return clOptions.reportError(ec.message());
 
   // Report any errors we might have.
-  for (auto [r, input] : llvm::zip(results, clOptions.inputs)) {
-    if (r.isError())
-      return clOptions.reportError(r.getDiagnostic().getMessage().get());
-    // Emit a semicolon-separated list of hashes for the provided PUTs in order.
-    if (r.isType<std::string>()) {
-      outFile->os() << r.get<std::string>() << ";";
-    } else if (r.isType<BufferRef>()) {
-      std::filesystem::path outPath = input.getOutputFilename();
-      StringRef buf = r.get<BufferRef>()->getBuffer();
-      // Emit to stdout (or the file), so print it and carry on.
-      if (outPath == "-") {
-        outFile->os() << buf << "\n";
-        continue;
-      }
-
-      // Otherwise, map a file and write the contents.
-      auto writeableBuf = WriteableBuffer::getFile(outPath, buf.size());
-      if (writeableBuf.isError())
-        return clOptions.reportError(writeableBuf.getError());
-
-      // Write the data to the mapped file.
-      (*writeableBuf)->pwrite(buf.data(), buf.size(), 0);
-    }
+  if (isa<EncodedDiagnostic>(operationResult)) {
+    auto &diag = cast<EncodedDiagnostic>(operationResult);
+    return clOptions.reportError(diag.getMessage().get());
   }
+
+  if (isa<std::string>(operationResult)) {
+    auto &str = cast<std::string>(operationResult);
+    outFile.os() << str;
+  } else if (isa<BufferRef>(operationResult)) {
+    auto &buf = cast<BufferRef>(operationResult);
+    outFile.os() << buf->getBuffer();
+  }
+  if (clOptions.append)
+    outFile.os() << ";";
+
   // Keep the output file.
-  outFile->keep();
+  outFile.keep();
   return 0;
 }
