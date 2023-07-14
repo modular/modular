d83586f8ef0eb92767b5ef4d743bb032dd47379f
Arjun Surendran <arjun@modular.com>
2023-07-14 19:37:50 +0000

    Revert "[Support] Make preferred alignment value uniform across `x86_64` targets." (#17068)
    Reverts #17017, Sigh! MSVC needs more macro to clearly distinguish. I
    don't know how it worked yesterday.
    
    MODULAR_ORIG_COMMIT_REV_ID: 627c8d1b86be8b97989abeaceed8f8d243c4e50b

--- Support/include/Support/AlignedAlloc.h
+++ Support/include/Support/AlignedAlloc.h
@@ -18,8 +18,10 @@
 
 namespace M {
 
-#if defined(__x86_64__)
+#if defined(__AVX512F__)
 static constexpr size_t kPreferredMemoryAlignment = 64;
+#elif defined(__AVX2__) || defined(__AVX__)
+static constexpr size_t kPreferredMemoryAlignment = 32;
 #elif defined(__ARM_NEON__) || defined(__ARM_NEON)
 static constexpr size_t kPreferredMemoryAlignment = 16;
 #else
