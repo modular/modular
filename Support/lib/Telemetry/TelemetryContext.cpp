4c83957d8bdd0fe83d861100424193df811229d6
Juan Galvez <juan@modular.com>
2023-07-19 08:24:05 -0500

    [Telemetry] Add logs API with initial Otel implementation (#16943)
    This adds the Modular API for logs with an initial OTel implementation.
    
    Example usage:
    ```
    auto logger = telemetryContext->getLogger("mojo");
    logger->getError("my-event") << "my log error message";
    logger->emitEvent("my-event", Severity::kError, "my log error message");
    logger->emitEvent("billing", Severity::kInfo);
    ```
    
    I wanted to have a `Logger::emitEvent(eventName, severity, body = "")`
    which is thread-safe and that maps to Otel's own `logger->EmitEvent`. At
    the same time, I wanted to have `logger::getXXX()` methods that return a
    llvm::raw_ostream and are basically wrappers around emitEvent. My
    solution is to have a subclass of raw_ostream where event emission
    happens when the stream goes out of scope. The stream needs a pointer to
    the logger to emit the event, which is why I made the Logger
    enable_shared_from_this, but I'm open to other suggestions. Also note
    that OTel's getLogger returns a shared_ptr, so I'm not changing anything
    in that sense, just making sure that the LogStream's pointer to Logger
    comes from this shared_ptr.
    
    MODULAR_ORIG_COMMIT_REV_ID: 3e49decc0e8b42b0e07e3bb3af4289984bb360f0

--- Support/lib/Telemetry/TelemetryContext.cpp
+++ Support/lib/Telemetry/TelemetryContext.cpp
@@ -8,10 +8,17 @@
 #include "Support/Telemetry/Telemetry.h"
 
 #ifdef MODULAR_ENABLE_TELEMETRY
+#include "opentelemetry/exporters/otlp/otlp_http_log_record_exporter.h"
+#include "opentelemetry/exporters/otlp/otlp_http_log_record_exporter_factory.h"
+#include "opentelemetry/exporters/otlp/otlp_http_log_record_exporter_options.h"
 #include "opentelemetry/exporters/otlp/otlp_http_metric_exporter.h"
 #include "opentelemetry/exporters/otlp/otlp_http_metric_exporter_factory.h"
 #include "opentelemetry/exporters/otlp/otlp_http_metric_exporter_options.h"
 #include "opentelemetry/metrics/provider.h"
+#include "opentelemetry/sdk/logs/event_logger_provider_factory.h"
+#include "opentelemetry/sdk/logs/logger_provider_factory.h"
+#include "opentelemetry/sdk/logs/processor.h"
+#include "opentelemetry/sdk/logs/simple_log_record_processor_factory.h"
 #include "opentelemetry/sdk/metrics/meter.h"
 #include "opentelemetry/sdk/metrics/meter_provider.h"
 #endif // MODULAR_ENABLE_TELEMETRY
@@ -20,6 +27,8 @@ namespace M::Telemetry {
 
 TelemetryContext::TelemetryContext() {
 #ifdef MODULAR_ENABLE_TELEMETRY
+
+  // -------- Metrics --------
   // Create OpenTelemetry OTLP HTTP exporter.
   opentelemetry::exporter::otlp::OtlpHttpMetricExporterOptions otlpOptions;
   otlpOptions.url = "http://localhost:4318/v1/metrics";
@@ -39,6 +48,21 @@ TelemetryContext::TelemetryContext() {
 
   meter = metricsProvider->GetMeter("modular");
 
+  // -------- Logs --------
+  opentelemetry::exporter::otlp::OtlpHttpLogRecordExporterOptions
+      oltpLogOptions;
+  oltpLogOptions.url = "http://localhost:4318/v1/logs";
+  // Create OTLP exporter instance
+  auto logExporter =
+      opentelemetry::exporter::otlp::OtlpHttpLogRecordExporterFactory::Create(
+          oltpLogOptions);
+  auto processor =
+      opentelemetry::sdk::logs::SimpleLogRecordProcessorFactory::Create(
+          std::move(logExporter));
+  loggerProvider = opentelemetry::sdk::logs::LoggerProviderFactory::Create(
+      std::move(processor));
+  eventLoggerProvider =
+      opentelemetry::sdk::logs::EventLoggerProviderFactory::Create();
 #endif // MODULAR_ENABLE_TELEMETRY
 }
 
