dccb4edb2b64adba78a11b2d62145bfc360635a4
Brian Gesiak <brian@modular.com>
2023-07-20 13:46:41 -0400

    [mojo-sdk] Self-test REPL post-installation (#17300)
    Add self-tests for `mojo repl`:
    
    * First off, modify the GitHub Actions workflow to run on PRs that touch
      the mojo-sdk installation CMake and Python script. This gives us
      signal as we expand the self-tests.
    * Since an `lldb` found on the user's `PATH` is unlikely to match the
      one we need to support the Mojo REPL, modify the `mojo repl`
      subcommand to exclusively look for an `lldb` installed alongside the
      `mojo` driver. To make this work for local builds, add a `mojo-lldb`
      CMake target that creates a symlink to the `lldb` we want. To make
      this work for mojo-sdk installations,  modify the installation target
      to copy lldb and related libraries to the install directory.
    * Unfortunately, this still succeeds with a false positive -- because we
      configure the Modular lldb with `-DLLDB_GLOBAL_INIT_DIRECTORY`
      pointing to the Modular `.derived` directory, the REPL succeeds if
      that specific directory is on the host machine. When we begin
      installing the Mojo SDK on user machines, they obviously won't have a
      `.derived` directory, so the REPL will fail for them. So, this pull
      request modifies the `repl` driver subcommand to look or a MojoLLDB
      plugin library relative to the driver, and removes the use of
      `-DLLDB_GLOBAL_INIT_DIRECTORY`. (However, this doesn't remove the
      potential for false positives itself. I think what we ought to do is
      delete the `.derived` directory before running self-tests, to catch
      issues such as this. But that will be done in a future commit.)
    
    MODULAR_ORIG_COMMIT_REV_ID: 6a2de2ed30289d61c1e2a850ce7a5b7bb094b710

--- KGEN/tools/mojo/REPL/mojo-repl.cpp
+++ KGEN/tools/mojo/REPL/mojo-repl.cpp
@@ -6,11 +6,16 @@
 
 #include "mojo-repl.h"
 #include "../Common/Telemetry.h"
+#include "REPL/MojoLLDB.h"
 
 #include "Support/Driver/DriverSupport.h"
+
 #include "llvm/Option/ArgList.h"
 #include "llvm/Option/OptTable.h"
+#include "llvm/Support/FormatVariadic.h"
 #include "llvm/Support/Program.h"
+
+#include <filesystem>
 
 using namespace M;
 
@@ -22,6 +27,26 @@ struct REPLOptTable : public llvm::opt::PrecomputedOptTable {
   REPLOptTable() : llvm::opt::PrecomputedOptTable(InfoTable, PrefixTable) {}
 };
 } // namespace
+
+/// Returns the path to a suitable `lldb` executable that can be used to launch
+/// the REPL, or an error if none exists.
+static llvm::ErrorOr<std::string> getLLDB(const std::string &executable) {
+  // Attempt to find an lldb installed alongside the driver.
+  return llvm::sys::findProgramByName(
+      "lldb",
+      /*Paths=*/{std::filesystem::path(executable).parent_path().c_str()});
+}
+
+/// Returns the path to a MojoLLDB dynamic library, or an error if none exists.
+/// This library implements Mojo's lldb plugin.
+static std::string getMojoLLDB(const std::string &executable) {
+  // Attempt to find a MojoLLDB installed relative to the driver: if the driver
+  // exists at "foo/bin/mojo", MojoLLDB should exist at "foo/bin/../lib/".
+  std::filesystem::path lib =
+      std::filesystem::path(executable).parent_path().parent_path() / "lib" /
+      MOJO_LLDB;
+  return lib.string();
+}
 
 /// Launches the Mojo REPL, which is in fact an invocation of
 /// `lldb --repl-language mojo`. Exits unsuccessfully if lldb could not be found
@@ -44,16 +69,23 @@ static int repl(const State &state) {
   auto telemetryCtx = LLCL::RCRef<Telemetry::TelemetryContext>::create();
   initializeTelemetry(telemetryCtx.copy(), state, args);
 
-  // Find the path to lldb.
-  llvm::ErrorOr<std::string> lldb = llvm::sys::findProgramByName("lldb");
+  // Find the path to the lldb executable and the MojoLLDB plugin library.
+  std::string executable =
+      llvm::sys::fs::getMainExecutable(state.programName, (void *)getLLDB);
+  llvm::ErrorOr<std::string> lldb = getLLDB(executable);
   if (!lldb)
-    return state.reportError("lldb must exist in our PATH to launch the REPL");
+    return state.reportError(
+        "lldb must be installed alongside mojo in order to launch the REPL");
+  std::string mojoLLDB = getMojoLLDB(executable);
 
   // We forward all unparsed command line arguments to lldb, as values for the
   // `--repl` option.
   SmallVector<StringRef> lldbArgs(state.arguments);
+  std::string loadCommand = llvm::formatv("plugin load \"{0}\"", mojoLLDB);
   lldbArgs.insert(lldbArgs.begin(),
-                  {lldb.get(), "--repl-language", "mojo", "--repl"});
+                  {lldb.get(), "-Q", "--one-line-before-file", loadCommand,
+                   "--one-line-before-file", "settings set show-progress false",
+                   "--repl-language", "mojo", "--repl"});
   return llvm::sys::ExecuteAndWait(lldb.get(), lldbArgs);
 }
 
