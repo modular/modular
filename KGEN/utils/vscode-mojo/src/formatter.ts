6e406ce2c4959b42493c22cbcaec922446baf450
River Riddle <riddleriver@gmail.com>
2023-07-20 10:22:06 -0700

    [mojo-vscode] Start plumbing in checks for the MojoSDK (#17308)
    This PR adds checks for if the MojoSDK is installed and prompts
    the user to install if one can't be located. There's an escape hatch
    for the internal builds, where we check for MODULAR_PATH.
    
    This also cleans up a few very small issues noticed, e.g. formatter
    arguments should be array instead of list, and some missing
    copyright notices on source files.
    
    MODULAR_ORIG_COMMIT_REV_ID: e847f28e8fccd5c238cdf0e294c183cc22700aa3

--- KGEN/utils/vscode-mojo/src/formatter.ts
+++ KGEN/utils/vscode-mojo/src/formatter.ts
@@ -8,20 +8,26 @@ import {exec} from 'child_process';
 import * as vscode from 'vscode';
 
 import {get} from './config';
+import {MOJOSDK} from './mojoSDK';
 
 export function registerFormatter(outputChannel: vscode.OutputChannel,
-                                  extension: string) {
-  return vscode.languages.registerDocumentFormattingEditProvider(extension, {
-    provideDocumentFormattingEdits(document, _options) {
+                                  mojoSDK: MOJOSDK) {
+  return vscode.languages.registerDocumentFormattingEditProvider('mojo', {
+    async provideDocumentFormattingEdits(document, _options) {
       const workspaceFolder = vscode.workspace.getWorkspaceFolder(document.uri);
       const backupFolder = vscode.workspace.workspaceFolders?.[0];
       const cwd = workspaceFolder?.uri?.fsPath || backupFolder?.uri.fsPath;
-
-      // Get the arguments to pass to the formatter.
       const args = get<string[]>('formatting.args', workspaceFolder, []);
-      return new Promise<vscode.TextEdit[]>((resolve, reject) => {
-        const originalDocumentText = document.getText();
-        const command = "mojo format --quiet " + args.join(' ') + ' -';
+
+      // Resolve the path to the formatter.
+      const mojoPath =
+          await mojoSDK.resolvePath('mojo', /*promptSDKInstall*/ true);
+      if (!mojoPath)
+        return [];
+
+      const originalDocumentText = document.getText();
+      const command = mojoPath + " format --quiet " + args.join(' ') + ' -';
+      return new Promise<vscode.TextEdit[]>(function(resolve, reject) {
         const process = exec(command, {cwd}, (error, stdout, stderr) => {
           // Process any errors/warnings during formatting. These aren't all
           // necessarily fatal, so this doesn't prevent edits from being
@@ -29,13 +35,12 @@ export function registerFormatter(outputChannel: vscode.OutputChannel,
           if (error) {
             outputChannel.appendLine(`Formatting error:\n${stderr}`);
             reject(error);
+            return;
           }
 
           // Formatter returned nothing, don't try to apply any edits.
-          if (originalDocumentText.length > 0 && stdout.length === 0) {
-            resolve([]);
+          if (originalDocumentText.length > 0 && stdout.length === 0)
             return;
-          }
 
           // Otherwise, the formatter returned the formatted text. Update the
           // document.
