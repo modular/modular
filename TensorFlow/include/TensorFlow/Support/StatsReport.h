85160301630e2e941c8f2311b6bdc58ff81ec5fd
Dan Moldovan <26628547+mdanatg@users.noreply.github.com>
2023-07-21 19:12:20 -0400

    [TF] Super scrappy op coverage stats recording (#16542)
    This follows the file output model from
    [Internal link] It's just a start -
    we can expand/change more in the future.
    
    The logging is controlled by two env vars that users can set:
    * `MODULAR_STATS_FILENAME` - a file to append stat reports to (one for
    each model load)
    * `MODULAR_STATS_ALWAYS_RECORD` - when this is set and the file name is
    not, a report will always be output to a temporary file that users can
    collect and send to us
    
    Open issues (to be addressed at a later time):
    * the report now includes op counts broken down by op name and
    signature, including attributes; because this can potentially expose
    unwanted data, the control for enabling these statistics is left to the
    user, and is off by default
    * more consistent means of controlling the output file (although this
    will be moot once the telemetry reporting is available)
    * a model name is *not* include in the report; turns out the file name
    (the best indicator) is a bit more involved through route through the
    CXX driver API
    
    Fixes #16494.
    
    cc @modularml/framework-frontends FYI - this is just a temporary
    solution, we want to decide whether this remains a utility to be used
    across lowerings (this implementation) or as a separate pass
    
    MODULAR_ORIG_COMMIT_REV_ID: 7ccff716dbea490d0d32df673fae32fb3c951e68

---
+++ TensorFlow/include/TensorFlow/Support/StatsReport.h
@@ -0,0 +1,42 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef TENSORFLOW_SUPPORT_STATSREPORT_H
+#define TENSORFLOW_SUPPORT_STATSREPORT_H
+
+#include "Support/LLVMForwardDecls.h"
+#include "mlir/IR/Operation.h"
+
+#include <string>
+#include <unordered_map>
+
+namespace M::TF {
+
+/// A helper API that can collect statistics about ops lowered vs. fallback
+/// ops during an external framework translation pass. For the fallback ops,
+/// counts for each separate signature are gathered as well.
+///
+/// The helper also has the functionality to record these statistics to a
+/// configurable report file.
+struct StatsReport {
+  StatsReport() : numTotalOps(0), numFallbackOps(0) {}
+
+  /// Record one instance of an input op.
+  void countOp();
+  /// Record one instance of an op that was sent to fallback.
+  void countFallback(mlir::Operation &op);
+  /// Write the statistics to file.
+  void writeToFile();
+
+private:
+  size_t numTotalOps;
+  size_t numFallbackOps;
+  std::unordered_map<std::string, int> fallbackHistogram;
+};
+
+} // namespace M::TF
+
+#endif // TENSORFLOW_SUPPORT_STATSREPORT_H
