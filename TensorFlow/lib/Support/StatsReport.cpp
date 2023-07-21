f194542a63aa5ff05255c34988711c39a19d118f
Dan Moldovan <26628547+mdanatg@users.noreply.github.com>
2023-07-21 19:12:20 -0400

    [TF] Super scrappy op coverage stats recording (#16542)
    This follows the file output model from
    [Internal link] It's just a start -
    we can expand/change more in the future.
    
    The logging is controlled by two env vars that users can set:
    * `MODULAR_STATS_FILENAME` - a file to append stat reports to (one for
    each model load)
    * `MODULAR_STATS_ALWAYS_RECORD` - when this is set and the file name is
    not, a report will always be output to a temporary file that users can
    collect and send to us
    
    Open issues (to be addressed at a later time):
    * the report now includes op counts broken down by op name and
    signature, including attributes; because this can potentially expose
    unwanted data, the control for enabling these statistics is left to the
    user, and is off by default
    * more consistent means of controlling the output file (although this
    will be moot once the telemetry reporting is available)
    * a model name is *not* include in the report; turns out the file name
    (the best indicator) is a bit more involved through route through the
    CXX driver API
    
    Fixes #16494.
    
    cc @modularml/framework-frontends FYI - this is just a temporary
    solution, we want to decide whether this remains a utility to be used
    across lowerings (this implementation) or as a separate pass
    
    MODULAR_ORIG_COMMIT_REV_ID: 7ccff716dbea490d0d32df673fae32fb3c951e68

---
+++ TensorFlow/lib/Support/StatsReport.cpp
@@ -0,0 +1,129 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#include "TensorFlow/Support/StatsReport.h"
+
+#include "llvm/ADT/SmallString.h"
+#include "llvm/Support/FileSystem.h"
+#include "llvm/Support/Process.h"
+#include "llvm/Support/Signals.h"
+#include "llvm/Support/Threading.h"
+#include "llvm/Support/ToolOutputFile.h"
+#include <sstream>
+#include <string>
+#include <system_error>
+
+using namespace M;
+using namespace TF;
+
+void M::TF::StatsReport::countOp() { numTotalOps++; }
+
+void M::TF::StatsReport::countFallback(mlir::Operation &op) {
+  numFallbackOps++;
+
+  // Print just the op signature - name, in/out types, attributes.
+
+  std::string key;
+  llvm::raw_string_ostream ss(key);
+  ss << op.getName().stripDialect();
+  ss << "(";
+  bool more = false;
+  for (auto t : op.getOperandTypes()) {
+    if (more)
+      ss << ", ";
+    else
+      more = true;
+    t.print(ss);
+  }
+
+  ss << ") -> (";
+
+  more = false;
+  for (auto t : op.getResultTypes()) {
+    if (more)
+      ss << ", ";
+    else
+      more = true;
+    t.print(ss);
+  }
+
+  ss << ") {";
+
+  more = false;
+  for (auto attr : op.getAttrs()) {
+    if (more)
+      ss << ", ";
+    else
+      more = true;
+    ss << attr.getName() << "=";
+
+    // Avoid writing excessively large attributes.
+    std::string attrVal;
+    llvm::raw_string_ostream attrs(attrVal);
+    attr.getValue().print(attrs);
+    if (attrVal.size() < 32)
+      ss << attrVal;
+    else
+      ss << attrVal.substr(32) << "...";
+  }
+  ss << "}";
+
+  if (fallbackHistogram.find(key) == fallbackHistogram.end()) {
+    fallbackHistogram.emplace(key, 0);
+  }
+  fallbackHistogram[key]++;
+}
+
+void M::TF::StatsReport::writeToFile() {
+  std::error_code ec{};
+  // TODO: Unify with the crash reporting var?
+  // TODO(#17500): Include some info about the model name.
+  std::optional<std::string> optFileName =
+      llvm::sys::Process::GetEnv("MODULAR_STATS_FILENAME");
+  std::optional<std::string> optAlwaysRecord =
+      llvm::sys::Process::GetEnv("MODULAR_STATS_ALWAYS_RECORD");
+  // Only record if a file is configured, or "always writing" is enabled.
+  llvm::SmallString<512> reportFileName;
+  if (!optFileName) {
+    if (optAlwaysRecord.has_value()) {
+      std::error_code tempEc = llvm::sys::fs::createTemporaryFile(
+          "modular", "coveragelog", reportFileName);
+      if (tempEc) {
+        llvm::errs() << "Note: could not create coverage stats file. Coverage "
+                        "reporting will not be available. Error message: "
+                     << tempEc.message() << "\n";
+        return;
+      }
+      llvm::errs()
+          << "MODULAR_STATS_ALWAYS_RECORD is on, writing coverage stats to "
+          << reportFileName << "\n.";
+    } else {
+      // No file name configured, and "always record" not enabled, skipping.
+      return;
+    }
+  } else {
+    reportFileName = *optFileName;
+  }
+
+  llvm::ToolOutputFile reportFile = llvm::ToolOutputFile(
+      reportFileName, ec, llvm::sys::fs::OpenFlags::OF_Append);
+  if (ec) {
+    llvm::errs() << "Note: could not create coverage stats file. Coverage "
+                    "reporting will not be available. Error message: "
+                 << ec.message() << "\n";
+    return;
+  }
+
+  reportFile.keep();
+  reportFile.os() << "TOTAL OPS\t" << numTotalOps << "\n";
+  reportFile.os() << "FALLBACK OPS\t" << numFallbackOps << "\n";
+
+  reportFile.os() << "\nFALLBACK OP LIST\n";
+  for (const auto &[key, value] : fallbackHistogram) {
+    reportFile.os() << value << "\t" << key << "\n";
+  }
+  reportFile.os() << "--------------------------------------\n\n";
+}
