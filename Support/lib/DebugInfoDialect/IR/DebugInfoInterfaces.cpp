b1502d48f614c4c5cb4e0a9f99218a36d4979430
Laszlo Kindrat <laszlo@modular.ai>
2023-07-25 14:34:40 -0400

    [Support][DebugInfo] Factor out a `SubprogramScoped` interface (#17852)
    The logic for verifying certain scope invariants is more general than restricting it to `KGEN::FunctionLike`. The patch also adds this new trait to `LIT::AsyncExecuteOp`.
    
    MODULAR_ORIG_COMMIT_REV_ID: 512a6652d387dd75d4ac92ee88a30bd66548a85a

--- Support/lib/DebugInfoDialect/IR/DebugInfoInterfaces.cpp
+++ Support/lib/DebugInfoDialect/IR/DebugInfoInterfaces.cpp
@@ -5,6 +5,8 @@
 //===----------------------------------------------------------------------===//
 
 #include "Support/DebugInfoDialect/IR/DebugInfoInterfaces.h"
+#include "Support/DebugInfoDialect/IR/DebugInfoAttrs.h"
+#include "Support/ErrorOr.h"
 
 using namespace M;
 using namespace M::DebugInfo;
@@ -12,5 +14,48 @@ using namespace M::DebugInfo;
 //===----------------------------------------------------------------------===//
 // ODS-Generated Definitions
 //===----------------------------------------------------------------------===//
+
+LogicalResult impl::verifySubprogramScoped(SubprogramScoped op) {
+  // If the function doesn't contain a location scope, we don't verify anything.
+  auto fusedLoc =
+      dyn_cast<mlir::FusedLocWith<DebugInfo::DIScopeAttr>>(op->getLoc());
+  if (!fusedLoc)
+    return success();
+
+  DebugInfo::DIScopeAttr scope = fusedLoc.getMetadata();
+  auto funcScope = dyn_cast<DebugInfo::DISubprogramAttr>(scope);
+  if (!funcScope) {
+    return op.emitOpError("must have subprogram scope in location, but got ")
+           << scope;
+  }
+
+  // We walk pre-order, and skip nested functions.
+  WalkResult res =
+      op.getBodyRegion().walk<mlir::WalkOrder::PreOrder>([&](Operation *op) {
+        if (isa<SubprogramScoped>(op))
+          return WalkResult::skip();
+
+        ErrorOr<DebugInfo::DIScopeAttr> scopeOr =
+            DebugInfo::getScopeWithinBody(op->getLoc());
+        if (scopeOr.isError()) {
+          res = op->emitOpError(scopeOr.getError());
+          return WalkResult::interrupt();
+        }
+
+        // We might find a lexical block scope, so we look through it.
+        while (auto lexBlock =
+                   dyn_cast_or_null<DebugInfo::DILexicalBlockAttr>(*scopeOr))
+          scopeOr = lexBlock.getScope();
+
+        if (funcScope != *scopeOr) {
+          res = op->emitOpError("location scope does not match scope of parent "
+                                "func location: ")
+                << funcScope;
+          return WalkResult::interrupt();
+        }
+        return WalkResult::advance();
+      });
+  return failure(res.wasInterrupted());
+}
 
 #include "Support/DebugInfoDialect/IR/DebugInfoInterfaces.cpp.inc"
