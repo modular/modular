4c58831a7b9153d4d849d59ab2be0cbef796630b
abdul dakkak <adakkak@modular.com>
2023-07-25 13:43:02 -0500

    [Stdlib] Add a seed method to the random generator in Mojo (#17867)
    This adds the `seed` method to the random number generator to allow
    folks to seed the rng. The seed is initially initialized with `0`,
    but a user can override that with the current time or an integral
    value.
    
    Closes #17172
    MODULAR_ORIG_COMMIT_REV_ID: b62b42d725dc520618177e10201817cf1191b694

--- KGEN/lib/CompilerRT/Random.cpp
+++ KGEN/lib/CompilerRT/Random.cpp
@@ -9,29 +9,45 @@
 #include "Support/SymbolExport.h"
 #include "llvm/ADT/StringRef.h"
 
-COMPILERRT_EXPORT COMPILERRT_VISIBILITY_EXPORT double
-KGEN_CompilerRT_RandomDouble(double min, double max) {
+COMPILERRT_EXPORT COMPILERRT_VISIBILITY_EXPORT void *
+KGEN_CompilerRT_GetRandomState() {
   static std::default_random_engine randEngine(/*seed=*/0);
+  return &randEngine;
+}
+
+COMPILERRT_EXPORT COMPILERRT_VISIBILITY_EXPORT void
+KGEN_CompilerRT_SetRandomStateSeed(std::default_random_engine *engine,
+                                   ssize_t seed) {
+  engine->seed(seed);
+}
+
+COMPILERRT_EXPORT COMPILERRT_VISIBILITY_EXPORT double
+KGEN_CompilerRT_RandomDouble(std::default_random_engine *engine, double min,
+                             double max) {
   std::uniform_real_distribution<double> dist(min, max);
-  return dist(randEngine);
+  return dist(*engine);
 }
 
 COMPILERRT_EXPORT COMPILERRT_VISIBILITY_EXPORT int64_t
-KGEN_CompilerRT_RandomSInt64(int64_t min, int64_t max) {
-  static std::default_random_engine randEngine(/*seed=*/0);
+KGEN_CompilerRT_RandomSInt64(std::default_random_engine *engine, int64_t min,
+                             int64_t max) {
   std::uniform_int_distribution<int64_t> dist(min, max);
-  return dist(randEngine);
+  return dist(*engine);
 }
 
 COMPILERRT_EXPORT COMPILERRT_VISIBILITY_EXPORT uint64_t
-KGEN_CompilerRT_RandomUInt64(uint64_t min, uint64_t max) {
-  static std::default_random_engine randEngine(/*seed=*/0);
+KGEN_CompilerRT_RandomUInt64(std::default_random_engine *engine, uint64_t min,
+                             uint64_t max) {
   std::uniform_int_distribution<uint64_t> dist(min, max);
-  return dist(randEngine);
+  return dist(*engine);
 }
 
 void M::KGEN::registerRandom(
     std::vector<std::pair<llvm::StringLiteral, void *>> &funcs) {
+  funcs.push_back({"KGEN_CompilerRT_GetRandomState",
+                   (void *)&KGEN_CompilerRT_GetRandomState});
+  funcs.push_back({"KGEN_CompilerRT_SetRandomStateSeed",
+                   (void *)&KGEN_CompilerRT_SetRandomStateSeed});
   funcs.push_back(
       {"KGEN_CompilerRT_RandomDouble", (void *)&KGEN_CompilerRT_RandomDouble});
   funcs.push_back(
