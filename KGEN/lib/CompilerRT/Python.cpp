30df7cc0b2406cdd77d3f412f3a57995555f2c59
Laszlo Kindrat <laszlo@modular.ai>
2023-07-28 11:55:29 -0400

    Revert "[KGEN] Remove `PythonInfo.PYTHONPATH`" (#18129)
    Reverts [Internal link] because it breaks a repl test locally on
    macOS:
    ```
    [...]KGEN/test/mojo-lldb-repl/import-matplotlib.lldb:15:14: error: CHECK-NOT: excluded string found in input
    # CHECK-NOT: ModuleNotFoundError: No module named 'matplotlib'
    ```
    
    MODULAR_ORIG_COMMIT_REV_ID: e2e9c8ad45f437e4076573fe3f053c89feccb695

--- KGEN/lib/CompilerRT/Python.cpp
+++ KGEN/lib/CompilerRT/Python.cpp
@@ -5,17 +5,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "KGEN/CompilerRT.h"
-#include "Support/ErrorOr.h"
-#include "Support/FileSystemExtras.h"
 #include "llvm/ADT/StringRef.h"
-#include "llvm/Support/MemoryBuffer.h"
-#include "llvm/Support/Process.h"
-#include "llvm/Support/Program.h"
-#include <cstdlib>
-
-#ifdef _WIN32
-#include <windows.h>
-#endif
 
 using namespace M;
 
@@ -52,82 +42,6 @@ KGEN_CompilerRT_Python_GetGlobalPython(ssize_t objSize,
 }
 
 //===----------------------------------------------------------------------===//
-// KGEN_CompilerRT_Python_SetPythonPath
-//===----------------------------------------------------------------------===//
-
-/// If not already set, this sets the `PYTHONPATH` environment variable to point
-/// to the typical directories that contain Python modules. These directories
-/// are discovered by invoking `python` and querying it for the paths it has
-/// been configured to use.
-///
-/// If an error prevents `PYTHONPATH` from being set, this returns a pointer to
-/// a non-empty string literal with an error message. Otherwise, this returns a
-/// pointer to an empty string literal.
-COMPILERRT_EXPORT COMPILERRT_VISIBILITY_EXPORT const char *
-KGEN_CompilerRT_Python_SetPythonPath() {
-  // `PYTHONPATH` isn't always set, but when it is, respect whatever it's been
-  // set to, rather than overwriting or appending to it.
-  if (llvm::sys::Process::GetEnv("PYTHONPATH"))
-    return "";
-
-  // If `PYTHONPATH` hasn't been set, then try to grab the host `python3` (or,
-  // failing that, its `python`). If we can't find either, then we can't do
-  // anything, so bail.
-  llvm::ErrorOr<std::string> pyOrErr = llvm::sys::findProgramByName("python3");
-  if (!pyOrErr)
-    pyOrErr = llvm::sys::findProgramByName("python");
-  if (!pyOrErr)
-    return "could not find 'python3' or 'python' executables";
-  std::string python = *pyOrErr;
-
-  // Create a temporary file to capture the output of the `python` invocation.
-  std::error_code ec;
-  std::filesystem::path tmpDirPath = std::filesystem::temp_directory_path(ec);
-  if (ec)
-    return "could not find temporary directory for 'python' output";
-  ErrorOr<TempFile> outOrErr =
-      TempFile::create((tmpDirPath / "python-out-%%%%%%.txt").string());
-  if (failed(outOrErr))
-    return "could not create temporary file to capture 'python' output";
-  std::string out = outOrErr->getPath().string();
-
-  // Invoke `python`, directing its output to the file.
-  const std::optional<StringRef> redirects[] = {
-      /*stdin=*/"",
-      /*stdout=*/out,
-      /*stderr=*/"",
-  };
-  // The Python program prints the list of global site-package directories,
-  // joined by the platform-specific PATH separator.
-  const StringRef args[] = {
-      python, "-c",
-      "import os; import site; print(os.pathsep.join(site.getsitepackages()))"};
-  if (llvm::sys::ExecuteAndWait(python, args, /*Env=*/std::nullopt,
-                                redirects) != 0)
-    return "could not execute 'python' to determine site-package directory";
-
-  // Read the output from the Python program.
-  auto bufferOrErr = llvm::MemoryBuffer::getFile(out);
-  if (!bufferOrErr)
-    return "could not read temporary file with 'python' output";
-
-  // Set the `PYTHONPATH` environment variable to the site-package paths.
-  StringRef buffer = bufferOrErr.get()->getBuffer();
-  const char *pythonPath = buffer.data();
-#if defined(_WIN32)
-  int result = SetEnvironmentVariableA("PYTHONPATH", pythonPath);
-  int failureCode = 0;
-#else
-  int result = setenv("PYTHONPATH", pythonPath, /*overwrite=*/1);
-  int failureCode = -1;
-#endif
-  if (result == failureCode)
-    return "an error occurred when attempting to set 'PYTHONPATH'";
-
-  return "";
-}
-
-//===----------------------------------------------------------------------===//
 // CompilerRT Registration
 //===----------------------------------------------------------------------===//
 
@@ -135,6 +49,4 @@ void KGEN::registerPython(
     std::vector<std::pair<llvm::StringLiteral, void *>> &funcs) {
   funcs.push_back({"KGEN_CompilerRT_Python_GetGlobalPython",
                    (void *)&KGEN_CompilerRT_Python_GetGlobalPython});
-  funcs.push_back({"KGEN_CompilerRT_Python_SetPythonPath",
-                   (void *)&KGEN_CompilerRT_Python_SetPythonPath});
 }
