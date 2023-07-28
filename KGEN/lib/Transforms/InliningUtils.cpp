cfd073c987143f30878880778e0b4d8246441c16
weiwei chen <weiwei.chen@modular.com>
2023-07-28 19:41:57 -0400

    [KGEN] Move some static functions out of Inlining.cpp (#18149)
    These functions will be shared by the new inliner.
    
    Co-authored-by: Mogball <jeff@modular.com>
    MODULAR_ORIG_COMMIT_REV_ID: aac44302447de38114084bbc5e41e393fa4a93f1

---
+++ KGEN/lib/Transforms/InliningUtils.cpp
@@ -0,0 +1,232 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#include "InliningUtils.h"
+#include "KGEN/HLCFDialect/HLCFDialect.h"
+#include "KGEN/HLCFDialect/HLCFOps.h"
+#include "KGEN/KGENDialect/KGENOps.h"
+#include "KGEN/KGENDialect/KGENParameters.h"
+#include "KGEN/KGENPasses.h"
+#include "KGEN/LITDialect/LITOps.h"
+#include "Support/Compiler/TimeProfilerTimingManager.h"
+#include "Support/DebugInfoDialect/IR/DebugInfoOps.h"
+#include "Support/TimeProfiler.h"
+#include "mlir/IR/IRMapping.h"
+#include "mlir/IR/PatternMatch.h"
+#include "mlir/Pass/PassManager.h"
+
+using namespace M;
+using namespace KGEN;
+
+//===----------------------------------------------------------------------===//
+// inlineRegion
+//===----------------------------------------------------------------------===//
+
+std::pair<Operation *, bool> KGEN::inlineRegion(IRMapping &map,
+                                                KGENCallOpInterface call,
+                                                Region &region, bool takeBody) {
+  StringAttr label = StringAttr::get(call.getContext(), "inlined_cf_scope");
+
+  mlir::IRRewriter b{OpBuilder(call)};
+  Operation *scope;
+  if (isa<CallOp>(&*call)) {
+    scope = b.create<HLCF::LoopOp>(call.getLoc(), call->getResultTypes(),
+                                   ValueRange(), label);
+  } else if (auto asyncCall = dyn_cast<LIT::AsyncCallOp>(&*call)) {
+    // Nested function-like op should retain scoped location of the callee.
+    scope = b.create<LIT::AsyncExecuteOp>(region.getParentOp()->getLoc(),
+                                          asyncCall.getType(), call.getLoc());
+  } else if (auto createClosure = dyn_cast<CreateClosureOp>(&*call)) {
+    // Nested function-like op should retain scoped location of the callee.
+    scope = b.create<StageClosureOp>(region.getParentOp()->getLoc(),
+                                     createClosure.getType(), call.getLoc());
+  } else {
+    llvm::report_fatal_error("unknown call operation '" +
+                             call->getName().getStringRef() +
+                             "' in inlining pass -- please file a bug!");
+  }
+
+  Region &scopeBody = scope->getRegion(0);
+  bool returnAtEnd = isa<ReturnOp>(region.front().getTerminator());
+  if (takeBody) {
+    scopeBody.takeBody(region);
+    for (auto [value, arg] :
+         llvm::zip(call->getOperands(), scopeBody.getArguments()))
+      arg.replaceAllUsesWith(value);
+    scopeBody.front().eraseArguments(0, call->getNumOperands());
+  } else {
+    Block *block = b.createBlock(&scopeBody);
+    for (auto [value, arg] :
+         llvm::zip(call->getOperands(), region.getArguments()))
+      map.map(arg, value);
+    for (BlockArgument trailing :
+         region.getArguments().drop_front(call->getNumOperands()))
+      map.map(trailing,
+              block->addArgument(trailing.getType(), trailing.getLoc()));
+    for (Operation &op : region.getOps())
+      b.clone(op, map);
+  }
+
+  unsigned numReturns = 0;
+  scopeBody.walk<mlir::WalkOrder::PreOrder>([&](Operation *op) {
+    if (isa<ReturnOp>(op)) {
+      b.setInsertionPoint(op);
+      if (isa<CallOp>(&*call)) {
+        b.replaceOpWithNewOp<HLCF::BreakOp>(op, op->getOperands(), label);
+      } else if (isa<CreateClosureOp>(*&call)) {
+        // Just `return` is ok.
+      } else if (isa<LIT::AsyncCallOp>(&*call)) {
+        b.replaceOpWithNewOp<LIT::AsyncReturnOp>(op, op->getOperands());
+      } else {
+        llvm::report_fatal_error("unknown call operation '" +
+                                 call->getName().getStringRef() +
+                                 "' in inlining pass -- please file a bug!");
+      }
+
+      ++numReturns;
+      return WalkResult::skip();
+    }
+    if (isa<LIT::AsyncExecuteOp, StageClosureOp>(op))
+      return WalkResult::skip();
+    return WalkResult::advance();
+  });
+  b.replaceOp(call, scope->getResults());
+  assert(numReturns > 0);
+  return std::make_pair(scope, numReturns == 1 && returnAtEnd);
+}
+
+//===----------------------------------------------------------------------===//
+// foldTrivialLoop
+//===----------------------------------------------------------------------===//
+
+void KGEN::foldTrivialLoop(Operation *op) {
+  TimeTraceScope traceScope("foldTrivialLoop");
+
+  auto loop = dyn_cast<HLCF::LoopOp>(op);
+  if (!loop)
+    return;
+
+  mlir::IRRewriter b{OpBuilder(op)};
+
+  Block &body = loop.getBody().front();
+  Operation *term = body.getTerminator();
+  b.inlineBlockBefore(&body, loop);
+  b.replaceOp(loop, term->getOperands());
+  b.eraseOp(term);
+}
+
+//===----------------------------------------------------------------------===//
+// updateScopeDebugInfo
+//===----------------------------------------------------------------------===//
+
+/// Starting from an inlining scope, update debug information as appropriate and
+/// fold the scope if requested. Recurse on nested scopes.
+static void updateScopeDebugInfoFrom(Operation *scope, IntegerAttr tag,
+                                     StringAttr updateAttrName) {
+  // Unpack the bits.
+  auto value = static_cast<uint8_t>(tag.getInt());
+  auto singleExit = static_cast<bool>(value);
+  auto noDebug = static_cast<bool>(value >> 1);
+
+  // The scope operations contains the location of the call.
+  Region &body = scope->getRegion(0);
+  Location callLoc = scope->getLoc();
+
+  // If the scope represents an `always_inline_no_debug` function, just nuke all
+  // debug info and locations from here.
+  if (noDebug) {
+    body.walk<mlir::WalkOrder::PreOrder>([&](Operation *op) {
+      if (isa<DebugInfo::ValueOp>(op)) {
+        op->erase();
+        return WalkResult::skip();
+      }
+
+      op->setLoc(callLoc);
+      if (isa<HLCF::LoopOp, LIT::AsyncExecuteOp, StageClosureOp>(op)) {
+        auto tag = op->getAttrOfType<IntegerAttr>(updateAttrName);
+        if (tag) {
+          updateScopeDebugInfoFrom(op, tag, updateAttrName);
+          return WalkResult::skip();
+        }
+      }
+      return WalkResult::advance();
+    });
+  } else {
+    bool scopeIsNotSubprogram = !isa<DebugInfo::SubprogramScoped>(scope);
+    body.walk<mlir::WalkOrder::PreOrder>([&](Operation *op) {
+      // Only update locations if the scope is not function-like.
+      if (scopeIsNotSubprogram)
+        DebugInfo::updateInlinedLoc(op, callLoc);
+
+      if (isa<HLCF::LoopOp, LIT::AsyncExecuteOp, StageClosureOp>(op)) {
+        auto tag = op->getAttrOfType<IntegerAttr>(updateAttrName);
+        if (tag) {
+          updateScopeDebugInfoFrom(op, tag, updateAttrName);
+          return WalkResult::skip();
+        }
+      }
+      return WalkResult::advance();
+    });
+  }
+
+  // If this scope is a trivial control-flow scope, fold it away.
+  if (singleExit)
+    foldTrivialLoop(scope);
+}
+
+void KGEN::updateScopeDebugInfo(FuncOp func, StringAttr updateAttrName) {
+  TimeTraceScope updateScopeDebugInfo(
+      "updateScopeDebugInfo", [&func] { return func.getSymName().str(); });
+  func.getBody()->walk<mlir::WalkOrder::PreOrder>([&](Operation *op) {
+    if (!isa<HLCF::LoopOp, LIT::AsyncExecuteOp, StageClosureOp>(op))
+      return WalkResult::advance();
+    auto tag = op->getAttrOfType<IntegerAttr>(updateAttrName);
+    if (!tag)
+      return WalkResult::advance();
+    updateScopeDebugInfoFrom(op, tag, updateAttrName);
+    return WalkResult::skip();
+  });
+}
+
+//===----------------------------------------------------------------------===//
+// PerThreadPassManager
+//===----------------------------------------------------------------------===//
+
+/// This class manages a pass manager instance for each thread.
+PerThreadPassManagers::PerThreadPassManagers(
+    MLIRContext *ctx, function_ref<void(mlir::OpPassManager &)> buildFuncPasses)
+    : ctx(ctx), buildFuncPasses(buildFuncPasses) {
+  // Reserve the thread-local cache map so that it never resizes.
+  pms.reserve(ctx->isMultithreadingEnabled()
+                  ? ctx->getThreadPool().getThreadCount()
+                  : 1);
+}
+
+/// Get the pass manager for the current thread, initializing it if one does
+/// not exist.
+mlir::PassManager &PerThreadPassManagers::getPassManager() {
+  int64_t threadId = llvm::get_threadid();
+  {
+    llvm::sys::SmartScopedReader<true> lock(mutex);
+    if (auto it = pms.find(threadId); it != pms.end())
+      return *it->second;
+  }
+
+  // Emplace a new pass manager for this thread.
+  mutex.lock();
+  mlir::PassManager &pm =
+      *pms.try_emplace(threadId, std::make_unique<mlir::PassManager>(
+                                     ctx, FuncOp::getOperationName()))
+           .first->second;
+  mutex.unlock();
+
+  // Initialize the pass manager.
+  buildFuncPasses(pm);
+  pm.enableVerifier(false);
+  // Enable time tracing on the nested pass manager.
+  pm.enableTiming(std::make_unique<TimeProfilerTimingManager>());
+  return pm;
+}
