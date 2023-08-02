a9b0d793468c5ef0f8400596f35d070141fe15b4
Jeff Niu <jeff@modular.com>
2023-08-02 16:29:09 -0700

    [Kernels] Call `fprintf` directly (#18520)
    Stacked PRs:
     * __->__#18520
     * #18516
    
    ### [Kernels] Call `fprintf` directly
    
    This allows us to reduce dependency on KGENCompilerRT. Hopefully it will
    work on windows.
    
    Fixes [Internal link]
    
    MODULAR_ORIG_COMMIT_REV_ID: 999174f2677bfe9816b297f1e1983588df7bf76e

--- KGEN/lib/CompilerRT/System.cpp
+++ KGEN/lib/CompilerRT/System.cpp
@@ -22,19 +22,6 @@ KGEN_CompilerRT_CoreCount() {
 }
 
 //===----------------------------------------------------------------------===//
-// Printing
-//===----------------------------------------------------------------------===//
-
-COMPILERRT_EXPORT COMPILERRT_VISIBILITY_EXPORT int
-KGEN_CompilerRT_fprintf(FILE *stream, const char *format, ...) {
-  va_list args;
-  va_start(args, format);
-  int result = vfprintf(stream, format, args);
-  va_end(args);
-  return result;
-}
-
-//===----------------------------------------------------------------------===//
 // Arguments
 //===----------------------------------------------------------------------===//
 
@@ -86,8 +73,6 @@ COMPILERRT_VISIBILITY_EXPORT void KGEN_CompilerRT_SetArgV(int argc,
 
 void M::KGEN::registerSystem(
     std::vector<std::pair<llvm::StringLiteral, void *>> &funcs) {
-  funcs.push_back(
-      {"KGEN_CompilerRT_fprintf", (void *)&KGEN_CompilerRT_fprintf});
   funcs.push_back(
       {"KGEN_CompilerRT_CoreCount", (void *)&KGEN_CompilerRT_CoreCount});
   funcs.push_back(
