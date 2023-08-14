d9410b107c6282ef81ed5a5be02cf002511ed188
Jeff Niu <jeff@modular.com>
2023-08-14 05:35:13 -0700

    [mojo-lang] Fix a strange issue when interpreting functions (#19170)
    It's coming. The closed beta bugs are coming! This fixes an issue
    reported by Lukas.
    
    Somehow the `recursivelyResolveFully` function is trying to resolve an
    unparsed argument, causing the decl resolver to fail. The recursive
    resolution isn't needed anymore anyways since we don't have lazy
    resolution in function bodies anymore. Just delete it.
    
    Fixes [Internal link]
    
    MODULAR_ORIG_COMMIT_REV_ID: 80f30a420b10e458f0b16f443565263e0b3d1122

--- KGEN/lib/MojoParser/DeclResolver.h
+++ KGEN/lib/MojoParser/DeclResolver.h
@@ -135,9 +135,6 @@ public:
     return resolve(decl, DeclResolvedness::fully, loc);
   }
 
-  /// Fully resolve the declaration and everything in it.
-  LogicalResult recursivelyResolveFully(ASTDecl &decl, llvm::SMLoc loc);
-
   /// Given the symbol for a lit type declaration, return the ASTDecl that
   /// corresponds to it.  This doesn't allow null symbols, so it always
   /// succeeds.
