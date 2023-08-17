1c449b4614a49c198d45d3c356704e1fdeb32ad1
Srini511 <srini@modular.com>
2023-08-17 10:06:29 -0700

    [LLCL] Fix MODULAR_PARANOID build. (#19501)
    This PR fixes `MODULAR_PARANOID` build that broke with #18963.
    
    MODULAR_ORIG_COMMIT_REV_ID: 54cbf6f4be2f0974eba9646bbd77fdaeb8ff6c15

--- LLCL/lib/Runtime/ThreadPoolWorkQueue.cpp
+++ LLCL/lib/Runtime/ThreadPoolWorkQueue.cpp
@@ -940,7 +940,7 @@ void ThreadPoolWorkQueue::await(ArrayRef<AnyAsyncValueRef> values) {
     // we signal the semaphore for this worker to make sure to wake it up if it
     // fell asleep.
     for (auto &value : values) {
-      value.andThenSync([&numRemaining, awaitingWorker]() {
+      value.andThenSync([&numRemaining, awaitingWorker, this]() {
         // Decrement the count of async values that we're waiting on.
         // TODO: This can probably use more relaxed memory consistency!
         if (numRemaining.fetch_sub(1, std::memory_order_seq_cst) != 1)
