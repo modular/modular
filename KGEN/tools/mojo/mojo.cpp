cd356a96395268514888802281f2637e09ac536d
Brian Gesiak <brian@modular.com>
2023-08-23 15:20:45 -0400

    [mojo] Include truncated SHA in `mojo --version` (#19962)
    Fixes #19935.
    
    MODULAR_ORIG_COMMIT_REV_ID: 96372b5b26ebdc41baf024132c62585de608f253

--- KGEN/tools/mojo/mojo.cpp
+++ KGEN/tools/mojo/mojo.cpp
@@ -93,8 +93,9 @@ int main(int argc, char **argv) {
   case options::OPT_version: {
     // Print the version and exit.
     ModularVersion version = getModularVersion();
-    llvm::outs() << "mojo " << version.major << '.' << version.minor << '.'
-                 << version.patch << version.label << "\n";
+    llvm::outs() << llvm::formatv("mojo {0}.{1}.{2}{3} ({4})\n", version.major,
+                                  version.minor, version.patch, version.label,
+                                  version.revision);
     return 0;
   }
   case options::OPT_help:
