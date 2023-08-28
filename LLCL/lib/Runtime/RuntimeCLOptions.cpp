7e99f2b859b6e576d4d5e1edcf079314e4636373
Srini511 <srini@modular.com>
2023-08-28 10:27:53 -0700

    [LLCL] Remove mainWillDonate implementation 2 (#19538)
    PR removed mainWillDonate flag from modular code base and simplifies few
    things in the ThreadPool.
    1) Removes notion of main thread which incarnates as worker0 as needed. With this PR, worker0 executes `runItemsImpl` loop like any other thread.
    2) Removes all special treatment given to worker0 in await() and
    runItems.
    3) Any thread other than workers will sleep on `await` and will
    not take part in task execution.
    4) Moves the `checkKnownCaller()` to the last entry point.
    
    MODULAR_ORIG_COMMIT_REV_ID: 260c9ccadef7f71e5089d9f384321edd03736b25

--- LLCL/lib/Runtime/RuntimeCLOptions.cpp
+++ LLCL/lib/Runtime/RuntimeCLOptions.cpp
@@ -44,8 +44,7 @@ RuntimeWorkQueueCLOptions::createRuntime(StringRef profileName) const {
   case WorkQueueType::kThreadPool:
     // Let the ThreadPoolWorkQueue decide on an appropriate number of threads
     // if it is zero. It may be more sophisticated than getNumThreads().
-    workQueue = createThreadPoolWorkQueue(numThreads,
-                                          /*mainWillDonate=*/true
+    workQueue = createThreadPoolWorkQueue(numThreads
 #if MODULAR_PARANOID
                                           ,
                                           paranoid
