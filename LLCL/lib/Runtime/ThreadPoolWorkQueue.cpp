modular/modular

commit 94d3a6fbaaf4904778390c670e1b79adfa559fdc

[triton] Add thread names to enable debugging (#18147)
This PR adds a `poolName` parameter to the thread pool queues which allow the creator of the runtime pool to specify a pool name which will be used to name the threads created in the pool.

The runtime config initialization checks the modular.cfg file for a thread identifier to use, if specified, under the section 'triton_modular_backend' and key 'context_name'. If found, use it as the context name, otherwise use 'LLCL Thread ' as the prefix for threads in the thread pool.

MODULAR_ORIG_COMMIT_REV_ID: b6abbb84e8b5d2948273a02e40aea53ceaeb04f1

LLCL/lib/Runtime/ThreadPoolWorkQueue.cpp
@@ -250,6 +250,8 @@ struct WorkQueueThread {
   /// Uses stack.
   SmallVector<ResourceUse> useStack;
 #endif
+  // The thread identifier prefix used to name the threads
+  std::string_view poolName;
 
   /// Create a WorkQueueThread representing the worker with workerID. If
   /// necessary, the underlying worker thread will be created and it will
@@ -258,10 +260,11 @@ struct WorkQueueThread {
                   LockFreeRingBuffer<WorkItem> &taskList,
                   std::mutex &overflowMutex,
                   SmallVectorImpl<WorkItem> &overflowTaskList, size_t workerID,
-                  size_t cpuID)
+                  size_t cpuID, std::string_view poolName)
       : sharedState(sharedState), affinityTaskList(kTaskListSlotsPerThread),
         taskList(taskList), overflowMutex(overflowMutex),
-        overflowTaskList(overflowTaskList), workerID(workerID), cpuID(cpuID) {
+        overflowTaskList(overflowTaskList), workerID(workerID), cpuID(cpuID),
+        poolName(poolName) {
     if (sharedState.mainWillDonate && workerID == 0) {
       // We can leave workerIDInTLS as zero.
       // Remember the caller is to be our 'main' thread, and will call
@@ -376,7 +379,7 @@ void WorkQueueThread::runOnThread() {
 
   // On systems that support it, give the thread a symbolic name that will show
   // up in profilers and debuggers.
-  llvm::set_thread_name("LLCL Thread " + llvm::Twine(workerID));
+  llvm::set_thread_name(poolName + llvm::Twine(workerID));
 
   // On systems that support it, give the thread affinity for one CPU.
   LLCL::setThreadAffinity(cpuID);
@@ -573,7 +576,7 @@ public:
   /// destructor.
   ThreadPoolWorkQueue(const std::vector<size_t> &cpuIDs,
                       size_t taskListCapacity, bool mainWillDonate,
-                      bool paranoid);
+                      bool paranoid, std::string_view poolName);
 
   ~ThreadPoolWorkQueue() override;
 
@@ -664,14 +667,16 @@ private:
   /// Only used when the taskList is full.
   std::mutex overflowMutex; // protects overflowTaskList
   SmallVector<WorkItem> overflowTaskList;
+  std::string poolName;
 };
 } // namespace
 
 ThreadPoolWorkQueue::ThreadPoolWorkQueue(const std::vector<size_t> &cpuIDs,
                                          size_t taskListCapacity,
-                                         bool mainWillDonate, bool paranoid)
+                                         bool mainWillDonate, bool paranoid,
+                                         std::string_view poolName)
     : numWorkers(cpuIDs.size()), sharedState(mainWillDonate, paranoid),
-      taskList(taskListCapacity) {
+      taskList(taskListCapacity), poolName(poolName) {
   assert(numWorkers <= kMaxWorkers && "too many workers for bitvec width");
   // Initialize each thread with its required state.
   // Note that we're constructing the array manually since WorkQueueThreads have
@@ -682,7 +687,7 @@ ThreadPoolWorkQueue::ThreadPoolWorkQueue(const std::vector<size_t> &cpuIDs,
   for (size_t workerID = 0; workerID < numWorkers; ++workerID)
     new (workers + workerID)
         WorkQueueThread(sharedState, taskList, overflowMutex, overflowTaskList,
-                        workerID, cpuIDs[workerID]);
+                        workerID, cpuIDs[workerID], poolName);
 }
 
 ThreadPoolWorkQueue::~ThreadPoolWorkQueue() {
@@ -978,7 +983,7 @@ bool ThreadPoolWorkQueue::callerIsForeign() const {
 
 std::unique_ptr<WorkQueue>
 M::LLCL::createThreadPoolWorkQueue(size_t numThreads, bool mainWillDonate,
-                                   bool paranoid) {
+                                   bool paranoid, std::string_view poolName) {
 #if MODULAR_PARANOID
 #ifdef NDEBUG
   llvm::dbgs() << "CAUTION: Asked for a MODULAR_PARANOID build with NDEBUG. "
@@ -1022,6 +1027,6 @@ M::LLCL::createThreadPoolWorkQueue(size_t numThreads, bool mainWillDonate,
              << "createThreadPoolWorkQueue: Task list has capacity of at least "
              << taskListCapacity << " slots.\n");
 
-  return std::make_unique<ThreadPoolWorkQueue>(cpuIDs, taskListCapacity,
-                                               mainWillDonate, paranoid);
+  return std::make_unique<ThreadPoolWorkQueue>(
+      cpuIDs, taskListCapacity, mainWillDonate, paranoid, poolName);
 }

LLCL/include/LLCL/Runtime/WorkQueue.h
@@ -256,9 +256,10 @@ std::unique_ptr<WorkQueue> createSingleThreadWorkQueue();
 ///
 /// If in a MODULAR_PARANOID build, the paranoid flag can be used to inject
 /// random delays into work items to attempt to tickle race conditions.
-std::unique_ptr<WorkQueue> createThreadPoolWorkQueue(size_t numThreads = 0,
-                                                     bool mainWillDonate = true,
-                                                     bool paranoid = false);
+std::unique_ptr<WorkQueue>
+createThreadPoolWorkQueue(size_t numThreads = 0, bool mainWillDonate = true,
+                          bool paranoid = false,
+                          std::string_view poolName = "LLCL Thread ");
 
 } // namespace M::LLCL
 

commit a56ec3d665ced70dd7bb6c95d992f4427874b011

[LLCL] Fix poolName use-after-free in ThreadPoolWorkQueue (#21006)
The following sequence of events resulted in a use-after-free:

1. "Pool name" string creation
2. `createThreadPoolWorkQueue` called
3. `ThreadPoolWorkQueue::ThreadPoolWorkQueue` copies `poolName`
`std::string_view` parameter into a `std::string` in `this`
4. `ThreadPoolWorkQueue::ThreadPoolWorkQueue` creates `WorkQueueThread`s
storing _originally passed_ `poolName` `std::string_view` (NOT a new
`std::string_view` pointing at the copied `std::string`)
5. `ThreadPoolWorkQueue::ThreadPoolWorkQueue` and
`createThreadPoolWorkQueue` finish and return control to caller
6. "Pool name" string destroyed
7. Work queue thread `WorkQueueThread::runOnThread` procedure reaches
`llvm::set_thread_name` call and uses `std::string_view` still referring
to the destroyed string

The bug was originally introduced in #18147, but the bug was not visible
until #20551 triggered just the right conditions, causing ASAN failures
on `main` when running the modbench tests.

To fix this, we change step 4 to pass in a new `std::string_view`
pointing at the copied string into the `WorkQueueThread` constructor,
rather than the original `std::string_view` that has too short a
lifetime.

MODULAR_ORIG_COMMIT_REV_ID: e6c3353cfc022bb9ab055fdce20c0f935ab7c2f9

LLCL/lib/Runtime/ThreadPoolWorkQueue.cpp
@@ -757,7 +757,7 @@ ThreadPoolWorkQueue::ThreadPoolWorkQueue(const std::vector<size_t> &cpuIDs,
   for (size_t workerID = 0; workerID < numWorkers; ++workerID)
     new (workers + workerID)
         WorkQueueThread(sharedState, taskList, overflowMutex, overflowTaskList,
-                        workerID, cpuIDs[workerID], poolName);
+                        workerID, cpuIDs[workerID], this->poolName);
 }
 
 ThreadPoolWorkQueue::~ThreadPoolWorkQueue() {