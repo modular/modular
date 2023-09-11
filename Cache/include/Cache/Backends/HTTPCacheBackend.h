c9891a66afd5fee775d5c4bd7a14e5a0d5e801cf
Aman LaChapelle <aman.lachapelle@gmail.com>
2023-09-11 13:45:25 -0700

    [modular-tool][Support][Cache] Move HTTPClient to Support. (#21036)
    This patch moves the HTTPClient code to the Support module - and in the process, to avoid a layering violation, moves the HTTPCacheBackend to the Cache module.
    
    MODULAR_ORIG_COMMIT_REV_ID: 50fbc2090a034304eba727887ad8903f7ef71788

---
+++ Cache/include/Cache/Backends/HTTPCacheBackend.h
@@ -0,0 +1,77 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef CACHE_HTTPCACHEBACKEND_H
+#define CACHE_HTTPCACHEBACKEND_H
+
+#include "Cache/BlobCache.h"
+#include "Support/HTTP/HTTPClient.h"
+
+/// Forward-declaration for the LLCL::Runtime.
+namespace M::LLCL {
+class Runtime;
+}
+
+namespace M::Cache {
+/// This class provides a wrapper around libcurl that conforms to the
+/// BlobCacheBackend interface. This enables the CAS to interact with an HTTP
+/// upstream.
+class HTTPCacheBackend : public BlobCacheBackend {
+public:
+  HTTPCacheBackend(HTTPContextRef c, std::string url, LLCL::Runtime &runtime)
+      : BlobCacheBackend(runtime), ctx(std::move(c)), url(std::move(url)) {}
+
+  /// Insert a binary blob with a PUT request.
+  // TODO: Currently unsupported
+  ErrorOrSuccess insertImpl(StringRef keyHash, BufferRef obj) override;
+
+  /// Check if we have the object we're looking for. Since really the only way
+  /// to check if we have the thing is to fetch it, we just get it and trust our
+  /// in-memory request cache.
+  ErrorOr<bool> containsImpl(StringRef keyHash) const override;
+
+  /// Find the object at `keyHash`. This essentially produces a GET request to
+  /// <url>/<urlsafe-b64-key-hash>, which is expected to return the bytes of the
+  /// object directly.
+  ErrorOr<std::optional<BufferRef>>
+  findImpl(StringRef keyHash,
+           std::optional<WriteableBufferRef> buf) const override;
+
+  /// We do not support clear for this backend impl.
+  ErrorOrSuccess clearImpl() override;
+
+private:
+  /// The HTTP context ref for any clients we spin up for a find operation.
+  HTTPContextRef ctx;
+  /// The base URL for requests.
+  std::string url;
+
+  /// Store a buffer in our local in-memory cache. We can't always rely on the
+  /// backends ahead of us for this, because our `containsImpl` just runs
+  /// `findImpl` manually. If we have a find first though, backends before us
+  /// (notably the in-memory backend) will perform this same function.
+  void cacheBuffer(StringRef keyHash, BufferRef buf);
+
+  /// Check if we have a buffer in our local in-memory cache. If we do, return
+  /// it.
+  std::optional<BufferRef> findBuffer(StringRef keyHash);
+
+  /// Local cache we can use to store buffers we've fetched from upstream. This
+  /// is important because of the way we had to implement `containsImpl` - HTTP
+  /// doesn't really provide a way to check if a resource exists without
+  /// just...getting the resource.
+  Shared<llvm::StringMap<BufferRef>> localCache;
+};
+
+using HTTPCacheBackendRef = RCRef<HTTPCacheBackend>;
+
+/// Get the HTTP CAS backend. The backend will use `url` as the base, and append
+/// the url-safe base-64 encoded key hash as the resource path.
+HTTPCacheBackendRef getHTTPCacheBackend(HTTPContextRef ctx, std::string url,
+                                        LLCL::Runtime &runtime);
+} // namespace M::Cache
+
+#endif // CACHE_HTTPCACHEBACKEND_H
