b903f2307c879beeac6dabcd7a529c97e32f951c
Mark Shields <110413041+mbs-modular@users.noreply.github.com>
2023-09-13 18:18:31 -0700

    [GML] Move M::Tensor to GML::Tensor and prepare for CUDA::Tensor (#21277)
    Even though we lower to buffer-semantic form, and already have
    `!cuda.buffer` and `CUDABuffer`, we'll also need a `!cuda.tensor` and
    corresponding runtime representation. Prepare for this by:
    - Move M::Tensor into GML::Tensor to make clear(er) it is CPU only (just
    like GML::BufferRef).
     - Derive from M::TensorBase template.
    - Remove cruft, mostly by exposing TensorBase::getBufferRef() instead of
    manually exposing the GML::BufferRef methods.
    
    At some point we'll need something like M::Tensor or M::GenericTensor to
    be an abstract 'some tensor on some device' representation, but not
    today.
    
    We sure do have a lot of copied boilerplate :-(
    
    MODULAR_ORIG_COMMIT_REV_ID: efb0e9adaf8a2690cc841ac1316d1e1fc19fa67c

---
+++ Support/include/Support/ML/TensorBase.h
@@ -0,0 +1,59 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+//
+// TensorBase combines a TensorSpec with an templated 'Buffer'.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef SUPPORT_ML_TENSORBASE_H
+#define SUPPORT_ML_TENSORBASE_H
+
+#include "Support/ML/TensorSpec.h"
+
+namespace M {
+
+/// A TensorBase is a pair of a 'BufferRefType' and TensorSpec.
+///
+/// BufferRefType must be moveable, and implement:
+///    // Returns byte size of buffer held by buffer reference.
+///    size_t getSize() const
+template <typename BufferRefType>
+class TensorBase {
+public:
+  TensorBase(BufferRefType &&bufferRef, TensorSpec &&spec)
+      : bufferRef(std::move(bufferRef)), spec(std::move(spec)) {
+    assert(this->spec.getSizeInBytes() == this->bufferRef.getSize() &&
+           "tensor buffer and spec disagree on byte size");
+  }
+
+  /// Returns the underlying buffer reference.
+  const BufferRefType &getBufferRef() const { return bufferRef; }
+  BufferRefType &getBufferRef() { return bufferRef; }
+
+  /// Returns the tensor spec/shape/element type.
+  const TensorSpec &getSpec() const { return spec; }
+  const TensorShape &getShape() const { return spec; }
+  DType getEltType() const { return spec.getEltType(); }
+
+  /// Returns the size of the underlying buffer in bytes.
+  size_t getSizeInBytes() const { return bufferRef.getSize(); }
+
+  /// Tensors cannot be implicitly copied.
+  void operator=(const TensorBase<BufferRefType> &) = delete;
+  TensorBase(const TensorBase<BufferRefType> &rhs) = delete;
+
+  /// Tensors can be moved.
+  TensorBase(TensorBase<BufferRefType> &&rhs) = default;
+  TensorBase &operator=(TensorBase<BufferRefType> &&rhs) = default;
+
+protected:
+  BufferRefType bufferRef;
+  TensorSpec spec;
+};
+
+} // namespace M
+
+#endif // GENERICML_SUPPORT_TENSORBASE_H
