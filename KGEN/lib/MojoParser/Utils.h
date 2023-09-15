30b2079cf864ed1ef6fdf7d76203253b2c540ddc
Laszlo Kindrat <laszlo@modular.ai>
2023-09-15 14:48:32 -0400

    [Mojo][NFC] Small reorg in call emission related code (#21304)
    Move `canZeroCostConvertSignature` to `Utils.{h,cpp}` and move around some code to make it more organized.
    
    MODULAR_ORIG_COMMIT_REV_ID: fd6a3e175479cdb0a26e82b260d6e123a9977573

--- KGEN/lib/MojoParser/Utils.h
+++ KGEN/lib/MojoParser/Utils.h
@@ -22,4 +22,8 @@ namespace M::KGEN::LIT {
 /// If the argument at the given index is of pack type, returns that type.
 /// therwise, returns null.
 POP::PackType getIfPackType(SignatureType sig, size_t index);
+
+/// Returns whether the two signatures match, i.e. if they only differ in
+/// argument names.
+bool canZeroCostConvertSignature(SignatureType from, SignatureType to);
 } // namespace M::KGEN::LIT
