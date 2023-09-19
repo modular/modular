d570aaedb2b9143bcc776f0a2c93c725703d50a3
Jeff Niu <jeff@modular.com>
2023-09-19 10:54:04 -0700

    [KGEN] Fix SROA incorrectly decomposing stack allocations (#21582)
    SROA is decomposing "stacks" in the form of `%0 = stack_allocation N x
    T`
    where `N > 1`. It does so even if there are uses of `%0` that are
    `pop.offset` with constant indices, but it does not check the users of
    the offset. This is incorrect, because `pop.offset` returns an alias of
    the original pointer and is not an element access -- its users need to
    be checked to ensure the pointer does not escape.
    
    Fixes #21576
    
    MODULAR_ORIG_COMMIT_REV_ID: 2d4eeb5c1f508060d911dfd43dfafe38901fcd3d

--- KGEN/lib/Transforms/SROA.cpp
+++ KGEN/lib/Transforms/SROA.cpp
@@ -369,6 +369,20 @@ struct ReplaceStack : public Replacer<ReplaceStack, POP::StackAllocationOp> {
         if (static_cast<int64_t>(index.getLimitedValue()) >=
             cast<IntegerAttr>(alloc.getCount()).getInt())
           return false;
+
+        // Offsets are aliases to the original stack allocation, not element
+        // accesses. Require that all uses of the alias access the element.
+        // We don't have to worry about handling constant offsets of constant
+        // offsets, because they are canonicalized down to just one offset.
+        for (Operation *user : offset->getUsers()) {
+          if (!isa<POP::StoreOp, POP::LoadOp, POP::ArrayGEPOp,
+                   POP::StructGEPOp>(user))
+            return false;
+          // If the user is the argument of the store, then we cannot elide.
+          if (auto store = dyn_cast<POP::StoreOp>(user))
+            if (store.getArg() == offset)
+              return false;
+        }
       }
     }
 
