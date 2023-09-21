9387dedd796221537bc30423912e8337f829a8f5
abdul dakkak <adakkak@modular.com>
2023-09-20 19:25:09 -0700

    [Stdlib] Properly free globals on program termination (#21649)
    This makes sure that we free any globals that we allocate in Mojo upon
    exit from main. Right now this is concentrating on the Python global,
    but there will be other globals that will join this such as the random
    number generator, global runtime, and any other singletons we want in
    Mojo.
    
    MODULAR_ORIG_COMMIT_REV_ID: 20f816f972b239f26f34505b1d6f68bc108c21f1

---
+++ KGEN/lib/CompilerRT/Globals.cpp
@@ -0,0 +1,64 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#include "KGEN/CompilerRT/Registration.h"
+#include "llvm/ADT/DenseMap.h"
+#include "llvm/ADT/StringRef.h"
+
+namespace {
+struct GlobalEntry {
+  void *value;
+  void (*destroyFn)(void *);
+
+  GlobalEntry(void *value, void (*destroyFn)(void *))
+      : value(value), destroyFn(destroyFn) {}
+
+  void destroy() {
+    if (destroyFn && value)
+      destroyFn(value);
+  }
+};
+} // namespace
+
+static llvm::SmallDenseMap<llvm::StringRef, GlobalEntry> getGlobalTable() {
+  static llvm::SmallDenseMap<llvm::StringRef, GlobalEntry> table{};
+  return table;
+}
+
+COMPILERRT_EXPORT COMPILERRT_VISIBILITY_EXPORT void *
+KGEN_CompilerRT_GetGlobalOr(llvm::StringRef name, void *(*initFn)(),
+                            void (*destroyFn)(void *)) {
+  auto globalTable = getGlobalTable();
+
+  auto it = globalTable.find(name);
+  if (it != globalTable.end())
+    return it->second.value;
+
+  GlobalEntry entry(initFn(), nullptr);
+  globalTable.insert({name, entry});
+
+  return entry.value;
+}
+
+COMPILERRT_EXPORT COMPILERRT_VISIBILITY_EXPORT void
+KGEN_CompilerRT_DestroyGlobals() {
+  auto globalTable = getGlobalTable();
+  for (auto entry : globalTable)
+    entry.second.destroy();
+  globalTable.clear();
+}
+
+//===----------------------------------------------------------------------===//
+// CompilerRT Registration
+//===----------------------------------------------------------------------===//
+
+void M::KGEN::registerGlobals(
+    std::vector<std::pair<llvm::StringLiteral, void *>> &funcs) {
+  funcs.push_back(
+      {"KGEN_CompilerRT_GetGlobalOr", (void *)&KGEN_CompilerRT_GetGlobalOr});
+  funcs.push_back({"KGEN_CompilerRT_DestroyGlobals",
+                   (void *)&KGEN_CompilerRT_DestroyGlobals});
+}
