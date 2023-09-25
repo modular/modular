da9ab1e64c849520023f041977d532faa84fd2ff
abdul dakkak <adakkak@modular.com>
2023-09-25 09:44:09 -0700

    [Stdlib] Add initial file I/O support to Mojo (#21983)
    This adds some basic file I/O to mojo. This does not implement the full
    python API (namely it does not contain readline support), but should be
    enough to work with.
    
    There are a few things that we can improve here:
    
    - We really do not need compilerrt to do any of this, but then I did not
    want to worry about linux/windows/macos differences in file handling
    - The context manager in mojo seems to be weak. For exmaple I should be
    able to write
    
    ```mojo
        with open(TEMP_FILE, "w") as f:
            f.write("The quick brown fox jumps over the lazy dog")
    ```
    
    but that currently results in the error
    
    ```
    /Users/abduld/code/modular/Kernels/test/test_file.mojo:33:14: error: use of uninitialized value 'anonymous*'
        with open(TEMP_FILE, "w") as f:
                 ^
    /Users/abduld/code/modular/Kernels/test/test_file.mojo:33:14: note: 'anonymous*' declared here
    /Users/abduld/code/modular/Kernels/test/test_file.mojo:33:34: warning: 'f' was declared as a 'var' but never mutated, consider switching to a 'let'
        with open(TEMP_FILE, "w") as f:
                                     ^
    ```
    
    due to some lifetime tracking issues
    
    MODULAR_ORIG_COMMIT_REV_ID: 707045d13a0a428c28932f7f65847f81928d5b19

---
+++ KGEN/lib/CompilerRT/IO.cpp
@@ -0,0 +1,191 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#include "KGEN/CompilerRT/Registration.h"
+#include "Support/AlignedAlloc.h"
+#include "llvm/Support/FileSystem.h"
+#include <filesystem>
+
+#ifdef _MSC_VER
+#include <io.h>
+#else
+#include <unistd.h>
+#endif
+
+using namespace M;
+
+static llvm::StringRef copyString(llvm::StringRef str) {
+  if (str.empty())
+    return {};
+  void *ptr = alignedAlloc(kPreferredMemoryAlignment, str.size());
+  memcpy(ptr, str.data(), str.size());
+  return llvm::StringRef(reinterpret_cast<const char *>(ptr), str.size());
+}
+
+namespace {
+struct FileHandle {
+
+  static FileHandle *get(llvm::StringRef path, llvm::StringRef mode,
+                         llvm::StringRef *errMsg) {
+    std::filesystem::path fsPath(path.str());
+    llvm::sys::fs::FileAccess fileAccess = getFileAccess(mode);
+    if (fileAccess & llvm::sys::fs::FileAccess::FA_Write) {
+      std::error_code err;
+      std::filesystem::create_directories(fsPath.parent_path(), err);
+      if (err) {
+        *errMsg = copyString((llvm::Twine("unable to create directories '") +
+                              fsPath.parent_path().string() + "' for write")
+                                 .str());
+        return nullptr;
+      }
+    } else if (fileAccess & llvm::sys::fs::FileAccess::FA_Read &&
+               !std::filesystem::exists(fsPath)) {
+      *errMsg = copyString(
+          (llvm::Twine("file path '") + path + "' not found for read").str());
+      return nullptr;
+    }
+
+    int handle;
+    std::error_code err = llvm::sys::fs::openFile(
+        path, handle, llvm::sys::fs::CreationDisposition::CD_OpenAlways,
+        fileAccess, llvm::sys::fs::OpenFlags::OF_None);
+
+    if (err) {
+      *errMsg = copyString(err.message());
+      return nullptr;
+    }
+
+    return new FileHandle(handle);
+  }
+
+  uint64_t size(llvm::StringRef *errMsg) {
+    llvm::sys::fs::file_status status;
+    if (std::error_code err = llvm::sys::fs::status(handle, status)) {
+      *errMsg = copyString(err.message());
+      return 0;
+    }
+
+    return status.getSize();
+  }
+
+  llvm::StringRef read(llvm::StringRef *errMsg) {
+    llvm::SmallVector<char> buf;
+    llvm::Error err = llvm::sys::fs::readNativeFileToEOF(
+        llvm::sys::fs::convertFDToNativeFile(handle), buf);
+    if (err) {
+      *errMsg = copyString(llvm::toString(std::move(err)));
+      return llvm::StringRef(nullptr, 0);
+    }
+
+    return copyString(llvm::StringRef(buf.data(), buf.size()));
+  }
+
+  uint64_t seek(uint64_t offset, llvm::StringRef *errMsg) {
+#ifdef _WIN32
+    return _lseeki64(handle, offset, SEEK_SET);
+#else
+    return lseek(handle, offset, SEEK_SET);
+#endif
+  }
+
+  void write(llvm::StringRef buf, llvm::StringRef *errMsg) {
+    llvm::raw_fd_ostream os(llvm::sys::fs::convertFDToNativeFile(handle),
+                            /*shouldClose=*/false, /*unbuffered=*/true);
+    os << buf;
+  }
+
+  void close(llvm::StringRef *errMsg) {
+    llvm::sys::fs::file_t file = llvm::sys::fs::convertFDToNativeFile(handle);
+    if (std::error_code err = llvm::sys::fs::closeFile(file))
+      *errMsg = copyString(err.message());
+  }
+
+private:
+  FileHandle(int handle) : handle(handle) {}
+  FileHandle(const FileHandle &other) = delete;
+  void operator=(const FileHandle &other) = delete;
+  ~FileHandle() = delete;
+
+  static llvm::sys::fs::FileAccess getFileAccess(llvm::StringRef mode) {
+    llvm::sys::fs::FileAccess res = (llvm::sys::fs::FileAccess)0;
+
+    if (mode.contains("r"))
+      res |= llvm::sys::fs::FileAccess::FA_Read;
+
+    if (mode.contains("w"))
+      res |= llvm::sys::fs::FileAccess::FA_Write;
+
+    return res;
+  }
+
+  int handle = 0;
+};
+
+struct FileHandleWrapper {
+  void *ptr;
+};
+
+} // namespace
+
+static FileHandle *unwrap(FileHandleWrapper ref) {
+  return reinterpret_cast<FileHandle *>(ref.ptr);
+}
+
+static FileHandleWrapper wrap(FileHandle *ptr) {
+  return FileHandleWrapper{ptr};
+}
+
+COMPILERRT_EXPORT COMPILERRT_VISIBILITY_EXPORT FileHandleWrapper
+KGEN_CompilerRT_IO_FileOpen(llvm::StringRef path, llvm::StringRef mode,
+                            llvm::StringRef *errMsg) {
+  return wrap(FileHandle::get(path, mode, errMsg));
+}
+
+COMPILERRT_EXPORT COMPILERRT_VISIBILITY_EXPORT void
+KGEN_CompilerRT_IO_FileClose(FileHandleWrapper file, llvm::StringRef *errMsg) {
+  unwrap(file)->close(errMsg);
+}
+
+COMPILERRT_EXPORT COMPILERRT_VISIBILITY_EXPORT uint64_t
+KGEN_CompilerRT_IO_FileSize(FileHandleWrapper file, llvm::StringRef *errMsg) {
+  return unwrap(file)->size(errMsg);
+}
+
+COMPILERRT_EXPORT COMPILERRT_VISIBILITY_EXPORT uint64_t
+KGEN_CompilerRT_IO_FileSeek(FileHandleWrapper file, uint64_t offset,
+                            llvm::StringRef *errMsg) {
+  return unwrap(file)->seek(offset, errMsg);
+}
+
+COMPILERRT_EXPORT COMPILERRT_VISIBILITY_EXPORT const char *
+KGEN_CompilerRT_IO_FileRead(FileHandleWrapper file, uint64_t *size,
+                            llvm::StringRef *errMsg) {
+  llvm::StringRef str = unwrap(file)->read(errMsg);
+  *size = str.size();
+  return str.data();
+}
+
+COMPILERRT_EXPORT COMPILERRT_VISIBILITY_EXPORT void
+KGEN_CompilerRT_IO_FileWrite(FileHandleWrapper file, const char *data,
+                             uint64_t size, llvm::StringRef *errMsg) {
+  unwrap(file)->write(llvm::StringRef(data, size), errMsg);
+}
+
+void M::KGEN::registerIO(
+    std::vector<std::pair<llvm::StringLiteral, void *>> &funcs) {
+  funcs.push_back(
+      {"KGEN_CompilerRT_IO_FileOpen", (void *)&KGEN_CompilerRT_IO_FileOpen});
+  funcs.push_back(
+      {"KGEN_CompilerRT_IO_FileClose", (void *)&KGEN_CompilerRT_IO_FileClose});
+  funcs.push_back(
+      {"KGEN_CompilerRT_IO_FileSize", (void *)&KGEN_CompilerRT_IO_FileSize});
+  funcs.push_back(
+      {"KGEN_CompilerRT_IO_FileSeek", (void *)&KGEN_CompilerRT_IO_FileSeek});
+  funcs.push_back(
+      {"KGEN_CompilerRT_IO_FileRead", (void *)&KGEN_CompilerRT_IO_FileRead});
+  funcs.push_back(
+      {"KGEN_CompilerRT_IO_FileWrite", (void *)&KGEN_CompilerRT_IO_FileWrite});
+}
