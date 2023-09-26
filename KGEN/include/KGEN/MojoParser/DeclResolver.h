aa567c9db6d9ccff5da2443fd1c398683ed859f4
Chris Lattner <lattner@users.noreply.github.com>
2023-09-26 16:52:53 -0700

    [mojo-lang] Expand DeclIRValue to hold XLValue. NFC. (#22133)
    This adds boilerplate for DeclIRValue to support this type, but it
    isn't used yet, this is just split out of a bigger patch.
    
    MODULAR_ORIG_COMMIT_REV_ID: 107622f64b85f3f2814fbe7b1b3abe57b9e448db

--- KGEN/include/KGEN/MojoParser/DeclResolver.h
+++ KGEN/include/KGEN/MojoParser/DeclResolver.h
@@ -48,7 +48,7 @@ struct ParsedArgument;
 /// as operations.  It stores RValues for parameters and SSA values as an
 /// RValue.
 using DeclIRValue = PointerUnion<Operation *, PValue, SRValue, MRValue, SBValue,
-                                 MBValue, MLValue>;
+                                 MBValue, MLValue, XLValue>;
 
 class DeclResolver : public SharedStateUser {
 public:
