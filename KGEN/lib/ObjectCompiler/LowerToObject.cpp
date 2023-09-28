35d355a8b8ae0d9da2fc60b1096a2678c7906424
Brian Gesiak <brian@modular.com>
2023-09-28 15:24:14 -0400

    [Support] Add `TargetInfoAttr::relocation_model` (#22218)
    Add a relocation model to the `TargetInfoAttr` attribute. This
    additional attribute ensures that two targets that are otherwise
    identical, but one with relocation model static compared to another with
    relocation model PIC, do not compare equal.
    
    This is important when determining whether an existing pre-built
    `.mojopkg` can be used for a target. If the package is built for the
    same architecture, but a different relocation model, it should not be
    used.
    
    Co-authored-by: River Riddle riddleriver@gmail.com
    MODULAR_ORIG_COMMIT_REV_ID: 88af9cf45b7a85089b086b46992008b53771461f

--- KGEN/lib/ObjectCompiler/LowerToObject.cpp
+++ KGEN/lib/ObjectCompiler/LowerToObject.cpp
@@ -291,7 +291,7 @@ KGEN::createTargetMachine(const CompilationOptions &options, bool isJIT) {
 
   std::unique_ptr<llvm::TargetMachine> machine(target->createTargetMachine(
       options.targetTriple, options.targetCpu, options.targetFeatures,
-      /*Options=*/{}, /*RM=*/llvm::Reloc::Model::PIC_, /*CM=*/{},
+      /*Options=*/{}, options.relocModel, /*CM=*/{},
       /*OL=*/options.getCodeGenOptLevel(), /*JIT=*/isJIT));
   if (!machine)
     return Error("unable to create target machine");
