94d061555de0a48a569e9a239688935e97777e60
River Riddle <riddleriver@gmail.com>
2023-09-29 12:00:32 -0700

    [mojo-tooling][windows] Fix display of 🔥 in mojo and MojoLLDB help (#22292)
    This requires compiling with /utf-8, so that source files are treated as
    unicode. We may want to consider enabling this across the codebase,
    but we need to validate we don't implicitly rely on the default
    codepage (which is tough right now because not everything builds/gets
    tested on windows).
    
    See #17121
    
    MODULAR_ORIG_COMMIT_REV_ID: 900f8a60d3e8c793df80bd90b934cab147765696

--- KGEN/lib/MojoLLDB/REPL/MojoREPL.cpp
+++ KGEN/lib/MojoLLDB/REPL/MojoREPL.cpp
@@ -433,9 +433,9 @@ createInstanceFromDebugger(Debugger &debugger, const char *replOptions) {
   cleanupOnError.release();
 
   if (isatty(STDIN_FILENO)) {
-    printf("Welcome to Mojo! 🔥\n\nExpressions are delimited by a blank "
-           "line.\nType `:quit` to exit the REPL and `:mojo help` for "
-           "further assistance.\n\n");
+    llvm::outs() << "Welcome to Mojo! 🔥\n\nExpressions are delimited by a "
+                    "blank line.\nType `:quit` to exit the REPL and `:mojo "
+                    "help` for further assistance.\n\n";
   }
   return repl;
 }
