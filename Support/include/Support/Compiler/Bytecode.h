4e8ac4074bc8256b29327d0f8a087df1de017093
Laszlo Kindrat <laszlo@modular.ai>
2023-10-13 10:04:38 -0700

    [Support] Add helpers for enum array bytecode encoding (#23174)
    A similar helper existed for a single enum, which this patch
    generalizes. The existing helper isn't tested in isolation, so the new
    ones aren't either; the lit dialect will rely on this later, which
    should provide adequate coverage.
    
    MODULAR_ORIG_COMMIT_REV_ID: 06d0e36c5dd8c439881a05b9a26cb35ea5808b32

--- Support/include/Support/Compiler/Bytecode.h
+++ Support/include/Support/Compiler/Bytecode.h
@@ -11,7 +11,7 @@
 #include "mlir/Bytecode/BytecodeImplementation.h"
 
 namespace M {
-/// ODS helper for parsing enums.
+/// ODS helper for parsing an enum.
 template <typename T>
 LogicalResult readEnum(mlir::DialectBytecodeReader &reader, T &result) {
   uint64_t value;
@@ -20,6 +20,27 @@ LogicalResult readEnum(mlir::DialectBytecodeReader &reader, T &result) {
   result = static_cast<T>(value);
   return success();
 }
+
+/// ODS helper for parsing an array of enums.
+template <typename T>
+LogicalResult readEnumArray(mlir::DialectBytecodeReader &reader,
+                            SmallVectorImpl<T> &result) {
+  return reader.readList(
+      result, [&](T &value) { return M::readEnum<T>(reader, value); });
+}
+
+/// ODS helper for printing an enum.
+template <typename T>
+void writeEnum(mlir::DialectBytecodeWriter &writer, T value) {
+  writer.writeVarInt(static_cast<uint64_t>(value));
+}
+
+/// ODS helper for printing an array of enums.
+template <typename T>
+void writeEnumArray(mlir::DialectBytecodeWriter &writer, ArrayRef<T> values) {
+  writer.writeList(values, [&](T value) { writeEnum(writer, value); });
+}
+
 } // namespace M
 
 #endif // SUPPORT_COMPILER_BYTECODE_H
