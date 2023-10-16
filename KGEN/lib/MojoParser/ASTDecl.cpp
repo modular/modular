53d8da975b4b46b8c0c8b1c02affe97f703626a1
Jeff Niu <jeff@modular.com>
2023-10-16 10:21:25 -0700

    [mojo-lang] Split out `ASTDecl.cpp` (NFC) (#23368)
    ASTDecl is special and deserves its own source file ;)
    
    MODULAR_ORIG_COMMIT_REV_ID: 651fbd1673c1ff7476d21ab85b12b37719349b1d

---
+++ KGEN/lib/MojoParser/ASTDecl.cpp
@@ -0,0 +1,163 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#include "KGEN/MojoParser/ASTDecl.h"
+#include "KGEN/LITDialect/LITAttrs.h"
+#include "KGEN/LITDialect/LITInterfaces.h"
+#include "KGEN/LITDialect/LITOps.h"
+#include "KGEN/MojoParser/DocString.h"
+#include "llvm/ADT/TypeSwitch.h"
+
+using namespace M;
+using namespace KGEN;
+using namespace LIT;
+
+DocStringAttr ASTDecl::getDocString() const {
+  if (auto astDeclOp = dyn_cast<ASTDeclInterface>(*this))
+    return astDeclOp.getDocStringAttr();
+  return {};
+}
+
+std::optional<DocString> ASTDecl::getParsedDocString() const {
+  if (auto rawDocStr = getDocString())
+    return DocString(rawDocStr);
+  return {};
+}
+
+ArrayRef<ASTDecl *> ASTDecl::lookupInCurrentScope(StringRef name) const {
+  return lookupInCurrentScope(StringAttr::get(getContext(), name));
+}
+
+/// Look up a name in this declaration's scope only: return null on failure.
+ArrayRef<ASTDecl *> ASTDecl::lookupInCurrentScope(StringAttr name) const {
+  assert(resolvedness == DeclResolvedness::fully &&
+         "cannot perform lookup in a decl that isn't fully resolved");
+  auto it = declsInScope.find(name);
+  if (it != declsInScope.end() && !it->second.empty())
+    return it->second;
+  return {};
+}
+
+StringAttr ASTDecl::getAnonymousLifetimeFor(const Twine &valueName) {
+  // Find the enclosing isolated from above decl that will scope parameter
+  // names.
+  ASTDecl *outermostFuncScope = nullptr;
+  ASTDecl *scope = this;
+  while (scope) {
+    // If we see a function scope, remember it but see if we are nested in some
+    // other function.
+    if (isa<LIT::FuncOp>(*scope))
+      outermostFuncScope = scope;
+
+    // If we found the file module, then we're at the top level, just use it.
+    if (isa<FileModuleOp>(*scope)) {
+      // If we have a function scope, make sure to use it, otherwise we really
+      // are at the top level.
+      if (!outermostFuncScope)
+        outermostFuncScope = scope;
+      break;
+    }
+
+    scope = scope->getParentDecl();
+  }
+  assert(outermostFuncScope && "couldn't find an enclosing function");
+  return StringAttr::get(
+      outermostFuncScope->getContext(),
+      "`" + valueName + Twine(outermostFuncScope->anonymousLifetimeCounter++));
+}
+
+void ASTDecl::dump() const {
+  // The value is either an operation or a type of MLIR `Value`.
+  TypeSwitch<DeclIRValue>(getIRValue())
+      .Case<Operation *>([](Operation *op) {
+        // Print without verifying, since IR could be in an invalid state.
+        op->print(llvm::errs(), mlir::OpPrintingFlags().printGenericOpForm());
+        llvm::errs() << "\n";
+      })
+      .Case<PValue, SRValue, MRValue, SBValue, MBValue, MLValue, XLValue>(
+          [](auto v) { v.dump(); })
+      .Default([](DeclIRValue v) { llvm::errs() << "<null decl>\n"; });
+}
+
+MLIRContext *ASTDecl::getContext() const {
+  if (auto *op = getIfOperation())
+    return op->getContext();
+  if (auto mv = dyn_cast<PValue>(irValue))
+    return mv.get().getContext();
+  if (auto dr = dyn_cast<SRValue>(irValue))
+    return dr.getContext();
+  if (auto value = dyn_cast_or_null<XLValue>(irValue))
+    return value.getContext();
+  if (auto value = dyn_cast_or_null<MRValue>(irValue))
+    return value.getContext();
+  return cast<MLValue>(irValue).getContext();
+}
+
+/// If this is an RValue, return it otherwise return null.
+CRValue ASTDecl::getIfRValue() const {
+  if (auto attr = dyn_cast_or_null<PValue>(irValue))
+    return attr;
+  if (auto value = dyn_cast_or_null<SRValue>(irValue))
+    return value;
+  if (auto value = dyn_cast_or_null<MRValue>(irValue))
+    return value;
+  // if (auto value = dyn_cast_or_null<XRValue>(irValue))
+  //   return value;
+  return {};
+}
+
+/// If this is an BValue, return it otherwise return null.
+BValue ASTDecl::getIfBValue() const {
+  if (auto attr = dyn_cast_or_null<PValue>(irValue))
+    return attr;
+  if (auto value = dyn_cast_or_null<SBValue>(irValue))
+    return value;
+  if (auto value = dyn_cast_or_null<MBValue>(irValue))
+    return value;
+  // if (auto value = dyn_cast_or_null<XBValue>(irValue))
+  //   return value;
+  return {};
+}
+
+std::optional<StringRef> ASTDecl::getNameIfOperation() const {
+  if (Operation *op = getIfOperation())
+    if (auto decl = dyn_cast<ASTDeclInterface>(op))
+      return decl.getDeclName().getValue();
+  return {};
+}
+
+PValue ASTDecl::getFuncAsPValue() const {
+  return SymbolConstantAttr::get(getSymbolRef(),
+                                 cast<LIT::FuncOp>(*this).getSignature());
+}
+
+/// Return the SymbolRefAttr for a declaration, including all scoping that may
+/// be needed, making it unique for every declaration.  This returns null for
+/// named values that do not have a declaration.
+SymbolRefAttr ASTDecl::getSymbolRef() const {
+  auto op = dyn_cast_if_present<mlir::SymbolOpInterface>(getIfOperation());
+  if (!op)
+    return {};
+  assert(
+      (!isa<LIT::FuncOp>(op) || resolvedness >= DeclResolvedness::signature) &&
+      "Functions don't have a symbol until their signatures are resolved");
+  return getFullyResolvedSymbolRef(op);
+}
+
+/// Given an MLIR op for a struct declaration, return the self type.
+Type ASTDecl::computeSelfTypeForStruct(StructDeclOp structOp) {
+  SmallVector<ParamBindAttr> parameters;
+  for (auto decl : structOp.getInputParams()) {
+    // We're using the parameter from the type declaration scope in the
+    // parameter binding list.
+    TypedAttr ref = ParamDeclRefAttr::get(decl);
+    parameters.push_back(ParamBindAttr::get(decl.getName(), ref));
+  }
+
+  // Methods on structs (but not classes) take the struct implicitly by
+  // pointer so they can use and mutate it.
+  return DeclRefType::get(getFullyResolvedSymbolRef(structOp), parameters);
+}
