43b03725ad5854d9a8d2887693446e63c5ea396d
Jeff Niu <jeff@modular.com>
2023-10-26 17:29:21 -0500

    [KGEN] Add a `pop.pointer.addrspacecast` op (#24151)
    Converts pointers between address spaces. Lowers to
    `llvm.addrspacecast`.
    
    Fixes [Internal link]
    
    MODULAR_ORIG_COMMIT_REV_ID: fe88ffec5756e901bac978e2e1c1dcb07fde78b4

--- KGEN/lib/POPDialect/POPOps.cpp
+++ KGEN/lib/POPDialect/POPOps.cpp
@@ -117,6 +117,19 @@ bool PointerBitcastOp::areCastCompatible(TypeRange inputs, TypeRange outputs) {
 }
 
 //===----------------------------------------------------------------------===//
+// PointerAddrSpaceCastOp
+//===----------------------------------------------------------------------===//
+
+bool PointerAddrSpaceCastOp::areCastCompatible(TypeRange inputs,
+                                               TypeRange outputs) {
+  if (inputs.size() != 1 || outputs.size() != 1)
+    return false;
+  auto inPtr = dyn_cast<PointerType>(inputs.front());
+  auto outPtr = dyn_cast<PointerType>(outputs.front());
+  return inPtr && outPtr && inPtr.getElementType() == outPtr.getElementType();
+}
+
+//===----------------------------------------------------------------------===//
 // CastOp
 //===----------------------------------------------------------------------===//
 
