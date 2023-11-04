ac5883edd40cde61502748283c5628ab7b23af1a
Jeff Niu <jeff@modular.com>
2023-11-04 16:30:00 -0700

    [KGEN] Add encoder and decoder for SourceNameAttr (#24784)
    This adds methods to serialize and deserialize SourceNameAttr in a way
    that is somewhat human readable.
    
    MODULAR_ORIG_COMMIT_REV_ID: 9f0636ce8191d5a3ab1c18e678ba851444355bab

--- KGEN/unittests/KGENDialect/KGENAttrsTest.cpp
+++ KGEN/unittests/KGENDialect/KGENAttrsTest.cpp
@@ -7,7 +7,9 @@
 #include "KGEN/KGENDialect/KGENAttrs.h"
 #include "KGEN/KGENDialect/KGENDialect.h"
 #include "Support/MDialect/MAttrs.h"
+#include "mlir/IR/BuiltinOps.h"
 #include "mlir/IR/MLIRContext.h"
+#include "mlir/Parser/Parser.h"
 #include "llvm/Support/TargetSelect.h"
 #include "llvm/TargetParser/Host.h"
 #include "gmock/gmock.h"
@@ -17,6 +19,10 @@ using namespace M;
 using namespace KGEN;
 using namespace mlir;
 using namespace testing;
+
+//===----------------------------------------------------------------------===//
+// PackageArchiveArrayAttrTest
+//===----------------------------------------------------------------------===//
 
 namespace {
 class PackageArchiveArrayAttrTest : public Test {
@@ -89,4 +95,67 @@ TEST_F(PackageArchiveArrayAttrTest, getTargetArchive_match) {
 
   // Now search for the second target.
   EXPECT_EQ(archives.getTargetArchive(targetInfoTwo), archiveTwo);
+}
+
+//===----------------------------------------------------------------------===//
+// SourceNameAttrTest
+//===----------------------------------------------------------------------===//
+
+namespace {
+class SourceNameAttrTest : public Test {
+protected:
+  MLIRContext ctx;
+
+  SourceNameAttrTest() { ctx.loadDialect<KGENDialect>(); }
+};
+} // namespace
+
+TEST_F(SourceNameAttrTest, TestEncodeDecode) {
+  StringRef testStr = R"mlir(
+    #builtin_name = #kgen.source_name<"builtin"[]()[]>
+    #test_name = #kgen.source_name<"test"[]()[]>
+    #int_name = #kgen.source_name<"int"[]()[] from #builtin_name>
+    #simd_name = #kgen.source_name<"simd"[]()[] from #builtin_name>
+    #Int_name = #kgen.source_name<"Int"[]()[] from #int_name>
+    #SIMD_name = #kgen.source_name<"SIMD"[#Int_name]()[] from #simd_name>
+    #func_name = #kgen.source_name<"func"[](#SIMD_name)["1"] from #test_name>
+
+    #strange = #kgen.source_name<"strange*"[]()[]>
+    #weird = #kgen.source_name<"weird&name"[]()[":struct<index> { 1 }", "^&*"] from #strange>
+
+    module attributes {
+      kgen.test0 = #func_name,
+      kgen.test1 = #weird
+    } {}
+  )mlir";
+
+  OwningOpRef<ModuleOp> module = mlir::parseSourceString<ModuleOp>(
+      testStr, mlir::ParserConfig(&ctx), "TestEncode_testStr");
+  ASSERT_TRUE(module);
+
+  {
+    auto sourceName = (*module)->getAttrOfType<SourceNameAttr>("kgen.test0");
+    ASSERT_TRUE(sourceName);
+
+    StringRef expected =
+        "test::func(builtin::simd::SIMD[builtin::int::Int])<1>";
+    EXPECT_EQ(sourceName.encode().getValue(), expected);
+
+    ErrorOr<SourceNameAttr> decoded = SourceNameAttr::decode(&ctx, expected);
+    ASSERT_FALSE(decoded.isError());
+    EXPECT_EQ(decoded.takeValue(), sourceName);
+  }
+
+  {
+    auto sourceName = (*module)->getAttrOfType<SourceNameAttr>("kgen.test1");
+    ASSERT_TRUE(sourceName);
+
+    StringRef expected =
+        "`strange*`::`weird&name`<`:struct<index> { 1 }`,`^&*`>";
+    EXPECT_EQ(sourceName.encode().getValue(), expected);
+
+    ErrorOr<SourceNameAttr> decoded = SourceNameAttr::decode(&ctx, expected);
+    ASSERT_FALSE(decoded.isError());
+    EXPECT_EQ(decoded.takeValue(), sourceName);
+  }
 }
