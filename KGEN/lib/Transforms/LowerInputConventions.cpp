ed28885ba03f695e43d02dc2eaeb9aa58d496402
Laszlo Kindrat <laszlo@modular.ai>
2023-11-06 14:38:42 -0500

    [KGEN] Implement input convention lowering for non-result arguments (#24724)
    The current implementation has some potential room for refactoring, but
    we should hold off with this until the pass gets the result of the
    functionality implemented.
    
    MODULAR_ORIG_COMMIT_REV_ID: 64476163f31c1a64ae90850b686039be8bdb5037

--- KGEN/lib/Transforms/LowerInputConventions.cpp
+++ KGEN/lib/Transforms/LowerInputConventions.cpp
@@ -25,73 +25,139 @@ struct LowerInputConventionsPass
 };
 } // namespace
 
-/// Return whether the given type needs could input convention lowering.
-static bool shouldLower(Type type) {
+/// Return the lowered type for an in-memory passed argument. If lowering is not
+/// needed, return null.
+static Type lowerOwnedOrBorrowedInMem(Type type) {
   // Only pointer types should be lowered.
   auto argPtr = dyn_cast<PointerType>(type);
   if (!argPtr)
-    return false;
+    return {};
 
-  // StructType carries passability information directly.
-  if (auto structType = dyn_cast<StructType>(argPtr.getElementAsType()))
-    return !structType.getIsMemoryOnly();
+  // We don't lower memory-only structs.
+  Type elType = argPtr.getElementAsType();
+  if (auto structType = dyn_cast<StructType>(elType))
+    if (structType.getIsMemoryOnly())
+      return {};
 
   // We must be dealing with something register passable (e.g. index).
-  return true;
+  return elType;
 }
 
-/// Return whether the given type with the given convention needs lowering.
-static bool shouldLower(Type type, ValueInputConvention convention) {
-  if (convention == ValueInputConvention::BorrowedInMem ||
-      convention == ValueInputConvention::OwnedInMem ||
-      convention == ValueInputConvention::ByRefResult ||
-      convention == ValueInputConvention::InitSelf)
-    return shouldLower(type);
-  return false;
-}
-
-/// Return the given signature with the argument conventions lowered. If no
-/// lowering is necessary, the function returns null.
-static SignatureType lowerSignature(SignatureType sig) {
+/// Lowers the given signature if needed, and returns the argument indices that
+/// needed to be changed.
+static std::pair<SignatureType, SmallVector<size_t>>
+lowerSignature(SignatureType sig) {
   ArrayRef<ValueInputConvention> oldConvs = sig.getInputConventions();
   SmallVector<ValueInputConvention> newConvs(oldConvs);
-  bool needsLowering = false;
+
+  ArrayRef<Type> oldInputTypes = sig.getValueInputs();
+  SmallVector<Type> newInputTypes(oldInputTypes);
+
+  SmallVector<size_t> changedIndices;
   for (auto [idx, argTy, convention] :
        llvm::enumerate(sig.getValueInputs(), oldConvs)) {
-    if (shouldLower(argTy, convention)) {
-      needsLowering = true;
-      newConvs[idx] = ValueInputConvention::None;
+    if (convention == ValueInputConvention::BorrowedInMem ||
+        convention == ValueInputConvention::OwnedInMem) {
+      if (Type newArgTy = lowerOwnedOrBorrowedInMem(argTy)) {
+        // Update the info needed for the new signature.
+        newConvs[idx] = ValueInputConvention::None;
+        newInputTypes[idx] = newArgTy;
+        changedIndices.push_back(idx);
+      }
     }
   }
-  if (!needsLowering)
-    return {};
-  return SignatureType::get(sig.getContext(), sig.getInputParamTypes(),
-                            sig.getResultParamTypes(), sig.getValues(),
-                            newConvs, sig.getFnEffects(), sig.getMetadata());
+
+  if (changedIndices.empty())
+    return std::make_pair(SignatureType(), std::move(changedIndices));
+  auto newFnType =
+      FunctionType::get(sig.getContext(), newInputTypes, sig.getValueResults());
+  auto newSig = SignatureType::get(newFnType, newConvs, sig.getFnEffects(),
+                                   sig.getMetadata());
+  return std::make_pair(newSig, std::move(changedIndices));
 }
 
 /// Lower the input conventions for a KGEN::CallOp if needed.
-static void lowerCallOp(Operation *op) {
-  auto callOp = dyn_cast<CallOp>(op);
-  if (!callOp)
+static void lowerCallOp(CallOp callOp) {
+  auto [newSig, changedIndices] = lowerSignature(callOp.getCalleeSignature());
+  if (!newSig)
     return;
-  if (SignatureType newSig = lowerSignature(callOp.getCalleeSignature())) {
-    callOp.setCalleeAttr(
-        SymbolConstantAttr::get(callOp.getCallee().getSymbol(), newSig));
+
+  OpBuilder b(callOp);
+  SmallVector<Value> newOperands(callOp.getOperands());
+  for (size_t idx : changedIndices) {
+    newOperands[idx] =
+        b.create<POP::LoadOp>(callOp.getLoc(), callOp.getOperand(idx));
   }
+  callOp->setOperands(newOperands);
+  callOp.setCalleeAttr(
+      SymbolConstantAttr::get(callOp.getCallee().getSymbol(), newSig));
+}
+
+/// Lower the input conventions for a KGEN::CallSignatureOp if needed.
+static void lowerCallSignatureOp(CallSignatureOp callOp) {
+  TypedValue<SignatureType> callee = callOp.getCallee();
+  auto [newSig, changedIndices] = lowerSignature(callee.getType());
+  if (!newSig)
+    return;
+
+  OpBuilder b(callOp);
+  SmallVector<Value> newOperands(callOp.getArguments());
+  for (size_t idx : changedIndices) {
+    newOperands[idx] =
+        b.create<POP::LoadOp>(callOp.getLoc(), callOp.getArguments()[idx]);
+  }
+  callOp->setOperands(1, newOperands.size(), newOperands);
+  callee.setType(newSig);
+}
+
+/// Lower the input conventions for a KGEN::FuncOp if needed.
+static void lowerFuncOp(FuncOp funcOp) {
+  SignatureType oldSig = funcOp.getSignature();
+  auto [newSig, changedIndices] = lowerSignature(oldSig);
+  if (!newSig)
+    return;
+
+  Region &body = funcOp.getBodyRegion();
+  auto b = OpBuilder::atBlockBegin(&body.front());
+  for (size_t idx : changedIndices) {
+    BlockArgument arg = body.getArgument(idx);
+    auto ptr = b.create<POP::StackAllocationOp>(arg.getLoc(), arg.getType(), 1);
+    auto storeOp = b.create<POP::StoreOp>(arg.getLoc(), arg, ptr);
+    arg.setType(newSig.getValueInputs()[idx]);
+    arg.replaceAllUsesExcept(ptr, storeOp);
+  }
+  funcOp.setSignature(newSig);
 }
 
 static void lowerInputConventions(Operation &op) {
   if (auto funcOp = dyn_cast<FuncOp>(op)) {
-    if (SignatureType newSig = lowerSignature(funcOp.getSignature()))
-      funcOp.setSignature(newSig);
-    op.walk(lowerCallOp);
+    lowerFuncOp(funcOp);
+
+    // Lower the ops in the function body.
+    op.walk([](Operation *op) {
+      if (auto callOp = dyn_cast<CallOp>(op))
+        return lowerCallOp(callOp);
+      if (auto callOp = dyn_cast<CallSignatureOp>(op))
+        return lowerCallSignatureOp(callOp);
+    });
+
+    // We must do this in a second pass, otherwise ops like kgen.call_signature
+    // would be difficult to identify for lowering (since their argument types
+    // would be lowered already).
+    op.walk([](Operation *op) {
+      mlir::AttrTypeReplacer replacer;
+      replacer.addReplacement([](SignatureType sig) {
+        auto [newSig, _] = lowerSignature(sig);
+        return newSig ? newSig : sig;
+      });
+      replacer.replaceElementsIn(op, /*replaceAttrs=*/true,
+                                 /*replaceLocs=*/true,
+                                 /*replaceAttrs=*/true);
+    });
   }
 }
 
 void LowerInputConventionsPass::runOnOperation() {
-  // TODO(#20700): implement the actual lowering instead of just nuking the
-  // conventions.
   mlir::parallelForEach(&getContext(), getOperation().getOps(),
                         lowerInputConventions);
 }
