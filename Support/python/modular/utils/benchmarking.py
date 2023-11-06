9b03ca83392093e50668382044281de88938580e
Tyler Kenney <tyler@modular.com>
2023-11-06 11:50:45 -0500

    Internal commit
    MODULAR_ORIG_COMMIT_REV_ID: d8b7b3b46abd786d90e3826a4761bf0b490cc6cd

---
+++ Support/python/modular/utils/benchmarking.py
@@ -0,0 +1,83 @@
+# ===----------------------------------------------------------------------=== #
+#
+# This file is Modular Inc proprietary.
+#
+# ===----------------------------------------------------------------------=== #
+
+__doc__ = """"
+Benchmarking Utility Library
+"""
+
+from dataclasses import dataclass, fields
+from pathlib import Path
+
+from modular.utils.typing import Dict, List, Optional
+
+
+@dataclass
+class BenchmarkResult:
+    """
+    Class to hold benchmark results
+    """
+
+    min_latency: Optional[float] = None
+    max_latency: Optional[float] = None
+    mean_latency: Optional[float] = None
+    percentile_5000: Optional[float] = None
+    percentile_9000: Optional[float] = None
+    percentile_9500: Optional[float] = None
+    percentile_9700: Optional[float] = None
+    percentile_9900: Optional[float] = None
+    percentile_9990: Optional[float] = None
+    qps: Optional[float] = None
+
+    @classmethod
+    def from_file(cls, path: Path):
+        """
+        Parses the given MLPerf benchmarking log and returns results.
+
+        Args:
+            path (Path): Path to logfile to be parsed.
+        """
+        with open(path, "r") as summary:
+            return BenchmarkResult.from_lines(summary)
+
+    @classmethod
+    def from_lines(cls, lines: List[str]):
+        """
+        Parses the given MLPerf benchmarking log and returns results.
+
+        Args:
+            lines (List[str]): Contents of logfile to be parsed.
+        """
+
+        def get_value(val: str):
+            return float(val.split(":")[1].strip())
+
+        result = BenchmarkResult()
+        for line in lines:
+            if "QPS w/o loadgen" in line:
+                result.qps = get_value(line)
+            elif "Min latency" in line:
+                result.min_latency = get_value(line)
+            elif "Max latency" in line:
+                result.max_latency = get_value(line)
+            elif "Mean latency" in line:
+                result.mean_latency = get_value(line)
+            elif "50.00 percentile" in line:
+                result.percentile_5000 = get_value(line)
+            elif "90.00 percentile" in line:
+                result.percentile_9000 = get_value(line)
+            elif "95.00 percentile" in line:
+                result.percentile_9500 = get_value(line)
+            elif "97.00 percentile" in line:
+                result.percentile_9700 = get_value(line)
+            elif "99.00 percentile" in line:
+                result.percentile_9900 = get_value(line)
+            elif "99.90 percentile" in line:
+                result.percentile_9990 = get_value(line)
+        return result
+
+    def to_dict(self) -> Dict:
+        """Return dictionary representation of dataclass"""
+        return {x.name: getattr(self, x.name) for x in fields(self)}
---
+++ Support/python/tests/utils/test_benchmarking.py
@@ -0,0 +1,76 @@
+# ===----------------------------------------------------------------------=== #
+#
+# This file is Modular Inc proprietary.
+#
+# ===----------------------------------------------------------------------=== #
+
+from modular.utils.benchmarking import BenchmarkResult
+
+benchmark_log = """================================================
+MLPerf Results Summary
+================================================
+SUT name : benchmark
+Scenario : SingleStream
+Mode     : PerformanceOnly
+90th percentile latency (ns) : 7132046
+Result is : VALID
+  Min duration satisfied : Yes
+  Min queries satisfied : Yes
+  Early stopping satisfied: Yes
+Early Stopping Result:
+ * Processed at least 64 queries (708).
+ * Would discard 51 highest latency queries.
+ * Early stopping 90th percentile estimate: 7171535
+ * Early stopping 99th percentile estimate: 10952369
+
+================================================
+Additional Stats
+================================================
+QPS w/ loadgen overhead         : 141.400
+QPS w/o loadgen overhead        : 141.528
+
+Min latency (ns)                : 6930533
+Max latency (ns)                : 10952369
+Mean latency (ns)               : 7065755
+50.00 percentile latency (ns)   : 7023578
+90.00 percentile latency (ns)   : 7132046
+95.00 percentile latency (ns)   : 7252009
+97.00 percentile latency (ns)   : 7340995
+99.00 percentile latency (ns)   : 7577115
+99.90 percentile latency (ns)   : 10952369
+
+================================================
+Test Parameters Used
+================================================
+samples_per_query : 1
+target_qps : 1000
+target_latency (ns): 0
+max_async_queries : 1
+min_duration (ms): 5000
+max_duration (ms): 0
+min_query_count : 20
+max_query_count : 0
+qsl_rng_seed : 0
+sample_index_rng_seed : 0
+schedule_rng_seed : 0
+accuracy_log_rng_seed : 0
+accuracy_log_probability : 0
+accuracy_log_sampling_target : 0
+print_timestamps : 0
+performance_issue_unique : 0
+performance_issue_same : 0
+performance_issue_same_index : 0
+performance_sample_count : 1
+
+No warnings encountered during test.
+
+No errors encountered during test.
+"""
+
+
+def test_from_lines():
+    lines = benchmark_log.split("\n")
+    res = BenchmarkResult.from_lines(lines)
+    assert res.qps == 141.528
+    assert res.min_latency == 6930533
+    assert res.percentile_9000 == 7132046
