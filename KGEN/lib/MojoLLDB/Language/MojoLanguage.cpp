8a9bb23b448aa14f1a53cd485cbb302ca6f01543
Walter Erquinigo <walter@modular.com>
2023-11-07 18:43:30 -0500

    [LLDB] Render nicely booleans (#24820)
    This adds the @lldb_formatter_wrapping_type decorator to Bool and adds the necessary type summary providers so that bools can be rendered as True or False in the REPL.
    This patch should have been really small, but we needed the handlers for summaries for REPL results types and for the types with the decorator.
    
    Sadly this doesn't work in a regular debug scenario because dwarf is wrong for bools. See [Internal link]
    
    MODULAR_ORIG_COMMIT_REV_ID: f6bafef475dc5950152543b8122204418a1ffced

--- KGEN/lib/MojoLLDB/Language/MojoLanguage.cpp
+++ KGEN/lib/MojoLLDB/Language/MojoLanguage.cpp
@@ -50,6 +50,25 @@ static bool kgenNoneSummaryProvider(ValueObject &valobj, Stream &stream,
   return true;
 }
 
+// Bool types are rendered nicely as True or False.
+static bool
+popScalarBoolSummaryProvider(ValueObject &valobj, Stream &stream,
+                             const TypeSummaryOptions &summaryOptions) {
+  ValueObjectSP valueChild(valobj.GetChildAtIndex(0, true));
+  if (!valueChild)
+    return false;
+  bool success = false;
+  int val = valueChild->GetValueAsUnsigned(/*default=*/0, &success);
+  if (success) {
+    if (val == 0)
+      stream << "False";
+    else
+      stream << "True";
+    return true;
+  }
+  return false;
+}
+
 static void
 LoadLibMojoFormatters(const lldb::TypeCategoryImplSP &mojoCategorySP) {
   if (!mojoCategorySP)
@@ -64,12 +83,12 @@ LoadLibMojoFormatters(const lldb::TypeCategoryImplSP &mojoCategorySP) {
   // this formatter acts like a sink that will match everything that doesn't
   // match the other formatters.
   AddCXXSynthetic(mojoCategorySP,
-                  MojoDecoratorBasedTypeSyntheticFrontEndCreator,
+                  mojoDecoratorBasedTypeSyntheticFrontEndCreator,
                   "Mojo decorator-based synthetic children", ".*", synthFlags,
                   /*regex=*/true);
   // This formatter will replace a REPLResultRef with the value of its inner
   // type. REPLResultRef owns a pointer, so we need to dereference the pointer.
-  AddCXXSynthetic(mojoCategorySP, MojoREPLResultRefTypeSyntheticFrontEndCreator,
+  AddCXXSynthetic(mojoCategorySP, mojoREPLResultRefTypeSyntheticFrontEndCreator,
                   "REPLResultRefType synthetic children",
                   "^!lit.replresultref<.*>$", synthFlags, /*regex=*/true);
   AddCXXSynthetic(
@@ -87,9 +106,23 @@ LoadLibMojoFormatters(const lldb::TypeCategoryImplSP &mojoCategorySP) {
       .SetDontShowValue(true)
       .SetShowMembersOneLiner(false)
       .SetHideItemNames(false);
+
+  // Summary providers are matched in reverse order, so this one that uses .*
+  // should be added first so that it's the last one to be matched against. In
+  // fact, this provider acts like a sink that will match everything that
+  // doesn't match the other providers.
+  AddCXXSummary(mojoCategorySP, mojoDecoratorBasedSummaryProvider,
+                "Mojo decorator-based summary provider", ".*", summaryFlags,
+                /*regex=*/true);
+  AddCXXSummary(mojoCategorySP, mojoREPLResultRefTypeSummaryProvider,
+                "REPLResultRefType summary provider",
+                "^!lit.replresultref<.*>$", summaryFlags, /*regex=*/true);
   AddCXXSummary(mojoCategorySP, kgenNoneSummaryProvider,
                 "!kgen.none summary provider", "!kgen.none", summaryFlags,
                 /*regex=*/false);
+  AddCXXSummary(mojoCategorySP, popScalarBoolSummaryProvider,
+                "!pop.scalar<bool> summary provider", "!pop.scalar<bool>",
+                summaryFlags, /*regex=*/false);
 }
 
 lldb::TypeCategoryImplSP MojoLanguage::GetFormatters() {
