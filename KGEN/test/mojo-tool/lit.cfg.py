b9eb93083e5c2db63e07ba5a4ed959ab359737f6
Brian Gesiak <brian@modular.com>
2023-11-13 16:34:29 -0500

    [KGEN] Add check-mojo-tool test target (#25009)
    Add a test target to run just the tests in the `KGEN/test/mojo-tool`
    directory.
    
    Doing so removes the last set of tests that were run directly as part of
    the `check-mojo` target, so convert the `check-mojo` target to an empty
    umbrella target.
    
    This also cleans up a TODO left for #17787 that has since been resolved;
    `mojo build` no longer relies on environment variables, and instead uses
    `modular.cfg`.
    
    MODULAR_ORIG_COMMIT_REV_ID: ca99689dd77881460b3a53127e0f5947a8c42171

--- KGEN/test/mojo-tool/lit.cfg.py
+++ KGEN/test/mojo-tool/lit.cfg.py
@@ -5,17 +5,15 @@
 # ===----------------------------------------------------------------------=== #
 
 import os
+import platform
 
 from lit.llvm import llvm_config
 
-# Configuration file for the 'lit' test runner.
-
-
 # name: The name of this test suite.
-config.name = "mojo"
+config.name = "mojo-tool"
 
 # suffixes: A list of file extensions to treat as test files.
-config.suffixes = [".mlir", ".mojo", ".test", ".🔥"]
+config.suffixes = [".mojo", ".test"]
 
 # test_source_root: The root path where tests are located.
 config.test_source_root = os.path.dirname(__file__)
@@ -23,28 +21,14 @@ config.test_source_root = os.path.dirname(__file__)
 # test_exec_root: The root path where tests should be run.
 config.test_exec_root = os.path.join(config.modular_obj_root, "KGEN", "test")
 
-# python tests shouldn't be handled by llvm-lit directly
-config.excludes.add("pytests")
-
-# These directories have their own test targets.
-config.excludes.add("kgen")
-config.excludes.add("mojo-debug")
-config.excludes.add("mojo-integration")
-config.excludes.add("mojo-jupyter")
-config.excludes.add("mojo-lsp-server")
-config.excludes.add("mojo-parser")
-config.excludes.add("mojo-repl")
+# Exclude directories that define Mojo packages; these are used as test inputs.
+config.excludes = ["test_package"]
 
 tool_dirs = [
     config.modular_tools_dir,
     config.mlir_tools_dir,
     config.llvm_tools_dir,
 ]
-tools = [
-    "llvm-objdump",
-    "mojo",
-]
+tools = ["mojo", "lldb"]
 
 llvm_config.add_tool_substitutions(tools, tool_dirs)
-
-llvm_config.with_system_environment(["PYTHONPATH"])
