e796f1061033fc61083918ca02b54eac8503e234
abdul dakkak <adakkak@modular.com>
2023-11-15 06:50:23 -0800

    [Support] Add initial minimal bindings to NVTX (#25685)
    This is to set the stage for us to plug it into the profiler (via a
    CMAKE option). Note that this does not link the nvtx library into any of
    our code and everything is done soley through a dlopen mechanism.
    
    MODULAR_ORIG_COMMIT_REV_ID: ca1205d7f0cdb9d35777dadc35b06a209d87681b

---
+++ Support/include/Support/CUDA/NVTX.h
@@ -0,0 +1,48 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+//
+// Defines a subset of the bindings need to report with NVTX markers.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef SUPPORT_CUDA_NVTX_H
+#define SUPPORT_CUDA_NVTX_H
+
+#include "Support/ErrorOr.h"
+#include "llvm/ADT/StringRef.h"
+
+namespace M::CUDA::NVTX {
+
+struct Event {
+
+  Event(StringRef category, StringRef message, uint32_t color = 0xFF880000)
+      : category(category), message(message), color(color) {}
+
+  struct RangeID {
+    RangeID(uint64_t id) : id(id) {}
+    ~RangeID();
+
+  private:
+    uint64_t id;
+  };
+
+  ErrorOrSuccess mark();
+
+  ErrorOr<RangeID> start();
+
+private:
+  // The cateogry for the event. This is used to disambiguate the different
+  // event types.
+  std::string category;
+  // The message attached to the event.
+  std::string message;
+  // The ARGB color (in Hex) of the event.
+  uint32_t color;
+};
+
+} // namespace M::CUDA::NVTX
+
+#endif // SUPPORT_CUDA_NVTX_H
