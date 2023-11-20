39d6b8fa5dd3a1a0b6c7cb91c482a88c072b9cbc
Jeff Niu <jeff@modular.com>
2023-11-20 14:32:40 -0800

    [mojo-lang] Split out `ParamParamEvaluator.cpp` from `DeclResolver.cpp` (NFC) (#26135)
    MODULAR_ORIG_COMMIT_REV_ID: 2686829f49ae8b6fbd1767500641ddbcd0a8df70

--- KGEN/lib/MojoParser/DeclResolver.cpp
+++ KGEN/lib/MojoParser/DeclResolver.cpp
@@ -133,6 +133,7 @@ ASTDecl &DeclResolver::createUnlistedDecl(DeclIRValue irValue, SMLoc loc,
 
   return *decl;
 }
+
 ASTDecl &DeclResolver::createUnlistedDecl(Operation *declOp, SMLoc loc,
                                           ASTDecl *parentDecl,
                                           LexerCursor cursor,
@@ -870,103 +871,6 @@ LogicalResult DeclResolver::resolveAllWildcardImports(ASTDecl &module) {
       return failure();
   }
   return success();
-}
-
-//===----------------------------------------------------------------------===//
-// ParserParamEvaluator implementation
-//===----------------------------------------------------------------------===//
-
-ParserParamEvaluator::ParserParamEvaluator(DeclResolver &resolver,
-                                           ArrayRef<ParamDeclAttr> paramDecls,
-                                           ArrayRef<TypedAttr> paramValues)
-    : ParameterEvaluator(paramDecls, paramValues),
-      InterpreterState(resolver.getContext()), resolver(resolver) {}
-
-FailureOr<TypedAttr>
-ParserParamEvaluator::evaluateFunctionCall(SymbolRefAttr symbol,
-                                           ArrayRef<Attribute> arguments) {
-  ErrorOr<Region *> body = lookupFunctionBody(symbol);
-  if (body.isError()) {
-    // Swallow the error.
-    DEBUG_WITH_TYPE("lit-parameter-evaluator", llvm::errs()
-                                                   << "[ParserParamEvaluator] "
-                                                   << body.getError() << "\n");
-    return failure();
-  }
-
-  ErrorTreeOr<SmallVector<Attribute>> result =
-      executeRegion(*body.takeValue(), arguments);
-  if (result.isError()) {
-    // Swallow the error.
-    DEBUG_WITH_TYPE("lit-parameter-evaluator",
-                    result.takeError().emit(
-                        (InFlightDiagnostic(*)(Location))mlir::emitError));
-    return failure();
-  }
-
-  return cast<TypedAttr>(result->front());
-}
-
-FailureOr<TypedAttr>
-ParserParamEvaluator::evaluateExpression(ParamOperatorAttr op) {
-  if (op.getOpcode() != POC::Apply)
-    return failure();
-
-  // We can only fold direct calls.
-  auto ref = dyn_cast<SymbolConstantAttr>(op.getOperands().front());
-  if (!ref)
-    return failure();
-
-  // All inputs must be simple constants.
-  ArrayRef<TypedAttr> inputs = op.getOperands().drop_front();
-  if (!llvm::all_of(inputs, ParameterAttr::isSimpleConstant))
-    return failure();
-
-  SmallVector<Attribute> arguments;
-  for (TypedAttr input : inputs)
-    arguments.push_back(input);
-
-  return evaluateFunctionCall(ref.getSymbol(), arguments);
-}
-
-ErrorOr<Region *>
-ParserParamEvaluator::lookupFunctionBody(SymbolRefAttr symbol) {
-  ASTDecl *decl = resolver.getDeclForFuncSymbol(symbol);
-  if (!decl)
-    return Error("function not found: " + mlir::debugString(symbol));
-
-  // Fail if the function is parameterized.
-  if (failed(resolver.resolveSignature(*decl, decl->getLoc())))
-    return Error("failed to resolve function signature");
-
-  auto func = cast<LIT::FuncOp>(*decl);
-  if (func.getInlineLevel() == InlineLevel::Automatic)
-    return Error("function is not always_inline");
-  LITSignatureType fullSig = func.getFullSignature();
-  if (!fullSig.getInputParamTypes().empty() ||
-      !fullSig.getResultParamTypes().empty())
-    return Error("function is parametric");
-
-  // Use of the interpreter's memory model requires a target specification,
-  // which the parser does not have.
-  if (fullSig.hasMemoryOnlyResult() || fullSig.hasInitSelfResult())
-    return Error("function has memory-only result");
-
-  // Make sure to fully resolve the body and everything within it.
-  if (failed(resolver.resolveFully(*decl, decl->getLoc())))
-    return Error("failed to fully resolve function");
-  return &func.getBodyRegion();
-}
-
-Type ParserParamEvaluator::refineType(Type type) {
-  mlir::AttrTypeReplacer replacer;
-  replacer.addReplacement([&](ParamOperatorAttr op) -> TypedAttr {
-    FailureOr<TypedAttr> result = evaluateExpression(op);
-    if (failed(result))
-      return op;
-    return *result;
-  });
-  return replacer.replace(type);
 }
 
 //===----------------------------------------------------------------------===//
