2d1bcd31c5b54272c2bef8496a1430ec3461b25e
Mark Shields <110413041+mbs-modular@users.noreply.github.com>
2023-11-28 07:59:35 -0700

    [MO][MGP][CUDA] mo.debug.tensor.print improvements for whole-model debugging (#26542)
    With the `--debug-print-all-tensors` option (currently only for `mof`
    models), all intermediate tensors are printed with a label derived from
    the MO model, respecting the `--result-output-style`,
    `--print-precision` and `--binary-output-dir` options.
    
    In particular, the `--result-output-style=binary` option is supported,
    which results in each tensor being written to its own file in the given
    directory in addition to the final model outputs.
    
    - New `DebugPrintAllTensorsPass` to insert all the
    `mo.debug.tensor.print`s.
    - Change the `mgp.debug.cpu.tensor.print` and `cuda.debug.tensor.print`
    primitives to take buffer and tensor spec operands instead of a tensor
    so that adding debug printing does not radically alter the generated
    mgp/cuda model.
    - Make sure `mo.debug.tensor.print` follows the device of its operand,
    again to avoid introducing device copies altering the model being
    observed.
    
    MODULAR_ORIG_COMMIT_REV_ID: dd627fa4c38a0ab93d6cb2fe4fdacdef66475bad

---
+++ GenericML/include/GenericML/Support/DebugPrint.h
@@ -0,0 +1,44 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef GENERICML_SUPPORT_DEBUGPRINT_H
+#define GENERICML_SUPPORT_DEBUGPRINT_H
+
+#include "Kernels/Misc.h"
+#include "Support/ErrorOr.h"
+#include "llvm/ADT/StringRef.h"
+#include "llvm/Support/raw_os_ostream.h"
+
+#include <string>
+
+namespace M {
+
+/// Options, conveyed via a runtime context object, which control the output
+/// format for debug printing of tensor values.
+struct DebugTensorPrintOptions {
+  /// Format to use.
+  Kernels::ResultOutputStyle style = Kernels::ResultOutputStyle::kCompact;
+  /// Precision of textual floating point numbers.
+  unsigned precision = 6;
+  /// If outputStyle is binary, the directory in which to create the files,
+  /// using the 'label' for the basename.
+  std::string binaryDir;
+
+  DebugTensorPrintOptions() = default;
+  DebugTensorPrintOptions(Kernels::ResultOutputStyle style, unsigned precision,
+                          std::string binaryDir)
+      : style(style), precision(precision), binaryDir(std::move(binaryDir)) {}
+
+  /// Prints the tensor with CPU-hosted buffer contents and spec, following
+  /// the options of this object, and using label if given to disambiguate
+  /// the result.
+  ErrorOrSuccess printTensor(const void *buffer, const TensorSpec &spec,
+                             StringRef label);
+};
+
+} // namespace M
+
+#endif // GENERICML_SUPPORT_DEBUGPRINT_H
