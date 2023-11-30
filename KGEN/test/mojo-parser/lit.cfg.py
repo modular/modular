5110f110ab16b2a2215e3ce1c1fecf16b931ff63
Laszlo Kindrat <laszlo@modular.ai>
2023-11-30 11:07:57 -0500

    [Mojo][NFC] Move parser determinism tests (#26744)
    It's probably better to test determinism in the presence of builtins, so
    this test shouldn't be isolated from them. Some CMake files are also
    refactored to allow cache warmup for all parser tests.
    
    MODULAR_ORIG_COMMIT_REV_ID: 0fa80ef7e4e4f67896468e12efacf2c1740e27f7

--- KGEN/test/mojo-parser/lit.cfg.py
+++ KGEN/test/mojo-parser/lit.cfg.py
@@ -12,7 +12,7 @@ from lit.llvm import llvm_config
 config.name = "mojo-parser"
 
 # suffixes: A list of file extensions to treat as test files.
-config.suffixes = [".mojo", ".🔥", ".test"]
+config.suffixes = [".mojo", ".🔥"]
 
 # test_source_root: The root path where tests are located.
 config.test_source_root = os.path.dirname(__file__)
