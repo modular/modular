ccc3354d972a10c61b5f97b082fad775d641c591
Christopher Nies <cniessigma@gmail.com>
2023-12-01 11:35:28 -0800

    [Telemetry] Pipe Telemetry Attributes into Instruments (#25959)
    We had no way of tagging OTel instruments with attributes. This remedies
    that by allowing you to pass in a StringMap that attaches to timers,
    histograms, and counters. This is used to enable passing in specific
    file identifiers to our mojo compiler timers
    
    [Internal link]
    
    This is my first real PR in our C++ codebase so if I committed any faux
    pas, assume ignorance :)
    
    MODULAR_ORIG_COMMIT_REV_ID: 70d3406548d89fd5315942cd0d7969508059e669

--- Support/include/Support/Telemetry/Instruments.h
+++ Support/include/Support/Telemetry/Instruments.h
@@ -8,9 +8,12 @@
 #define SUPPORT_TELEMETRY_INSTRUMENTS_H
 
 #include "Support/Telemetry/ForwardDecls.h"
+#include "llvm/ADT/DenseMap.h"
+#include "llvm/ADT/StringMap.h"
 #include <chrono>
 #ifdef MODULAR_ENABLE_TELEMETRY
 #include "opentelemetry/metrics/sync_instruments.h"
+#include "opentelemetry/sdk/resource/resource.h"
 #endif // MODULAR_ENABLE_TELEMETRY
 
 namespace M::Telemetry {
@@ -18,6 +21,13 @@ namespace M::Telemetry {
 // -------- Counter --------
 
 #ifndef MODULAR_ENABLE_TELEMETRY
+
+using MetricAttributeValue =
+    std::variant<bool, int32_t, int64_t, uint32_t, double, const char *,
+                 std::string, uint64_t, llvm::StringRef>;
+
+using AttributeMap =
+    std::unordered_map<class Key, class Tp><std::string, MetricAttributeValue>;
 
 template <typename T>
 class Counter {
@@ -32,9 +42,25 @@ private:
 
 #else // MODULAR_ENABLE_TELEMETRY
 
+using MetricAttributeValue = opentelemetry::common::AttributeValue;
+// The "Record" methods in OTel are very particular with the typing --
+// you need to use a KeyValueIterableView, which has a specific implementation
+// that makes some assumptions about the iterator you pass in (namely that each
+// entry has a `first` attribute you can access directly.) Needs to be an
+// unordered_map because DenseMap seems not to work with owned strings.
+using AttributeMap = std::unordered_map<std::string, MetricAttributeValue>;
+
 template <typename T>
 class Counter {
 public:
+  void add(T value) {
+    auto attrs =
+        opentelemetry::common::KeyValueIterableView<decltype(attributes)>{
+            attributes};
+    counter->Add(value, attributes);
+  }
+
+  // Allow overriding attributes and context.
   template <class... TArgs>
   void add(T value, TArgs &&...args) {
     counter->Add(value, std::forward<TArgs>(args)...);
@@ -46,10 +72,16 @@ public:
 private:
   friend class TelemetryContext;
 
-  Counter(std::unique_ptr<opentelemetry::metrics::Counter<T>> counter)
-      : counter(std::move(counter)) {}
+  Counter(std::unique_ptr<opentelemetry::metrics::Counter<T>> counter,
+          const llvm::StringMap<MetricAttributeValue> &additionalAttributes)
+      : counter(std::move(counter)) {
+    for (auto &attr : additionalAttributes) {
+      attributes[attr.first().str()] = attr.second;
+    }
+  }
 
   std::unique_ptr<opentelemetry::metrics::Counter<T>> counter;
+  AttributeMap attributes;
 };
 
 #endif // MODULAR_ENABLE_TELEMETRY
@@ -82,16 +114,33 @@ private:
 template <typename T>
 class Histogram {
 public:
-  void record(T value) { histogram->Record(value, context); }
+  void record(T value) {
+    auto attrs =
+        opentelemetry::common::KeyValueIterableView<decltype(attributes)>{
+            attributes};
+    histogram->Record(value, attrs, context);
+  }
+
+  // Allow overriding attributes and context.
+  template <class... TArgs>
+  void record(T value, TArgs &&...args) {
+    histogram->Record(value, std::forward<TArgs>(args)...);
+  }
 
   Histogram(Histogram &&) = default;
   Histogram &operator=(Histogram &&) = default;
+  AttributeMap attributes;
 
 private:
   friend class TelemetryContext;
 
-  Histogram(std::unique_ptr<opentelemetry::metrics::Histogram<T>> histogram)
-      : histogram(std::move(histogram)) {}
+  Histogram(std::unique_ptr<opentelemetry::metrics::Histogram<T>> histogram,
+            const llvm::StringMap<MetricAttributeValue> &additionalAttributes)
+      : histogram(std::move(histogram)) {
+    for (auto &attr : additionalAttributes) {
+      attributes[attr.first().str()] = attr.second;
+    }
+  }
 
   std::unique_ptr<opentelemetry::metrics::Histogram<T>> histogram;
   opentelemetry::context::Context context{};
@@ -109,7 +158,10 @@ public:
     if (histogram) {
       auto end = ClockType::now();
       auto duration = std::chrono::duration_cast<DurationT>(end - start);
-      histogram->Record(duration.count(), context);
+      auto attrs =
+          opentelemetry::common::KeyValueIterableView<decltype(attributes)>{
+              attributes};
+      histogram->Record(duration.count(), attrs, context);
     }
   }
 
@@ -119,14 +171,18 @@ public:
 private:
   friend class TelemetryContext;
 
-  /// TODO: Allow passing in attributes to attach to the recorded entry.
-  Timer(std::unique_ptr<opentelemetry::metrics::Histogram<T>> histogram)
+  Timer(std::unique_ptr<opentelemetry::metrics::Histogram<T>> histogram,
+        const llvm::StringMap<MetricAttributeValue> &additionalAttributes)
       : histogram(std::move(histogram)) {
+    for (auto &attr : additionalAttributes) {
+      attributes[attr.first().str()] = attr.second;
+    }
     start = ClockType::now();
   }
 
   std::unique_ptr<opentelemetry::metrics::Histogram<T>> histogram;
   opentelemetry::context::Context context{};
+  AttributeMap attributes;
   /// The start time.
   TimePointType start;
 };
