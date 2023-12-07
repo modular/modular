1a6c0da69f11fd9bfd3c109de50281392ce0df22
Mark Shields <110413041+mbs-modular@users.noreply.github.com>
2023-12-06 19:48:57 -0700

    [MO] Switch `mo.while` to not be `IsolatedFromAbove` (#27258)
    This removes the `IsolatedFromAbove` interface from `mo.while`, and
    simplifies the lowering to `grt.while`, deferring most of the work to
    `fixupIsolatedFromAbove`. Also simplify the lowering of `mo.if` to
    `grt.if` to similarly depend on `fixupIsolatedFromAbove`.
    
    The upside is only those SSA values which must be 'updated' by the loop
    body need be spelled out in the input parameter list, and code which
    wants to just do syntactic use-def chasing can 'see' across `mo.while`
    boundaries for the 'unchanged' values.
    
    The downside is all `mo.while` operands must be 'renamed' using an `as`
    clause. Eg:
    ```
      %x = ...
      %y = ...
      mo.while (%x as %xi: !foo) {
        ... %xi ...   // ok, 'current' version
        ... %y ...    // ok, outer version
        ... %x ...    // ok (**), outer version
      } do {
        %new_x = some.op(%xi)
        mo.yield %new_x
      }
    ```
    
    (**) Note it's not possible to reject this since passes are free to
    introduce references to outer values inside the while body independently
    of the while signature.
    
    While in there add the forgotten `HasOnlyGraphRegion` interface to
    `mo.{while,if}`.
    
    I've *not* updated:
     - ONNX/lib/Conversion/MONNXToMO/ControlFlowOps.cpp
     - PyTorch/lib/Conversion/TorchToMO/ControlFlowOps.cpp
     - TensorFlow/lib/Conversion/TFToMO/TFToMO.cpp
    
    to remove the free-variable binding logic to exploit the relaxed
    constraints. Some of that code does more than just free-var binding so
    decided to leave alone.
    
    MODULAR_ORIG_COMMIT_REV_ID: 5ecee975a5071ab2eb34aa96fbb5b0d985ecd661

--- Support/include/Support/Compiler/ParserUtils.h
+++ Support/include/Support/Compiler/ParserUtils.h
@@ -15,19 +15,42 @@
 
 namespace M {
 
-/// Parses a parenthesized operand list with optional 'as' clauses. Each
-/// operand contributes both to the operation state's operand list and to
+/// Parses a region, using argumentInfo as the block arguments. If
+/// isIsolatedFromAbove is true then it is legal for argumentInfo SSA values
+/// to shadow those already bound in an outer scope.
+ParseResult parseRegion(OpAsmParser &parser,
+                        const SmallVector<OpAsmParser::Argument> &argumentInfo,
+                        Region &region, bool isIsolatedFromAbove);
+
+/// Prints a region. If isIsolatedFromAbove is true then it is legal for the
+/// region's argument to shadow the given operands.
+void printRegion(OpAsmPrinter &printer, const OperandRange &operands,
+                 Region &region, bool isIsolatedFromAbove);
+
+/// Parses a parenthesized operand list with optional or required 'as' clauses.
+/// Each operand contributes both to the operation state's operand list and to
 /// the argumentInfo array to be used by the op's nested blocks.
+///
+/// If isIsolatedFromAbove is true then the 'as' clauses are aptional.
+/// Otherwise the op must spell out the mapping from 'outer' bound SSA values
+/// to 'inner' argument SSA values.
 ///
 /// Eg: Basic form:
 ///   (%a: i32, %b: f32)
 /// Eg: With corresponding block argument name distinct from the operand name
 ///   (%a: i32, %a as %b: f32)
-///
-/// Intended to pair with parseRegionWithShadowing for nested regions.
 ParseResult parseParenOperandListWithShadowing(
     OpAsmParser &parser, OperationState &state,
-    SmallVectorImpl<OpAsmParser::Argument> &argumentInfo);
+    SmallVectorImpl<OpAsmParser::Argument> &argumentInfo,
+    bool isIsolatedFromAbove);
+
+/// Prints a parenthesized operand list, matching the syntax parsed by
+/// parseParenOperandListWithShadowing.
+///
+/// Intended to pair with printRegionWithShadowing for nested regions.
+void printParenOperandListWithShadowing(
+    OpAsmPrinter &printer, const OperandRange &operands,
+    const Block::BlockArgListType &arguments, bool isIsolatedFromAbove);
 
 /// Parses a parenthesized operand list with optional type annotations.
 /// The given optional type is used if no type annotation is given.
@@ -40,34 +63,11 @@ ParseResult parseParenOperandListWithDefaultType(OpAsmParser &parser,
                                                  OperationState &state,
                                                  Type defaultType);
 
-/// Parses a region, using argumentInfo as the block arguments, which are
-/// allowed to shadow outer SSA values.
-///
-/// The operands and argumentInfo are expected to have come from
-/// parseParenOperandList.
-ParseResult
-parseRegionWithShadowing(OpAsmParser &parser,
-                         const SmallVector<OpAsmParser::Argument> &argumentInfo,
-                         Region &region);
-
-/// Prints a parenthesized operand list, matching the syntax parsed by
-/// parseParenOperandListWithShadowing.
-///
-/// Intended to pair with printRegionWithShadowing for nested regions.
-void printParenOperandListWithShadowing(
-    OpAsmPrinter &printer, const OperandRange &operands,
-    const Block::BlockArgListType &arguments);
-
 /// Prints a parenthesized operand list, matching the syntax parsed by
 /// parseParenOperandListWithDefaultType.
 void printParenOperandListWithDefaultType(OpAsmPrinter &printer,
                                           const OperandRange &operands,
                                           Type defaultType);
-
-/// Prints a region. If possible, block argument names will shadow the names
-/// of operands.
-void printRegionWithShadowing(OpAsmPrinter &printer,
-                              const OperandRange &operands, Region &region);
 
 /// Parses an 'in/out signature' of the form:
 ///    ( in|out|mut %x : type, ... )
