4d327e1ca5cbc047c5efd192ae3301e62fb94d09
Mark Shields <110413041+mbs-modular@users.noreply.github.com>
2023-12-12 10:44:46 -0700

    [LLCL][NOT FOR REVIEW] Support notion of 'current runtime', and expose it to Mojo (#27437)
    [NOT READY FOR REVIEW. Want some bake time in ci first.]
    
    Currently, we rely on the KGEN::OutputChain to convey context down into
    Mojo parallelize, including on which runtime the sub-tasks should be
    launched into. We want to get rid of that, and instead allow parallelize
    to just use the Mojo 'global runtime'. However, when Mojo is used via
    the C API, the Runtime has already been established and used
    pervasively. Allow that Runtime to be picked up.
    
     - Cleanup the LLCL globals handling.
    - Introduce LLCL::CompactRuntimePtr::{get,set}CurrentRuntime so the
    'owning' runtime can be associated with a thread.
    - Introduce WorkQueue::associateRuntime to tell a work queue which
    runtime to associated with its threads.
    - Introduce KGEN_CompilerRT_LLCL_GetCurrentRuntime to return the current
    runtime associated with the callers thread.
     - Update the Mojo _get_global_runtime to use the above when available.
    
    In this way we can continue to have multiple concurrently-active
    Runtimes (which is already baked into the C API), but avoid Mojo having
    to be exposed to any of that complexity.
    
    (For the record I think we should just have one global Runtime, but one
    step at a time.)
    
    MODULAR_ORIG_COMMIT_REV_ID: 6d17e196f9af7ad40e49bc12ee09d75f0cd7fbed

--- LLCL/lib/Runtime/Runtime.cpp
+++ LLCL/lib/Runtime/Runtime.cpp
@@ -11,7 +11,7 @@
 #include "LLCL/Runtime/Runtime.h"
 #include "LLCL/Runtime/Allocator.h"
 #include "LLCL/Runtime/AsyncValueRef.h"
-#include "LLCL/Runtime/Globals/CompactRuntimeTable.h"
+#include "LLCL/Runtime/CompactRuntimePtr.h"
 #include "LLCL/Runtime/WorkQueue.h"
 #include "LLCL/Support/Chain.h"
 #include "llvm/ADT/ArrayRef.h"
@@ -47,8 +47,10 @@ Runtime::Runtime(std::unique_ptr<Allocator> allocator,
     : signature(TypeID::getSignature() ^ CompactRuntimePtr::getSignature()),
       allocator(std::move(allocator)), workQueue(std::move(workQueue)),
       profileFilename(profileFilename),
-      runtimeIndex(M::LLCL::Globals::addRuntime(this)),
+      runtimeIndex(Detail::RuntimeTable::getSingleton().addRuntime(this)),
       readyChain(createReadyChain(*this)) {
+  this->workQueue->associateRuntime(CompactRuntimePtr(runtimeIndex));
+
   // NOTE: Users can't pass in profileFilename AND activate the time
   // profiler in the caller.
   if (!profileFilename.empty())
@@ -64,7 +66,7 @@ Runtime::~Runtime() {
 
   // Clear cancellation value if present.
   restartFromCancellation();
-  M::LLCL::Globals::clearRuntime(runtimeIndex);
+  Detail::RuntimeTable::getSingleton().clearRuntime(runtimeIndex);
 
   // We're done with profiling.
   if (profiler) {
