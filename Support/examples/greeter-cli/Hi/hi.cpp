ee756fb796736ddc9aae8d24f948d4e295f24ecc
Brian Gesiak <brian@modular.com>
2023-12-13 14:50:57 -0500

    [Support] Add docs and examples for writing CLIs (#27693)
    This adds a "CLI Guide" doc that explains the tools we use at Modular
    for writing command line interfaces.
    
    The bulk of the documentation is actually in the form of an example CLI
    that is also added by this patch: `greeter-cli`. The source code in the
    example can be copy-pasted to quickly create a new CLI application at
    Modular, and it contains code comments that explain what it's doing and
    how to modify it.
    
    (Note that unlike examples in the LLVM and MLIR project, this patch
    doesn't add CMake options to include its targets or build it
    conditionally, such as `LLVM_BUILD_EXAMPLES` -- the example is living
    documentation, and it should be built in CI when all other targets are
    built. I could add an option, but then I'd just update our CI workflows
    to enable that option most of the time, which is silly.)
    
    Fixes #16240.
    
    MODULAR_ORIG_COMMIT_REV_ID: e947a918af154243647e3c0b310f44bf5bd81151

---
+++ Support/examples/greeter-cli/Hi/hi.cpp
@@ -0,0 +1,77 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#include "hi.h"
+#include "Support/Driver/DriverSupport.h"
+#include "llvm/Option/ArgList.h"
+#include "llvm/Option/OptTable.h"
+#include "llvm/Option/Option.h"
+#include "llvm/Support/FormatVariadic.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace M;
+
+// As with the top-level `greeter-cli.cpp`, we must generate the C++ boilerplate
+// that LLVMOption requires for option parsing.
+#define DRIVER_OPTIONS_PATH "Hi/HiOptions.inc"
+#include "Support/Driver/OptTable.inc"
+
+namespace {
+struct HiOptTable : public llvm::opt::PrecomputedOptTable {
+  HiOptTable() : llvm::opt::PrecomputedOptTable(InfoTable, PrefixTable) {}
+};
+} // namespace
+
+static int hi(const State &state) {
+  // The top-level main function only parses the first argument, to determine
+  // which subcommand to invoke. We parse the remaining arguments here.
+  HiOptTable options;
+  unsigned missingIndex = 0;
+  unsigned missingCount = 0;
+  llvm::opt::InputArgList args =
+      options.ParseArgs(state.arguments, missingIndex, missingCount);
+
+  // Before anything else, if the `--help` option appears anywhere, print help
+  // text and exit successfully. We want people to be able to add `--help`
+  // anywhere in their CLI invocation and get help, even if other arguments are
+  // invalid.
+  if (args.hasArg(options::OPT_help)) {
+    return state.printHelp(
+#include "Hi/HiOptionsHelpText.inc"
+    );
+  }
+
+  // Exit unsuccessfully if we have unrecognized options anywhere.
+  // (The flipside of being able to write any kind of command line parsing
+  // behavior we wish is that rejecting incorrect options is not done "for you"
+  // at any point; you must include explicit code such as this. After all, some
+  // CLI wish to `--allow any -and --all /arguments`.)
+  if (int result = state.rejectUnknownArguments(args, options::OPT_UNKNOWN))
+    return result;
+
+  // Our `hi` subcommand can take a single subject of a greeting; verify that we
+  // have only one, and then print the greeting.
+  std::vector<std::string> inputs = args.getAllArgValues(options::OPT_INPUT);
+  if (inputs.size() > 1)
+    return state.reportError(llvm::formatv(
+        "too many things to greet, cannot handle both '{0}' and '{1}'",
+        inputs[0], inputs[1]));
+
+  std::string greeting = "Hi";
+  if (args.hasArg(options::OPT_hello))
+    greeting = "Hello";
+
+  if (inputs.empty())
+    llvm::outs() << greeting << "!\n";
+  else
+    llvm::outs() << greeting << ", " << inputs.front() << "!\n";
+
+  return EXIT_SUCCESS;
+}
+
+void M::registerHiSubcommand(SubcommandRegistry &registry) {
+  registry.addCallback("hi", hi);
+}
