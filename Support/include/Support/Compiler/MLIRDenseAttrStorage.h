b862b4f686d877734e2703a0d12212c6790e5f02
Brian Gesiak <brian@modular.com>
2024-01-03 12:31:27 -0500

    [Support] Fix "the an array" typo (NFC) (#28365)
    The docblock for `shouldUseOutOfLineAttrStorage` contains a typo "the an
    array" -- fix it to just "an", and reflow the paragpraph line breaks.
    
    MODULAR_ORIG_COMMIT_REV_ID: c440e861237602fa062c3a79b78e96b6b887467a

--- Support/include/Support/Compiler/MLIRDenseAttrStorage.h
+++ Support/include/Support/Compiler/MLIRDenseAttrStorage.h
@@ -10,10 +10,10 @@
 #include "Support/LLVMCompilerForwardDecls.h"
 
 namespace M {
-/// Returns true if the an array with the given number of elements is
-/// sufficiently large that out-of-line storage should be used. This indicates
-/// to the caller that the data is big enough to treat specially, e.g. that it
-/// shouldn't be stored in the MLIRContext, folded unconditionally, etc.
+/// Returns true if an array with the given number of elements is sufficiently
+/// large that out-of-line storage should be used. This indicates to the caller
+/// that the data is big enough to treat specially, e.g. that it shouldn't be
+/// stored in the MLIRContext, folded unconditionally, etc.
 inline bool shouldUseOutOfLineAttrStorage(size_t numElements) {
   // A sufficiently large element threshold is used to avoid treating large
   // arrays as "free". The storage, constant folding, etc. of large arrays
