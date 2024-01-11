c88d84aa39fac2ea6674be36049323b8eaac2021
Juan Galvez <juan@modular.com>
2024-01-11 15:33:52 -0600

    [Support][Frameworks] Change StatsReport API and add op failures (#28791)
    This makes some changes to the StatsReport API that is used to
    collect op statistics during lowering for all frameworks, to
    make it clearer and easier to use, and add capability to record
    op failures.
    
    Previously, countOp() would be used to record any instance of an
    op encountered during lowering (whether it is lowered or not, goes
    to fallback, etc). This IMO made the API confusing because
    countOp and countFallback were overlapping events, so you would
    always call countOp and additionally call countFallback if it
    went to fallback. Besides the confusion, it also makes it harder
    to use from ONNX and PT.
    
    Instead, the API is now countLowering(), countFallback() and
    countFailure(), which for a particular instance of an op only one
    can occur (it is either lowered to MO, goes to fallback or fails).
    
    MODULAR_ORIG_COMMIT_REV_ID: 4391a1595e48aaad2b5910d28cfe4b22922519da

--- Support/include/Support/Frameworks/StatsReport.h
+++ Support/include/Support/Frameworks/StatsReport.h
@@ -20,35 +20,40 @@ namespace M::Frameworks {
 
 /// A helper API that can collect statistics about ops lowered vs. fallback
 /// ops during an external framework translation pass. For the fallback ops,
-/// counts for each separate signature are gathered as well.
+/// counts for each separate *signature* (not just op name) are gathered as
+/// well.
 ///
 /// The helper also has the functionality to record these statistics to a
 /// configurable report file.
 struct StatsReport {
   explicit StatsReport(llvm::StringRef framework, llvm::StringRef modelName)
-      : framework(framework), modelName(modelName), numTotalOps(0),
-        numFallbackOps(0) {}
+      : framework(framework), modelName(modelName), numLoweredOps(0),
+        numFallbackOps(0), numFailedOps(0) {}
 
   StatsReport(llvm::StringRef framework) : StatsReport(framework, "") {}
 
-  /// Record one instance of an input op.
-  void countOp(mlir::Operation &op);
+  /// Record one instance of an input op lowered to MO.
+  void countLowering(mlir::Operation &op);
   /// Record one instance of an op that was sent to fallback.
   void countFallback(mlir::Operation &op);
+  /// Record one instance of an op lowering+fallback failure.
+  void countFailure(mlir::Operation &op);
   /// Write the statistics to file.
   void writeToFile();
 
-  // Emits telemetry info on the ops collected
+  // Emits telemetry info on the ops collected.
   void emitTelemetry(M::Telemetry::TelemetryContext *telemetryCtx);
 
 private:
   /// Framework name: pytorch, tf, onnx, etc.
   std::string framework;
   std::string modelName;
-  size_t numTotalOps;
+  size_t numLoweredOps;
   size_t numFallbackOps;
+  size_t numFailedOps;
+  llvm::StringMap<size_t> loweredHistogram;
   llvm::StringMap<size_t> fallbackHistogram;
-  llvm::StringMap<size_t> opHistogram{};
+  llvm::StringMap<size_t> failureHistogram;
 };
 
 } // namespace M::Frameworks
