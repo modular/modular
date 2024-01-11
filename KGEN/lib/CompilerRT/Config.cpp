d24fce0b66ad639fd32bf339d208bc0c1d2c3881
Aditya Bhagwat <adityab@modular.com>
2024-01-11 17:15:18 +0530

    [MIN] Fix memory leak in MOBI and Engine bindings (#28726)
    While there also address remaining comments from stack:
    [Internal link]
    
    MODULAR_ORIG_COMMIT_REV_ID: 53083f92d229552fbdf9a05cf20c8fccf0f3c487

--- KGEN/lib/CompilerRT/Config.cpp
+++ KGEN/lib/CompilerRT/Config.cpp
@@ -12,7 +12,7 @@ using namespace M;
 
 COMPILERRT_EXPORT COMPILERRT_VISIBILITY_EXPORT const char *
 KGEN_CompilerRT_getConfigValue(const char *key) {
-  llvm::StringRef value;
+  StringRef value;
   ErrorOr<Config> configOr = Config::open();
   if (configOr.isError())
     value = "";
@@ -22,8 +22,8 @@ KGEN_CompilerRT_getConfigValue(const char *key) {
   // getValue may return empty string if key does not exist,
   // in which case we finally return an empty string.
   value = cfg.getValue(key);
-  char *cvalue = static_cast<char *>(
-      alignedAlloc(kPreferredMemoryAlignment, value.size() + 1));
+  char *cvalue =
+      (char *)alignedAlloc(kPreferredMemoryAlignment, value.size() + 1);
   strcpy(cvalue, value.str().c_str());
   return cvalue;
 }
