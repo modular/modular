465dc16cb55137bf571bd11a23a538c4c1b445a7
Arjun Surendran <arjun@modular.com>
2024-01-24 18:39:39 -0800

    [Support][GenericML] Move Shared library name functions to support (#29842)
    These are useful and nothing ML specific in it. So move them to Support. While there do a little switcharoo from macro to functions.
    
    MODULAR_ORIG_COMMIT_REV_ID: ac0b08971bfab20b4b557f2f98f75415eebfa375

---
+++ Support/lib/PlatformLibNames.cpp
@@ -0,0 +1,22 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#include "Support/PlatformLibNames.h"
+#include "llvm/ADT/StringRef.h"
+
+using namespace M;
+
+std::string PlatformLibrary::getSharedLibraryName(llvm::StringRef name) {
+  auto libName =
+      std::string(SHARED_LIBRARY_PREFIX) + name.str() + SHARED_LIBRARY_SUFFIX;
+  return libName;
+}
+
+std::string PlatformLibrary::getStaticLibraryName(llvm::StringRef name) {
+  auto libName =
+      std::string(STATIC_LIBRARY_PREFIX) + name.str() + STATIC_LIBRARY_SUFFIX;
+  return libName;
+}
