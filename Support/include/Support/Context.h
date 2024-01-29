7de1ba87c76a463c5174677381f5af790c0a4926
Aman LaChapelle <aman.lachapelle@gmail.com>
2024-01-29 08:37:08 -0800

    [Support] Add M::Context object. (#29739)
    This patch adds the M::Context object with the eventual goal of replacing the `LLCL::Runtime::*Context` functions. For now, this patch doesn't migrate usage to the new object, but we will begin to use it with systems like Entitlements.
    
    For additional context, see [Internal link]
    
    MODULAR_ORIG_COMMIT_REV_ID: 27ae3ed1c27f4f049cddf2b2be8c21215d52f9f7

---
+++ Support/include/Support/Context.h
@@ -0,0 +1,59 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef SUPPORT_CONTEXT_H
+#define SUPPORT_CONTEXT_H
+
+#include "Support/ADT/GenericUniquePtrSet.h"
+#include "Support/ErrorOr.h"
+#include "llvm/ADT/FunctionExtras.h"
+
+namespace M {
+class Context {
+public:
+  /// Transfers ptr into the context object set.
+  template <typename T>
+  void set(std::unique_ptr<T> ptr) {
+    storage.set<T>(std::move(ptr));
+  }
+
+  /// Emplaces a new object of type T into the context object set and returns a
+  /// reference to it.
+  template <typename T, typename... Args>
+  T &emplace(Args &&...args) {
+    return storage.emplace<T, Args...>(std::forward<Args>(args)...);
+  }
+
+  /// Returns a reference to the object of type T held by the context object
+  /// set. If it does not contain such an object, emplaces a new object and
+  /// returns a reference to it.
+  template <typename T, typename... Args>
+  T &emplaceIfMissing(Args &&...args) {
+    return storage.emplaceIfMissing<T, Args...>(std::forward<Args>(args)...);
+  }
+
+  /// Returns a pointer to the object of type T held by the context object set.
+  /// If it does not contain such an object, calls the creator function to
+  /// create one and install. Returns any error the creator function returns.
+  template <typename T>
+  ErrorOr<T *> createIfMissing(
+      llvm::unique_function<ErrorOr<std::unique_ptr<T>>()> creator) {
+    return storage.createIfMissing<T>(std::move(creator));
+  }
+
+  /// Returns a pointer to the context object of type T held by the context
+  /// object set, or nullptr if no such object exists.
+  template <typename T>
+  T *get() {
+    return storage.get<T>();
+  }
+
+private:
+  GenericUniquePtrSet storage;
+};
+} // namespace M
+
+#endif // SUPPORT_CONTEXT_H
