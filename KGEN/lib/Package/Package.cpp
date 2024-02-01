8231389bea6db581f90c6728b38d96beb87925a8
Jeff Niu <jeff@modular.com>
2024-01-31 16:47:40 -0800

    [KGEN] Split out a PackageUtils.cpp (NFC) (#30495)
    And move MaterializePackages.cpp back to KGENTransforms. This should
    hopefully fix this tricky circular dependency.
    
    MODULAR_ORIG_COMMIT_REV_ID: 3872e5c80709526542cd55a10a0fb2230acef150

--- KGEN/lib/Package/Package.cpp
+++ KGEN/lib/Package/Package.cpp
@@ -9,6 +9,7 @@
 #include "KGEN/Compiler/ObjectCompiler.h"
 #include "KGEN/KGENDialect/KGENOps.h"
 #include "KGEN/ToolCommon/KGENPasses.h"
+#include "KGEN/Transforms/PackageUtils.h"
 #include "LLCL/Runtime/Algorithms.h"
 #include "LLCL/Runtime/Runtime.h"
 #include "Support/Buffer.h"
@@ -23,91 +24,6 @@
 
 using namespace M;
 using namespace KGEN;
-
-//===----------------------------------------------------------------------===//
-// loadPreElaboratedModuleBytecode
-//===----------------------------------------------------------------------===//
-
-/// Load the given bytecode module and return it.
-static ErrorOr<OwningOpRef<ModuleOp>>
-loadPreElaboratedModuleBytecode(DenseResourceElementsAttr bytecodeAttr) {
-  mlir::AsmResourceBlob *blob = bytecodeAttr.getRawHandle().getBlob();
-  if (!blob)
-    return Error("unable to find the pre-elaborated module blob");
-  ArrayRef<char> bytecode = blob->getData();
-  llvm::MemoryBufferRef bufferRef(StringRef(bytecode.begin(), bytecode.size()),
-                                  "");
-
-  // Read the entirety of the bytecode in the buffer; no lazy loading.
-  auto sourceMgr = std::make_shared<llvm::SourceMgr>();
-  mlir::ParserConfig parserConfig(bytecodeAttr.getContext());
-  mlir::BytecodeReader reader(bufferRef, parserConfig, /*lazyLoad=*/false,
-                              sourceMgr);
-  Block block;
-  if (failed(reader.readTopLevel(&block)))
-    return Error("unable to read pre-elaborated module blob");
-
-  // Take ownership of the parsed module by removing it from the block so that
-  // we can return it.
-  ModuleOp module = cast<ModuleOp>(block.front());
-  module->remove();
-  return module;
-}
-
-//===----------------------------------------------------------------------===//
-// loadPreElaboratedModuleForLinking
-//===----------------------------------------------------------------------===//
-
-ErrorOr<OwningOpRef<ModuleOp>> M::KGEN::loadPreElaboratedModuleForLinking(
-    DenseResourceElementsAttr bytecodeAttr) {
-  ErrorOr<OwningOpRef<ModuleOp>> packageModuleOr =
-      loadPreElaboratedModuleBytecode(bytecodeAttr);
-  if (packageModuleOr.isError())
-    return packageModuleOr.takeError();
-
-  // Strip the implicit package exports, we don't need these because we're going
-  // to link the package into an existing module as-is.
-  for (ExportInterface op : (*packageModuleOr)->getOps<ExportInterface>())
-    if (op.isPackageExported())
-      op.setNotExported();
-
-  // Materialize all of the dependent packages now, making sure they also
-  // get linked in properly.
-  mlir::PassManager pm((*packageModuleOr)->getContext());
-  pm.addPass(createMaterializePackages());
-  if (failed(pm.run(**packageModuleOr)))
-    return Error("unable to materialize dependent packages");
-
-  return std::move(*packageModuleOr);
-}
-
-//===----------------------------------------------------------------------===//
-// loadPreElaboratedBytecodeForLinking
-//===----------------------------------------------------------------------===//
-
-ErrorOr<DenseResourceElementsAttr> M::KGEN::loadPreElaboratedBytecodeForLinking(
-    DenseResourceElementsAttr bytecodeAttr) {
-  ErrorOr<OwningOpRef<ModuleOp>> packageModuleOr =
-      loadPreElaboratedModuleForLinking(bytecodeAttr);
-  if (packageModuleOr.isError())
-    return packageModuleOr.takeError();
-
-  // Write the package bytecode to the given buffer. This will be attached to
-  // the exported high level functions.
-  WriteableBufferRef str = WriteableBuffer::get();
-  if (failed(mlir::writeBytecodeToFile(**packageModuleOr, *str)))
-    return Error("could not write bytecode for package module");
-
-  // Hash the bytecode itself - this will give us a unique'd attr name that
-  // shouldn't clash even when a large number of packages get imported - and
-  // if they do clash, they're guaranteed to be exactly the same.
-  auto hash = llvm::BLAKE3::hash(
-      ArrayRef<uint8_t>((const uint8_t *)str->getBufferStart(),
-                        (const uint8_t *)str->getBufferEnd()));
-  return createResourceAttr((*packageModuleOr)->getContext(), str->getBuffer(),
-                            "bytecode_" +
-                                llvm::toHex(hash, /*LowerCase=*/true));
-}
 
 //===----------------------------------------------------------------------===//
 // createElaboratedBytecodeAttr
