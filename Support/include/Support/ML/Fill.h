2e83964ca78c2196dd591b860cf91a4436b09da7
abdul dakkak <adakkak@modular.com>
2024-02-01 19:18:18 -0800

    [Support] Move fill operations from Kernels/Misc to Support/ML (#30666)
    These operations are so primitive, self contained, and have really not
    too much to do with the "traditional" kernels we write in mojo. Moving
    them to support also simplifies the layering of GenericML
    
    Part of #30587
    
    MODULAR_ORIG_COMMIT_REV_ID: 9ebf2feff7931df66f553d733eeb91d88c70ab6d

---
+++ Support/include/Support/ML/Fill.h
@@ -0,0 +1,43 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef SUPPORT_ML_FILL_H
+#define SUPPORT_ML_FILL_H
+
+#include "Support/ML/DType.h"
+
+namespace M {
+//===----------------------------------------------------------------------===//
+// Scalar value generation
+//===----------------------------------------------------------------------===//
+
+/// This kernel fills the specified generic buffer with a single "1" or "1.0"
+/// real value.  Complex numbers have their imaginary component set to zero.
+ErrorOrSuccess getScalarOne(void *destPtr, DType eltType);
+
+/// This kernel fills the specified generic buffer with a single "-1" or "-1.0"
+/// real value.  Complex numbers have their imaginary component set to zero.
+ErrorOrSuccess getScalarNegativeOne(void *destPtr, DType eltType);
+
+//===----------------------------------------------------------------------===//
+// Memory Fills
+//===----------------------------------------------------------------------===//
+
+/// This kernel fills the specified generic buffer with a constant value
+/// specified by "element".  This returns a non-empty error on failure.
+ErrorOrSuccess fillHomogeneous(void *destPtr, size_t numElements, DType eltType,
+                               const void *elementPtr);
+
+/// This kernel fills the specified generic buffer with a random values.
+/// This returns a non-empty error on failure.
+///
+/// TODO: This is not implemented in a very general way, the bounds should be
+/// passed it or something.
+ErrorOrSuccess fillRandom(void *destPtr, size_t numElements, DType eltType);
+
+} // namespace M
+
+#endif // SUPPORT_ML_FILL_H
