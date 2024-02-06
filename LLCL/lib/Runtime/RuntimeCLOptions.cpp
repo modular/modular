5c6d7cd178644fb78eb380ba850056515f2cc20c
Laszlo Kindrat <laszlo@modular.ai>
2024-02-05 22:34:22 -0500

    [break-glass] Revert "[LLCL] Move RuntimeOptions::createRuntime to Runtime.cpp (#30629)" (#30979)
    This reverts commit bba61acfbb36065d55d81bc050a4fe8bbb264c46 because it
    breaks some tests in local builds due to a failing assert in LLCL. See
    [Internal link]
    
    Co-authored-by: Laszlo Kindrat <laszlo@modular.com>
    MODULAR_ORIG_COMMIT_REV_ID: fc8826060a99f4efadaaff31745ad78f7101cdc9

---
+++ LLCL/lib/Runtime/RuntimeCLOptions.cpp
@@ -0,0 +1,60 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#include "LLCL/Runtime/RuntimeCLOptions.h"
+#include "LLCL/Runtime/Runtime.h"
+
+using namespace M::LLCL;
+
+std::unique_ptr<Runtime>
+RuntimeOptions::createRuntime(StringRef profileName) const {
+  RuntimeOptions runtimeOptions; //{*this};
+  switch (allocatorType) {
+  case RuntimeOptions::AllocatorType::kMalloc:
+    break;
+  case RuntimeOptions::AllocatorType::kTCMalloc:
+    runtimeOptions.tcmallocAllocator = true;
+    break;
+  case RuntimeOptions::AllocatorType::kLeakChecker:
+    runtimeOptions.leakCheckedAllocator = true;
+    break;
+  case RuntimeOptions::AllocatorType::kProfiler:
+    runtimeOptions.profilingAllocator = true;
+    break;
+  case RuntimeOptions::AllocatorType::kUseAfterFree:
+#if defined(HAVE_MODULAR_USE_AFTER_FREE_ALLOCATOR)
+    runtimeOptions.useAfterFreeAllocator = true;
+#else
+    llvm::errs() << "The use-after-free allocator is not available for this "
+                    "target. Using the leak-checker runtime instead.";
+    options.leakCheckedAllocator = true;
+#endif
+    break;
+  }
+  // runtimeOptions.workQueueType = getWorkQueueType();
+  switch (getWorkQueueType()) {
+  case RuntimeOptions::WorkQueueType::kDefault:
+    assert(0 && "should be resolved");
+  case RuntimeOptions::WorkQueueType::kSingleThread:
+    runtimeOptions.singleThreaded = true;
+    break;
+  case RuntimeOptions::WorkQueueType::kThreadPool:
+    runtimeOptions.numThreads = numThreads;
+    runtimeOptions.threadBusyWaitTime = threadBusyWaitTime;
+#if MODULAR_PARANOID
+    options.paranoid = paranoid;
+#endif
+    break;
+  }
+  runtimeOptions.profileFilename = profileName;
+  // TODO(#28412): Ideally this would be createUniqueRuntime since it's silly to
+  // be asking to create a runtime from the current cl options when one has
+  // already been established, presumably via some other configuration
+  // mechanism. However modular-api-executor.cpp has already established a
+  // runtime via the C API when it invokes
+  // setupMLIRAndRunWithLeakCheckedRuntime.
+  return LLCL::createNestedRuntime(runtimeOptions);
+}
