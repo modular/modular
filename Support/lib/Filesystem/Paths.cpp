d73ca40b630555bef6508b2f43dfe67e05208833
Ahmed S. Taei <asaadaldien@users.noreply.github.com>
2024-02-06 09:07:07 -0800

    [Mojo] Disallow __init__.[mojo|🔥] directory names  (#31011)
    Disallow `__init__.mojo` and   `__init__.🔥` in a package.
    
    Fixes #29587
    
    MODULAR_ORIG_COMMIT_REV_ID: 248fe7d93539c4f864311ee886f69713f03049bc

--- Support/lib/Filesystem/Paths.cpp
+++ Support/lib/Filesystem/Paths.cpp
@@ -12,8 +12,11 @@ using namespace Filesystem;
 bool M::Filesystem::isMojoSourcePackagePath(const std::filesystem::path &path) {
   std::error_code ec;
   if (std::filesystem::is_directory(path, ec) && !ec) {
-    return std::filesystem::exists(path / "__init__.mojo", ec) ||
-           std::filesystem::exists(path / "__init__.🔥", ec);
+    bool exist = std::filesystem::exists(path / "__init__.mojo", ec) ||
+                 std::filesystem::exists(path / "__init__.🔥", ec);
+    bool isDir = std::filesystem::is_directory(path / "__init__.mojo", ec) ||
+                 std::filesystem::is_directory(path / "__init__.🔥", ec);
+    return exist && !isDir;
   }
   return false;
 }
