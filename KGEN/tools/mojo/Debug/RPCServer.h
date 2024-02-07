f711f86fbcf9550e613e53e4feeb657be22fc05d
Walter Erquinigo <walter@modular.com>
2024-02-06 18:07:06 -0800

    [Mojo][Debug] Implement the RPC mode for `mojo debug` (#30964)
    This replicates the behavior of `vscode-debug` in `mojo debug`.
    
    MODULAR_ORIG_COMMIT_REV_ID: 02de6c329687205b743fdbfccb8e8aa7a16567e4

---
+++ KGEN/tools/mojo/Debug/RPCServer.h
@@ -0,0 +1,32 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef MOJO_DEBUG_RPC_SERVER_H
+#define MOJO_DEBUG_RPC_SERVER_H
+
+#include "Support/ErrorOr.h"
+#include "Support/LLVMForwardDecls.h"
+#include <optional>
+
+namespace M {
+/// Starts an `attach` debug session with an existing RPC debug server.
+/// If `dryRun` is specified, then the request payload is printed to the
+/// standard output instead.
+ErrorOrSuccess invokeAttachRPC(bool dryRun, int rpcPort,
+                               const std::optional<StringRef> &secret,
+                               const std::optional<StringRef> &pid,
+                               const std::optional<StringRef> &processName);
+
+/// Starts a `launch` debug session with an existing RPC debug server.
+/// If `dryRun` is specified, then the request payload is printed to the
+/// standard output instead.
+ErrorOrSuccess invokeLaunchRPC(bool dryRun, int rpcPort,
+                               const std::optional<StringRef> &secret,
+                               StringRef target, ArrayRef<std::string> runArgs,
+                               StringRef rpcTerminal);
+} // namespace M
+
+#endif // MOJO_DEBUG_RPC_SERVER_H
