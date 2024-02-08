727b6149961e2968f6f6ee116f611787da15617c
Aman LaChapelle <aman.lachapelle@gmail.com>
2024-02-08 09:43:37 -0800

    [Support][Settings] Clean up Settings impl. (#30480)
    MODULAR_ORIG_COMMIT_REV_ID: 4a5116d498f9bc185e26d33dbdac1a68ab1395da

--- Support/include/Support/Settings/Settings.h
+++ Support/include/Support/Settings/Settings.h
@@ -11,6 +11,9 @@
 #include "Support/Settings/Setting.h"
 
 namespace M {
+/// Forward-declare the Context object.
+class Context;
+
 /// This class provides the collection of system settings. Users should use the
 /// `open` method to get the current settings, and should use `refresh` when
 /// it's necessary to do so. Refreshing the settings means intentionally looking
@@ -22,11 +25,36 @@ namespace M {
 /// the carrier for both signals is exactly the same.
 class Settings {
 public:
-  /// Open the current system settings.
-  static ErrorOr<Settings> open(HTTPClient *client);
+  /// Explicit destructor so that the private impl can be handled correctly.
+  ~Settings();
+
+  /// Define the various constructors - this object must be default
+  /// constructible, it must be non-copyable, and it must be move-able.
+  Settings() = default;
+  Settings(const Settings &other) = delete;
+  Settings(Settings &&other) = default;
+
+  /// Open the current system settings and emplace them in the context object.
+  /// If we don't have secured settings and `createIfMissing` is false, then we
+  /// will not fetch the secured settings. To be concrete, there are 4 states
+  /// that we could be in:
+  ///  (1) No secured settings, don't bother getting them (this should go away).
+  ///  (2) No secured settings, get them.
+  ///  (3) Have secured settings, use them.
+  ///  (4) Have secured settings, fetch them again (this is refresh).
+  /// `createIfMissing` selects between states (1) and (2). State (3) is always
+  /// the default.
+  static ErrorOrSuccess emplace(Context &ctx, HTTPClient *client,
+                                bool createIfMissing = false);
 
   /// Get a setting by its key. This corresponds to Config::getValue.
-  const Setting *get(StringRef key);
+  const Setting *get(StringRef key) const;
+
+  /// Get a setting of type T by its key. Asserts that the setting is of type T.
+  template <typename T>
+  T get(StringRef key) const {
+    return llvm::cast_if_present<T>(get(key));
+  }
 
   /// Refresh the settings if it's necessary to do so. This will refresh all
   /// configurations and the entitlement store. The user can configure a policy
