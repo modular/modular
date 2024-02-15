e08d17728ebe9ba68a3c1137273ac3bfe0af3def
Zheng Hao Tan <zheng@modular.com>
2024-02-14 19:08:42 -0800

    [Engine API] New model compilation progress bar that prints a dot every second and cycles after five dots (#31999)
    This PR introduces a new compilation progress bar that prints a dot
    every second, for up to 5 dots or seconds, repeats the pattern until
    model compilation is over.
    
    ## Related tickets:
    1. Fixes [Internal link]
    
    MODULAR_ORIG_COMMIT_REV_ID: e6c0ac4a122e8522c64821477048b0fcf49940b3

--- Support/lib/ProgressBar.cpp
+++ Support/lib/ProgressBar.cpp
@@ -5,17 +5,24 @@
 //===----------------------------------------------------------------------===//
 
 #include "Support/ProgressBar.h"
+#include "llvm/Support/raw_ostream.h"
 
 #include <chrono>
 #include <condition_variable>
 #include <mutex>
+#include <string>
 #include <thread>
 
 using namespace M;
 using namespace std::chrono_literals;
 
-// A thread safe progress bar that emits dots based on the configured update
-// period.
+namespace {
+
+constexpr std::chrono::seconds kDefaultRefreshPeriod{1};
+constexpr size_t kDefaultMaxBarLength{5};
+
+// A thread safe progress bar that emits dots based on the configured
+// update period.
 class ThreadSafeDotEmittingProgressBar : public ProgressBar {
 public:
   ThreadSafeDotEmittingProgressBar(llvm::raw_ostream &os) : os(os) {}
@@ -32,10 +39,15 @@ private:
   std::thread updater;
   std::mutex mu;
   std::condition_variable cv;
+  std::size_t currWidth = 0;
+  std::chrono::seconds refreshPeriod = kDefaultRefreshPeriod;
+  size_t maxBarLength = kDefaultMaxBarLength;
   bool enabled = false;
 
   std::chrono::time_point<std::chrono::system_clock> lastUpdate;
 };
+
+} // namespace
 
 void ThreadSafeDotEmittingProgressBar::enable() {
   std::lock_guard<std::mutex> lk(mu);
@@ -43,7 +55,6 @@ void ThreadSafeDotEmittingProgressBar::enable() {
     return;
 
   enabled = true;
-  os << "Compiling model";
   updater = std::thread([this] { this->update(); });
 }
 
@@ -74,14 +85,25 @@ void ThreadSafeDotEmittingProgressBar::emit(
   auto elapsedTime =
       std::chrono::duration_cast<std::chrono::seconds>(now - lastUpdate);
   if (elapsedTime >= refreshPeriod) {
-    os << ".";
+    if (currWidth >= maxBarLength)
+      currWidth = 0;
+    else
+      ++currWidth;
+
     lastUpdate = now;
+
+    std::string prefix{"Compiling model"};
+    llvm::raw_string_ostream ss(prefix);
+
+    // print dots up to maxBarLength, then goes back to zero dots and repeat.
+    ss << std::string(currWidth, '.');
+    ss << std::string(maxBarLength - currWidth, ' ');
+    os << "\r" << ss.str();
   }
 }
 
 ErrorOr<std::unique_ptr<ProgressBar>>
 M::makeProgressBar(llvm::raw_ostream &os) {
-  os.SetUnbuffered();
   std::unique_ptr<ProgressBar> r =
       std::make_unique<ThreadSafeDotEmittingProgressBar>(os);
   return std::move(r);
