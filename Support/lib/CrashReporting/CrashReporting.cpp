4bdb5f052965ee4df11417c88949e43713922e90
Adin Scannell <amscanne@modular.com>
2024-02-14 17:23:44 -0800

    [Support] [CrashReporting] Add settings support (#31723)
    This effectively adds support for setting up crashpad via the Settings object,
    instead of via a configuration. This is important for the eventual
    centralization of the context, which will set these things up at the same time.
    
    These functions are essentially copied, and the old versions will be deleted in
    a subsequent change in the same stack.
    
    MODULAR_ORIG_COMMIT_REV_ID: ed705a6f92435f4f98e94f5d89c5a1858efb8ad0

--- Support/lib/CrashReporting/CrashReporting.cpp
+++ Support/lib/CrashReporting/CrashReporting.cpp
@@ -9,6 +9,7 @@
 #include "Config/Version.h"
 #include "Support/Configuration.h"
 #include "Support/ErrorOr.h"
+#include "Support/Settings/Settings.h"
 
 #include "llvm/Support/Path.h"
 #include "llvm/Support/Program.h"
@@ -31,6 +32,16 @@ M::getCrashDatabasePath(Config &config,
   StringRef fromConfig = config.getValue("crash_reporting.database_path");
   if (!fromConfig.empty())
     return std::string_view(fromConfig);
+  return dataFolder / "crashdb";
+}
+
+std::filesystem::path
+M::getCrashDatabasePath(Settings &settings,
+                        const std::filesystem::path &dataFolder) {
+  auto *setting = settings.get("crash_reporting.database_path");
+  auto path = llvm::dyn_cast_if_present<StringRef>(setting);
+  if (!path.empty())
+    return std::string_view(path);
   return dataFolder / "crashdb";
 }
 
@@ -60,7 +71,7 @@ ErrorOr<std::filesystem::path> M::getCrashpadHandlerPath(Config &config,
   // case.
   if (program.empty()) {
     std::string mainExecutable = llvm::sys::fs::getMainExecutable(
-        argv0, reinterpret_cast<void *>(initCrashpadForProgram));
+        argv0, reinterpret_cast<void *>(generateNonFatalDump));
     StringRef parent = llvm::sys::path::parent_path(mainExecutable);
     if (!parent.empty()) {
       // N.B.: Errors from findProgramByName are intentionally ignored for the
@@ -76,6 +87,24 @@ ErrorOr<std::filesystem::path> M::getCrashpadHandlerPath(Config &config,
     // same reason as above.
     if (auto programOr = llvm::sys::findProgramByName(kHandlerProgramName))
       program = std::move(*programOr);
+  }
+  // No luck.
+  if (program.empty())
+    return Error("unable to locate crashpad handler executable");
+  return std::string_view(program);
+}
+
+ErrorOr<std::filesystem::path> M::getCrashpadHandlerPath(Settings &settings) {
+  auto *handlerPath = settings.get("crash_reporting.handler_path");
+  auto program = llvm::dyn_cast_if_present<StringRef>(handlerPath);
+  std::string foundProgram;
+  if (program.empty()) {
+    // N.B.: Errors from findProgramByName are intentionally ignored for the
+    // same reason as above.
+    if (auto programOr = llvm::sys::findProgramByName(kHandlerProgramName)) {
+      foundProgram = std::move(*programOr);
+      program = foundProgram;
+    }
   }
   // No luck.
   if (program.empty())
@@ -158,8 +187,80 @@ static ErrorOrSuccess tryInitCrashpad(const char *argv0, const char *program) {
   return success();
 }
 
+static ErrorOrSuccess tryInitCrashpad(StringRef program, Settings &settings) {
+  bool enabled = settings.getBool("crash_reporting.enabled", true);
+  if (!enabled)
+    return success();
+
+  // Crashpad needs a few paths and other configuration bits:
+  //   - Path of the handler executable (This runs alongside the Mojo driver;
+  //     in case the driver crashes, the handler inspects the driver in its
+  //     crashed state and generates a crash report)
+  //   - Crash database, to put the crashes in before they are sent off
+  //   - URL to upload crash reports to
+  auto dataFolderOr = Config::getModularDataFolderPath();
+  if (dataFolderOr.isError())
+    return dataFolderOr.takeError();
+  std::filesystem::path databasePath =
+      getCrashDatabasePath(settings, *dataFolderOr);
+
+  auto handlerPathOr = getCrashpadHandlerPath(settings);
+  if (handlerPathOr)
+    return Error(llvm::Twine("while locating crashpad handler: ") +
+                 handlerPathOr.getError());
+  std::filesystem::path handlerPath = std::move(*handlerPathOr);
+  auto *urlSetting = settings.get("crash_reporting.url");
+  auto url = llvm::dyn_cast_if_present<StringRef>(urlSetting);
+  std::string defaultURL;
+
+  // If the URL is empty, construct a URL by appending to the default URL
+  // above. This is one way to communicate a high-level categorization without
+  // having to dissemble the minidump on the server-side. However, most
+  // attributes should go into the attribute map below.
+  if (url.empty()) {
+    defaultURL = std::string(kDefaultURL) + "/" + std::string(program);
+    url = defaultURL;
+  }
+
+  // Update the database if we have a URL and reporting is not enabled. In most
+  // cases this will just read the existing database settings and not change.
+  if (!url.empty()) {
+    auto database =
+        crashpad::CrashReportDatabase::Initialize(base::FilePath(databasePath));
+    bool uploads_enabled = false;
+    if (database != nullptr && database->GetSettings() != nullptr &&
+        (!database->GetSettings()->GetUploadsEnabled(&uploads_enabled) ||
+         !uploads_enabled))
+      database->GetSettings()->SetUploadsEnabled(true);
+  }
+
+  // Setup all the annotations.
+  std::map<std::string, std::string> annotations;
+  annotations["program"] = std::string(program);
+  annotations["version"] = getModularVersionString();
+
+  // Launch Crashpad handler.
+  crashpad::CrashpadClient client;
+  if (!client.StartHandler(
+          base::FilePath(handlerPath), base::FilePath(databasePath),
+          /*metrics_dir=*/base::FilePath(databasePath), std::string(url),
+          /*annotations=*/annotations,
+          /*arguments=*/{}, /*restartable=*/true,
+          /*asynchronous_start=*/false))
+    return Error("crashpad failed to start handler");
+  return success();
+}
+
 void M::initCrashpadForProgram(const char *argv0, const char *program) {
   if (auto error = tryInitCrashpad(argv0, program))
+    llvm::errs() << "Failed to initialize Crashpad.  "
+                    "Crash reporting will not be available.  "
+                    "Cause: "
+                 << error.getError() << "\n";
+}
+
+void M::initCrashpadForProgram(StringRef program, Settings &settings) {
+  if (auto error = tryInitCrashpad(program, settings))
     llvm::errs() << "Failed to initialize Crashpad.  "
                     "Crash reporting will not be available.  "
                     "Cause: "
