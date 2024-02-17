e611e422d9b7bcd623252d2ee0d761b36a58f5f9
William G Hatch <william@hatch.uno>
2024-02-16 17:52:00 -0700

    [Support] add IPRational class in support of FloatLiteral (#30485)
    [Support] add IPRational class in support of FloatLiteral
    
    IE infinite precision rational numbers based on IPInt.  The overall
    design for FloatLiteral is here:
    
    [Internal link]
    
    The key rationale for using rational numbers is that all float
    literals are rational numbers, but many reasonable float literals like
    `0.2` have no finite binary float representation, meaning we would
    immediately have to choose a bit width for literals like that.
    
    MODULAR_ORIG_COMMIT_REV_ID: 51909d96ff2ca2941e4b616b973a3118e79b96a3

---
+++ Support/include/Support/IPRational.h
@@ -0,0 +1,71 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file defines an IPRational class, which uses two IPInts to
+// represent arbitrary precision rational numbers.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef SUPPORT_IPRATIONAL_H
+#define SUPPORT_IPRATIONAL_H
+
+#include "Support/IPInt.h"
+#include "llvm/ADT/Hashing.h"
+#include "llvm/Support/raw_ostream.h"
+
+namespace M {
+
+/// IPRational class for arbitrary precision rational numbers.  Represented as
+/// two IPInts, one for each of numerator and denominator.
+class IPRational {
+public:
+  /// Constructs a rational with the given numerator and denominator.  The user
+  /// is responsible for ensuring that the denominator is never zero.
+  IPRational(const IPInt n, const IPInt d);
+  /// Constructs a rational with the given numerator and denominator of 1.
+  IPRational(const IPInt &numerator);
+  /// Constructs a rational with same numerator and denominator as the given
+  /// rational.
+  IPRational(const IPRational &val);
+  /// Constructs a rational with denominator 1 and numerator from the given
+  /// APInt.
+  IPRational(const llvm::APInt &val);
+  /// Constructs a rational with denominator 1 and numerator from the given
+  /// uint.
+  IPRational(uint64_t val);
+  IPRational();
+
+  const IPInt &getNumerator() const;
+  const IPInt &getDenominator() const;
+
+  IPRational &operator=(const IPRational &rhs);
+
+  bool operator==(const IPRational &rhs) const;
+  bool operator!=(const IPRational &rhs) const;
+  bool operator<(const IPRational &rhs) const;
+  bool operator<=(const IPRational &rhs) const;
+  bool operator>(const IPRational &rhs) const;
+  bool operator>=(const IPRational &rhs) const;
+  IPRational operator+(const IPRational &rhs) const;
+  IPRational operator-(const IPRational &rhs) const;
+  IPRational operator-() const;
+  IPRational operator*(const IPRational &rhs) const;
+  IPRational operator/(const IPRational &rhs) const;
+  IPRational abs() const;
+
+  friend llvm::hash_code hash_value(const IPRational &arg);
+
+  friend llvm::raw_ostream &operator<<(llvm::raw_ostream &os,
+                                       const IPRational &arg);
+
+private:
+  IPInt numerator;
+  IPInt denominator;
+};
+
+} // namespace M
+
+#endif // SUPPORT_IPRATIONAL_H
