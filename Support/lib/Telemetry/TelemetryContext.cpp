ef9f89c358704a88de495d390497c0fefc73de78
Matt Terry <matt.terry@gmail.com>
2024-02-27 19:41:58 -0800

    [Telemetry] Add getLogger() that adds fixed attributes (#33063)
    This creates a Logger object that has many attributes pre-set. The
    emitted events will have the union of attributes from the
    logger_provider, logger, and event.
    
    This functionality will be useful for metering events that have a large
    number of fixed attributes. Instead of explicitly storing and merging
    the fixed & dynamic attributes, we can pre-set the fixed attributes on
    the logger itself.
    
    This also removes the duplicated `TelemetryContext::AttributeValue` in
    favor of `Logs::AttributeValue`
    
    MODULAR_ORIG_COMMIT_REV_ID: 97f0e8f8487acdde6560b592af670bbecdd7964b

--- Support/lib/Telemetry/TelemetryContext.cpp
+++ Support/lib/Telemetry/TelemetryContext.cpp
@@ -141,7 +141,7 @@ std::pair<std::string, std::string> M::Telemetry::createLocalIDs() {
 
 TelemetryContext::TelemetryContext(
     const EntitlementStore &entitlementStore,
-    const llvm::StringMap<TelemetryContext::AttributeValue> &resources,
+    const llvm::StringMap<M::Telemetry::Logs::AttributeValue> &resources,
     std::optional<Config> config) {
 #ifdef MODULAR_ENABLE_TELEMETRY
   using namespace opentelemetry::sdk::resource;
