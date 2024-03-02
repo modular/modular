3f2abc7353a3d1c98cc01a300a98bacdef50fa78
Tracy Sharpe <99041446+raiseirql@users.noreply.github.com>
2024-03-01 19:27:20 -0800

    [Integrate] Bump LLVM to 2c30180f72b9c39dda1fd77bcbec8022e19bec23 (#31536)
    Changes needed:
    
    - lldb::SyntheticChildrenFrontEnd::Update changed from returning a bool
    to an enum. Updated the code to use this enum.
    
    ---------
    
    Co-authored-by: Mogball <jeff@modular.com>
    MODULAR_ORIG_COMMIT_REV_ID: 431d824048f85a64f72cdb4e3e7ac956dda6c03b

--- KGEN/lib/MojoLLDB/Language/Formatters/MojoKGENVariantTypeFormatter.h
+++ KGEN/lib/MojoLLDB/Language/Formatters/MojoKGENVariantTypeFormatter.h
@@ -22,7 +22,7 @@ public:
 
   lldb::ValueObjectSP GetChildAtIndex(size_t idx) override;
 
-  bool Update() override;
+  lldb::ChildCacheState Update() override;
 
   bool MightHaveChildren() override;
 
