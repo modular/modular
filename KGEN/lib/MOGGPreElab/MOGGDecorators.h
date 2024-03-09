fd68ea79a84098bcd7c7fd82993a753f3690ae90
abdul dakkak <adakkak@modular.com>
2024-03-09 08:43:51 -0800

    [Kernels][Stdlib] Move utils._annotations into its own package (#34325)
    The _annotations package marks the kernels to be registrable in Mogg/Max
    and we do not want to open source them. This moves these functions out
    but still maintains the same package name as defined in
    [Internal link]
    
    Closes [Internal link]
    MODULAR_ORIG_COMMIT_REV_ID: 3f7cab6001f5194e149ed1752910cda47b7ba435

--- KGEN/lib/MOGGPreElab/MOGGDecorators.h
+++ KGEN/lib/MOGGPreElab/MOGGDecorators.h
@@ -13,29 +13,28 @@ namespace M::KGEN::MOGGPreElab {
 
 // The decorators we will look for on the generator to identify it as a MO
 // kernel.
-constexpr StringLiteral registerDecorator =
-    "stdlib::utils::_annotations::mogg_register";
+constexpr StringLiteral registerDecorator = "register::register::mogg_register";
 // TODO(#27757): Temporary as transition to Mojo async/await.
 constexpr StringLiteral willBecomeAsyncDecorator =
-    "stdlib::utils::_annotations::mogg_will_become_async";
+    "register::register::mogg_will_become_async";
 constexpr StringLiteral registerOverrideDecorator =
-    "stdlib::utils::_annotations::mogg_register_override";
+    "register::register::mogg_register_override";
 
 constexpr StringLiteral tensorAllocDecorator =
-    "stdlib::utils::_annotations::mogg_tensor_allocator";
+    "register::register::mogg_tensor_allocator";
 constexpr StringLiteral tensorCopyConstructDecorator =
-    "stdlib::utils::_annotations::mogg_tensor_copy_constructor";
+    "register::register::mogg_tensor_copy_constructor";
 constexpr StringLiteral tensorDeconstructDecorator =
-    "stdlib::utils::_annotations::mogg_tensor_deconstructor";
+    "register::register::mogg_tensor_deconstructor";
 
 constexpr StringLiteral elementwiseHook =
-    "stdlib::utils::_annotations::mogg_elementwise_hook";
+    "register::register::mogg_elementwise_hook";
 constexpr StringLiteral tensorEnableFusion =
-    "stdlib::utils::_annotations::mogg_enable_fusion";
+    "register::register::mogg_enable_fusion";
 constexpr StringLiteral tensorInputFusionHook =
-    "stdlib::utils::_annotations::mogg_input_fusion_hook";
+    "register::register::mogg_input_fusion_hook";
 constexpr StringLiteral tensorOutputFusionHook =
-    "stdlib::utils::_annotations::mogg_output_fusion_hook";
+    "register::register::mogg_output_fusion_hook";
 
 } // namespace M::KGEN::MOGGPreElab
 
