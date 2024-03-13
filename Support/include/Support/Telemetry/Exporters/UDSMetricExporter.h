cb75614ac0e70d9f5fa137fd9952162cbcb743a4
Rashid Kaleem <rashid@modular.com>
2024-03-13 06:42:41 -0500

    [otel] UDS metric exporter (#30814)
    Branch to implement unix domain socket client for exporting otel data.
    Currently there are changes that will not be merged, primarily to make
    testing easier.
    * Support/tools/otel-client.go is a minimal client to deserialize
    protobuf
    * CMakeLists.txt changes are to enable otel tests to run standalone
    * TelemetryContext changes includes changes to enable UDSMetricExporter
    to work by default - which is not the final state
    * Connection failures/retries not currently handled.
    * Metrics exporter and log exporter both work, but logs exporter
    requires a patch for the recordable type to be used without linking with
    protobuf.
    
    We also need to export the protobuf conversion function by adding the
    following to
    `opentelemetry-src/exporters/otlp/src/otlp_http_metric_exporter.cc` as
    well as the corresponding header file. Doing so will allow us to convert
    the metrics data to protobuf without having to link with any protobuf
    libs/headers which leads to a lot of problems.
    
    ```
    std::vector<std::byte> ConvertGenericMessageToJsonExported(
        const opentelemetry::sdk::metrics::ResourceMetrics &data)
    {
    
      google::protobuf::ArenaOptions arena_options;
      // It's easy to allocate datas larger than 1024 when we populate basic resource and attributes
      arena_options.initial_block_size = 1024;
      // When in batch mode, it's easy to export a large number of spans at once, we can alloc a lager
      // block to reduce memory fragments.
      arena_options.max_block_size = 65536;
      google::protobuf::Arena arena{arena_options};
    
      proto::collector::metrics::v1::ExportMetricsServiceRequest *service_request =
          google::protobuf::Arena::Create<proto::collector::metrics::v1::ExportMetricsServiceRequest>(
              &arena);
    
      OtlpMetricUtils::PopulateRequest(data, service_request);
      auto body_size = service_request->ByteSizeLong();
      std::vector<std::byte> buff;
    
      buff.resize(body_size);
      service_request->SerializeToArray(buff.data(), body_size);
      std::cout << "Metric Protobuf size :: " << buff.size() << "\n";
      return buff;
    }
    ```
    
    MODULAR_ORIG_COMMIT_REV_ID: 05427b6d09b171086504aa783003b8605439a7d5

---
+++ Support/include/Support/Telemetry/Exporters/UDSMetricExporter.h
@@ -0,0 +1,91 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef SUPPORT_TELEMETRY_EXPORTERS_UDSMetricExporter_H
+#define SUPPORT_TELEMETRY_EXPORTERS_UDSMetricExporter_H
+
+#include "Support/HTTP/HTTPClient.h"
+#include <llvm/Support/Debug.h>
+#ifdef MODULAR_ENABLE_TELEMETRY
+#include "opentelemetry/exporters/otlp/otlp_http_metric_exporter.h"
+#include "opentelemetry/sdk/metrics/push_metric_exporter.h"
+#include <filesystem>
+
+#endif // MODULAR_ENABLE_TELEMETRY
+
+namespace M::Telemetry::Exporter {
+
+#ifdef MODULAR_ENABLE_TELEMETRY
+
+#define USE_NATIVE_SOCKET 0
+
+class UDSMetricExporter final
+    : public opentelemetry::sdk::metrics::PushMetricExporter {
+public:
+  explicit UDSMetricExporter(const std::string &socketName,
+                             const std::string &metricsUrl = "http://metrics/")
+      : socketName(socketName), metricsUrl(metricsUrl),
+        client(HTTPContext::init()) {}
+  UDSMetricExporter(UDSMetricExporter &) = delete;
+
+  opentelemetry::sdk::common::ExportResult
+  Export(const opentelemetry::sdk::metrics::ResourceMetrics &data) noexcept
+      override {
+
+    auto protoBin =
+        opentelemetry::v1::exporter::otlp::ConvertGenericMessageToProtoExported(
+            data);
+    HTTPRequest req{metricsUrl};
+    client.noAuthNeeded();
+    req.udsName = socketName;
+    req.method = HTTPRequest::POST;
+    req.bodyLen = protoBin.size();
+
+    req.body = ContainerReadCallbackAdaptor(protoBin);
+    auto response = client.executeRequest(req, llvm::outs());
+    if (response.kind != HTTPResponse::Kind::Success) {
+      llvm::errs() << "failed to export metric via UDS"
+                   << response.asError().getError() << "\n";
+      return opentelemetry::sdk::common::ExportResult::kFailure;
+    }
+
+    return opentelemetry::sdk::common::ExportResult::kSuccess;
+  }
+
+  /// Get the AggregationTemporality for the exporter.
+  opentelemetry::sdk::metrics::AggregationTemporality GetAggregationTemporality(
+      opentelemetry::sdk::metrics::InstrumentType instrument_type)
+      const noexcept override {
+    return opentelemetry::v1::sdk::metrics::AggregationTemporality::
+        kUnspecified;
+  }
+
+  /// Force flush the exporter.
+  bool ForceFlush(std::chrono::microseconds timeout =
+                      std::chrono::microseconds::max()) noexcept override {
+
+    return true;
+  }
+
+  /// Shut down the exporter, with optional timeout.
+  bool Shutdown(std::chrono::microseconds timeout =
+                    std::chrono::microseconds::max()) noexcept override {
+
+    return true;
+  }
+
+private:
+  /// Logs are exported to this file.
+  std::string socketName;
+  std::string metricsUrl;
+  HTTPClient client;
+};
+
+#endif // MODULAR_ENABLE_TELEMETRY
+
+} // namespace M::Telemetry::Exporter
+
+#endif // SUPPORT_TELEMETRY_EXPORTERS_UDSMetricExporter_H
