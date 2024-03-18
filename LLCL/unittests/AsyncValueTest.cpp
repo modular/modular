96752187686b4fe44426024d67ca142eeb79ad07
Denali Lumma <denali@modular.com>
2024-03-18 10:28:08 -0700

    Revert "[LLCL] Retry - remove redundant singleThreaded flag in runtime" (#34989)
    Reverts [Internal link]
    
    This has failed again in post submit on main:
    [Internal link]
    
    MODULAR_ORIG_COMMIT_REV_ID: a4e584b5d47ed9b9d4f9eb78b3c46f9930eb2a58

--- LLCL/unittests/AsyncValueTest.cpp
+++ LLCL/unittests/AsyncValueTest.cpp
@@ -29,7 +29,8 @@ protected:
                                          bool mainWillDonate = true) {
     RuntimeOptions runtimeOptions;
     runtimeOptions.leakCheckedAllocator = true;
-    runtimeOptions.numThreads = GetParam() == kSingleThread ? 1 : numThreads;
+    runtimeOptions.singleThreaded = GetParam() == kSingleThread;
+    runtimeOptions.numThreads = numThreads;
     runtimeOptions.mainWillDonate = mainWillDonate;
     return M::LLCL::createUniqueRuntime(runtimeOptions);
   }
