92dc973d849d92639b7e9705d26d288214249dd9
Adin Scannell <amscanne@modular.com>
2024-03-20 10:34:14 -0700

    [Support] Add generic context to MDialect (#35126)
    Certain passes require the use of an LLCL runtime. Unfortunately, there
    is far too much implicit plumbing and code generation around the passes
    to reliability plumb the runtime through to where it is needed
    explicitly.
    
    In general, the solution to such a problem might be to hang the Runtime
    off the MLIRContext or other such state that is available to passes.
    Unfortunately, there is no good general purpose way to do this. Instead,
    the recommendation is to invent a synthetic dialect, and hang the
    runtime state off of this. The MLIRContext serves as a holder of the
    registry that allows access to this dialect, which contains the runtime.
    
    This commit adds the MDialect, which holds a reference to a generic
    context. This context can be used to hold the runtime and other things
    over time, and replace the explicit runtime plumbing in LLVM passes.
    
    MODULAR_ORIG_COMMIT_REV_ID: 842e949ddba4ccd2c1a677b068ace3aafc07ae23

--- Support/include/Support/MDialect/MDialect.h
+++ Support/include/Support/MDialect/MDialect.h
@@ -7,8 +7,20 @@
 #ifndef SUPPORT_MDIALECT_MDIALECT_H
 #define SUPPORT_MDIALECT_MDIALECT_H
 
+#include "Support/Context.h"
 #include "Support/LLVMCompilerForwardDecls.h"
 #include "mlir/IR/Dialect.h"
+
+namespace M {
+
+/// Registers a context in via the MDialect.
+void registerContext(mlir::DialectRegistry &registry, context::ContextRef &ref);
+
+/// Loads a context from the given MLIRContext. It must have been previously
+/// registered via a call to registerContext or this will trigger an assertion.
+context::ContextRef loadContext(mlir::MLIRContext *ctx);
+
+} // namespace M
 
 //===----------------------------------------------------------------------===//
 // ODS-Generated Declarations
