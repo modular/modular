b7304ea228682a73be71aa6132146f9e7dec0f89
weiwei chen <weiwei.chen@modular.com>
2024-03-21 22:26:38 -0400

    [KGEN] Improve LLVM Moduel per function splitting logic. (#35359)
    Stacked PRs:
     * #35360
     * __->__#35359
     * #35358
     * #35357
     * #35356
    
    --- --- ---
    
    ### [KGEN] Improve LLVM Moduel per function splitting logic.
    
    MODULAR_ORIG_COMMIT_REV_ID: 2ae193ed4887b668c17d30047248356d1463eefe

--- KGEN/test/kgen/lit.cfg.py
+++ KGEN/test/kgen/lit.cfg.py
@@ -14,7 +14,7 @@ from lit.llvm import llvm_config
 config.name = "kgen"
 
 # suffixes: A list of file extensions to treat as test files.
-config.suffixes = [".mlir"]
+config.suffixes = [".mlir", ".mojo"]
 
 # test_source_root: The root path where tests are located.
 config.test_source_root = os.path.dirname(__file__)
