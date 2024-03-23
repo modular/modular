01c8041481a1be38c14072b3af3ffbc7b448759b
Walter Erquinigo <walter@modular.com>
2024-03-23 13:28:44 -0400

    [REPL] Disable introspecting variants (#35593)
    As mentioned in [Internal link]
    introspecting (and reading) variants causes the REPL to crash.
    This PR disables introspecting variants so that we mitigate crashes
    before the next SDK release.
    
    This is how the REPL looks like right now. The same behavior will happen
    with any type that recursively owns a variant.
    
    ```
      1> from collections.optional import Optional
      2. var a = Optional(1)
      3.
    (Optional[Int]) a =
      3>
    ```
    
    MODULAR_ORIG_COMMIT_REV_ID: 586f988eba61983113766083567eb632bc80afa9

--- KGEN/lib/MojoLLDB/TypeSystem/MojoTypeDataLayout.cpp
+++ KGEN/lib/MojoLLDB/TypeSystem/MojoTypeDataLayout.cpp
@@ -107,6 +107,9 @@ MojoTypeDataLayoutContext::Impl::calculateForPack(MojoASTTypeRef typeRef,
 std::optional<MojoTypeDataLayout>
 MojoTypeDataLayoutContext::Impl::calculateForVariant(MojoASTTypeRef typeRef,
                                                      VariantType variantType) {
+  // FIXME(35592): We are disabling printing of variants for the time being,
+  // because they can't be introspected.
+  return {};
   std::optional<uint64_t> overallAlign = variantType.getTypeAlign(targetInfo);
   if (!overallAlign)
     return {};
