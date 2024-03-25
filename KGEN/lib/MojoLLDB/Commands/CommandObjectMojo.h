655d9597eb9b94ba585a562e603e53b8cc15da79
Adin Scannell <amscanne@modular.com>
2024-03-25 10:08:55 -0700

    [KGEN] Add Mojo LLDB plugin support for Init::createContext (#35366)
    This standardizes the creation of global singletons.
    
    MODULAR_ORIG_COMMIT_REV_ID: c2f26db556696c4be30f53b0115b37074ee34968

--- KGEN/lib/MojoLLDB/Commands/CommandObjectMojo.h
+++ KGEN/lib/MojoLLDB/Commands/CommandObjectMojo.h
@@ -7,17 +7,12 @@
 #ifndef KGEN_LIB_MOJOLLDB_REPL_COMMANDOBJECTMOJO_H
 #define KGEN_LIB_MOJOLLDB_REPL_COMMANDOBJECTMOJO_H
 
+#include "Support/Context.h"
 #include "lldb/API/LLDB.h"
-
-namespace M {
-namespace LLCL {
-class Runtime;
-} // namespace LLCL
-} // namespace M
 
 namespace M::KGEN::Mojo {
 /// Register all related `mojo` commands in the given debugger.
-void registerMojoCommands(lldb::SBDebugger debugger, LLCL::Runtime &runtime);
+void registerMojoCommands(lldb::SBDebugger debugger, M::ContextRef ctx);
 } // namespace M::KGEN::Mojo
 
 #endif // KGEN_LIB_MOJOLLDB_REPL_COMMANDOBJECTMOJO_H
