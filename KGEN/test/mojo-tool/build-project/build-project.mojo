1516f08b3dbdbbcdbe19ffe2b124a1a573b3ba75
Brian Gesiak <brian@modular.com>
2024-03-28 12:58:21 -0400

    [mojo] `mojo build-project` messages build server (#36106)
    As a proof of concept, this connects the `mojo build-project` command to
    the `mojo-build-server`.
    
    This is part of the Mojo project manifest & build tool work. For more
    information, see [our roadmap]. A design doc on the build server itself
    is forthcoming, and will be populated [here].
    
    Some aspects of this patch are for the long term, while some aspects are
    expected to change.
    
    What will likely stay the same:
    
    - The `mojo build-project` command will likely continue to launch,
      initialize, and terminate an instance of `mojo-build-server`.
    - The `check-mojo-tool` test suite will likely continue to test many
      interactions between `mojo build-project` and `mojo-build-server`.
    
    What will likely change:
    
    - Rather than define `mojo-build-server` as a `check-mojo-tool`
      dependency, it will be built as part of the `mojo-sdk` target.
    - The path to `mojo-build-server` will be stored in `modular.cfg`, not
      simply found relative to the `mojo` executable.
    - `mojo build-project` will send standard JSON-RPC messages to the build
      server, not messages delimited with `// -----`.
    - Temporary files will not be used to send those messages; they will be
      sent directly.
    
    [our roadmap]: [Internal link]
    [here]: [Internal link]
    
    MODULAR_ORIG_COMMIT_REV_ID: 3d36c23112932e4312b5fdc938c903a4f8a20fde

---
+++ KGEN/test/mojo-tool/build-project/build-project.mojo
@@ -0,0 +1,8 @@
+# ===----------------------------------------------------------------------=== #
+#
+# This file is Modular Inc proprietary.
+#
+# ===----------------------------------------------------------------------=== #
+
+# RUN: mojo build-project | FileCheck %s
+# CHECK: "displayName": "mojo-build-server"
--- KGEN/tools/mojo/BuildProject/mojo-build-project.cpp
+++ KGEN/tools/mojo/BuildProject/mojo-build-project.cpp
@@ -7,10 +7,16 @@
 #include "mojo-build-project.h"
 
 #include "Support/Driver/DriverSupport.h"
+#include "Support/FileSystemExtras.h"
 
 #include "llvm/Option/ArgList.h"
 #include "llvm/Option/OptTable.h"
 #include "llvm/Option/Option.h"
+#include "llvm/Support/FileSystem.h"
+#include "llvm/Support/Program.h"
+
+#include <optional>
+#include <string>
 
 using namespace M;
 
@@ -24,10 +30,30 @@ struct BuildProjectOptTable : public llvm::opt::PrecomputedOptTable {
 };
 } // namespace
 
-/// For now, an empty stub command that does nothing. Eventually, this will
-/// interface with a separate `mojo-build-server` executable.
+/// Returns the path to the `mojo-build-server` executable, or an error if none
+/// could be found.
+static ErrorOr<std::string> getMojoBuildServerPath(const char *programName) {
+  std::string executable = llvm::sys::fs::getMainExecutable(
+      programName, (void *)getMojoBuildServerPath);
+  if (executable.empty())
+    return Error("could not determine `mojo` path");
+
+  // For now, `mojo-build-server` is found relative to `mojo` -- it exists in
+  // the same directory. In the future, its path will be stored in the Modular
+  // config file.
+  std::string path = std::filesystem::path(executable).parent_path().string();
+  return toModularErrorOr(
+      llvm::sys::findProgramByName("mojo-build-server",
+                                   /*Paths=*/ArrayRef<StringRef>(path)));
+}
+
+/// For now, this simply launches a `mojo-build-server` executable and sends it
+/// initialization and exit messages. Eventually, this will send messages
+/// that result in the compilation of a Mojo project.
 static int buildProject(const State &state) {
-  // Parse command line arguments.
+  //===--------------------------------------------------------------------===//
+  // Command line argument parsing
+  //===--------------------------------------------------------------------===//
   BuildProjectOptTable options;
   unsigned missingIndex = 0;
   unsigned missingCount = 0;
@@ -47,7 +73,33 @@ static int buildProject(const State &state) {
   // Assert that we've parsed all command line arguments.
   state.assertNoUnusedArguments(args);
 
-  return EXIT_SUCCESS;
+  //===--------------------------------------------------------------------===//
+  // Build server communication
+  //===--------------------------------------------------------------------===//
+
+  auto serverPathOrErr = getMojoBuildServerPath(state.programName);
+  if (serverPathOrErr.isError())
+    return state.reportError(serverPathOrErr.getError());
+  std::string serverPath = *serverPathOrErr;
+
+  // For now, as a proof of concept, send delimited messages to the build
+  // server.
+  auto inOrErr = writeTempFile(
+      "mojo-build-project-%%%%%%.json", [](llvm::raw_ostream &in) {
+        in << "{\"jsonrpc\":\"2.0\",\"id\":0,\"method\":\"build/initialize\","
+           << "\"params\":{\"displayName\":\"mojo-build-project\"}}\n";
+        in << "// -----\n";
+        in << "{\"jsonrpc\":\"2.0\",\"method\":\"exit\"}\n";
+      });
+  if (inOrErr.isError())
+    return state.reportError(inOrErr.getError());
+  TempFile in = std::move(*inOrErr);
+
+  return llvm::sys::ExecuteAndWait(
+      serverPath, {serverPath},
+      /*Env=*/std::nullopt,
+      /*Redirects=*/
+      {in.getPath().c_str(), std::nullopt, std::nullopt});
 }
 
 void M::registerBuildProjectSubcommand(SubcommandRegistry &registry) {
