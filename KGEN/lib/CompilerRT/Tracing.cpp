888c2c5589d1fbf313834e4b5096428edb3cb9c3
Brendan Duke <bduke@modular.com>
2024-04-04 17:04:59 -0400

    [Kernels] Defer most profile event name copies (#36513)
    Defer interning of Mojo profile event names until an explicit `intern()`
    call when there is no detail string.
    The previous behaviour was to copy every string twice: once in
    `Trace.__enter__` and again greedily in the `Label()` constructor for
    the `BeginEvent`.
    However there was no reason to do so in most cases where the event name
    is a static string (Mojo `StringLiteral`).
    
    Furthermore, we were enumerating tasks with a detail string
    unnecessarily: `TimeTraceProfiler` supports copy-free storage of a
    "name" + "index".
    
    This removes the 20-30% profiling overhead I was seeing when profiling
    `Q4_0` Llama token generation from `mojo run`.
    
    MODULAR_ORIG_COMMIT_REV_ID: c4feb7f35202ec49fdd22099761bda5f9c6ec9bb

--- KGEN/lib/CompilerRT/Tracing.cpp
+++ KGEN/lib/CompilerRT/Tracing.cpp
@@ -4,7 +4,6 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "KGEN/CompilerRT/MojoCallContext.h"
 #include "KGEN/CompilerRT/Registration.h"
 #include "Support/Profiling/TimeProfiler.h"
 #include "Support/SymbolExport.h"
@@ -12,13 +11,33 @@
 using namespace M;
 
 COMPILERRT_EXPORT COMPILERRT_VISIBILITY_EXPORT size_t
+KGEN_CompilerRT_TimeTraceProfilerBeginTask(const char *namePtr, size_t nameLen,
+                                           size_t parentId, size_t taskId) {
+  // NOTE: Must be always enabled.
+  return ProfilerEntry<true, Trace::kMojo>::createWithParent(
+             parentId, InternableString(namePtr, nameLen), (uint64_t)taskId)
+      .getId();
+}
+
+COMPILERRT_EXPORT COMPILERRT_VISIBILITY_EXPORT size_t
+KGEN_CompilerRT_TimeTraceProfilerBeginDetail(const char *namePtr,
+                                             size_t nameLen,
+                                             const char *detailPtr,
+                                             size_t detailLen,
+                                             size_t parentId) {
+  // NOTE: Must be always enabled.
+  return ProfilerEntry<true, Trace::kMojo>::createWithParent(
+             parentId, InternableString(namePtr, nameLen),
+             StringRef(detailPtr, detailLen))
+      .getId();
+}
+
+COMPILERRT_EXPORT COMPILERRT_VISIBILITY_EXPORT size_t
 KGEN_CompilerRT_TimeTraceProfilerBegin(const char *namePtr, size_t nameLen,
-                                       const char *detailPtr, size_t detailLen,
                                        size_t parentId) {
   // NOTE: Must be always enabled.
   return ProfilerEntry<true, Trace::kMojo>::createWithParent(
-             parentId, StringRef(namePtr, nameLen),
-             StringRef(detailPtr, detailLen))
+             parentId, InternableString(namePtr, nameLen))
       .getId();
 }
 
@@ -44,6 +63,10 @@ void M::KGEN::registerTracing(
     std::vector<std::pair<llvm::StringLiteral, void *>> &funcs) {
   funcs.push_back({"KGEN_CompilerRT_TimeTraceProfilerBegin",
                    (void *)&KGEN_CompilerRT_TimeTraceProfilerBegin});
+  funcs.push_back({"KGEN_CompilerRT_TimeTraceProfilerBeginDetail",
+                   (void *)&KGEN_CompilerRT_TimeTraceProfilerBeginDetail});
+  funcs.push_back({"KGEN_CompilerRT_TimeTraceProfilerBeginTask",
+                   (void *)&KGEN_CompilerRT_TimeTraceProfilerBeginTask});
   funcs.push_back({"KGEN_CompilerRT_TimeTraceProfilerEnd",
                    (void *)&KGEN_CompilerRT_TimeTraceProfilerEnd});
   funcs.push_back({"KGEN_CompilerRT_TimeTraceProfilerGetCurrentId",
