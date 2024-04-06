modular/modular

commit 50eb885e70ef284d6ae46fa4727366d87b23485c

[Kernels][layout] Restrict __del__ to GENERIC address space tensors (#36980)
Otherwise we hit #36962 during during jitting the code.

Fixes #36962

LAYOUT_ORIG_REV_ID: 96cd4e81143ee024ea0c0ebee3db29b3c932db41

mojo/kernels/src/layout/layout_tensor.mojo
@@ -85,6 +85,10 @@ struct LayoutTensor[
         self.owning = False
 
     fn __del__(owned self):
+        # Owned tensors live in GENERIC address space only.
+        @parameter
+        if address_space != AddressSpace.GENERIC:
+            return
         if self.owning:
             self.ptr.free()
 

commit 2d4eb4a64790db0a15ed46a171b7f6d684d9fda2

[Kernels][layout] always_inline outer_product_acc (#36998)
We want to keep the math inlined for better optimization, this recover the performance regression caused by #36980, however we still need to understand why the regression was introduced in the first place.!

LAYOUT_ORIG_REV_ID: 80cf6f61f48bf67909a0338c0724aaff0a19dbd4

mojo/kernels/src/layout/layout_tensor.mojo
@@ -925,6 +925,7 @@ fn stack_allocation_like[
 
 # Updates res with the outer product of lhs, rhs vectors, res += outer(lhs, rhs).
 #
+@always_inline
 fn outer_product_acc[
     dtype: DType,
     *,