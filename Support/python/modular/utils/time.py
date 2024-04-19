7d532ecf48fee5fe3e5a61626051f90d052b2a01
Tyler Kenney <tyler@modular.com>
2024-04-19 17:03:14 -0400

    [model] Track compile time & memory usage (#37901)
    ### Summary
    
    This PR adds three new metrics to the `report.csv` generated by `model
    benchmark`. The high-level changes are:
      - Split compilation into explicit phase for modular frameworks
      - Prefix compile & benchmark `mt` commands with `/usr/bin/time`
      - Parse time logs & write to CSV
    
    ### Notes
    
    Design doc is
    [Internal link]
    
    Related Issues:
      - #35820
      - #34416
      - #29540
    
    MODULAR_ORIG_COMMIT_REV_ID: 48eb7bc84b7b17a585068cbcb7222a699c50c863

---
+++ Support/python/modular/utils/time.py
@@ -0,0 +1,127 @@
+# ===----------------------------------------------------------------------=== #
+#
+# This file is Modular Inc proprietary.
+#
+# ===----------------------------------------------------------------------=== #
+
+__doc__ = """
+Utility Library for Parsing Output from /usr/bin/time
+"""
+
+from dataclasses import dataclass
+from pathlib import Path
+from platform import system
+
+from modular.utils.typing import Iterable, Optional
+
+THIS_OS = system()
+
+
+@dataclass
+class RUsageResult:
+    """Class to hold statistics printed by /usr/bin/time -v"""
+
+    time_real_ns: Optional[int] = None
+    time_user_ns: Optional[int] = None
+    time_sys_ns: Optional[int] = None
+    peak_rss_bytes: Optional[int] = None
+    ctx_switches_voluntary: Optional[int] = None
+    ctx_switches_involuntary: Optional[int] = None
+    page_faults_major: Optional[int] = None
+    page_faults_minor: Optional[int] = None
+
+    @classmethod
+    def from_file(
+        cls, time_log_path: Path, os: str = THIS_OS
+    ) -> "RUsageResult":
+        """
+        Parses the given time log and returns results.
+        Args:
+            time_log_path (Path): Path to logfile to be parsed.
+            os (str): name of operating system (indicates log format).
+        """
+        with open(time_log_path, "r") as log:
+            return RUsageResult.from_lines(log, os)
+
+    @classmethod
+    def from_lines(
+        cls, time_log_lines: Iterable[str], os: str = THIS_OS
+    ) -> "RUsageResult":
+        """
+        Parses the given time log and returns results.
+
+        Args:
+            time_log_lines (Iterable[str]): Contents of logfile to be parsed.
+            os (str): name of operating system (indicates log format).
+        """
+        if os == "Darwin":
+            return cls._from_lines_macos(time_log_lines)
+        elif os == "Linux":
+            return cls._from_lines_linux(time_log_lines)
+        else:
+            raise ValueError(f"Unknown operating system: {os}")
+
+    @classmethod
+    def _from_lines_macos(cls, time_log_lines: Iterable[str]) -> "RUsageResult":
+        def get_int(val: str, idx: int = 0):
+            return int(val.strip().split()[idx].strip())
+
+        def get_float(val: str, idx: int = 0):
+            return float(val.strip().split()[idx].strip())
+
+        result = RUsageResult()
+        for line in time_log_lines:
+            if "real" in line:
+                result.time_real_ns = int(get_float(line, 0) * 1e9)
+            if "user" in line:
+                result.time_user_ns = int(get_float(line, 2) * 1e9)
+            if "sys" in line:
+                result.time_sys_ns = int(get_float(line, 4) * 1e9)
+            if "maximum resident set size" in line:
+                result.peak_rss_bytes = get_int(line)
+            elif "involuntary context switches" in line:
+                result.ctx_switches_involuntary = get_int(line)
+            elif "voluntary context switches" in line:
+                result.ctx_switches_voluntary = get_int(line)
+            elif "page faults" in line:
+                result.page_faults_major = get_int(line)
+        return result
+
+    @classmethod
+    def _from_lines_linux(cls, time_log_lines: Iterable[str]) -> "RUsageResult":
+        def get_int(line: str):
+            return int(line.split(":")[1].strip())
+
+        def get_float(line: str):
+            return float(line.split(":")[1].strip())
+
+        def get_time(line: str):
+            val = line.split("):")[1].strip()
+            if val.count(":") == 1:
+                hrs = 0
+                mins, secs = val.split(":")
+            elif val.count(":") == 2:
+                hrs, mins, secs = val.split(":")
+            else:
+                raise ValueError(f"Unexpected time format: '{line}'")
+            return (float(hrs) * 60 + float(mins)) * 60 + float(secs)
+
+        result = RUsageResult()
+        for line in time_log_lines:
+            if "Elapsed (wall clock) time (h:mm:ss or m:ss)" in line:
+                result.time_real_ns = int(get_time(line) * 1e9)
+            if "User time (seconds)" in line:
+                result.time_user_ns = int(get_float(line) * 1e9)
+            if "System time (seconds)" in line:
+                result.time_sys_ns = int(get_float(line) * 1e9)
+            if "Maximum resident set size (kbytes)" in line:
+                result.peak_rss_bytes = get_int(line) * 1000
+            if "Voluntary context switches" in line:
+                result.ctx_switches_voluntary = get_int(line)
+            if "Involuntary context switches" in line:
+                result.ctx_switches_involuntary = get_int(line)
+            if "Major (requiring I/O) page faults" in line:
+                result.page_faults_major = get_int(line)
+            if "Minor (reclaiming a frame) page faults" in line:
+                result.page_faults_minor = get_int(line)
+        return result
---
+++ Support/python/tests/utils/test_time.py
@@ -0,0 +1,101 @@
+# ===----------------------------------------------------------------------=== #
+#
+# This file is Modular Inc proprietary.
+#
+# ===----------------------------------------------------------------------=== #
+
+import os
+import platform
+import pytest
+
+from modular.utils.subprocess import run_shell_command
+from modular.utils.time import RUsageResult
+
+time_log_linux = """
+        Command being timed: "sha256sum stable-diffusion-v1.5-unet.tar"
+        User time (seconds): 36.01
+        System time (seconds): 2.06
+        Percent of CPU this job got: 74%
+        Elapsed (wall clock) time (h:mm:ss or m:ss): 0:51.40
+        Average shared text size (kbytes): 0
+        Average unshared data size (kbytes): 0
+        Average stack size (kbytes): 0
+        Average total size (kbytes): 0
+        Maximum resident set size (kbytes): 1920
+        Average resident set size (kbytes): 0
+        Major (requiring I/O) page faults: 0
+        Minor (reclaiming a frame) page faults: 132
+        Voluntary context switches: 21563
+        Involuntary context switches: 83
+        Swaps: 0
+        File system inputs: 13433840
+        File system outputs: 0
+        Socket messages sent: 0
+        Socket messages received: 0
+        Signals delivered: 0
+        Page size (bytes): 4096
+        Exit status: 0
+"""
+
+time_log_macos = """
+       24.36 real        22.79 user         0.83 sys
+             1589248  maximum resident set size
+                   0  average shared memory size
+                   0  average unshared data size
+                   0  average unshared stack size
+                 214  page reclaims
+                   1  page faults
+                   0  swaps
+                   0  block input operations
+                   0  block output operations
+                   0  messages sent
+                   0  messages received
+                   0  signals received
+                  17  voluntary context switches
+               12323  involuntary context switches
+        242953331230  instructions retired
+         74930184152  cycles elapsed
+             1410048  peak memory footprint
+"""
+
+
+def test_from_lines_static_linux():
+    rusage = RUsageResult.from_lines(time_log_linux.split("\n"), "Linux")
+    assert rusage.time_real_ns == 51400000000
+    assert rusage.time_user_ns == 36010000000
+    assert rusage.time_sys_ns == 2060000000
+    assert rusage.peak_rss_bytes == 1920000
+    assert rusage.ctx_switches_voluntary == 21563
+    assert rusage.ctx_switches_involuntary == 83
+    assert rusage.page_faults_minor == 132
+    assert rusage.page_faults_major == 0
+
+
+def test_from_lines_static_macos():
+    rusage = RUsageResult.from_lines(time_log_macos.split("\n"), "Darwin")
+    assert rusage.time_real_ns == 24360000000
+    assert rusage.time_user_ns == 22790000000
+    assert rusage.time_sys_ns == 830000000
+    assert rusage.peak_rss_bytes == 1589248
+    assert rusage.ctx_switches_voluntary == 17
+    assert rusage.ctx_switches_involuntary == 12323
+    assert rusage.page_faults_minor is None
+    assert rusage.page_faults_major == 1
+
+
+@pytest.mark.skipif(
+    not os.path.exists("/usr/bin/time"), reason="binary unavailable"
+)
+def test_from_lines_dynamic():
+    switch = "-l" if platform.system() == "Darwin" else "-v"
+    command = f"/usr/bin/time {switch} echo Modular's Magnificent Menagerie"
+    proc = run_shell_command(command.split(), capture_output=True)
+    lines = proc.stderr.decode("utf-8").rstrip().split("\n")
+    rusage = RUsageResult.from_lines(lines)
+    assert rusage.time_real_ns is not None
+    assert rusage.time_user_ns is not None
+    assert rusage.time_sys_ns is not None
+    assert rusage.peak_rss_bytes is not None
+    assert rusage.ctx_switches_voluntary is not None
+    assert rusage.ctx_switches_involuntary is not None
+    assert rusage.page_faults_major is not None
