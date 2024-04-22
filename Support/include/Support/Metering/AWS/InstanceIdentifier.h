fe89e4e78f4fd24acbfc1938c313e50821273b0e
Yihua Lou <1237500+yihualou@users.noreply.github.com>
2024-04-22 15:04:54 -0700

    [Metering] Add timeout to instance request to fix hang (#38347)
    Fixes [Internal link]
    
    MODULAR_ORIG_COMMIT_REV_ID: 85a420ecc1b6a7851d30b974adba758653e9d565

--- Support/include/Support/Metering/AWS/InstanceIdentifier.h
+++ Support/include/Support/Metering/AWS/InstanceIdentifier.h
@@ -11,9 +11,12 @@
 
 namespace M::Metering {
 
-/// Identifies AWS region and instance type by querying the IMDSv2 HTTP API.
+/// Identifies AWS region and instance type by querying the IMDSv{1, 2} API.
 class InstanceIdentifier {
 public:
+  using ClockType = std::chrono::steady_clock;
+  using DurationType = std::chrono::duration<ClockType::rep, ClockType::period>;
+
   InstanceIdentifier(HTTPContextRef ctx) : client(ctx->client()) {}
 
   ErrorOrSuccess fetch();
@@ -22,9 +25,13 @@ public:
   StringRef getInstanceType() const { return instanceType; }
 
 private:
+  ErrorOr<std::string> send(const HTTPRequest &req);
+
   ErrorOrSuccess fetchInfo(StringRef token, bool isIPv4);
   ErrorOrSuccess fetchV1();
   ErrorOrSuccess fetchV2();
+
+  const DurationType timeout = std::chrono::seconds(1);
 
   std::unique_ptr<HTTPClient> client;
   std::string region;
