d7967dea7dc81bcaa69ec2453b3d5588c2d32b9a
Billy Zhu <billyzhu@modular.com>
2024-04-25 09:59:23 -0700

    [DebugInfo] Target-specific debuginfo massaging (NFC) (#38350)
    Our previous massaging routine for debuginfo does not apply to NV GPUs.
    To make it easier to add nv-specific debuginfo massaging, this PR adds a
    super basic infra for choosing a massaging routine based on the target.
    
    Each target returns a pair of function pointers: one for
    pre-translation, one for post-translation.
    
    Existing behavior is not changed. Code is only moved around.
    
    MODULAR_ORIG_COMMIT_REV_ID: 4a82800228e9c534dc4ec0c5deb8ed646e9a38a9

---
+++ Support/lib/DebugInfoDialect/DebugInfoToLLVM/Target/TargetAdapter.h
@@ -0,0 +1,64 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef SUPPORT_DEBUGINFODIALECT_DEBUGINFOTOLLVM_TARGET_TARGETADAPTER_H
+#define SUPPORT_DEBUGINFODIALECT_DEBUGINFOTOLLVM_TARGET_TARGETADAPTER_H
+
+#include "Support/MDialect/MAttrs.h"
+#include "mlir/IR/BuiltinOps.h"
+
+namespace M::DebugInfo {
+//===----------------------------------------------------------------------===//
+// TargetAdapter
+//===----------------------------------------------------------------------===//
+struct TargetAdapter {
+  using DebugAdapterFn = std::function<void(mlir::ModuleOp)>;
+
+  DebugAdapterFn preTranslationAdapter;
+  DebugAdapterFn postTranslationAdapter;
+};
+
+/// Get the corresponding adapter for the target.
+TargetAdapter getTargetAdapter(M::TargetInfoAttr target);
+
+/// Default adapter for targets without a more specific adapter.
+TargetAdapter getFallbackAdapter();
+
+//===----------------------------------------------------------------------===//
+// Common Routines
+//===----------------------------------------------------------------------===//
+/// Sink kill Debug Value ops so that they are the last instructions from
+/// their source line. This way variables are guaranteed to be killed only at
+/// the end of the line.
+void sinkDebugKills(mlir::Operation *op);
+
+/// This function converts instances of llvm.dbg.value to llvm.dbg.declare when
+/// desirable. LLVM optimizations and codegen often muck up the use of
+/// llvm.dbg.value (and other debug intrinsics), which creates subpar debugging
+/// experiences. Converting to llvm.dbg.declare provides a more stable debugging
+/// environment, and more closely matches what a traditional frontend would
+/// provide in O0 modes.
+///
+/// The current conversion policy considers two separate axes:
+/// - The number of dbg.values for a variable (regardless of whether the value
+/// is undef or not) determines whether dbg.value or dbg.declare is used.
+/// - The number of non-undef dbg.values for a variable determines whether we
+/// allocate to stack or not.
+///
+/// Or, listing out the possible combinations:
+/// - =1 dbg.value: use dbg.declare, allocate to stack (i.e. var is alive for
+/// its entire scope)
+/// - >1 dbg.value, =1 non-undef: use dbg.value, allocate to stack (i.e. var is
+/// stationary for its entire lifetime)
+/// - >1 dbg.value, >1 non-undef: use dbg.value, don't allocate to stack (i.e.
+/// var moves around, or exists as fragments)
+///
+/// TODO: As we grow support we may want to consider making this optional
+/// depending on the debug mode.
+void convertDbgValueToDeclare(mlir::ModuleOp module);
+} // namespace M::DebugInfo
+
+#endif // SUPPORT_DEBUGINFODIALECT_DEBUGINFOTOLLVM_TARGET_TARGETADAPTER_H
