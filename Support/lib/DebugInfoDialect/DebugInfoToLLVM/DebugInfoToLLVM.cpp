a6d61fb59e54d2dc55058d96cad22c2555e25f41
Billy Zhu <billyzhu@modular.com>
2024-04-25 09:59:23 -0700

    [DebugInfo] Target-specific debuginfo massaging (NFC) (#38350)
    Our previous massaging routine for debuginfo does not apply to NV GPUs.
    To make it easier to add nv-specific debuginfo massaging, this PR adds a
    super basic infra for choosing a massaging routine based on the target.
    
    Each target returns a pair of function pointers: one for
    pre-translation, one for post-translation.
    
    Existing behavior is not changed. Code is only moved around.
    
    MODULAR_ORIG_COMMIT_REV_ID: 4a82800228e9c534dc4ec0c5deb8ed646e9a38a9

--- Support/lib/DebugInfoDialect/DebugInfoToLLVM/DebugInfoToLLVM.cpp
+++ Support/lib/DebugInfoDialect/DebugInfoToLLVM/DebugInfoToLLVM.cpp
@@ -9,6 +9,8 @@
 #include "Support/DebugInfoDialect/IR/DebugInfoDialect.h"
 #include "Support/DebugInfoDialect/IR/DebugInfoOps.h"
 #include "Support/DebugInfoDialect/Transforms/Conversion.h"
+#include "Support/MDialect/MAttrs.h"
+#include "Target/TargetAdapter.h"
 #include "mlir/Conversion/LLVMCommon/Pattern.h"
 #include "mlir/Dialect/LLVMIR/LLVMDialect.h"
 #include "mlir/Pass/Pass.h"
@@ -521,252 +523,7 @@ struct DebugInfoToLLVMPass
 
   void runOnOperation() override;
 };
-
-/// Summary of all the variables that are tracked with debug info in a function.
-///
-/// - A "variable" refers to a source variable (described by the
-///   DILocalVariableAttr of a DbgValueOp)
-/// - A "value" refers to an IR Value that is used to define the value of a
-///   variable at some program location (as an operand of a DbgValueOp).
-struct DebugVariableSummary {
-public:
-  /// Debug info for variables that have at most one non-undef definition.
-  struct SingleDefVariable {
-    SingleDefVariable(LLVM::DbgValueOp op, bool undefs)
-        : debugValue(op), hasAdditionalUndefs(undefs) {}
-
-    /// If all definitions of this variable is undef, this is the first debug
-    /// value op for this variable. Otherwise, this debug value tracks the
-    /// single non-undef definition of this variable.
-    LLVM::DbgValueOp debugValue;
-    /// Whether there exists additional undef values for this variable.
-    /// This affects whether the definition can be easily allocated to stack.
-    bool hasAdditionalUndefs;
-  };
-
-  llvm::MapVector<Value, SmallVector<SingleDefVariable>> trackers;
-};
 } // namespace
-
-/// Filter out the debug values that are not needed, and summarize the rest in
-/// a DebugVariableSummary.
-static DebugVariableSummary
-filterAndSummarizeDebugVariables(mlir::FunctionOpInterface func) {
-  // Summarize debug values by variable.
-  llvm::MapVector<LLVM::DILocalVariableAttr,
-                  DebugVariableSummary::SingleDefVariable>
-      uniqueDebugValues;
-  func->walk([&](LLVM::DbgValueOp op) {
-    Value value = op.getValue();
-    // Don't build debug info for token values.
-    if (isa<LLVM::LLVMTokenType>(value.getType())) {
-      op->erase();
-      return;
-    }
-
-    if (value.getDefiningOp<LLVM::UndefOp>()) {
-      auto [iter, inserted] =
-          uniqueDebugValues.try_emplace(op.getVarInfo(), op, false);
-      if (!inserted)
-        iter->second.hasAdditionalUndefs = true;
-      return;
-    }
-
-    // Not undef.
-    auto [iter, inserted] =
-        uniqueDebugValues.try_emplace(op.getVarInfo(), op, false);
-    // If this variable was seen before, either override initial undef, or
-    // invalidate this variable altogether.
-    if (!inserted && iter->second.debugValue) {
-      if (iter->second.debugValue.getValue().getDefiningOp<LLVM::UndefOp>()) {
-        iter->second.debugValue = op;
-        iter->second.hasAdditionalUndefs = true;
-      } else {
-        iter->second.debugValue = nullptr;
-      }
-    }
-  });
-
-  DebugVariableSummary summary;
-  // Re-categorize debug values by operand Value, skipping those that have
-  // been invalidated due to having more than one non-undef definition.
-  for (auto &[_, dbgValueInfo] : uniqueDebugValues) {
-    if (!dbgValueInfo.debugValue)
-      continue;
-
-    summary.trackers[dbgValueInfo.debugValue.getValue()].push_back(
-        dbgValueInfo);
-  }
-
-  return summary;
-}
-
-/// This function converts instances of llvm.dbg.value to llvm.dbg.declare when
-/// desirable. LLVM optimizations and codegen often muck up the use of
-/// llvm.dbg.value (and other debug intrinsics), which creates subpar debugging
-/// experiences. Converting to llvm.dbg.declare provides a more stable debugging
-/// environment, and more closely matches what a traditional frontend would
-/// provide in O0 modes.
-///
-/// The current conversion policy considers two separate axes:
-/// - The number of dbg.values for a variable (regardless of whether the value
-/// is undef or not) determines whether dbg.value or dbg.declare is used.
-/// - The number of non-undef dbg.values for a variable determines whether we
-/// allocate to stack or not.
-///
-/// Or, listing out the possible combinations:
-/// - =1 dbg.value: use dbg.declare, allocate to stack (i.e. var is alive for
-/// its entire scope)
-/// - >1 dbg.value, =1 non-undef: use dbg.value, allocate to stack (i.e. var is
-/// stationary for its entire lifetime)
-/// - >1 dbg.value, >1 non-undef: use dbg.value, don't allocate to stack (i.e.
-/// var moves around, or exists as fragments)
-///
-/// TODO: As we grow support we may want to consider making this optional
-/// depending on the debug mode.
-static void convertDbgValueToDeclare(ModuleOp module) {
-  // A lot more logic is required to make this reverse-mem2reg work when
-  // multiple DbgValueOps for one variable exists. Going with the simplest
-  // solution for now until we decide to retire this altogether.
-  for (auto func : module.getOps<mlir::FunctionOpInterface>()) {
-    DebugVariableSummary debugVariableSummary =
-        filterAndSummarizeDebugVariables(func);
-
-    for (auto &[value, trackers] : debugVariableSummary.trackers) {
-      // Don't build debug information for simple constants.
-      if (value.getDefiningOp<LLVM::ConstantOp>() &&
-          isa<IntegerType, FloatType>(value.getType()))
-        continue;
-
-      // The converted alloca op that will hold the value if it is converted to
-      // a stack allocation.
-      LLVM::AllocaOp allocaOp;
-
-      // Get the allocaOp for this value. If one has not already been created,
-      // create one and save it for the next invocation.
-      auto getAllocaOp = [&, value = value](LLVM::DbgValueOp op) {
-        if (allocaOp)
-          return allocaOp;
-
-        // Build a new allocation to store the intermediate value.
-        OpBuilder allocBuilder = OpBuilder::atBlockBegin(&func.front());
-        Location erasedLoc = UnknownLoc::get(op->getContext());
-        auto allocSize = allocBuilder.create<LLVM::ConstantOp>(
-            erasedLoc, allocBuilder.getI32Type(), 1);
-
-        allocaOp = allocBuilder.create<LLVM::AllocaOp>(
-            erasedLoc, LLVM::LLVMPointerType::get(value.getContext()),
-            value.getType(), allocSize, 0);
-        return allocaOp;
-      };
-
-      // Converter for a single DbgValueOp.
-      // `hasLimitedScope` controls whether the DbgValueOp is converted into a
-      // DbgDeclareOp or is kept, because the scope of a DbgDeclareOp cannot be
-      // limited to a subset of the parent scope.
-      auto convertDbgValue = [&, value = value](LLVM::DbgValueOp op,
-                                                bool hasLimitedScope) {
-        if (hasLimitedScope) {
-          ArrayRef<LLVM::DIExpressionElemAttr> location =
-              op.getLocationExpr().getOperations();
-          SmallVector<LLVM::DIExpressionElemAttr> newLocations = {
-              LLVM::DIExpressionElemAttr::get(op.getContext(),
-                                              llvm::dwarf::DW_OP_deref, {})};
-          newLocations.append(location.begin(), location.end());
-          op.setOperand(getAllocaOp(op));
-          op.setLocationExprAttr(
-              LLVM::DIExpressionAttr::get(op->getContext(), newLocations));
-        } else {
-          ArrayRef<LLVM::DIExpressionElemAttr> location =
-              op.getLocationExpr().getOperations();
-          if (!isa<BlockArgument>(op.getValue()) && !location.empty() &&
-              location.front().getOpcode() == llvm::dwarf::DW_OP_deref) {
-            // For cases where the locationExpr begins with a deref, just
-            // pop off the initial deref and convert directly into a
-            // DbgDeclareOp. In this case no alloca needs to be created.
-            // Block args however are not compatible directly with DbgDeclare.
-            auto refLocation = LLVM::DIExpressionAttr::get(
-                op->getContext(), location.drop_front());
-            OpBuilder(op).create<LLVM::DbgDeclareOp>(
-                op.getLoc(), value, op.getVarInfo(), refLocation);
-          } else {
-            // For all other cases, create alloca and use dbg declare with it.
-            OpBuilder(op).create<LLVM::DbgDeclareOp>(
-                op.getLoc(), getAllocaOp(op), op.getVarInfo(),
-                op.getLocationExpr());
-          }
-          op->erase();
-        }
-      };
-
-      // Run converter on all single-def variables.
-      for (auto &singleDefInfo : trackers) {
-        LLVM::DbgValueOp op = singleDefInfo.debugValue;
-        const bool hasUndefs = singleDefInfo.hasAdditionalUndefs;
-
-        // If additional undef dbg.values exist for this variable, cannot create
-        // a dbg.declare for it as they don't allow undef dbg.values to limit
-        // their live ranges. Keep the dbg.value ops but reference the stack
-        // allocation instead.
-        // Otherwise, if it only has one dbg.value, replace the old dbg.value
-        // with a dbg.declare.
-        convertDbgValue(op, hasUndefs);
-      }
-
-      // If no alloca was created for this value, nothing else is needed.
-      // Otherwise, all users of the original value need to go thru the alloca.
-      if (!allocaOp)
-        continue;
-
-      // Update all of the old value uses to route through the alloca instead
-      // of using the value directly.
-      for (auto it = value.user_begin(), e = value.user_end(); it != e;) {
-        // Grab the next unique user.
-        auto *user = *it;
-        while (++it != e && *it == user)
-          continue;
-
-        // If the user is another dbg.value, it must be for a variable that has
-        // multiple non-undef definitions. Cannot convert to dbg.declare as it
-        // has a limited scope.
-        if (auto dbgUser = dyn_cast<LLVM::DbgValueOp>(user)) {
-          convertDbgValue(dbgUser, true);
-          continue;
-        }
-
-        // If the user was already converted to a declare, skip it.
-        if (isa<LLVM::DbgDeclareOp>(user))
-          continue;
-
-        // Otherwise, this is a normal use, replace it with a load from an
-        // alloca.
-        OpBuilder loadBuilder(user);
-        user->replaceUsesOfWith(
-            value, loadBuilder.create<LLVM::LoadOp>(user->getLoc(),
-                                                    value.getType(), allocaOp));
-      }
-
-      // Store into the alloca at the place where the value was defined.
-      if (auto *valueOp = value.getDefiningOp()) {
-        OpBuilder storeBuilder(valueOp->getNextNode());
-        storeBuilder.create<LLVM::StoreOp>(value.getLoc(), value, allocaOp);
-      } else {
-        // If the value is a block argument, we need to search for an insertion
-        // point after the start of the block.
-        auto insertPt = value.getParentBlock()->begin();
-        while (isa<LLVM::DbgValueOp, LLVM::DbgDeclareOp, LLVM::AllocaOp,
-                   LLVM::ConstantOp>(*insertPt))
-          ++insertPt;
-
-        // Block arguments might not contain debuginfo scope (which can trip up
-        // verifiers later), so to keep it simple, we also use erasedLoc.
-        OpBuilder storeBuilder(&*insertPt);
-        Location erasedLoc = UnknownLoc::get(value.getContext());
-        storeBuilder.create<LLVM::StoreOp>(erasedLoc, value, allocaOp);
-      }
-    }
-  }
-}
 
 /// Returns whether this debug value is unsupported by LLVM.
 static bool isUnsupportedDebugValue(DebugInfo::ValueOp op) {
@@ -778,134 +535,12 @@ static bool isUnsupportedDebugValue(DebugInfo::ValueOp op) {
   return walkResult.wasInterrupted();
 }
 
-namespace {
-/// A linearized canonical representation of an inline call stack (as opposed to
-/// a binary-tree-based representation used by CallSiteLoc) that allows easy
-/// ancestor-child comparison.
-class CallStack {
-public:
-  CallStack() = default;
-  CallStack(Location overallLoc) {
-    walkLocation(overallLoc, LocWalkPolicy::CallerPriority, [&](Location loc) {
-      if (auto fusedLoc = dyn_cast<mlir::FusedLocWith<DIScopeAttr>>(loc)) {
-        if (fusedLoc.getLocations().size() != 1)
-          return WalkResult::advance();
-
-        if (auto fileLineCol =
-                dyn_cast<FileLineColLoc>(fusedLoc.getLocations().front()))
-          frames.emplace_back(fusedLoc.getMetadata(), fileLineCol.getLine());
-      }
-      return WalkResult::advance();
-    });
-  }
-
-  /// The call stack ordered from caller to callee.
-  /// Each frame encodes the scope of the location & the line number.
-  using Frame = std::pair<DIScopeAttr, unsigned>;
-  SmallVector<Frame> frames;
-};
-
-/// Maps each frame of a CallStack to some user-defined data `T`.
-template <typename T>
-class CallStackWith {
-public:
-  bool empty() const { return dataFrames.empty(); }
-
-  /// Reference to the data value mapped to the last (innermost) frame.
-  T &backData() { return dataFrames.back().second; }
-
-  /// Update the internal call stack to represent `newStack` instead.
-  /// Any stack frame that will no longer exist is considered invalidated, and
-  /// will be returned in the order of their positions in the call stack.
-  /// Each newly added stack frame will come with a default-constructed `T`.
-  ///
-  /// For example, calling with
-  ///   dataFrames = [(L0, T0), (L1, T1), (L2, T2), (L3, T3)]
-  ///   newStack   = [L0, L1, L4, L5]
-  /// results in
-  ///   dataFrames = [(L0, T0), (L1, T1), (L4, T()), (L5, T())]
-  /// and returns [T2, T3].
-  SmallVector<T> updateTo(const CallStack &newStack) {
-    // Walk until `newStack.frames` & `dataFrames` diverge.
-    auto thisIter = dataFrames.begin();
-    auto newIter = newStack.frames.begin();
-    for (; thisIter != dataFrames.end() && newIter != newStack.frames.end();
-         ++thisIter, ++newIter)
-      if (thisIter->first != *newIter)
-        break;
-
-    SmallVector<T> invalidated;
-    // Diverged in the middle of `dataFrames`. Invalidate everything afterwards.
-    if (thisIter != dataFrames.end()) {
-      std::transform(thisIter, dataFrames.end(),
-                     std::back_inserter(invalidated),
-                     [](auto it) { return it.second; });
-      dataFrames.truncate(dataFrames.size() - invalidated.size());
-    }
-
-    // Append anything at or after `newIter` to `dataFrames`.
-    for (; newIter != newStack.frames.end(); ++newIter)
-      dataFrames.emplace_back(*newIter, T());
-
-    return invalidated;
-  }
-
-private:
-  /// The call stack ordered from caller to callee.
-  /// Each frame encodes both a location and a custom data `T`.
-  SmallVector<std::pair<CallStack::Frame, T>> dataFrames;
-};
-} // namespace
-
-/// Pre-Massaging to bridge the semantics between DebugInfo's DebugValues &
-/// LLVM's counterpart. Also removes unsupported cases.
-///
-/// - Sink kill Debug Value ops so that they are the last instructions from
-/// their source line. This way variables are guaranteed to be killed only at
-/// the end of the line.
-/// - Remove Debug Value ops that contain unsupported components by LLVM.
-static void preAdaptDebugValuesToLLVM(Operation *op) {
-  for (Region &region : op->getRegions()) {
-    for (Block &block : region.getBlocks()) {
-      // The kill Debug Value Ops corresponding to the current line at each
-      // inlined scope.
-      CallStackWith<SmallVector<Operation *>> pendingKillsByLoc;
-      for (Operation &op : llvm::make_early_inc_range(block.getOperations())) {
-        // Ops without a location follow the location of the previous op.
-        const CallStack callStack(op.getLoc());
-        if (!callStack.frames.empty()) {
-          SmallVector<SmallVector<Operation *>> invalidated =
-              pendingKillsByLoc.updateTo(callStack);
-          // This is the start of a new line. Move all pending kill debug values
-          // before this op.
-          for (SmallVector<Operation *> &kills : invalidated)
-            for (Operation *kill : kills)
-              kill->moveBefore(&op);
-        }
-
-        if (ValueOp dbgValue = dyn_cast<ValueOp>(op)) {
-          if (isUnsupportedDebugValue(dbgValue)) {
-            dbgValue->erase();
-            continue;
-          }
-        } else if (!pendingKillsByLoc.empty() && isa<KillOp>(op)) {
-          pendingKillsByLoc.backData().push_back(&op);
-        }
-
-        preAdaptDebugValuesToLLVM(&op);
-      }
-
-      // Any still pending kills can be moved before the last op of the block.
-      if (!pendingKillsByLoc.empty()) {
-        Operation *lastOp = &block.back();
-        SmallVector<SmallVector<Operation *>> invalidated =
-            pendingKillsByLoc.updateTo({});
-        for (SmallVector<Operation *> &kills : invalidated)
-          for (Operation *kill : kills)
-            kill->moveBefore(lastOp);
-      }
-    }
-  }
+/// Remove debug values that are unsupported by LLVM.
+static void removeUnsupportedDebugValues(Operation *op) {
+  op->walk([](DebugInfo::ValueOp value) {
+    if (isUnsupportedDebugValue(value))
+      value->erase();
+  });
 }
 
 void DebugInfoToLLVMPass::runOnOperation() {
@@ -944,13 +579,17 @@ void DebugInfoToLLVMPass::runOnOperation() {
   mlir::RewritePatternSet patterns(&getContext());
   populateDebugInfoToLLVMPatterns(replacer, patterns);
 
-  // Massage DebugValues before conversion.
-  preAdaptDebugValuesToLLVM(getOperation());
+  // Massage DebugInfo before conversion.
+  TargetAdapter targetAdapter = getTargetAdapter(getTargetInfo(getOperation()));
+  targetAdapter.preTranslationAdapter(getOperation());
+
+  // Legalize DebugValues.
+  removeUnsupportedDebugValues(getOperation());
 
   if (failed(mlir::applyPartialConversion(getOperation(), target,
                                           std::move(patterns))))
     return signalPassFailure();
 
-  // Clean up the generated LLVM.
-  convertDbgValueToDeclare(getOperation());
+  // Massage DebugInfo after conversion.
+  targetAdapter.postTranslationAdapter(getOperation());
 }
