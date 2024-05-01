66f1d591fd2e9f564feb5e77aef60f03c8593e83
Patrick Dougherty <patrick.dougherty@modular.com>
2024-05-01 13:30:39 -0500

    [MAX] Support MAX release channels (#39014)
    To support MAX Nightlies, we need a way to specify different release
    channels. This change mirrors the implementation for Mojo from #25828.
    It:
    
    - Adds a new CMake variable `MODULAR_MAX_INSTALL_CHANNEL` that is
    expected to take values like `nightly` or `stable`
    - Makes the `modular.cfg` section for the build dynamic based on the
    `MAX_CONFIG_SECTION` variable derived from the release channel
    - Makes queries against the config dynamic to account for the section
    name
    
    MODULAR_ORIG_COMMIT_REV_ID: 770bcb2705529c6a1bef03224de5039ebc50e8c2

--- KGEN/lib/CompilerRT/Config.cpp
+++ KGEN/lib/CompilerRT/Config.cpp
@@ -9,16 +9,22 @@
 #include "Support/AlignedAlloc.h"
 #include "Support/Configuration.h"
 #include "Support/SymbolExport.h"
+#include "llvm/ADT/Twine.h"
 
 using namespace M;
 
+#define _STRINGIFY(str) #str
+#define _X_STRINGIFY(str) _STRINGIFY(str)
+#define STRINGIFY_MAX_CONFIG _X_STRINGIFY(MAX_CONFIG_SECTION)
+
 COMPILERRT_EXPORT COMPILERRT_VISIBILITY_EXPORT const char *
-KGEN_CompilerRT_getConfigValue(const char *key) {
+KGEN_CompilerRT_getMAXConfigValue(const char *key) {
   ErrorOr<Config> configOr = Config::open();
   if (configOr.isError())
     return nullptr;
 
-  StringRef value = configOr->getValue(key);
+  llvm::Twine configKey = llvm::Twine(STRINGIFY_MAX_CONFIG).concat(key);
+  StringRef value = configOr->getValue(configKey.str());
   char *res = (char *)KGEN_CompilerRT_AlignedAlloc(kPreferredMemoryAlignment,
                                                    value.size() + 1);
   strncpy(res, value.str().c_str(), value.size());
@@ -29,6 +35,6 @@ KGEN_CompilerRT_getConfigValue(const char *key) {
 void M::KGEN::registerConfig(
     std::vector<std::pair<llvm::StringLiteral, void *>> &funcs) {
   funcs.emplace_back(
-      std::pair{llvm::StringLiteral("KGEN_CompilerRT_getConfigValue"),
-                (void *)&KGEN_CompilerRT_getConfigValue});
+      std::pair{llvm::StringLiteral("KGEN_CompilerRT_getMAXConfigValue"),
+                (void *)&KGEN_CompilerRT_getMAXConfigValue});
 }
