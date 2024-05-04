modular/modular

commit 5b09e0c278dc0f5d9870e142a0a73ae25f8555a8

[mojo-doc] Fix processing of kw only parameters in signatures (#36421)
This adds the same kind of processing for parameters as
we already have for arguments to ensure that the `/` and `*`
signifiers are inserted into the list correctly.

Fixes #36114

MODULAR_ORIG_COMMIT_REV_ID: 0b62c6e92bb1b62bc632bdd7aa854b693dd2f12d

KGEN/lib/MojoTooling/ASTDeclView.cpp
@@ -406,6 +406,7 @@ llvm::json::Object ParameterDeclView::toJSON(MojoParserContext &ctx) const {
   llvm::json::Object object{{"kind", getKindAsString()},
                             {"name", getName().str()},
                             {"type", type},
+                            {"passingKind", stringifyPassingKind(passingKind)},
                             {"description", description}};
   if (defaultValue)
     object["default"] = *defaultValue;
@@ -590,6 +591,31 @@ std::string FunctionDeclView::getMarkdownDocString() const {
   return markdown;
 }
 
+/// Print the signatures for a list of arguments or parameters.
+template <typename T>
+static void printArgOrParameterSignature(
+    ArrayRef<T> args, SmallVectorImpl<std::pair<unsigned, unsigned>> *offsets,
+    llvm::raw_string_ostream &os) {
+  SmallVector<PassingKind> passingKinds = llvm::map_to_vector(
+      args, [](const T &arg) { return arg.getPassingKind(); });
+  PassingKindPrinter passingKindPrinter(os, passingKinds);
+  size_t idx = 0;
+  auto printArg = [&](const T &arg) {
+    passingKindPrinter.printOptionalStarSlash(idx);
+
+    unsigned argStart = os.str().size();
+    os << arg.getDeclarationSnippet();
+    if (offsets)
+      offsets->push_back({argStart, os.str().size()});
+
+    // Check if we are at the end; if so, we might still have to print a '/'.
+    passingKindPrinter.printOptionalTrailingSlash(idx++);
+  };
+  os << (std::is_same_v<T, ArgumentDeclView> ? "(" : "[");
+  interleaveComma(args, os, printArg);
+  os << (std::is_same_v<T, ArgumentDeclView> ? ")" : "]");
+}
+
 std::string FunctionDeclView::getSignature(
     SmallVectorImpl<std::pair<unsigned, unsigned>> *parameterOffsets,
     SmallVectorImpl<std::pair<unsigned, unsigned>> *argumentOffsets,
@@ -601,40 +627,12 @@ std::string FunctionDeclView::getSignature(
   signatureOS << getName().split('(').first;
 
   // Emit the parameters of the function.
-  if (!parameters.empty()) {
-    signatureOS << "[";
-    interleaveComma(getParameters(), signatureOS, [&](const auto &param) {
-      unsigned paramStart = signature.size();
-      signatureOS << param.getDeclarationSnippet();
-      if (parameterOffsets)
-        parameterOffsets->push_back({paramStart, signature.size()});
-    });
-    signatureOS << "]";
-  }
+  if (!parameters.empty())
+    printArgOrParameterSignature(ArrayRef(parameters), parameterOffsets,
+                                 signatureOS);
 
   // Emit the arguments of the function.
-  SmallVector<PassingKind> passingKinds =
-      llvm::map_to_vector(args, [](const M::ArgumentDeclView &arg) {
-        return arg.getPassingKind();
-      });
-  PassingKindPrinter passingKindPrinter(signatureOS, passingKinds,
-                                        /*suppressSlashAfterSelf=*/isMethod());
-  size_t idx = 0;
-  auto printArg = [&](const M::ArgumentDeclView &arg) {
-    passingKindPrinter.printOptionalStarSlash(idx);
-
-    unsigned argStart = signature.size();
-    signatureOS << arg.getDeclarationSnippet();
-    if (argumentOffsets)
-      argumentOffsets->push_back({argStart, signature.size()});
-
-    // Check if we are at the end; if so, we might still have to print a '/'.
-    passingKindPrinter.printOptionalTrailingSlash(idx++);
-  };
-
-  signatureOS << "(";
-  interleaveComma(args, signatureOS, printArg);
-  signatureOS << ")";
+  printArgOrParameterSignature(ArrayRef(args), argumentOffsets, signatureOS);
 
   // Emit the result type.
   if (returnOffset)
@@ -746,7 +744,7 @@ FunctionDeclView::FunctionDeclView(MojoASTDeclRef declRef)
     parameters.push_back(
         ParameterDeclView(demangleIfNeeded(param).getName().getValue(),
                           generateTypeString(param.getType(), selfType),
-                          std::move(defaultValue)));
+                          paramPassingKinds[index], std::move(defaultValue)));
   }
 
   // Grab the result type, if it's non-none.
@@ -927,16 +925,8 @@ std::string StructDeclView::getDeclarationSnippet(
   llvm::raw_string_ostream os(snippet);
   os << "struct " << getName();
 
-  if (!parameters.empty()) {
-    os << "[";
-    interleaveComma(getParameters(), os, [&](const auto &param) {
-      unsigned paramStart = snippet.size();
-      os << param.getDeclarationSnippet();
-      if (parameterOffsets)
-        parameterOffsets->push_back({paramStart, snippet.size()});
-    });
-    os << "]";
-  }
+  if (!parameters.empty())
+    printArgOrParameterSignature(ArrayRef(parameters), parameterOffsets, os);
 
   return snippet;
 }
@@ -980,7 +970,8 @@ StructDeclView::StructDeclView(MojoASTDeclRef declRef)
       decl(declRef) {
   auto structOp = cast<StructDeclOp>(declRef.getIfOperation());
   TypeSignatureType signature = structOp.getSignature();
-  DefaultValueHandler defaultParamHandler(signature.getParamPassingKinds(),
+  ArrayRef<PassingKind> passingKinds = signature.getParamPassingKinds();
+  DefaultValueHandler defaultParamHandler(passingKinds,
                                           signature.getDefaultPosParams(),
                                           signature.getDefaultKwOnlyParams());
 
@@ -989,9 +980,10 @@ StructDeclView::StructDeclView(MojoASTDeclRef declRef)
     std::optional<std::string> defaultValue;
     if (auto defaultAttr = defaultParamHandler.getDefault(index))
       defaultValue = generatePValueString(defaultAttr);
-    parameters.push_back(ParameterDeclView(
-        demangleIfNeeded(param).getName().getValue(),
-        generateTypeString(param.getType()), std::move(defaultValue)));
+    parameters.push_back(
+        ParameterDeclView(demangleIfNeeded(param).getName().getValue(),
+                          generateTypeString(param.getType()),
+                          passingKinds[index], std::move(defaultValue)));
   }
 
   if (auto docStr = decl->getParsedDocString()) {

KGEN/include/KGEN/MojoTooling/ASTDeclView.h
@@ -130,9 +130,12 @@ private:
 class ParameterDeclView : public DeclView {
 public:
   ParameterDeclView(StringRef name, StringRef type,
+                    KGEN::LIT::PassingKind passingKind,
                     std::optional<std::string> defaultValue)
       : DeclView(DeclViewKind::DK_ParameterDeclView, name), type(type),
-        defaultValue(std::move(defaultValue)) {}
+        passingKind(passingKind), defaultValue(std::move(defaultValue)) {}
+
+  KGEN::LIT::PassingKind getPassingKind() const { return passingKind; };
 
   /// Return the default value of this argument, if any.
   std::optional<StringRef> getDefaultValue() const { return defaultValue; }
@@ -155,6 +158,7 @@ public:
   ///    "description": string,
   ///    "name": string,
   ///    "type": string,
+  ///    "passingKind": string,
   ///    "defaultValue": string?
   ///  }
   llvm::json::Object toJSON(MojoParserContext &ctx) const override;
@@ -169,6 +173,7 @@ public:
 
 private:
   std::string type;
+  KGEN::LIT::PassingKind passingKind;
   std::optional<std::string> defaultValue;
 
   //===----------------------------------------------------------------------===//

KGEN/test/mojo-tool/doc/mojo-doc.mojo
@@ -205,17 +205,25 @@ fn fn_with_parameter_references[
 # CHECK:      "parameters": [
 # CHECK:          "description": "This is a parameter."
 # CHECK:          "name": "param"
+# CHECK:          "passingKind": "pos",
 # CHECK:          "type": "dtype"
-# CHECK:      "signature": "fn_with_params[param: dtype]()"
+# CHECK:          "description": "This is a second parameter."
+# CHECK:          "name": "param2"
+# CHECK:          "passingKind": "kw",
+# CHECK:          "type": "dtype"
+# CHECK:      "signature": "fn_with_params[param: dtype, /, *, param2: dtype]()"
 
 
-fn fn_with_params[param: __mlir_type.`!kgen.dtype`]():
+fn fn_with_params[
+    param: __mlir_type.`!kgen.dtype`, /, *, param2: __mlir_type.`!kgen.dtype`
+]():
     """This is a function summary.
 
     The is some kind of description.
 
     Parameters:
         param: This is a parameter.
+        param2: This is a second parameter.
     """
     return
 

commit 70ec64844124c166600a33cf17f9fa2606af0fbb

[mojo-tooling] Properly supress `/` after self when generating signature docs (#39304)
This was a regression after #36421 when some of the code was refactored to
share things between args/parameters (and the LSP tests were disabled at
that time, which would have caught this).

Fixes MOTO-286

MODULAR_ORIG_COMMIT_REV_ID: 2d399f72baf3a5f4d14adb6f423c0148ef43dbe8

KGEN/lib/MojoTooling/ASTDeclView.cpp
@@ -643,9 +643,10 @@ std::string FunctionDeclView::getMarkdownDocString() const {
 template <typename T>
 static void printArgOrParameterSignature(
     ArrayRef<T> args, SmallVectorImpl<std::pair<unsigned, unsigned>> *offsets,
-    llvm::raw_string_ostream &os) {
+    llvm::raw_string_ostream &os, bool suppressSlashAfterSelf = false) {
   PassingKindPrinter passingKindPrinter(
-      os, args.size(), [&](size_t idx) { return args[idx].getPassingKind(); });
+      os, args.size(), [&](size_t idx) { return args[idx].getPassingKind(); },
+      suppressSlashAfterSelf);
   size_t idx = 0;
   auto printArg = [&](const T &arg) {
     passingKindPrinter.printOptionalStarSlash(idx);
@@ -679,7 +680,8 @@ std::string FunctionDeclView::getSignature(
                                  signatureOS);
 
   // Emit the arguments of the function.
-  printArgOrParameterSignature(ArrayRef(args), argumentOffsets, signatureOS);
+  printArgOrParameterSignature(ArrayRef(args), argumentOffsets, signatureOS,
+                               /*suppressSlashAfterSelf=*/isMethodFlag);
 
   // Emit the result type.
   if (returnOffset)

KGEN/unittests/mojo-lsp-server/HoverTest.cpp
@@ -65,10 +65,9 @@ TEST(HoverTest, testHoverFunctionDecls) {
       .hover(doc, rangeInit.start,
              [&](const lsp::Hover &hover) {
                EXPECT_EQ(hover.range, rangeInit);
-               // FIXME(38725): There's an unexpected `/` argument.
                EXPECT_EQ(hover.contents.value,
                          R"(```mojo
-(function) fn __init__(inout self: Self, /, borrowed_input: Int, init_arg: Int, owned owned_input: Int, *init_kargs: Int)
+(function) fn __init__(inout self: Self, borrowed_input: Int, init_arg: Int, owned owned_input: Int, *init_kargs: Int)
 ```
 ---
 

KGEN/unittests/mojo-lsp-server/NotebookTest.cpp
@@ -126,13 +126,10 @@ SomeStruct()
                        ASSERT_EQ((int)signatureHelp.signatures.size(), 2);
                        EXPECT_EQ(signatureHelp.activeSignature, 0);
                        EXPECT_EQ(signatureHelp.activeParameter, 1);
-                       // FIXME(38725): There's an unexpected `/` argument.
                        EXPECT_EQ(signatureHelp.signatures[0].label,
-                                 "fn __init__(inout self: Self, /)");
-                       // FIXME(38725): There's an unexpected `/` argument.
-                       EXPECT_EQ(
-                           signatureHelp.signatures[1].label,
-                           "fn __init__(inout self: Self, /, a_field: Int)");
+                                 "fn __init__(inout self: Self)");
+                       EXPECT_EQ(signatureHelp.signatures[1].label,
+                                 "fn __init__(inout self: Self, a_field: Int)");
                      })
       .execute();
 }

KGEN/unittests/mojo-lsp-server/SignatureHelpTest.cpp
@@ -93,10 +93,9 @@ fn test():
                        EXPECT_EQ(signatureHelp.activeSignature, 0);
                        EXPECT_EQ(signatureHelp.activeParameter, 1);
                        EXPECT_EQ(signatureHelp.signatures[0].label,
-                                 "fn __init__(inout self: Self, /)");
-                       EXPECT_EQ(
-                           signatureHelp.signatures[1].label,
-                           "fn __init__(inout self: Self, /, a_field: Int)");
+                                 "fn __init__(inout self: Self)");
+                       EXPECT_EQ(signatureHelp.signatures[1].label,
+                                 "fn __init__(inout self: Self, a_field: Int)");
                      })
       .execute();
 }