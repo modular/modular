4db531a4274cb48aa7fd8ad88caba86d7b4bd981
Billy Zhu <billyzhu@modular.com>
2024-05-07 14:32:01 -0700

    [Support][DebugInfo][GPU] Add NVPTX conversion for LineTableLocOp (#39423)
    NVPTX does not have a "nop" instruction. Add a custom lowering for the
    LineTableLocOp into a nop-equivalent instruction. Currently using
    "[pmevent.mask
    0](https://docs.nvidia.com/cuda/parallel-thread-execution/index.html#miscellaneous-instructions-pmevent)"
    (which is supposed to do nothing) and it's trivial to change this later.
    I've tested to make sure it is preserved by the nvptx compiler (such a
    test will be added with the PR that inserts LineTableLocOps).
    
    MODULAR_ORIG_COMMIT_REV_ID: 0bad08acba494b52c9e4634b144da7bbfefec8b1

--- Support/lib/DebugInfoDialect/DebugInfoToLLVM/Target/NVPTXAdapter.cpp
+++ Support/lib/DebugInfoDialect/DebugInfoToLLVM/Target/NVPTXAdapter.cpp
@@ -6,10 +6,51 @@
 
 #include "NVPTXAdapter.h"
 
+#include "Support/DebugInfoDialect/IR/DebugInfoAttrs.h"
 #include "Support/DebugInfoDialect/IR/DebugInfoOps.h"
+#include "mlir/Dialect/LLVMIR/LLVMDialect.h"
+#include "mlir/IR/PatternMatch.h"
 
 using namespace M;
 using namespace M::DebugInfo;
+
+namespace LLVM = mlir::LLVM;
+
+//===----------------------------------------------------------------------===//
+// Conversion Patterns
+//===----------------------------------------------------------------------===//
+
+namespace {
+/// Convert LineTableLocOp into a "nop" equivalent in PTX.
+struct ConvertLineTableLocOp : public OpRewritePattern<LineTableLocOp> {
+  ConvertLineTableLocOp(MLIRContext *ctx, DIAttrTypeReplacer &replacer)
+      : OpRewritePattern<LineTableLocOp>(ctx), replacer(replacer) {}
+
+  LogicalResult matchAndRewrite(LineTableLocOp op,
+                                PatternRewriter &rewriter) const override {
+    rewriter.create<LLVM::InlineAsmOp>(
+        replacer.replace<LocationAttr>(op.getLoc()), TypeRange{}, ValueRange{},
+        "pmevent.mask 0;", "", /*has_side_effects=*/true,
+        /*is_align_stack=*/false,
+        LLVM::AsmDialectAttr::get(op.getContext(), LLVM::AsmDialect::AD_ATT),
+        ArrayAttr());
+    rewriter.eraseOp(op);
+    return success();
+  }
+
+  /// The replacer used to update attributes.
+  DIAttrTypeReplacer &replacer;
+};
+} // namespace
+
+static void populateNVPTXConversionPatterns(DIAttrTypeReplacer &replacer,
+                                            RewritePatternSet &patterns) {
+  patterns.add<ConvertLineTableLocOp>(patterns.getContext(), replacer);
+}
+
+//===----------------------------------------------------------------------===//
+// Custom Massaging
+//===----------------------------------------------------------------------===//
 
 /// NVPTX does not support variables that have more than one location. This
 /// means we cannot have a variable that has limited lifetime. Remove KillOps so
@@ -19,7 +60,11 @@ static void removeDebugKills(mlir::ModuleOp module) {
   module->walk([](DebugInfo::KillOp kill) { kill->erase(); });
 }
 
+//===----------------------------------------------------------------------===//
+// getNVPTXAdapter
+//===----------------------------------------------------------------------===//
+
 TargetAdapter DebugInfo::getNVPTXAdapter() {
-  return TargetAdapter{populateFallbackConversionPatterns, removeDebugKills,
+  return TargetAdapter{populateNVPTXConversionPatterns, removeDebugKills,
                        convertDbgValueToDeclare};
 }
