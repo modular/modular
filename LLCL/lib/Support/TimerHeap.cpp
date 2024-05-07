f249ac900ddeab1f68472fe6682ef6c4e439660e
Adin Scannell <amscanne@modular.com>
2024-05-07 12:21:56 -0700

    [LLCL] Add TimerHeap support class (#39116)
    This will likely be replaced/evolve in the future with a proper async
    runtime. This is just providing a simple capability that is needed for
    the moment.
    
    MODULAR_ORIG_COMMIT_REV_ID: 2580e6c0547b957bb4dc65f54b05d655b6aa2667

---
+++ LLCL/lib/Support/TimerHeap.cpp
@@ -0,0 +1,48 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#include "LLCL/Support/TimerHeap.h"
+#include "LLCL/Runtime/Algorithms.h"
+
+using namespace M::LLCL;
+
+void TimerHeap::push(const deadline expiration, AsyncValueRef<Chain> chain) {
+  std::unique_lock lk(mu);
+  entries.emplace(Entry(expiration, std::move(chain)));
+  cv.notify_one();
+}
+
+void TimerHeap::stop() {
+  {
+    std::unique_lock lk(mu);
+    if (!running)
+      return;
+    running = false;
+    cv.notify_one();
+  }
+  thread.join();
+}
+
+void TimerHeap::run() {
+  std::unique_lock lk(mu);
+  while (running) {
+    if (entries.empty()) {
+      cv.wait(lk, [&] { return !running || !entries.empty(); });
+      continue; // Recheck running.
+    }
+    const Entry &next = entries.top();
+    auto now = std::chrono::steady_clock::now();
+    if (now >= next.expiration) {
+      // The entry has expired, trigger the chain and drop it.
+      next.expired.copy().emplace();
+      entries.pop();
+    } else {
+      // Wait until this point, or we are signalled.
+      auto delta = next.expiration - now;
+      cv.wait_for(lk, delta, [&] { return !running; });
+    }
+  }
+}
