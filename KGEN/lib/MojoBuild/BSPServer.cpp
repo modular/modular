0d66d0c23d036937e8e14967fb405c4fbc5c1311
Brian Gesiak <brian@modular.com>
2024-05-15 22:09:56 -0400

    [MojoBuild] Extract `MojoBuildServer` (NFC) (#39594)
    Extract Mojo-specific logic added in #39320 out of
    `MojoBuild::BSPServer` into `MojoBuild::MojoBuildServer`, since the
    former is intended to handle only the build server protocol. No
    functional change here; this is just a refactor.
    
    MODULAR_ORIG_COMMIT_REV_ID: 5b2adcfeac5f4df4645a70212c40dda7f344dbdd

--- KGEN/lib/MojoBuild/BSPServer.cpp
+++ KGEN/lib/MojoBuild/BSPServer.cpp
@@ -5,17 +5,15 @@
 //===----------------------------------------------------------------------===//
 
 #include "BSPServer.h"
+#include "MojoBuildServer.h"
 
 #include "Config/Version.h"
 #include "KGEN/MojoBuild/Protocol.h"
-#include "KGEN/Support/Configuration.h"
 #include "Support/ErrorOr.h"
-#include "Support/Filesystem/Paths.h"
 
 #include "mlir/Tools/lsp-server-support/Logging.h"
 #include "mlir/Tools/lsp-server-support/Transport.h"
 #include "llvm/Support/FormatVariadic.h"
-#include "llvm/Support/Program.h"
 #include "llvm/Support/raw_ostream.h"
 
 #include <filesystem>
@@ -121,110 +119,31 @@ void BSPServer::onBuildInitialize(
       BuildServerCapabilities{CompileProvider{{"mojo"}}}});
 }
 
-/// Returns the path to the `mojo` executable, or an error if none could be
-/// found.
-static ErrorOr<std::string> getMojoDriverPath(KGEN::MojoConfig &config) {
-  std::error_code ec;
-  StringRef path = config.getDriverPath();
-  if (!std::filesystem::exists(path.str(), ec) || ec)
-    return Error(
-        llvm::formatv("unable to resolve the mojo path at '{0}'", path));
-  return path.str();
-}
-
-/// Returns all Mojo source package directories at the top level of the given
-/// `root` directory.
-static SmallVector<std::filesystem::path>
-getMojoSourcePackageDirectories(const std::filesystem::path &root,
-                                std::error_code &ec) {
-  SmallVector<std::filesystem::path> result;
-  std::filesystem::directory_iterator it(root, ec);
-  if (ec)
-    return result;
-
-  for (const auto &path : it)
-    if (Filesystem::isMojoSourcePackagePath(path))
-      result.push_back(path);
-
-  return result;
-}
-
 void BSPServer::onBuildTargetCompile(
     const CompileParams &params, mlir::lsp::Callback<CompileResult> callback) {
   if (auto err = errorIfUninitialized())
     return callback(std::move(err));
 
-  // Grab the path to the `mojo` driver.
-  ErrorOr<KGEN::MojoConfig> configOr = KGEN::MojoConfig::open();
-  if (configOr.isError())
-    return callback(error(
-        llvm::formatv("could not read 'modular.cfg': {0}", configOr.getError()),
-        lsp::ErrorCode::RequestFailed));
-  ErrorOr<std::string> driverPathOr = getMojoDriverPath(*configOr);
-  if (driverPathOr.isError())
+  ErrorOr<BuildResult> buildResultOr =
+      MojoBuildServer::buildWorkspace(workspacePath);
+  if (buildResultOr.isError())
     return callback(
-        error(driverPathOr.getError(), lsp::ErrorCode::RequestFailed));
+        error(buildResultOr.getError(), lsp::ErrorCode::RequestFailed));
 
-  // Prepare the compile result object.
   CompileResult result;
   if (params.originId)
     result.originId = *params.originId;
-
-  // We do not yet define a Mojo project manifest, so we cannot be certain what
-  // to build within our workspace. For the time being, look for a directory
-  // named 'src' at the root of the workspace, and build a Mojo package from
-  // each Mojo source package directory within 'src'. If there's nothing to
-  // build, return a "cancelled" result.
-  auto respondWithCancelled = [&]() {
+  switch (*buildResultOr) {
+  case BuildResult::Success:
+    result.statusCode = StatusCode::Ok;
+    break;
+  case BuildResult::Failure:
+    result.statusCode = StatusCode::Error;
+    break;
+  case BuildResult::NothingToDo:
     result.statusCode = StatusCode::Cancelled;
-    callback(std::move(result));
-  };
-
-  std::error_code ec;
-  std::filesystem::path srcDir = workspacePath / "src";
-  if (!std::filesystem::is_directory(srcDir, ec) || ec)
-    return respondWithCancelled();
-
-  SmallVector<std::filesystem::path> packageDirs =
-      getMojoSourcePackageDirectories(srcDir, ec);
-  if (packageDirs.empty() || ec)
-    return respondWithCancelled();
-
-  // Create a directory for build artifacts.
-  std::filesystem::path buildDirectory = workspacePath / ".build";
-  std::filesystem::create_directory(buildDirectory, ec);
-  if (ec)
-    return callback(
-        error(llvm::formatv("could not create build directory '{0}': {1}",
-                            buildDirectory, ec.message()),
-              lsp::ErrorCode::RequestFailed));
-
-  // For now, as a simple proof of concept, this sequentially builds the
-  // packages in the workspace. In the future, this should be parallelized.
-  for (const auto &packageDir : packageDirs) {
-    SmallVector<StringRef> driverArgs{*driverPathOr, "package",
-                                      packageDir.c_str(), "-o",
-                                      buildDirectory.c_str()};
-    std::string errorMessage;
-    int exitCode = llvm::sys::ExecuteAndWait(
-        *driverPathOr, driverArgs,
-        /*Env=*/std::nullopt, /*Redirects=*/{}, /*SecondsToWait=*/0,
-        /*MemoryLimit=*/0, /*ErrMsg=*/&errorMessage);
-
-    // An error launching the driver is an internal server error, and we treat
-    // it as distinct from a compilation error.
-    if (!errorMessage.empty())
-      return callback(error(errorMessage, lsp::ErrorCode::InternalError));
-
-    // For now, we stop building as soon as any package fails to build.
-    if (exitCode) {
-      result.statusCode = StatusCode::Error;
-      return callback(std::move(result));
-    }
+    break;
   }
-
-  // We built one or more packages successfully.
-  result.statusCode = StatusCode::Ok;
   callback(std::move(result));
 }
 
