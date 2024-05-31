2252c7b3d25bd742ce98ea8014335d12443af0b4
weiwei chen <weiwei.chen@modular.com>
2024-05-31 10:35:21 -0400

    [KGEN] Re-enable DeadArgumentElimination pass. (#40854)
    Stacked PRs:
     * __->__#40854
     * #40853
    
    --- --- ---
    
    ### Re-enable DeadArgumentElimination pass.
    
    - [x] Re-enable DeadArgumentElimination pass in KGEN pipeline.
    
    MODULAR_ORIG_COMMIT_REV_ID: 313240e47c6fc8729c338a96fe6c391ed2580808

--- KGEN/lib/Compiler/Pipeline/Pipeline.cpp
+++ KGEN/lib/Compiler/Pipeline/Pipeline.cpp
@@ -253,9 +253,7 @@ void KGEN::buildPostElaborationPipeline(mlir::PassManager &pm,
   pm.addNestedPass<FuncOp>(createLowerLoops());
   // Lower async functions and closures as late as possible.
   pm.addPass(createLowerClosures());
-  // FIXME(MOCO-756): Dead argument elimination can miscompile code if there is
-  // sufficient function pointer indirection.
-  // if (options.optimizationLevel >= 2)
-  //   pm.addPass(createDeadArgumentElimination());
+  if (options.optimizationLevel >= 2)
+    pm.addPass(createDeadArgumentElimination());
   pm.addPass(createEliminateDeadSymbols());
 }
