7265335155267f7e748500ba162b1cb56ac72f41
Adin Scannell <amscanne@modular.com>
2024-06-07 22:47:24 -0700

    [LLCL] Make TimerHeap more efficient by adding cancelation (#39568)
    Since we expect many items to be added, we add proactive cancelation to
    prevent excessive wakeups. The main timer thread will still wake up on
    the next expiration, but will clean up any canceled items and set the
    next wakeup to the next current, legitimate wakeup scheduled. (Rather
    than rescheduling to the next wakeup that has since been canceled.)
    
    MODULAR_ORIG_COMMIT_REV_ID: e1b7d886cff43f06cc018086abffa36b6e5a0afb

--- LLCL/lib/Support/TimerHeap.cpp
+++ LLCL/lib/Support/TimerHeap.cpp
@@ -9,10 +9,34 @@
 
 using namespace M::LLCL;
 
-void TimerHeap::push(const deadline expiration, AsyncValueRef<Chain> chain) {
+void TimerHeap::push(const deadline expiration, AsyncValueRef<Chain> &chain) {
   std::unique_lock<std::mutex> lk(mu);
-  entries.emplace(Entry(expiration, std::move(chain)));
-  cv.notify_one();
+  bool notify = entries.empty() || entries[0].expiration > expiration;
+  entries.emplace_back(Entry(expiration, chain.copy()));
+  std::push_heap(entries.begin(), entries.end());
+  if (notify)
+    cv.notify_one();
+}
+
+void TimerHeap::cancel(AsyncValueRef<Chain> &chain) {
+  std::unique_lock<std::mutex> lk(mu);
+  bool reheap = false;
+  unsigned long i = 0;
+  while (i < entries.size()) {
+    auto &entry = entries[i];
+    if (entry.expired.getPointer() == chain.getPointer()) {
+      entry.expired.copy().emplace();
+      if (i < entries.size() - 1) {
+        std::swap(entries[i], entries.back());
+        reheap = true;
+      }
+      entries.pop_back();
+      continue;
+    }
+    i++;
+  }
+  if (reheap)
+    std::make_heap(entries.begin(), entries.end());
 }
 
 void TimerHeap::stop() {
@@ -33,12 +57,15 @@ void TimerHeap::run() {
       cv.wait(lk, [&] { return !running || !entries.empty(); });
       continue; // Recheck running.
     }
-    const Entry &next = entries.top();
+    const Entry &next = entries[0];
     auto now = std::chrono::steady_clock::now();
     if (now >= next.expiration) {
-      // The entry has expired, trigger the chain and drop it.
-      next.expired.copy().emplace();
-      entries.pop();
+      // The entry has expired, trigger the chain and drop it. This only
+      // happens if the reference hasn't been canceled/clear. See above.
+      if (next.expired.getPointer())
+        next.expired.copy().emplace();
+      std::pop_heap(entries.begin(), entries.end());
+      entries.pop_back();
     } else {
       // Wait until this point, or we are signalled.
       auto delta = next.expiration - now;
--- LLCL/unittests/TimerHeapTest.cpp
+++ LLCL/unittests/TimerHeapTest.cpp
@@ -29,7 +29,7 @@ struct TimerHeapTest : public testing::Test {
   AsyncValueRef<Chain> in(int64_t ns) {
     AsyncValueRef<Chain> out = AsyncValueRef<Chain>::allocate(*runtime);
     TimerHeap::deadline expiration = steady_clock::now() + nanoseconds(ns);
-    heap.push(expiration, out.copy());
+    heap.push(expiration, out);
     return out;
   }
 
@@ -42,18 +42,28 @@ TEST_F(TimerHeapTest, Serial) {
   await(in(0));
   await(in(10));
   await(in(100));
-  await(in(1000));
-  passed(1000);
+  await(in(1'000));
+  passed(1'000);
 }
 
 TEST_F(TimerHeapTest, OutOfOrder) {
-  auto a = in(1000);
-  auto b = in(100000);   // 100us.
-  auto c = in(10000000); // 10ms.
+  auto a = in(1'000);
+  auto b = in(100'000);    // 100us.
+  auto c = in(10'000'000); // 10ms.
   await(c);
-  passed(10000000);
+  passed(10'000'000);
   await(b);
   await(a);
+}
+
+TEST_F(TimerHeapTest, Cancelation) {
+  auto a = in(1'000);
+  auto b = in(100'000);    // 100us.
+  auto c = in(10'000'000); // 10ms.
+  heap.cancel(a);
+  heap.cancel(b);
+  await(c);
+  passed(10'000'000);
 }
 
 } // namespace
