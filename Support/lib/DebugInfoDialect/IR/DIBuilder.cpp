2a73edcb0d3cb5da2fcdfc533deb8eabd04d9834
Billy Zhu <billyzhu@modular.com>
2024-06-10 15:23:55 -0700

    [DebugInfo] Fix scope of subprogram (#41677)
    Fixes [MOCO-826 : \[mojo-parser\]\[debuginfo\] Wrong subprogram scope
    for imported
    [Internal link]
    Have the generated scope of subprograms just be the file scope. This is
    not correct for nested functions but this is no different from the
    previous behavior, which at best would find a file scope from the scope
    stack (and may not even be correct).
    
    I'm planning on a larger refactor of how debug scopes are handled in the
    parser. It seems a global scope stack may not be well suited for our
    parser, since it doesn't always parse in a stack/tree manner (it can
    jump to a sibling branch in the ASTDecl tree when resolving calls for
    example). It may be easier to just track the debug scopes alongside the
    AST/IR tree.
    
    MODULAR_ORIG_COMMIT_REV_ID: 3138b40871cffae0841671cce896bf6e4ac73812

--- Support/lib/DebugInfoDialect/IR/DIBuilder.cpp
+++ Support/lib/DebugInfoDialect/IR/DIBuilder.cpp
@@ -61,14 +61,9 @@ DISubprogramAttr DIBuilder::createSubprogram(SourceNameAttr name,
                                              unsigned int scopeLine,
                                              SubprogramFlags subprogramFlags,
                                              DISubroutineType type) {
-  // Get the last non-local scope to use as the parent for the subprogram.
-  auto range = llvm::reverse(scopes);
-  auto it = llvm::find_if(range, [](DIScopeAttr scope) {
-    return !isa_and_nonnull<DILocalScopeAttr>(scope);
-  });
-  assert(it != range.end() &&
-         "didn't find a non-local scope -- forgot to push one?");
-  return DISubprogramAttr::get(compileUnit, *it, name, linkageName, file, line,
+  // The only non-local scope we have is the file scope.
+  // TODO(MOCO-834): Properly handle nested function.
+  return DISubprogramAttr::get(compileUnit, file, name, linkageName, file, line,
                                scopeLine, subprogramFlags, type);
 }
 
