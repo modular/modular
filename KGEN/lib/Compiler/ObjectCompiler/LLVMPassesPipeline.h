c514fdc2df008b2b7b21e2754447d7d8b49f63a4
weiwei chen <weiwei.chen@modular.com>
2024-06-24 10:30:50 -0400

    [KGEN] Get some control on NVPTXTargetMachine for passes. (#42070)
    - [x] `MachineSink` pass for nvptx backend seems to be doing things that
    [hurts
    [Internal link]
    and [even wrong
    [Internal link]
    - [x] Dancing around intensively to get some control on how NVPTX
    backend constructs the llc pipeline so that we can disable the
    `MachineSink` pass for nvptx.
    
    Fixes: [Internal link]
    Fixes: [Internal link]
    MODULAR_ORIG_COMMIT_REV_ID: be81c5b8f9b0286b03b4d2645273a54a3bf801ba

--- KGEN/lib/Compiler/ObjectCompiler/LLVMPassesPipeline.h
+++ KGEN/lib/Compiler/ObjectCompiler/LLVMPassesPipeline.h
@@ -7,8 +7,8 @@
 #ifndef KGEN_LLVMPASSESPIPELINE_H
 #define KGEN_LLVMPASSESPIPELINE_H
 
-#include "llvm/IR/PassManager.h"
-#include "llvm/Support/CodeGen.h"
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Target/TargetMachine.h"
 
 namespace M::KGEN {
 class CompilationOptions;
@@ -16,6 +16,15 @@ class CompilationOptions;
 /// Build a module pass pipeline for a given set of compilation options.
 llvm::ModulePassManager
 buildLLVMOptimizationPipeline(const CompilationOptions &options);
+
+bool addPassesToEmitFile(CompilationOptions &options,
+                         llvm::LLVMTargetMachine &targetMachine,
+                         llvm::legacy::PassManagerBase &pm,
+                         llvm::raw_pwrite_stream &out,
+                         llvm::raw_pwrite_stream *dwoOut,
+                         llvm::CodeGenFileType fileType, bool disableVerify,
+                         llvm::MachineModuleInfoWrapperPass *mmiwp);
+
 } // namespace M::KGEN
 
 #endif // KGEN_LLVMPASSESPIPELINE_H
