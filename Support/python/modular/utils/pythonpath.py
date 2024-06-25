3225ddc0808bda937777328ce382f1efe1807ace
Mikhail Zolotukhin <mvz@modular.com>
2024-06-25 14:56:42 -0700

    [PT] Fix logic for locating libpython in LIT config. (#42247)
    The current logic is using `find_libpython` which works fine everywhere
    except on Mac. On Mac it returns a location of `Python` executable,
    whereas what we need is a `.dylib` file. This PR extends this logic to
    look at several extra locations hoping to find the right file.
    
    Verification:
    * Without this change `build check-torch` fails on Mac  on tests trying
      to find torchvision and libpython (e.g.
      `PyTorch/test/EndToEnd/torchvision.mlir`).
    * With this PR that command passes.
    
    MODULAR_ORIG_COMMIT_REV_ID: 03979ea45f0059420f06e45b240372957f5d5e4c

---
+++ Support/python/modular/utils/pythonpath.py
@@ -0,0 +1,87 @@
+# ===----------------------------------------------------------------------=== #
+#
+# This file is Modular Inc proprietary.
+#
+# ===----------------------------------------------------------------------=== #
+#
+# Utilities to fetch paths of python libraries.
+#
+# ===----------------------------------------------------------------------=== #
+from importlib import machinery
+import sys
+import os
+from pathlib import Path
+from sysconfig import get_config_var as var
+from itertools import product
+from find_libpython import find_libpython
+from typing import Optional
+
+
+def get_lib_path(base_path: Path, name: str) -> Optional[Path]:
+    """
+    Get the path of `name`.
+    Returns:
+        Path to the `name` library.
+    """
+    loader_details = (
+        machinery.ExtensionFileLoader,
+        machinery.EXTENSION_SUFFIXES + [".dylib"],
+    )
+
+    extfinder = machinery.FileFinder(str(base_path), loader_details)
+    ext_specs = extfinder.find_spec(name)
+    return None if ext_specs is None else Path(str(ext_specs.origin))
+
+
+def get_libtorch_python_path() -> Optional[Path]:
+    """
+    Get the path of libtorch_python.
+    Returns:
+        Path to the libtorch_python library.
+    """
+    try:
+        import torch
+    except ImportError:
+        return None
+
+    torch_path = Path(os.path.dirname(torch.__file__)) / "lib"
+    return get_lib_path(torch_path, "libtorch_python")
+
+
+def get_libtorchvision_path() -> Optional[Path]:
+    """
+    Get the path of libtorchvision.
+    Returns:
+        Path to the torchvision library.
+    """
+    try:
+        import torchvision
+    except ImportError:
+        return None
+
+    torchvision_path = Path(os.path.dirname(torchvision.__file__))
+    return get_lib_path(torchvision_path, "_C")
+
+
+def get_libpython():
+    is_windows = os.name == "nt"
+    if is_windows:
+        ext = "dll"
+    elif os.name == "posix":
+        ext = "dylib" if sys.platform == "darwin" else "so"
+
+    pyver = var("py_version_short")
+    folders = [Path(var("LIBPL")), Path(var("LIBDIR"))]
+    binaries = [var("LDLIBRARY"), f"libpython{pyver}.{ext}"]
+
+    # Make a list of potential locations of libtorch shared lib - we'll start
+    # with what `find_libpython` returns and then add some more possible
+    # locations. We then go over these locations until we find an existing file
+    # in one of them.
+    paths = [Path(str(find_libpython()))]
+    for folder, binary in product(folders, binaries):
+        paths.append(folder / binary)
+    for path in paths:
+        if path.exists() and path.is_file() and path.suffix == f".{ext}":
+            return path
+    return "LIBPYTHON NOT FOUND"
