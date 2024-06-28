280be08583d300be4e4acc4a377b1fe7755aa58b
Joshua Peterson <josh@modular.com>
2024-06-28 12:52:29 -0400

    [Support] Make ASSERT_STREAM message more general (#42409)
    Since `ASSERT_STREAM` is now part of the shared `Support` library, it
    should not have a message specific for any tool or executable.
    
    This change removes the tool-specific message for MAX (it will re-appear
    in a later PR, specific to the graph compiler tools). The change also
    makes the assert message more similar to that of `assert`, with the hope
    that developers will be able to easily parse it visually.
    
    Fixes GRA-666
    
    MODULAR_ORIG_COMMIT_REV_ID: 54f15906aac060983f38bf37901ffc8aa2fae242

--- Support/include/Support/AssertStream.h
+++ Support/include/Support/AssertStream.h
@@ -20,24 +20,20 @@ namespace M {
 /// Structure holding information about a failed assertion. Allows user to
 /// append information to the error message.
 struct FailedAssertion {
-  LLVM_ATTRIBUTE_NOINLINE FailedAssertion(llvm::StringRef line,
+  LLVM_ATTRIBUTE_NOINLINE FailedAssertion(llvm::StringRef file_path,
                                           int64_t lineno) {
-    getStorage().os << line << ":" << lineno << ": ";
+    getStorage().message << file_path << ":" << lineno << ": ";
   }
   [[noreturn]] ~FailedAssertion() {
-    llvm::errs()
-        << "=================================================================\n"
-           "    !!! MAX AI Engine has encountered an internal error !!!\n"
-           "=================================================================\n"
-        << getStorage().os.str() << "\n";
+    llvm::errs() << getStorage().message.str() << "\n";
     abort();
   }
   // Store error message info as a thread local struct. Avoids creating data for
   // each assertion which can blow the stack.
   struct Storage {
-    std::string message;
-    llvm::raw_string_ostream os;
-    Storage() : message(), os(message) {}
+    std::string message_storage;
+    llvm::raw_string_ostream message;
+    Storage() : message_storage(), message(message_storage) {}
   };
   LLVM_ATTRIBUTE_NOINLINE static Storage &getStorage() {
     static thread_local Storage storage;
@@ -54,12 +50,12 @@ struct FailedAssertion {
 /// Like `assert`, this does nothing if the NDEBUG symbol is defined.
 ///
 #ifdef NDEBUG
-#define ASSERT_STREAM(condition, message)
+#define ASSERT_STREAM(condition, message_stream)
 #else
-#define ASSERT_STREAM(condition, message)                                      \
+#define ASSERT_STREAM(condition, message_stream)                               \
   if (LLVM_UNLIKELY(!(condition)))                                             \
-  ::M::FailedAssertion(__FILE__, __LINE__).getStorage().os                     \
-      << #condition << " is false.\n" message
+  ::M::FailedAssertion(__FILE__, __LINE__).getStorage().message                \
+      << "Assertion failed: (" << #condition << ") is false.\n" message_stream
 #endif // NDEBUG
 
 #endif // SUPPORT_ASSERT_STREAM_H
