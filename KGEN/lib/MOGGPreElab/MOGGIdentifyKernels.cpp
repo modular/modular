6797efafb2bbc33e665e726559c65967633afe9c
Feras Boulala <feras.boulala@modular.com>
2024-07-10 10:31:38 -0400

    [MOGG][KGEN] Add basic support for identifying DPS Tensor API kernels (#43017)
    This pass will identify DPS Tensor API kernels and label the execute and
    shape `lit.func`.
    
    Going for the simple, concise passes.
    
    Related to GRA-671
    Fixes GRA-730
    
    MODULAR_ORIG_COMMIT_REV_ID: 9de83bb2afd74c156bf3b6b97ff5eb329bb89e73

---
+++ KGEN/lib/MOGGPreElab/MOGGIdentifyKernels.cpp
@@ -0,0 +1,131 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#include "llvm/ADT/StringSet.h"
+
+#include "KGEN/KGENDialect/KGENOps.h"
+#include "KGEN/LITDialect/LITTypes.h"
+#include "KGEN/MOGGPreElab/MOGGDecorators.h"
+#include "KGEN/MOGGPreElab/Passes.h"
+#include "mlir/Pass/Pass.h"
+
+#include "Helpers.h"
+
+using namespace M;
+using namespace KGEN;
+using namespace MOGGPreElab;
+
+namespace M::KGEN::MOGGPreElab {
+#define GEN_PASS_DEF_MOGGIDENTIFYKERNELS
+#include "KGEN/MOGGPreElab/MOGGPreElabPasses.h.inc"
+} // namespace M::KGEN::MOGGPreElab
+
+static constexpr std::array<llvm::StringLiteral, 3> kMaxRegistrationDecorator =
+    {"max", "compiler", "register"};
+static constexpr llvm::StringLiteral kExecuteFuncName = "execute";
+static constexpr llvm::StringLiteral kShapeFuncName = "shape";
+
+namespace {
+class MOGGIdentifyKernelsPass
+    : public M::KGEN::MOGGPreElab::impl::MOGGIdentifyKernelsBase<
+          MOGGIdentifyKernelsPass> {
+public:
+  void runOnOperation() override {
+    ModuleOp moduleOp = getOperation();
+    OpBuilder builder{moduleOp.getContext()};
+
+    const auto identifyKernelsLambda =
+        [&](LIT::StructDeclOp structDeclOp) -> WalkResult {
+      auto loc = structDeclOp->getLoc();
+      std::optional<StringAttr> registrationName;
+      auto decorators = structDeclOp.getDecorators();
+      if (decorators.empty())
+        return WalkResult::advance();
+
+      // Iterate over the decorators and to find max.compiler.register.
+      for (auto decorator : decorators) {
+        auto apply = dyn_cast<ParamOperatorAttr>(decorator);
+        if (!apply || apply.getNumOperands() != 2)
+          continue;
+
+        auto sym = dyn_cast<SymbolConstantAttr>(apply.getOperand(0));
+        if (!sym)
+          continue;
+
+        if (sym.getSymbol().getNestedReferences().size() !=
+            kMaxRegistrationDecorator.size())
+          continue;
+
+        bool valid = true;
+        for (auto [i, ref] :
+             llvm::enumerate(sym.getSymbol().getNestedReferences())) {
+          valid = ref.getValue().starts_with(kMaxRegistrationDecorator[i]);
+        }
+
+        if (!valid)
+          continue;
+
+        auto [_, nameAttr] =
+            cast<LIT::LITStructAttr>(apply.getOperand(1)).getValues().front();
+        auto name = dyn_cast<StringAttr>(nameAttr);
+        if (!name)
+          continue;
+
+        if (registrationName) {
+          emitError(loc, "Only one op can be registered per kernel struct");
+          return WalkResult::interrupt();
+        }
+        registrationName = name;
+      }
+
+      if (!registrationName)
+        return WalkResult::advance();
+
+      // Search for the `execute` and the optional `shape` functions. Until
+      // traits can enforce @staticmethod, this has to be done in this pass.
+      LIT::FuncOp executeFunc, shapeFunc;
+      for (auto &op : structDeclOp.getFields().front()) {
+        auto func = dyn_cast<LIT::FuncOp>(op);
+        if (!func)
+          continue;
+
+        if (func.getSourceName() == kExecuteFuncName)
+          executeFunc = func;
+        else if (func.getSourceName() == kShapeFuncName)
+          shapeFunc = func;
+      }
+
+      if (!executeFunc) {
+        emitError(loc, "Kernels must have an execution entry point named " +
+                           kExecuteFuncName);
+        return WalkResult::interrupt();
+      }
+
+      if (!executeFunc.getIsStatic()) {
+        emitError(loc, "Kernel entry point must be a static function");
+        return WalkResult::interrupt();
+      }
+
+      if (shapeFunc && !shapeFunc.getIsStatic()) {
+        emitError(loc, "Kernel shape function must be a static function");
+        return WalkResult::interrupt();
+      }
+
+      executeFunc->setAttr(builder.getStringAttr(kMOGGExecuteFunctionLabel),
+                           *registrationName);
+      if (shapeFunc) {
+        shapeFunc->setAttr(builder.getStringAttr(kMOGGShapeFunctionLabel),
+                           *registrationName);
+      }
+
+      return WalkResult::advance();
+    };
+
+    if (moduleOp.walk(identifyKernelsLambda).wasInterrupted())
+      signalPassFailure();
+  }
+};
+} // namespace
---
+++ KGEN/test/mojo-mogg/lit.cfg.py
@@ -0,0 +1,40 @@
+# ===----------------------------------------------------------------------=== #
+#
+# This file is Modular Inc proprietary.
+#
+# ===----------------------------------------------------------------------=== #
+
+import os
+
+from lit.llvm import llvm_config
+
+# name: The name of this test suite.
+config.name = "mojo-mogg"
+
+config.parser_stubs_source = os.path.join(
+    config.modular_src_root, "KGEN", "test", "test-packages"
+)
+
+# suffixes: A list of file extensions to treat as test files.
+config.suffixes = [".mojo", ".🔥", ".test"]
+
+# test_source_root: The root path where tests are located.
+config.test_source_root = os.path.dirname(__file__)
+
+# test_exec_root: The root path where tests should be run.
+config.test_exec_root = os.path.join(
+    config.modular_obj_root, "KGEN", "test", "mojo-mogg"
+)
+config.excludes = ["max_stub"]
+
+tool_dirs = [
+    config.modular_tools_dir,
+    config.mlir_tools_dir,
+    config.llvm_tools_dir,
+]
+tools = [
+    "kgen-translate",
+    "kgen-opt",
+]
+
+llvm_config.add_tool_substitutions(tools, tool_dirs)
