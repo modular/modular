57d18fecfc0661823961ee06d0a72d71862301e1
Jeff Niu <jeff@modular.com>
2024-07-16 13:39:58 -0700

    [KGEN] Slightly optimize sliceDependencies (#43396)
    Add a visited set to avoid walking deeply nested parameter expressions
    multiple times. Also reuse an IRMapping to slightly speed up cloning.
    This didn't end up making much of a different. Cloning is actually the
    major bottleneck here.
    
    MODULAR_ORIG_COMMIT_REV_ID: f6c61eef259aff4d08348867d87c532abca95dc1

--- KGEN/lib/Compiler/ObjectCompiler/ObjectCompiler.cpp
+++ KGEN/lib/Compiler/ObjectCompiler/ObjectCompiler.cpp
@@ -625,42 +625,75 @@ ObjectCompiler::lowerAllFuncsToLLVM(llvm::LLVMContext &ctx, ModuleOp module) {
 // produceStandaloneModule
 //===----------------------------------------------------------------------===//
 
+/// Dig into an attribute or type to find references to other symbols. If we see
+/// a new one, copy it over.
+template <typename AttrOrType>
+static void sliceDependenciesFrom(AttrOrType value, SymbolTable &sliceSymtab,
+                                  const SymbolTable &symtab,
+                                  DenseSet<const void *> &visited,
+                                  IRMapping &reusedMapping,
+                                  std::vector<Operation *> &worklist) {
+  // If we've already visited this value, we know we've extracted all
+  // dependencies from it and its subtree.
+  if (!visited.insert(value.getAsOpaquePointer()).second)
+    return;
+
+  // Check if this is a symbol reference.
+  if constexpr (std::is_same_v<AttrOrType, Attribute>) {
+    if (auto ref = dyn_cast<FlatSymbolRefAttr>(value)) {
+      // We know this is a new symbol because this is the first time we've
+      // visited this attribute.
+      StringAttr name = ref.getAttr();
+      Operation *symbol = symtab.lookup(name);
+      // If the symbol reference attribute doesn't reference a symbol, somehow
+      // invalid IR made it to the ObjectCompiler.
+      assert(symbol && "missing symbol, invalid IR?");
+
+      // Clone the symbol into the new symbol table. Reuse an IRMapping to save
+      // memory pressure.
+      reusedMapping.clear();
+      Operation *copy = symbol->clone(reusedMapping);
+      sliceSymtab.insert(copy);
+
+      // We need to recurse on this newly cloned operation.
+      worklist.push_back(copy);
+
+      // There are no further subelements. We can exit.
+      return;
+    }
+  }
+
+  // Recurse into subelements.
+  value.walkImmediateSubElements(
+      [&](Attribute attr) {
+        sliceDependenciesFrom(attr, sliceSymtab, symtab, visited, reusedMapping,
+                              worklist);
+      },
+      [&](Type type) {
+        sliceDependenciesFrom(type, sliceSymtab, symtab, visited, reusedMapping,
+                              worklist);
+      });
+}
+
 /// Slice the dependencies of an operation out of the existing module into the
 /// self-contained slice module.
 static void sliceDependencies(Operation *op, SymbolTable &sliceSymtab,
-                              const SymbolTable &symtab) {
-  // Extract a dependency from the IR parent module and place it into the slice
-  // module if it does not already exist. If a symbol was copied, return it.
-  auto extractDependency = [&](StringAttr name) -> Operation * {
-    // Don't copy the symbol if it is already copied.
-    if (sliceSymtab.lookup(name))
-      return nullptr;
-
-    Operation *symbol = symtab.lookup(name);
-    // If the symbol reference attribute doesn't reference a symbol, somehow
-    // invalid IR made it to the ObjectCompiler.
-    assert(symbol && "invalid IR?");
-
-    // Clone the symbol into the new symbol table.
-    Operation *copy = symbol->clone();
-    sliceSymtab.insert(copy);
-    return copy;
-  };
-
+                              const SymbolTable &symtab,
+                              IRMapping &reusedMapping,
+                              DenseSet<const void *> &visited) {
   std::vector<Operation *> worklist;
-  mlir::AttrTypeWalker walker;
-  walker.addWalk([&](FlatSymbolRefAttr ref) {
-    if (Operation *decl = extractDependency(ref.getAttr()))
-      worklist.push_back(decl);
-  });
+  auto visit = [&](auto value) {
+    sliceDependenciesFrom(value, sliceSymtab, symtab, visited, reusedMapping,
+                          worklist);
+  };
   auto extractDependencies = [&](Operation *op) {
     // Extract references to type declarations.
-    walker.walk(op->getAttrDictionary());
+    visit(op->getAttrDictionary());
     for (Type type : op->getResultTypes())
-      walker.walk(type);
+      visit(type);
     for (Region &region : op->getRegions())
       for (Type type : region.getArgumentTypes())
-        walker.walk(type);
+        visit(type);
   };
 
   worklist.push_back(op);
@@ -692,12 +725,14 @@ ObjectCompiler::produceStandaloneModule(const SymbolTable &symtab,
   // Create a new symbol table for the sliced module.
   SymbolTable sliceSymtab(*singleModule);
 
+  IRMapping reusedMapping;
+  DenseSet<const void *> visited;
   for (auto [sym, exportVal] : exportedSymbols) {
     auto func = symtab.lookup<ExportInterface>(sym);
     assert(func && "Unknown exported symbol");
 
     // Traverse the call graph and clone all the callees into this module.
-    sliceDependencies(func, sliceSymtab, symtab);
+    sliceDependencies(func, sliceSymtab, symtab, reusedMapping, visited);
 
     // Clone the func into this new module. We don't want to remove it from
     // the current module. Make sure the function is also exported in the slice.
