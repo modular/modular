ec0f6b95a1986edd155ffb16cec569a542015cde
Eric Hein <ehein@modular.com>
2024-07-24 16:40:51 -0400

    [AsyncRT] RuntimeGlobals dep cleanup (#44013)
    RuntimeGlobals transitively depended on Runtime (via CompilerSupport),
    even though it only includes AsyncValue.h in order to to provide a
    definition for AsyncValue::totalAllocatedAsyncValues.
    
    This patch breaks the dependency by converting the counter to a global
    variable, allowing us to drop most dependencies of RuntimeGlobals.
    
    MODULAR_ORIG_COMMIT_REV_ID: 7ed749d016d0062bcb37e7add341f5ccbcb85629

--- AsyncRT/include/AsyncRT/Runtime/Globals/Globals.h
+++ AsyncRT/include/AsyncRT/Runtime/Globals/Globals.h
@@ -9,6 +9,7 @@
 
 #include "Support/SymbolExport.h"
 
+#include <atomic>
 #include <functional>
 
 namespace M::AsyncRT {
@@ -19,22 +20,29 @@ namespace Detail {
 class RuntimeTable;
 } // namespace Detail
 
+class Globals {
+
+public:
+  /// This is a TLS CompactRuntimePtr pointing to the runtime on behalf of
+  /// which the thread is processing work items. That thread may be a 'worker'
+  /// thread of the runtime's work queue, or a 'main' thread which is also
+  /// donating itself to processing work items for the runtime.
+  ///
+  /// NOTE: MSVC does not allow a thread_local to have DLL linkage, so we must
+  /// hide this under a function.
+  static MODULAR_CXX_EXPORT CompactRuntimePtr &getCurrentRuntimeInTLS();
+
+  static MODULAR_CXX_EXPORT Detail::RuntimeTable &
+  getRuntimeTableSingleton(const std::function<Detail::RuntimeTable *()> &ctor);
+
+private:
+  friend class AsyncValue;
+  /// This is a global counter of the number of AsyncValue instances currently
+  /// live in the process.  This is intended to be used for debugging only, and
+  /// is only kept in sync if `isAllocationTrackingEnabled()` returns true.
+  static MODULAR_CXX_EXPORT std::atomic<ssize_t> totalAllocatedAsyncValues;
+};
+
 } // namespace M::AsyncRT
-
-namespace M::AsyncRT::Globals {
-
-/// This is a TLS CompactRuntimePtr pointing to the runtime on behalf of
-/// which the thread is processing work items. That thread may be a 'worker'
-/// thread of the runtime's work queue, or a 'main' thread which is also
-/// donating itself to processing work items for the runtime.
-///
-/// NOTE: MSVC does not allow a thread_local to have DLL linkage, so we must
-/// hide this under a function.
-extern MODULAR_CXX_EXPORT CompactRuntimePtr &getCurrentRuntimeInTLS();
-
-extern MODULAR_CXX_EXPORT Detail::RuntimeTable &
-getRuntimeTableSingleton(const std::function<Detail::RuntimeTable *()> &ctor);
-
-} // namespace M::AsyncRT::Globals
 
 #endif
