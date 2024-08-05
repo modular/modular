b0f373fbe1c3f5cdc033f3d160ef5bf1b5394a08
Lily Brown <lbrown@modular.com>
2024-08-05 13:57:43 -0700

    [mojo-test] Build entrypoint from source file (#44353)
    This builds on #44282 to invoke `mojo build` with provided compilation
    options. This will generate a temporary entrypoint executable
    (optionally retained via a flag) that can be used to invoke each unit
    test without needing to rebuild the target each time.
    
    MODULAR_ORIG_COMMIT_REV_ID: 9719ed43153ed58303f8c55db270b0b192ddf4d1

--- KGEN/tools/mojo/Test/mojo-test.cpp
+++ KGEN/tools/mojo/Test/mojo-test.cpp
@@ -12,13 +12,17 @@
 #include "KGEN/Compiler/KGENCompiler.h"
 #include "KGEN/MojoParser/EntryPoint.h"
 #include "KGEN/MojoTesting/Test.h"
+#include "KGEN/Support/Configuration.h"
 #include "Support/Driver/DriverSupport.h"
 #include "Support/FileSystemExtras.h"
 #include "Support/LLVMForwardDecls.h"
 #include "Support/Telemetry/Telemetry.h"
 #include "llvm/Option/ArgList.h"
 #include "llvm/Option/OptTable.h"
+#include "llvm/Support/Program.h"
 #include "llvm/Support/SourceMgr.h"
+#include <llvm/ADT/DenseSet.h>
+#include <mlir/Support/IndentedOstream.h>
 
 using namespace M;
 using namespace mlir;
@@ -66,6 +70,124 @@ static std::optional<int> parseArgs(State &state,
 }
 
 //===----------------------------------------------------------------------===//
+// Test entrypoint building
+//===----------------------------------------------------------------------===//
+
+/// Returns the path to the mojo tool, or an error if not found.
+static ErrorOr<std::string> getMojoDriver() {
+  ErrorOr<KGEN::MojoConfig> configOr = KGEN::MojoConfig::open();
+  if (failed(configOr))
+    return Error(Twine("failed to parse 'modular.cfg': ") +
+                 configOr.getError());
+  std::error_code ec;
+  StringRef driver = configOr->getDriverPath();
+  if (!std::filesystem::exists(driver.str(), ec) || ec)
+    return Error("unable to resolve the mojo program path");
+  return driver.str();
+}
+
+static std::vector<TestID> filterUnitTests(const Test &root) {
+  std::vector<TestID> unitTests;
+  std::function<void(const Test &test)> visit = [&unitTests,
+                                                 &visit](const Test &test) {
+    TestID id = test.getTestID();
+    if (id.getTest()) {
+      if (!id.getTestSuite()) {
+        unitTests.push_back(std::move(id));
+      }
+    } else {
+      for (const Test &child : test.getChildren()) {
+        visit(child);
+      }
+    }
+  };
+  visit(root);
+
+  return unitTests;
+}
+
+static ErrorOr<TempFile> generateEntrypointSource(ArrayRef<TestID> unitTests) {
+  ErrorOr<TempFile> sourceFile = TempFile::create("test-%%%%%%.mojo");
+  if (!failed(sourceFile)) {
+    llvm::raw_fd_ostream rawOs(sourceFile->getFD(), /*shouldClose=*/false);
+    mlir::raw_indented_ostream os(rawOs);
+    for (const TestID &id : unitTests)
+      os << "import `" << id.getFilePath().stem() << "`\n";
+
+    os << "from sys import argv\n";
+    os << "fn main() raises:\n";
+
+    os.indent();
+    os << "var testName = argv()[1]\n";
+
+    for (const TestID &id : unitTests) {
+      StringRef testName = *id.getTest();
+      assert(testName.consume_back("()") &&
+             "id does not reference a valid test");
+
+      ErrorOr<SmallVector<std::string>> names =
+          TestID::parseScopedName(testName);
+      assert(!names.isError());
+      assert(names->size() == 1 && "id does not reference a valid test");
+
+      os << formatv("if testName == \"{0}\":\n", id.strref());
+      os.indent();
+      os << formatv("`{0}`.`{1}`()\n", id.getFilePath().stem(),
+                    StringRef(names->back()));
+      os.unindent();
+    }
+
+    os.unindent();
+  }
+
+  return sourceFile;
+}
+
+static ErrorOr<TempFile> buildEntrypoint(std::vector<std::string> buildArgs,
+                                         ArrayRef<TestID> unitTests,
+                                         const TempFile &sourceFile) {
+  ErrorOr<std::string> driverPath = getMojoDriver();
+  if (driverPath.isError())
+    return Error(driverPath.getError());
+
+  // Append each test file's directory as an include path. This allows the
+  // entrypoint source to import each test function.
+  std::vector<std::string> testIncludeDirs{""};
+  for (const TestID &id : unitTests)
+    llvm::append_range(
+        buildArgs,
+        ArrayRef<std::string>{"-I", id.getFilePath().parent_path().string()});
+
+  ErrorOr<TempFile> entrypointOutput =
+      TempFile::create("test-entrypoint-%%%%%%");
+  if (entrypointOutput.isError())
+    return Error(entrypointOutput.getError());
+
+  entrypointOutput->close();
+
+  buildArgs.emplace_back("-o");
+  std::string outputPath = entrypointOutput->getPath().string();
+  buildArgs.push_back(entrypointOutput->getPath().string());
+  buildArgs.push_back(sourceFile.getPath().string());
+
+  SmallVector<StringRef> buildCommand{*driverPath, "build"};
+  llvm::append_range(buildCommand, buildArgs);
+
+  std::string errorMessage;
+  int result =
+      llvm::sys::ExecuteAndWait(*driverPath, buildCommand, std::nullopt,
+                                std::nullopt, 0, 0, &errorMessage);
+  if (!errorMessage.empty())
+    return Error(errorMessage);
+
+  if (result)
+    return Error(llvm::formatv(
+        "couldn't build the test executable. Exit code {0}", result));
+
+  return entrypointOutput;
+}
+
+//===----------------------------------------------------------------------===//
 // Mojo test input
 //===----------------------------------------------------------------------===//
 
@@ -94,6 +216,27 @@ static int test(const State &subcommandState) {
   if (args.hasArg(options::OPT_I))
     additionalImportPaths = args.getAllArgValues(options::OPT_I);
 
+  // Collect compilation args now. For some reason, if we do this after reading
+  // OPT_INPUT, the argument parser ends up in an invalid state.
+  std::vector<std::string> buildArgStrings;
+  {
+    // We need to re-parse the options, otherwise we'll get errors due to
+    // options being disposed after we extract them.
+    TestOptTable options;
+    unsigned unused = 0;
+    auto args = options.ParseArgs(state.arguments, unused, unused);
+    auto filtered =
+        args.filtered(options::OPT_CompilationOptionGroup,
+                      options::OPT_ExperimentalCompilationOptionGroup,
+                      options::OPT_TargetOptionGroup);
+    for (auto &arg : filtered) {
+      buildArgStrings.push_back(arg->getSpelling().str());
+      assert(arg->getNumValues() <= 1);
+      for (const char *value : arg->getValues())
+        buildArgStrings.emplace_back(value);
+    }
+  }
+
   // If an input was provided, use that as the test id. Otherwise, fallback to
   // the current working directory.
   TestID testID;
@@ -109,6 +252,8 @@ static int test(const State &subcommandState) {
   std::filesystem::path testIDFilePath = testID.getFilePath();
   if (std::filesystem::is_directory(testIDFilePath)) {
     additionalImportPaths.push_back(testIDFilePath);
+    buildArgStrings.emplace_back("-I");
+    buildArgStrings.push_back(testIDFilePath.string());
   }
 
   AsyncRT::Runtime &runtime = *ctx->get<AsyncRT::Runtime>();
@@ -145,53 +290,23 @@ static int test(const State &subcommandState) {
     return 0;
   }
 
-  // Find a full list of unit tests. We'll generate an entry point with these.
-  std::vector<TestID> unitTests;
-  std::function<void(const Test &test)> visit = [&unitTests,
-                                                 &visit](const Test &test) {
-    TestID id = test.getTestID();
-    if (id.getTest()) {
-      if (!id.getTestSuite()) {
-        unitTests.push_back(std::move(id));
-      }
-    } else {
-      for (const auto &child : test.getChildren()) {
-        visit(child);
-      }
-    }
-  };
-  visit(*test);
+  std::vector<TestID> unitTests = filterUnitTests(*test);
+  ErrorOr<TempFile> entrypointSource = generateEntrypointSource(unitTests);
+  if (entrypointSource.isError())
+    return state.reportError(entrypointSource.getError());
 
-  ErrorOr<TempFile> testFileOr = TempFile::create("test-%%%%%%.mojo");
-  if (failed(testFileOr))
-    return state.reportError(testFileOr.getError());
-
-  {
-    llvm::raw_fd_ostream os(testFileOr->getFD(), /*shouldClose=*/false);
-    for (const TestID &id : unitTests)
-      os << "import `" << id.getFilePath().stem() << "`\n";
-
-    os << "fn main(args) raises:\n";
-    os << "\ttestName = args[1]\n";
-
-    for (const TestID &id : unitTests) {
-      StringRef testName = *id.getTest();
-      assert(testName.consume_back("()") &&
-             "id does not reference a valid test");
-
-      ErrorOr<SmallVector<std::string>> names =
-          TestID::parseScopedName(testName);
-      assert(!names.isError());
-      assert(names->size() == 1 && "id does not reference a valid test");
-      os << formatv("\tif testName == \"{0}\":\n\t\t`{1}`.`{2}`()\n",
-                    id.strref(), id.getFilePath().stem(), names->back());
-    }
-  }
+  ErrorOr<TempFile> entrypoint =
+      buildEntrypoint(std::move(buildArgStrings), unitTests, *entrypointSource);
+  if (entrypoint.isError())
+    return state.reportError(entrypoint.getError());
 
   if (args.hasArg(options::OPT_keep_entrypoint)) {
-    testFileOr->keep();
-    llvm::outs() << "Keeping entrypoint source code at "
-                 << testFileOr->getPath() << "\n";
+    entrypointSource->keep();
+    entrypoint->keep();
+    llvm::errs() << "Entrypoint source can be found at "
+                 << entrypointSource->getPath() << "\n";
+    llvm::errs() << "Built entrypoint can be found at " << entrypoint->getPath()
+                 << "\n";
   }
 
   // Execute the test and print the results.
