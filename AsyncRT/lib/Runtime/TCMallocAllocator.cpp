cc28cbac15380d0a8a9e3d4cd2590b6d39591139
Eric Hein <ehein@modular.com>
2024-08-13 13:27:41 -0400

    [AsyncRT] Move TCmalloc Into RuntimeGlobals (redux) (#45091)
    Fixes RUNP-313
    
    Stacked on [Internal link]
    
    Originally merged via #44032
    Reverted in #44999
    
    MODULAR_ORIG_COMMIT_REV_ID: 24fc4572ebcc94515a6a0e298be10d0b179d9194

--- AsyncRT/lib/Runtime/TCMallocAllocator.cpp
+++ AsyncRT/lib/Runtime/TCMallocAllocator.cpp
@@ -5,35 +5,33 @@
 //===----------------------------------------------------------------------===//
 
 #include "AsyncRT/Runtime/Allocator.h"
-#include "Support/AlignedAlloc.h"
+#include "AsyncRT/Runtime/Globals/Globals.h"
 
-using namespace M;
-using namespace M::AsyncRT;
-
+namespace M::AsyncRT {
 #ifdef USE_TCMALLOC
-#include <gperftools/tcmalloc.h>
-namespace {
+
 /// This is an implementation of the Allocator interface that just calls to
 /// tc_new/tc_delete.
+/// NOTE: TCMalloc uses static global variables that do not live within an
+/// instance of this class. These methods make a call into RuntimeGlobals.
 class TCMallocAllocator : public Allocator {
   /// Allocate the specified number of bytes with the specified alignment.
   void *allocateBytes(size_t size, size_t alignment) override {
     TimeTraceScope scope(MemAllocFreeProfilerEntry::create("mem.alloc.tcmalloc",
                                                            (uint64_t)size));
-    return tc_new_aligned(size, std::align_val_t(alignment));
+    return TCMallocGlobals::tc_new(size, alignment);
   }
 
   /// Deallocate the specified pointer that has the specified size.
   void deallocateBytes(void *ptr, size_t size) override {
     TimeTraceScope scope(
         MemAllocFreeProfilerEntry::create("mem.free.tcmalloc", (uint64_t)size));
-    return tc_delete(ptr);
+    return TCMallocGlobals::tc_delete(ptr);
   }
 };
-} // namespace
 #endif // USE_TCMALLOC
 
-std::unique_ptr<Allocator> M::AsyncRT::createTCMallocAllocator() {
+std::unique_ptr<Allocator> createTCMallocAllocator() {
 #ifdef USE_TCMALLOC
   return std::make_unique<TCMallocAllocator>();
 #else  // USE_TCMALLOC
@@ -41,3 +39,5 @@ std::unique_ptr<Allocator> M::AsyncRT::createTCMallocAllocator() {
                            "use a different allocator");
 #endif // USE_TCMALLOC
 }
+
+} // end namespace M::AsyncRT
