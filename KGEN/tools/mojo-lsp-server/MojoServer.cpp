9a8acb2ef6ee18a9519374922f15a310d30d2ef4
Joe Loser <joe@modular.com>
2024-08-13 21:25:29 -0600

    [stdlib] Rename `python/object.mojo` to `python/python_object.mojo`
    This helps avoid a clash with the `builtin/object.mojo`. It's slightly
    inconvenient having the same filename when reading error messages
    quickly (since both filenames are `object.mojo`. Fix this by simply
    renaming `python/object.mojo` to `python/python_object.mojo`.
    
    MODULAR_ORIG_COMMIT_REV_ID: 6e24b9c7482144bad72accd14362200d1e86ce7a

--- KGEN/tools/mojo-lsp-server/MojoServer.cpp
+++ KGEN/tools/mojo-lsp-server/MojoServer.cpp
@@ -1963,7 +1963,7 @@ void MojoNotebookDocument::parseDocumentImpl() {
     // imported symbols.
     std::string pythonCell;
     llvm::raw_string_ostream os(pythonCell);
-    os << "from python.object import PythonObject\n\n";
+    os << "from python import PythonObject\n\n";
     for (auto &symbol : *symbolsOr)
       os << "var " << symbol->getName() << ": PythonObject\n";
     int pythonCellId = getSourceMgr().AddNewSourceBuffer(
