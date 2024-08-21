1447e6342d67748c9ce64165c3f19aa391954e40
Jeff Niu <jeff@modular.com>
2024-08-21 00:56:11 -0400

    [KGEN] Add check-kernels alias (#45642)
    [KGEN] Add check-kernels alias
    
    MODULAR_ORIG_COMMIT_REV_ID: 789cf62ffec318331de1bd923688748f2ad99080

--- KGEN/start-modular.sh
+++ KGEN/start-modular.sh
@@ -65,6 +65,8 @@ get_build_alias() {
         echo "GenericML/graph-compiler/integration-test/..."
     elif [ "$1" = "check-mojo-stdlib" ]; then
         echo "open-source/mojo/stdlib/test/..."
+    elif [ "$1" = "check-kernels" ]; then
+        echo "Kernels/test/..."
     fi
 }
 
