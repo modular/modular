df696a76593ad5adf8ca437044a9db85eb86000f
Joshua Peterson <josh@modular.com>
2024-08-26 14:49:34 -0400

    [MO] Remove AddDevicesPass and DeviceSpecMap (#43901)
    This pass and type are no longer used, so remove them. Instead of check
    with the runtime to find the available devices, we use the
    `AssignDevicesPass` and take input from options to determine which
    device we should assign to the top-level graph up.
    
    While this is not a non-functional change, it really should not impact
    the user-facing behavior in any cases.
    
    Towards GRA-658
    
    MODULAR_ORIG_COMMIT_REV_ID: 4b4822ccf3f41bb7cabfd2255b90402f73e301cc

--- Support/include/Support/DeviceSpecs.h
+++ Support/include/Support/DeviceSpecs.h
@@ -138,20 +138,6 @@ struct DeviceSpec {
   static ErrorOr<DeviceSpec> deserializeFromJSON(StringRef json);
 };
 
-/// A map from device references (from the 'required' devices) to
-/// the pair of device specs (required, provided), where provided is the
-/// matching devices spec available in the runtime environment.
-using DeviceSpecMap =
-    llvm::DenseMap<DeviceRef, std::pair<DeviceSpec, DeviceSpec>>;
-
-/// A reference counted version of DeviceSpecMap, suitable for use in the
-/// runtime.
-struct RuntimeDeviceSpecMap : public ReferenceCounted<RuntimeDeviceSpecMap> {
-  DeviceSpecMap map;
-
-  RuntimeDeviceSpecMap(DeviceSpecMap map) : map(std::move(map)) {}
-};
-
 //===----------------------------------------------------------------------===//
 // DeviceSpecCollection
 //===----------------------------------------------------------------------===//
@@ -169,24 +155,6 @@ struct DeviceSpecCollection {
   static ErrorOr<DeviceSpecCollection>
   deserializeFromJSON(const llvm::json::Value *json);
   static ErrorOr<DeviceSpecCollection> deserializeFromJSON(StringRef json);
-
-  /// Returns a map from each device reference in required to the pair of
-  /// devices specs (requiredSpec, providedSpec) where:
-  ///  - requiredSpec is the device spec in the required collection.
-  ///  - providedSpec is the matching device spec in this collection which
-  ///    meets the requiredSpec's requirements.
-  ///
-  /// Returns an error if there's a target in required which has no satisfying
-  /// target in this collection. A device specification in this collection can
-  /// appear in the result map at most once. It is ok for this collection to
-  /// have more device specifications than required.
-  ///
-  /// This method is generally called on a collection representing all the
-  /// available devices on the host. The result can be used by primitives within
-  /// the model's 'init' block to guide which 'physical' devices (eg a CUDA
-  /// device id) to use for each 'virtual' device used in the model.
-  ErrorOr<DeviceSpecMap>
-  reconcileDeviceSpecs(const DeviceSpecCollection &required) const;
 
   /// Returns the device spec which matches device reference, or an error if
   /// no match.
--- Support/unittests/DeviceSpecsTest.cpp
+++ Support/unittests/DeviceSpecsTest.cpp
@@ -74,47 +74,4 @@ TEST(DeviceSpecs, FindDeviceSpec) {
   }
 }
 
-TEST(DeviceSpecs, ReconcileDeviceSpecs) {
-  DeviceSpecCollection required = createDeviceSpecCollection();
-  DeviceSpecCollection provided = createDeviceSpecCollection();
-
-  // Drop the second required.
-  required.devices.erase(required.devices.end() - 1);
-
-  // Change the labels and ids of the provided.
-  provided.host.label = "cpux";
-  provided.devices[0].ref.label = "cpux";
-  provided.devices[0].target.features.emplace_back("avx512");
-  provided.devices[1].ref.label = "cudax";
-  provided.devices[1].ref.id = 3;
-  provided.devices[1].target.arch = "sm_75";
-  provided.devices[2].ref.label = "cudax";
-  provided.devices[2].ref.id = 7;
-  provided.devices[2].target.arch = "sm_90";
-
-  // The map should be from required refs to (required, provided) specs.
-  ErrorOr<DeviceSpecMap> mapOr = provided.reconcileDeviceSpecs(required);
-  ASSERT_FALSE(mapOr.isError());
-  ASSERT_EQ(mapOr->size(), 2u);
-
-  {
-    auto itr = mapOr->find(DeviceRef("cpu"));
-    ASSERT_TRUE(itr != mapOr->end());
-    ASSERT_EQ(itr->first.label, "cpu");
-  }
-
-  {
-    auto itr = mapOr->find(DeviceRef("cuda"));
-    ASSERT_TRUE(itr != mapOr->end());
-    // provided
-    ASSERT_EQ(itr->second.first.ref.label, "cuda");
-    ASSERT_EQ(itr->second.first.ref.id, (size_t)0);
-    ASSERT_EQ(itr->second.first.target.arch, "sm_80");
-    // required
-    ASSERT_EQ(itr->second.second.ref.label, "cudax");
-    ASSERT_EQ(itr->second.second.ref.id, (size_t)7);
-    ASSERT_EQ(itr->second.second.target.arch, "sm_90");
-  }
-}
-
 } // namespace
