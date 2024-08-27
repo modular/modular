f6bdbe08ff56bf30ec466705727810a38671c7f9
Lily Brown <lbrown@modular.com>
2024-08-27 15:57:48 -0700

    [vscode] Run integration tests in CI (#45861)
    This adds a new GHA workflow for running the Mojo VS Code extension's
    integration tests against PRs.
    
    This would, in a vacuum, be better implemented as a Bazel target.
    However, it's possible we will move the extension out of this repository
    in the future, so having a workflow we can carry over when that happens
    will also be helpful. It also means we don't need to make changes to
    Bazel or the extension, which requires a `modular.cfg` file (environment
    variable overrides are not supported).
    
    MODULAR_ORIG_COMMIT_REV_ID: 9ca669e66b56175e8884371e83214690865cc655

--- KGEN/utils/vscode-mojo/.vscode-test.mjs
+++ KGEN/utils/vscode-mojo/.vscode-test.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from '@vscode/test-cli';
 
 export default defineConfig({
+  platform: 'desktop',
+  workspaceFolder: '../../../',
+  version: '1.92.2',
   files: 'out/**/*.test.js',
   mocha: {
     timeout: 5000, // 5 seconds
