7c0d202ca4b24f89eeed3501d0a4c2fd0f5e3528
Shaurya Sharma <shaurya@modular.com>
2024-08-29 14:09:15 +0200

    [Support] Add OutputJsonStrategy specialization for outputting JSON formatted MLIR pass timing reports (#45726)
    Stacked PRs:
     * #45730
     * #45729
     * #45727
     * __->__#45726
    
    --- --- ---
    
    ### [Support] Add OutputJsonStrategy specialization for outputting JSON
    formatted MLIR pass timing reports
    
    MODULAR_ORIG_COMMIT_REV_ID: 93d363b1fd9db210560415f1dba6824910232aaf

---
+++ Support/include/Support/Compiler/PassTimingUtils.h
@@ -0,0 +1,23 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef SUPPORT_COMPILER_PASSTIMINGUTILS_H
+#define SUPPORT_COMPILER_PASSTIMINGUTILS_H
+
+#include "Support/ErrorOr.h"
+#include "mlir/Pass/PassManager.h"
+#include "llvm/Support/raw_ostream.h"
+
+namespace M {
+/// Enables pass timing on the `PassManger` object and dumps the tree structured
+/// JSON to a temp file in `outDir`. The ostream object is initialized in the
+/// function
+ErrorOrSuccess
+configureMLIRPassTimingJSONOutput(mlir::PassManager &pm, llvm::StringRef outDir,
+                                  llvm::StringRef passPipelineName);
+} // namespace M
+
+#endif // SUPPORT_COMPILER_PASSTIMINGUTILS_H
---
+++ Support/lib/Compiler/PassTimingUtils.cpp
@@ -0,0 +1,129 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#include "Support/Compiler/PassTimingUtils.h"
+#include "Support/ErrorOr.h"
+#include "Support/FileSystemExtras.h"
+#include "mlir/Support/Timing.h"
+#include "llvm/Support/Format.h"
+#include "llvm/Support/JSON.h"
+#include "llvm/Support/raw_ostream.h"
+#include <filesystem>
+
+namespace {
+
+//===----------------------------------------------------------------------===//
+// OutputStrategy
+//===----------------------------------------------------------------------===//
+
+/// This class represents an OutputStrategy which we can use to structure the
+/// JSON string that is produced when running MLIR passes with timing enabled.
+class OutputJsonStrategy : public mlir::OutputStrategy {
+private:
+  std::unique_ptr<llvm::raw_fd_ostream> ostream;
+  llvm::json::OStream jsonStream;
+  llvm::StringRef pipelineName;
+  uint32_t depth{0};
+
+public:
+  OutputJsonStrategy(std::unique_ptr<llvm::raw_fd_ostream> os,
+                     llvm::StringRef pipelineName)
+      : OutputStrategy(*os), ostream(std::move(os)), jsonStream(*ostream),
+        pipelineName(pipelineName) {}
+
+  void printHeader(const mlir::TimeRecord &total) override {
+    jsonStream.objectBegin();
+    jsonStream.attribute("pipeline_name", pipelineName);
+    jsonStream.attributeBegin("data");
+    jsonStream.arrayBegin();
+  }
+
+  void printFooter() override {
+    jsonStream.arrayEnd();     // data
+    jsonStream.attributeEnd(); // data
+    jsonStream.objectEnd();
+  }
+
+  void printTime(const mlir::TimeRecord &time,
+                 const mlir::TimeRecord &total) override {
+    jsonStream.attributeBegin("wall");
+    jsonStream.object([&] {
+      jsonStream.attributeBegin("duration");
+      jsonStream.rawValueBegin() << llvm::format("%.4f", time.wall);
+      jsonStream.rawValueEnd();
+      jsonStream.attributeEnd();
+
+      jsonStream.attributeBegin("percentage");
+      jsonStream.rawValueBegin()
+          << llvm::format("%.1f", 100.0 * time.wall / total.wall);
+      jsonStream.rawValueEnd();
+      jsonStream.attributeEnd();
+    });
+    jsonStream.attributeEnd(); // wall
+  }
+
+  void printListEntry(llvm::StringRef name, const mlir::TimeRecord &time,
+                      const mlir::TimeRecord &total, bool lastEntry) override {
+    jsonStream.object([&] {
+      jsonStream.attribute("name", name);
+      printTime(time, total);
+      jsonStream.attributeArray("passes", [] {});
+    });
+  }
+
+  void printTreeEntry(unsigned indent, llvm::StringRef name,
+                      const mlir::TimeRecord &time,
+                      const mlir::TimeRecord &total) override {
+    depth = indent;
+    jsonStream.objectBegin();
+    jsonStream.attribute("name", name);
+    printTime(time, total);
+    jsonStream.attributeBegin("passes");
+    jsonStream.arrayBegin();
+  }
+
+  void printTreeEntryEnd(unsigned indent, bool lastEntry) override {
+    depth = indent;
+    jsonStream.arrayEnd();     // passes
+    jsonStream.attributeEnd(); // passes
+    jsonStream.objectEnd();
+  }
+};
+
+} // namespace
+
+M::ErrorOrSuccess
+M::configureMLIRPassTimingJSONOutput(mlir::PassManager &pm,
+                                     llvm::StringRef outDir,
+                                     llvm::StringRef passPipelineName) {
+  auto tm = std::make_unique<mlir::DefaultTimingManager>();
+  tm->setEnabled(true);
+  tm->setDisplayMode(mlir::DefaultTimingManager::DisplayMode::Tree);
+
+  auto tempFileOr = TempFile::create(
+      (std::filesystem::path(outDir.str()) / "pass-timing-%%%%%%.json")
+          .string());
+  if (tempFileOr.isError()) {
+    return tempFileOr.takeError();
+  }
+
+  auto &tempFile = tempFileOr.get();
+  tempFile.keep();
+
+  std::error_code errorCode;
+  std::unique_ptr<llvm::raw_fd_ostream> ostream =
+      std::make_unique<llvm::raw_fd_ostream>(tempFile.getPath().string(),
+                                             errorCode);
+  if (errorCode) {
+    return Error("Unable to open file: " + tempFile.getPath().string() +
+                 ", reason: " + errorCode.message());
+  }
+
+  tm->setOutput(std::make_unique<OutputJsonStrategy>(std::move(ostream),
+                                                     passPipelineName));
+  pm.enableTiming(std::move(tm));
+  return M::success();
+}
