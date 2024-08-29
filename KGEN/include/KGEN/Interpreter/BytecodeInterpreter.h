9b6ad6ddb4fa5f3a86ee37d411c9c83b615363ef
Jeff Niu <jeff@modular.com>
2024-08-29 02:57:32 +0100

    [KGEN][Support] Make ReallocatingTrivialVector an implementation detail (NFC) (#46202)
    Due to concern about this data structure causing fragmentation, I have
    moved it to make it implementation detail of the bytecode interpreter.
    It's specifically designed for that use case.
    
    MODULAR_ORIG_COMMIT_REV_ID: 5df926d80c18d5866eef60116e8f23a79e3c3f17

--- KGEN/include/KGEN/Interpreter/BytecodeInterpreter.h
+++ KGEN/include/KGEN/Interpreter/BytecodeInterpreter.h
@@ -8,7 +8,6 @@
 #define SUPPORT_INTERPRETER_BYTECODEINTERPRETER_H
 
 #include "KGEN/Interpreter/InterpreterState.h"
-#include "Support/ADT/ReallocatingTrivialVector.h"
 #include "mlir/IR/OpDefinition.h"
 
 namespace M {
@@ -72,6 +71,56 @@ class BytecodeCompiler {
 public:
   virtual ~BytecodeCompiler() = default;
   virtual const FunctionIRBytecode *compileBytecode(Region &region) = 0;
+};
+
+/// This data structure is a resizable vector that contains only trivial types:
+/// types which have trivial destructors, constructors, and copy constructors.
+/// This vector is useful for carrying a varying number of such elements,
+/// resizing with minimal memory pressure.
+///
+/// The vector doubles in capacity each time it increases in size.
+template <typename T>
+class ReallocatingTrivialVector {
+public:
+  static_assert(std::is_trivially_copy_constructible_v<T> &&
+                    std::is_trivially_move_constructible_v<T> &&
+                    std::is_trivially_destructible_v<T>,
+                "T must be trivial");
+
+  /// Initialize the vector with an initial size.
+  explicit ReallocatingTrivialVector(unsigned initialSize)
+      : ptr((T *)malloc(sizeof(T) * initialSize)), size(initialSize) {}
+
+  ReallocatingTrivialVector() : ptr(nullptr), size(0) {}
+  ReallocatingTrivialVector(ReallocatingTrivialVector<T> &&other)
+      : ptr(other.ptr), size(other.size) {
+    other.ptr = nullptr;
+    other.size = 0;
+  }
+  ~ReallocatingTrivialVector() { free(ptr); }
+
+  /// Access the element at `i`.
+  T &operator[](unsigned i) { return ptr[i]; }
+  const T &operator[](unsigned i) const { return ptr[i]; }
+
+  /// Ensure the vector can contain at least this many elements. This
+  /// aggressively reserves up to the next power of 2 elements.
+  void reserve(unsigned requiredSize) {
+    requiredSize = llvm::NextPowerOf2(requiredSize);
+    if (size >= requiredSize)
+      return;
+    // `realloc` copies the data over for us.
+    ptr = (T *)realloc(ptr, sizeof(T) * requiredSize);
+    size = requiredSize;
+  }
+
+  /// Directly access the underlying data.
+  T *data() { return ptr; }
+  const T *data() const { return ptr; }
+
+private:
+  T *ptr;
+  unsigned size;
 };
 
 /// The bytecode interpreter is an implementation of the interpreter execution
