bccf8b32efefb7013cae3ce6e4fc1da703f8b38a
Stef Lindall <stef@modular.com>
2024-09-11 19:40:49 -0700

    [Driver] BFloat16 support (#47045)
    Adds BFloat16 support to MAX driver tensors via `__setitem__` and
    `.item()`.
    
    Since `__bf16` is only supported on certain hardware in clang/llvm, and
    `std::bfloat16_t` isn't enabled until C++23, I add a simple type which
    converts to/from `float` via truncation.
    
    Towards [MSDK-893: \[DLPack\] Support bf16 and float16 in our dlpack
    [Internal link]
    
    MODULAR_ORIG_COMMIT_REV_ID: 6c08e3d445601cfbcf269fe8265ca8dc15382374

--- Support/include/Support/ML/DType.h
+++ Support/include/Support/ML/DType.h
@@ -16,6 +16,7 @@
 
 #include "Support/FunctionExtras.h"
 #include "Support/LLVMForwardDecls.h"
+#include "Support/ML/BFloat16.h"
 #include "llvm/ADT/ArrayRef.h"
 #include "llvm/ADT/DenseMapInfo.h"
 #include "llvm/ADT/STLExtras.h"
@@ -367,6 +368,7 @@ DECLARE_TYPE_MAPPING(si32, int32_t);
 DECLARE_TYPE_MAPPING(ui32, uint32_t);
 DECLARE_TYPE_MAPPING(si64, int64_t);
 DECLARE_TYPE_MAPPING(ui64, uint64_t);
+DECLARE_TYPE_MAPPING(bf16, M::bfloat::bfloat16_t);
 DECLARE_TYPE_MAPPING(f32, float);
 DECLARE_TYPE_MAPPING(f64, double);
 /// TODO: Add long double when sizeof(long double) != sizeof(double).
