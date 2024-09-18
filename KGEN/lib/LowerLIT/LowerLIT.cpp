5d3ed294886a130d418eafb9edce6f08872a5822
Jeff Niu <jeff@modular.com>
2024-09-18 16:34:31 -0700

    [KGEN] Reverse order of ODS parameters for ParamDeclareRegionOp (NFC) (#47442)
    `isolated` is not something that users should be setting automatically.
    
    MODULAR_ORIG_COMMIT_REV_ID: 5873b97a773f69f0bbbe5aa54c1391bc64523fc9

--- KGEN/lib/LowerLIT/LowerLIT.cpp
+++ KGEN/lib/LowerLIT/LowerLIT.cpp
@@ -306,7 +306,7 @@ void LITLowerer::lowerNestedFunction(LIT::FuncOp func) {
 
   auto region = b.create<ParamDeclareRegionOp>(
       decl, func.getSignature(), func.getFunctionType(), inputParams,
-      /*isolated=*/false, func.getInlineLevel());
+      func.getInlineLevel());
   region.getBodyRegion().takeBody(func.getBodyRegion());
   func.erase();
 }
