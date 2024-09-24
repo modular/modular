b3787216ae63acef6a5ff4e21836802dfddf1e17
weiwei chen <weiwei.chen@modular.com>
2024-09-24 17:16:17 -0400

    [KGEN] Add base MCLink infra. (#47338)
    - [x] Add infrastructure for linking at MC level.
    
    Stacked PRs:
     * #47654
     * #47341
     * #47340
     * #47339
     * __->__#47338
     * #47337
     * #47336
    
    --- --- ---
    
    ### [KGEN] Add base MCLink infra.
    
    MODULAR_ORIG_COMMIT_REV_ID: 641619eb8f7f7dfc157f4bffb615cb7101faf696

---
+++ KGEN/lib/Compiler/ObjectCompiler/MCLinker.cpp
@@ -0,0 +1,194 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#include "MCLinker.h"
+#include "LLVMAccessorHelper.h"
+#include "llvm/Analysis/TargetLibraryInfo.h"
+#include "llvm/Bitcode/BitcodeReader.h"
+#include "llvm/Bitcode/BitcodeWriter.h"
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/IR/PassManager.h"
+#include "llvm/Linker/Linker.h"
+#include "llvm/Target/TargetLoweringObjectFile.h"
+#include "llvm/Target/TargetMachine.h"
+
+using namespace M;
+using namespace KGEN;
+
+MCLinker::MCLinker(SmallVectorImpl<SymbolAndMCInfo *> &symbolAndMCInfos,
+                   llvm::TargetMachine &targetMachine,
+                   CompilationOptions options)
+    : symbolAndMCInfos(symbolAndMCInfos), targetMachine(targetMachine),
+      options(std::move(options)) {
+
+  llvm::LLVMTargetMachine &llvmTargetMachine =
+      static_cast<llvm::LLVMTargetMachine &>(targetMachine);
+
+  machineModInfoPass =
+      new llvm::MachineModuleInfoWrapperPass(&llvmTargetMachine);
+}
+
+ErrorOrSuccess MCLinker::linkLLVMModules(StringRef modularName) {
+  linkedModule = std::make_unique<llvm::Module>(modularName, linkCtx);
+  llvm::Linker linker(*linkedModule);
+
+  for (auto [i, smcInfos] : llvm::enumerate(symbolAndMCInfos)) {
+    for (auto &iter : smcInfos->symbolLinkageTypes)
+      symbolLinkageTypes.insert(&iter);
+
+    for (auto [j, mcInfo] : llvm::enumerate(smcInfos->mcInfos)) {
+      mcInfos.push_back(mcInfo.get());
+
+      // Modules have to be in the same LLVMContext to be linked.
+      llvm::Expected<std::unique_ptr<llvm::Module>> moduleOr =
+          llvm::parseBitcodeFile(
+              llvm::MemoryBufferRef(
+                  StringRef(mcInfo->moduleBuf->getBufferStart(),
+                            mcInfo->moduleBuf->getBufferSize()),
+                  ""),
+              linkCtx);
+      if (!moduleOr)
+        return Error("failed to serialize post-llc modules");
+
+      std::unique_ptr<llvm::Module> module = std::move(moduleOr.get());
+      if (linker.linkInModule(std::move(module)))
+        return Error("failed to link post-llc modules");
+    }
+  }
+
+  // Restore linkage type.
+  for (llvm::GlobalValue &global : linkedModule->globals()) {
+    if (!global.hasWeakLinkage())
+      continue;
+    auto iter = symbolLinkageTypes.find(global.getName().str());
+    if (iter == symbolLinkageTypes.end())
+      continue;
+
+    global.setLinkage(iter->second);
+    global.setDSOLocal(true);
+  }
+
+  for (llvm::Function &fn : linkedModule->functions()) {
+    // FIXME(MOTO-696) This is a temporary hack for mojo_debugger_raise_hook
+    if (fn.getName() == "__mojo_debugger_raise_hook") {
+      fn.setLinkage(llvm::GlobalValue::LinkageTypes::WeakAnyLinkage);
+      continue;
+    }
+
+    if (!fn.hasWeakLinkage())
+      continue;
+
+    auto iter = symbolLinkageTypes.find(fn.getName().str());
+    if (iter == symbolLinkageTypes.end())
+      continue;
+
+    fn.setLinkage(iter->second);
+    fn.setDSOLocal(true);
+  }
+
+  return {};
+}
+
+void MCLinker::prepareMachineModuleInfo() {
+  llvm::LLVMTargetMachine &llvmTargetMachine =
+      static_cast<llvm::LLVMTargetMachine &>(targetMachine);
+
+  for (auto [i, smcInfos] : llvm::enumerate(symbolAndMCInfos)) {
+    for (auto [j, mcInfo] : llvm::enumerate(smcInfos->mcInfos)) {
+      // Move MachineFunctions from each split's codegen result
+      // into machineModInfoPass to print out together in one .o
+      llvm::DenseMap<const llvm::Function *,
+                     std::unique_ptr<llvm::MachineFunction>> &machineFunctions =
+          getMachineFunctionsFromMachineModuleInfo(*mcInfo->machineModuleInfo);
+
+      llvm::StringMap<const llvm::Function *> &fnNameToFnPtr =
+          mcInfo->fnNameToFnPtr;
+
+      mcInfo->machineModuleInfo->getContext().setObjectFileInfo(
+          llvmTargetMachine.getObjFileLowering());
+
+      for (auto &fn : linkedModule->functions()) {
+        if (fn.isDeclaration())
+          continue;
+        if (machineModInfoPass->getMMI().getMachineFunction(fn))
+          continue;
+
+        auto fnPtrIter = fnNameToFnPtr.find(fn.getName().str());
+        if (fnPtrIter == fnNameToFnPtr.end())
+          continue;
+        auto mfPtrIter = machineFunctions.find(fnPtrIter->second);
+        if (mfPtrIter == machineFunctions.end())
+          continue;
+
+        llvm::Function &origFn = mfPtrIter->second->getFunction();
+
+        machineModInfoPass->getMMI().insertFunction(
+            fn, std::move(mfPtrIter->second));
+
+        // FIXME(MOTO-696) This is a temporary hack for
+        // mojo_debugger_raise_hook
+        if (origFn.getName() == "__mojo_debugger_raise_hook") {
+          origFn.setLinkage(llvm::GlobalValue::LinkageTypes::WeakAnyLinkage);
+          continue;
+        }
+
+        // Restore function linkage types.
+        if (!origFn.hasWeakLinkage())
+          continue;
+
+        auto iter = symbolLinkageTypes.find(fn.getName().str());
+        if (iter == symbolLinkageTypes.end())
+          continue;
+
+        origFn.setLinkage(iter->second);
+        origFn.setDSOLocal(true);
+      }
+
+      // Restore global variable linkage types.
+      for (auto &global : mcInfo->moduleAndContext->globals()) {
+        if (!global.hasWeakLinkage())
+          continue;
+        auto iter = symbolLinkageTypes.find(global.getName().str());
+        if (iter == symbolLinkageTypes.end())
+          continue;
+
+        global.setLinkage(iter->second);
+        global.setDSOLocal(true);
+      }
+    }
+  }
+}
+
+ErrorOr<WriteableBufferRef> MCLinker::linkAndPrint(StringRef moduleName) {
+  // link at llvm::Module level.
+  ErrorOrSuccess lmResult = linkLLVMModules(moduleName);
+  if (lmResult.isError())
+    return Error(lmResult.getError());
+
+  prepareMachineModuleInfo();
+
+  WriteableBufferRef linkedObj = WriteableBuffer::get();
+
+  llvm::legacy::PassManager passMgr;
+  // Add an appropriate TargetLibraryInfo pass for the module's
+  // triple.
+  llvm::TargetLibraryInfoImpl targetLibInfo(
+      llvm::Triple(linkedModule->getTargetTriple()));
+
+  llvm::LLVMTargetMachine &llvmTargetMachine =
+      static_cast<llvm::LLVMTargetMachine &>(targetMachine);
+
+  // Add AsmPrint pass and run the pass manager.
+
+  // Release some of the AsyncValue memory to avoid
+  // wrong version of LLVMContext destructor being called due to
+  // multiple LLVM being statically linked in dylibs that have
+  // access to this code path.
+  for (SymbolAndMCInfo *smcInfo : symbolAndMCInfos)
+    smcInfo->mcInfos.clear();
+
+  return linkedObj;
+}
---
+++ KGEN/lib/Compiler/ObjectCompiler/MCLinker.h
@@ -0,0 +1,121 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef KGEN_COMPILER_OBJECTCOMPILER_MCLINK_H
+#define KGEN_COMPILER_OBJECTCOMPILER_MCLINK_H
+
+#include "KGEN/Compiler/LLVMIRUtils.h"
+#include "KGEN/ToolCommon/CompilationOptions.h"
+#include "Support/Buffer.h"
+#include "llvm/ADT/StringMap.h"
+#include "llvm/CodeGen/MachineFunction.h"
+#include "llvm/CodeGen/MachineModuleInfo.h"
+#include "llvm/Target/TargetMachine.h"
+
+namespace M::KGEN {
+/// This file defines data structures to help linking LLVM modules
+/// at MC level (right after codegen) and AsmPrint into one .o or .s file.
+/// This linking is needed because we parallelize the llvm opt and
+/// llc pipelines by splitting LLVMModule into multiple splits
+/// with symbol linkage changes.
+/// Linking at MC level helps to fix the temporary symbol linkage change,
+/// deduplicate multiple symbols among the splits.
+/// This allows mojo compilation to produce 1 .o file for each program
+/// (instead of one .a file with multiple .o files in .a) with reduced
+/// object file size (due to symbol dedup and linkage restoration), and
+/// allows NVPTX compilation to share the same parallelized emitArchive
+/// code path to produce an assembly output.
+struct MCInfo {
+  MCInfo(WriteableBufferRef moduleBuf,
+         std::unique_ptr<llvm::MachineModuleInfo> &&machineModuleInfo,
+         LLVMModuleAndContext &&moduleAndContext,
+         llvm::StringMap<const llvm::Function *> &fnNameToFnPtr,
+         std::unique_ptr<llvm::TargetMachine> &&targetMachine,
+         std::unique_ptr<llvm::MCContext> &&mcContext,
+         std::optional<int> splitIdx)
+      : moduleBuf(std::move(moduleBuf)),
+        moduleAndContext(std::move(moduleAndContext)),
+        mcContext(std::move(mcContext)),
+        machineModuleInfo(std::move(machineModuleInfo)),
+        fnNameToFnPtr(std::move(fnNameToFnPtr)),
+        targetMachine(std::move(targetMachine)), splitIdx(splitIdx) {};
+
+  MCInfo(MCInfo &&other)
+      : moduleBuf(std::move(other.moduleBuf)),
+        moduleAndContext(std::move(other.moduleAndContext)),
+        mcContext(std::move(other.mcContext)),
+        machineModuleInfo(std::move(other.machineModuleInfo)),
+        fnNameToFnPtr(std::move(other.fnNameToFnPtr)),
+        targetMachine(std::move(other.targetMachine)),
+        splitIdx(other.splitIdx) {}
+
+  /// Serialize the llvm::Module in parallel and deserialize back to put into
+  /// the same LLVMContext which is required for llvm::Linker.
+  WriteableBufferRef moduleBuf;
+
+  /// Keep original module split alive because llvm::Function is kept as
+  /// reference in llvm::MachineFunctions and will be used during codegen.
+  LLVMModuleAndContext moduleAndContext;
+
+  /// ExternContext to MachineModuleInfo to work around the upstream bug
+  /// with the move constructor of MachineModuleInfo.
+  std::unique_ptr<llvm::MCContext> mcContext;
+
+  /// This is where all the MachineFunction live that we need for AsmPrint.
+  std::unique_ptr<llvm::MachineModuleInfo> machineModuleInfo;
+
+  /// llvm::Function name to llvm::Function* map for concatenating the
+  /// MachineFunctions map.
+  llvm::StringMap<const llvm::Function *> fnNameToFnPtr;
+
+  /// Keep targetMachine alive.
+  std::unique_ptr<llvm::TargetMachine> targetMachine;
+
+  /// parallel llvm module split id, mostly for debugging.
+  std::optional<int> splitIdx;
+};
+
+struct SymbolAndMCInfo {
+  SymbolAndMCInfo(SymbolAndMCInfo &&other)
+      : symbolLinkageTypes(std::move(other.symbolLinkageTypes)),
+        mcInfos(std::move(other.mcInfos)) {}
+
+  /// Book-keeping original symbol linkage type if they are changed due to
+  /// splitting for parallel compilation.
+  llvm::StringMap<llvm::GlobalValue::LinkageTypes> symbolLinkageTypes;
+
+  /// Vector of codegen results for each parallel split before AsmPrint.
+  SmallVector<std::unique_ptr<MCInfo>> mcInfos;
+};
+
+class MCLinker {
+public:
+  MCLinker(SmallVectorImpl<SymbolAndMCInfo *> &symbolAndMCInfos,
+           llvm::TargetMachine &targetMachine, CompilationOptions options);
+
+  /// Link multiple MC results and AsmPrint into one .o file.
+  ErrorOr<WriteableBufferRef> linkAndPrint(StringRef moduleName);
+
+private:
+  SmallVectorImpl<SymbolAndMCInfo *> &symbolAndMCInfos;
+  llvm::TargetMachine &targetMachine;
+  CompilationOptions options;
+  SmallVector<MCInfo *> mcInfos;
+  llvm::LLVMContext linkCtx;
+  std::unique_ptr<llvm::Module> linkedModule = nullptr;
+  llvm::StringMap<llvm::GlobalValue::LinkageTypes> symbolLinkageTypes;
+  llvm::MachineModuleInfoWrapperPass *machineModInfoPass = nullptr;
+
+  /// Link llvm::Modules from each split.
+  ErrorOrSuccess linkLLVMModules(StringRef modularName);
+
+  /// Prepare MachineModuleInfo before AsmPrinting.
+  void prepareMachineModuleInfo();
+};
+
+} // namespace M::KGEN
+
+#endif // KGEN_COMPILER_OBJECTCOMPILER_MCLINK_H
