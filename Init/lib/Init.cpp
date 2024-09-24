6b9e6cb601e47396c9307b1d42580572931c78b2
Ansley Ussery <59447410+ansley@users.noreply.github.com>
2024-09-24 12:04:08 -0400

    [Graph API] Make `stats_report` a property of `InferenceSession` (#47402)
    Previously, `stats_report` was a property of `Model`, which meant that
    users could only access it if they were using the MAX Graph API. We want
    users to be able to get fallback stats if they're using `torch.compile`
    too, so we need to make `stats_report` a property of `InferenceSession`.
    
    MODULAR_ORIG_COMMIT_REV_ID: 99dd13b6ba74556b4581bc69e39316def9e3de86

--- Init/lib/Init.cpp
+++ Init/lib/Init.cpp
@@ -10,6 +10,7 @@
 #include "Support/Configuration.h"
 #include "Support/Context.h"
 #include "Support/CrashReporting/CrashReporting.h"
+#include "Support/Frameworks/StatsReport.h"
 #include "Support/MArchTarget/Host.h"
 #include "Support/Settings/Settings.h"
 #include "Support/Telemetry/Telemetry.h"
@@ -99,6 +100,8 @@ ErrorOr<ContextRef> Init::createContext(StringRef programName,
   // Move everything into the context. Construct here may used the settings.
   ctx->emplace<HTTPContextRef>(std::move(httpCtx));
   ctx->emplace<TelemetryContext>(settings, options.resources);
+  ctx->emplace<std::shared_ptr<Frameworks::StatsReport>>(
+      std::make_shared<Frameworks::StatsReport>("pytorch"));
 
   // Create a new runtime (if needed).
   if (options.runtimeOptions) {
