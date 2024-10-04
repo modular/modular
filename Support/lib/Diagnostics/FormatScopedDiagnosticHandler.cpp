22c3e22bcb3eb13f4ff79f6d27e50a9924f8817a
Joshua Peterson <josh@modular.com>
2024-10-04 12:18:24 -0400

    [Support] A diagnostic handler to format messages (#48507)
    This change adds a new scoped diagnostics handler that can be used to
    format diagnostics messages. It hooks into the existing MLIR diagnostics
    system to watch for messages in callees and record them.
    
    It formats messages on-demand, only when requested.
    
    MODULAR_ORIG_COMMIT_REV_ID: 2fd5ab991bde83ee4ee02feddaf93376b920d7aa

---
+++ Support/lib/Diagnostics/FormatScopedDiagnosticHandler.cpp
@@ -0,0 +1,65 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#include "Support/Diagnostics/FormatScopedDiagnosticHandler.h"
+#include "mlir/IR/BuiltinOps.h"
+
+using namespace M;
+using namespace mlir;
+
+static StringRef severityToString(mlir::DiagnosticSeverity severity) {
+  if (severity == mlir::DiagnosticSeverity::Note)
+    return "note";
+  if (severity == mlir::DiagnosticSeverity::Warning)
+    return "warning";
+  if (severity == mlir::DiagnosticSeverity::Error)
+    return "error";
+  if (severity == mlir::DiagnosticSeverity::Remark)
+    return "remark";
+
+  llvm_unreachable("Unexpected diagnostic severity enum value");
+}
+
+static std::string locationToString(Location location) {
+  auto fileLoc = dyn_cast<mlir::FileLineColLoc>(location);
+  if (!fileLoc)
+    return std::string();
+
+  return Twine(Twine(fileLoc.getFilename()) + ":" + Twine(fileLoc.getLine()) +
+               ":" + Twine(fileLoc.getColumn()))
+      .str();
+}
+
+static void emitDiagnosticToStream(raw_ostream &os, const Diagnostic &diag) {
+  // Only display the location if it is meaningful.
+  std::string location = locationToString(diag.getLocation());
+  if (!location.empty())
+    os << location << ": ";
+
+  os << severityToString(diag.getSeverity()) << ": ";
+  os << diag;
+  os << "\n";
+
+  // Display each note, indented two spaces
+  const char *indentation = "  ";
+  for (Diagnostic &note : diag.getNotes()) {
+    os << indentation;
+    emitDiagnosticToStream(os, note);
+  }
+}
+
+FormatScopedDiagnosticHandler::FormatScopedDiagnosticHandler(MLIRContext *ctx)
+    : mlir::ScopedDiagnosticHandler(ctx, [&](Diagnostic &diag) {
+        diagnostics.push_back(std::move(diag));
+      }) {}
+
+std::string FormatScopedDiagnosticHandler::formatMessage() const {
+  std::string message;
+  llvm::raw_string_ostream messageStream(message);
+  for (auto &diagnostic : diagnostics)
+    emitDiagnosticToStream(messageStream, diagnostic);
+  return message;
+}
