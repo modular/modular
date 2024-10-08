f32ec29159b7fb450d191fd2e92b40c1d43099e3
Eric Hein <ehein@modular.com>
2024-10-08 16:34:00 -0400

    [MOTO] Mojo build links shared CompilerRT  (#47636)
    ## TL;DR: `mojo build` no longer produces a standalone binary, it
    depends on the shared CompilerRT dylib instead of the static CompilerRT.
     
    Link with the shared CompilerRT in mojo build instead of the static
    CompilerRT. This ensures that we only pick up one copy of CompilerRT and
    associated global variables, even if we later load a dynamic library
    that depends on them. It also eliminates a major difference between the
    behavior of `mojo build` and `mojo run`. See
    [Internal link]
    for more rationale.
    
    When a mojo executable runs, it now needs to find the shared CompilerRT
    and the other shared library dependencies. For the MAX installation,
    this is straightforward, since we can just put the install paths in the
    config file. Making this work within Bazel rules is more difficult.
    Bazel rules for building mojo binaries, mojo tests, LIT tests, and C++
    unit tests have been updated, but these fixes are fragile. See
    [MOCO-1315: Mojo is a compiler, not a build
    [Internal link]
    for a better long term solution.
    
    The static CompilerRT is no longer used for any build targets, but will
    be retained at the request of @Mogball for local development.
    
    Fixes RUNP-386
    Fixes RUNP-279
    Fixes RUNP-238
    
    MODULAR_ORIG_COMMIT_REV_ID: f09dfa31d6fa19e79fc807de652b7b7be9f907be

--- KGEN/include/KGEN/Support/Configuration.h
+++ KGEN/include/KGEN/Support/Configuration.h
@@ -67,9 +67,6 @@ public:
   /// Return the path to the kgen-compiler-rt library.
   StringRef getCompilerRTPath();
 
-  /// Return the path to the static kgen-compiler-rt library.
-  StringRef getStaticCompilerRTPath();
-
   /// Return the path to the orc-rt library.
   StringRef getOrcRTPath();
 
@@ -104,6 +101,10 @@ public:
   /// Return the system libraries to link with Mojo when building a standalone
   /// binary.
   void getSystemLibraryLinkArgs(SmallVectorImpl<StringRef> &libs);
+
+  /// Return the shared library arguments to link with Mojo when building a
+  /// standalone binary.
+  void getSharedLibraryLinkArgs(SmallVectorImpl<StringRef> &args);
 
   /// Return the section used for this mojo build.
   StringRef getMojoConfigSection();
