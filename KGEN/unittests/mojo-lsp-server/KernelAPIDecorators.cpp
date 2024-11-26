1602b0d613ffd5f7705f098a592cebb54db72e4c
Kenan <kanalbant@gmail.com>
2024-11-26 11:39:53 -0500

    [LSP] Unittests for kernel API decorators (#51581)
    MODULAR_ORIG_COMMIT_REV_ID: 47ca945777b909476cf499faa521e5dd890e5d52

---
+++ KGEN/unittests/mojo-lsp-server/KernelAPIDecorators.cpp
@@ -0,0 +1,68 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#include "Support.h"
+#include "gtest/gtest.h"
+
+using namespace M;
+
+TEST(KernelAPIDecoratorsTest, mutableDecoratorInvalidArgName) {
+  Document doc("test:///foo.mojo", R"(
+import compiler_internal as compiler
+from tensor_utils import ManagedTensorSlice, foreach
+
+@compiler.register("mutable", num_dps_outputs=0)
+struct Mutable:
+    @compiler.mutable("output")
+    @staticmethod
+    fn execute[
+        type: DType,
+    ](input: ManagedTensorSlice[type, rank=2]):
+        x = input[0, 0]
+        x += 1
+        input[0, 0] = x
+)");
+
+  createTestClient()
+      .open(doc)
+      .onDiagnostics(doc,
+                     [](const std::vector<lsp::Diagnostic> &diags) {
+                       ASSERT_EQ((int)diags.size(), 1);
+                       EXPECT_EQ(diags[0].message,
+                                 "mutable decorator: 'output' does not name "
+                                 "any of the arguments of Mutable::execute");
+                     })
+      .execute();
+}
+
+TEST(KernelAPIDecoratorsTest, enableFusionForInvalidArgName) {
+  Document doc("test:///foo.mojo", R"(
+import compiler_internal as compiler
+from tensor_utils import ManagedTensorSlice
+
+@compiler.register("fusion")
+struct Fusion:
+    @compiler.enable_fusion_for("output")
+    @staticmethod
+    fn execute[
+        synchronous: Bool,
+        target: StringLiteral,
+    ](z: ManagedTensorSlice, x: ManagedTensorSlice, y: ManagedTensorSlice):
+        ...
+)");
+
+  createTestClient()
+      .open(doc)
+      .onDiagnostics(doc,
+                     [](const std::vector<lsp::Diagnostic> &diags) {
+                       ASSERT_EQ((int)diags.size(), 1);
+                       EXPECT_EQ(
+                           diags[0].message,
+                           "enable_fusion_for decorator: 'output' does not "
+                           "name any of the arguments of Fusion::execute");
+                     })
+      .execute();
+}
