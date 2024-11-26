4d66f038a038dc5f5cbe603049bd2048c99b43d5
weiwei chen <weiwei.chen@modular.com>
2024-11-26 09:45:46 -0500

    [KGEN] Add emit elaborated mlir as an EmitAs type. (#51531)
    Stacked PRs:
     * #51532
     * __->__#51531
    
    --- --- ---
    
    ### [KGEN] Add emit elaborated mlir as an EmitAs type.
    
    MODULAR_ORIG_COMMIT_REV_ID: 83f106e594196d3d9da72af90bb18dd69366ee33

--- KGEN/lib/KGENDialect/KGENUtils.cpp
+++ KGEN/lib/KGENDialect/KGENUtils.cpp
@@ -863,9 +863,17 @@ static ParseResult parseOperatorOperands(AsmParser &p, uint32_t opcode,
         return failure();
       std::optional<EmitAs> kind = symbolizeEmitAs(emissionKind);
       if (!kind) {
+
+#ifndef MODULAR_PRODUCTION
+        return p.emitError(
+            p.getCurrentLocation(),
+            "the immediate emission kind must be either "
+            "'=llvm', '=asm', '=llvm-opt', '=sharedobj', or '=elabmlir'");
+#else
         return p.emitError(p.getCurrentLocation(),
                            "the immediate emission kind must be either "
                            "'=llvm', '=asm', '=llvm-opt', or '=sharedobj'");
+#endif
       }
       operands.emplace_back(EmitAsAttr::get(p.getContext(), *kind));
     } else if (parseParamValue(p, operands.emplace_back(),
