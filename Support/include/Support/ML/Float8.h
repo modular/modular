ed918562a48ca83baed033d6a4478c0c88db1a69
abdul dakkak <adakkak@modular.com>
2024-12-05 08:35:07 -0800

    [API][GPU] Adds support for FP8FNUZ (E4M3 and E5M2) at the API Level (#52140)
    Following what is done in #51748, this plumbs the fnuz variants of fp8
    to the API level. The fnuz variants are used by AMD GPUs.
    
    MODULAR_ORIG_COMMIT_REV_ID: 7155e16ebe2429a93326c3aa922f4e0713c704ab

--- Support/include/Support/ML/Float8.h
+++ Support/include/Support/ML/Float8.h
@@ -13,39 +13,33 @@
 
 namespace M::Float8 {
 
-struct float8_e4m3_t {
-  // https://en.wikipedia.org/wiki/Bfloat16_floating-point_format
-  // Minimum negative value found by enabling sign bit on maximum value
-  static constexpr uint8_t MIN_BITS = 0xFE;
-  static constexpr uint8_t MAX_BITS = 0x7E;
-
-  float8_e4m3_t(float v) : bits(floatToE4M3Bits(v)) {}
-  explicit float8_e4m3_t(uint8_t rawBits) : bits(rawBits) {}
-
-  static float8_e4m3_t min() { return float8_e4m3_t(MIN_BITS); }
-  static float8_e4m3_t max() { return float8_e4m3_t(MAX_BITS); }
+namespace Detail {
+template <llvm::APFloat::Semantics Semantics>
+struct float8_generic_t {
+  float8_generic_t(float v) : bits(toBits(v)) {}
+  explicit float8_generic_t(uint8_t rawBits) : bits(rawBits) {}
 
   operator float() {
     // We use APFloat to do the heavy lifting here. This is probably not the
     // most efficient way, but it should be battle tested.
     llvm::APInt apInt(8, bits);
-    llvm::APFloat apFloat(llvm::APFloat::Float8E4M3(), apInt);
-    bool _;
+    llvm::APFloat apFloat(llvm::APFloat::EnumToSemantics(Semantics), apInt);
+    bool ignore;
     apFloat.convert(llvm::APFloat::IEEEsingle(),
-                    llvm::APFloat::rmNearestTiesToEven, &_);
+                    llvm::APFloat::rmNearestTiesToEven, &ignore);
     return apFloat.convertToFloat();
   }
 
   operator uint8_t() const { return bits; }
 
 private:
-  static inline uint8_t floatToE4M3Bits(float v) {
+  static inline uint8_t toBits(float v) {
     // We use APFloat to do the heavy lifting here. This is probably not the
     // most efficient way, but it should be battle tested.
     llvm::APFloat apFloat(v);
-    bool _;
-    apFloat.convert(llvm::APFloat::Float8E4M3(),
-                    llvm::APFloat::rmNearestTiesToEven, &_);
+    bool ignore;
+    apFloat.convert(llvm::APFloat::EnumToSemantics(Semantics),
+                    llvm::APFloat::rmNearestTiesToEven, &ignore);
 
     // APInt will store a uint64_t array, which in this case should be
     // singleton. We will index into this, taking endianness into account.
@@ -56,48 +50,14 @@ private:
 
   uint8_t bits;
 };
+} // namespace Detail
 
-struct float8_e5m2_t {
-  static constexpr uint8_t MIN_BITS = 0xFB;
-  static constexpr uint8_t MAX_BITS = 0x7B;
-
-  float8_e5m2_t(float v) : bits(floatToE5M2Bits(v)) {}
-  explicit float8_e5m2_t(uint8_t rawBits) : bits(rawBits) {}
-
-  static float8_e5m2_t min() { return float8_e5m2_t(MIN_BITS); }
-  static float8_e5m2_t max() { return float8_e5m2_t(MAX_BITS); }
-
-  operator float() {
-    // We use APFloat to do the heavy lifting here. This is probably not the
-    // most efficient way, but it should be battle tested.
-    llvm::APInt apInt(8, bits);
-    llvm::APFloat apFloat(llvm::APFloat::Float8E5M2(), apInt);
-    bool _;
-    apFloat.convert(llvm::APFloat::IEEEsingle(),
-                    llvm::APFloat::rmNearestTiesToEven, &_);
-    return apFloat.convertToFloat();
-  }
-
-  operator uint8_t() const { return bits; }
-
-private:
-  static inline uint8_t floatToE5M2Bits(float v) {
-    // We use APFloat to do the heavy lifting here. This is probably not the
-    // most efficient way, but it should be battle tested.
-    llvm::APFloat apFloat(v);
-    bool _;
-    apFloat.convert(llvm::APFloat::Float8E5M2(),
-                    llvm::APFloat::rmNearestTiesToEven, &_);
-
-    // APInt will store a uint64_t array, which in this case should be
-    // singleton. We will index into this, taking endianness into account.
-    const uint64_t *rawData = apFloat.bitcastToAPInt().getRawData();
-    constexpr size_t index = M::is_little_endian ? 0 : 7;
-    return reinterpret_cast<const uint8_t *>(rawData)[index];
-  }
-
-  uint8_t bits;
-};
+struct float8_e4m3_t : Detail::float8_generic_t<llvm::APFloat::S_Float8E4M3> {};
+struct float8_e4m3fnuz_t
+    : Detail::float8_generic_t<llvm::APFloat::S_Float8E4M3FNUZ> {};
+struct float8_e5m2_t : Detail::float8_generic_t<llvm::APFloat::S_Float8E5M2> {};
+struct float8_e5m2fnuz_t
+    : Detail::float8_generic_t<llvm::APFloat::S_Float8E5M2FNUZ> {};
 
 } // namespace M::Float8
 
