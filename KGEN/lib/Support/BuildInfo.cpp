16f9cdaea7f00cabd654e7a7146bf9ede24917a6
Keith Smiley <keithbsmiley@gmail.com>
2025-01-08 12:45:25 -0800

    [Support] Hoist build ID code into support (#53488)
    This is used by mojo and will be used by the graph compiler
    
    MODULAR_ORIG_COMMIT_REV_ID: 8eca2c4b5738df2602565eadf7dbe1d105bc8dae

--- KGEN/lib/Support/BuildInfo.cpp
+++ KGEN/lib/Support/BuildInfo.cpp
@@ -6,65 +6,16 @@
 
 #include "KGEN/Support/BuildInfo.h"
 #include "Config/Version.h"
+#include "Support/BinaryID.h"
 
 #include "llvm/ADT/StringRef.h"
 #include "llvm/Support/Format.h"
 #include "llvm/Support/raw_ostream.h"
 
-#include <cassert>
-
-#if defined(__APPLE__)
-#include <dlfcn.h>
-#include <mach-o/loader.h>
-#include <stdint.h>
-#else
-#include <elf.h>
-
-extern char __build_id_start;
-extern char __build_id_end;
-#endif // __APPLE__
-
 using namespace M;
 using namespace KGEN;
 
-static std::string getBuildID() {
-  std::string str;
-  llvm::raw_string_ostream os(str);
-
-#if defined(__APPLE__)
-  // NOTE: This code can run in a dylib or executable, we need to fetch the
-  // address of whichever one it comes from
-  Dl_info info;
-  if (dladdr((void *)&getBuildID, &info) == 0)
-    assert(false && "dladdr failed");
-  auto *execHeader = (const struct mach_header_64 *)info.dli_fbase;
-
-  // Get the header of the current binary or shared library, and find the UUID
-  // load command
-  uintptr_t command = (uintptr_t)execHeader + sizeof(struct mach_header_64);
-  for (uint32_t idx = 0; idx < execHeader->ncmds; ++idx) {
-    if (((const struct load_command *)command)->cmd == LC_UUID) {
-      const struct uuid_command *cmd = (const struct uuid_command *)command;
-      for (unsigned char i : cmd->uuid)
-        os << llvm::format("%02x", i);
-      break;
-    } else {
-      command += ((const struct load_command *)command)->cmdsize;
-    }
-  }
-#else
-  Elf64_Nhdr *hdr = (Elf64_Nhdr *)&__build_id_start;
-  assert(hdr->n_type == NT_GNU_BUILD_ID && "invalid section type");
-
-  const char *s = s = &__build_id_start + sizeof(Elf64_Nhdr) + hdr->n_namesz;
-  for (; s < &__build_id_end; ++s)
-    os << llvm::format("%02hhx", *s);
-#endif // __APPLE__
-
-  return str;
-}
-
 std::string M::KGEN::getVersionString() {
-  return M::getModularVersionString() + getBuildID() + "-" +
+  return M::getModularVersionString() + M::getBinaryID() + "-" +
          M::getModularVersion().buildType;
 }
---
+++ Support/include/Support/BinaryID.h
@@ -0,0 +1,16 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef SUPPORT_BINARYID_H
+#define SUPPORT_BINARYID_H
+
+#include <string>
+
+namespace M {
+std::string getBinaryID();
+} // namespace M
+
+#endif // SUPPORT_BINARYID_H
