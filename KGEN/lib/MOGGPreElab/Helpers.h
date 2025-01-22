174dec48be038b0b80d414331351bd80c4c2d994
Spenser Bauman <spenser@modular.com>
2025-01-22 17:00:37 +0000

    [MOGGPreElab] Remove dead code (#54373)
    Helpers.h defines a LambdaTemplate class that is unused by any
    transform. With the removal of SliceMOGGDPSFuncs, there is only one
    transform that would potentially use it, and that transform defines its
    own version of the type. Lets just delete this one.
    
    MODULAR_ORIG_COMMIT_REV_ID: cfd96a23bf6a63ab71e190010863d51a55c48895

--- KGEN/lib/MOGGPreElab/Helpers.h
+++ KGEN/lib/MOGGPreElab/Helpers.h
@@ -7,34 +7,9 @@
 #ifndef KGEN_LIB_MOGGPREELAB_HELPERS_H
 #define KGEN_LIB_MOGGPREELAB_HELPERS_H
 
-#include "KGEN/KGENDialect/KGENOps.h"
 #include "KGEN/LITDialect/LITOps.h"
-#include "KGEN/MOGGPreElab/MOGGDecorators.h"
 
 namespace M::KGEN::MOGGPreElab {
-
-/// We have a special mojo hook which show us what the canonical lambda
-/// looks like and a call which tells us the resulting type with the lambda
-/// applied.
-struct LambdaTemplate {
-  LambdaTemplate() = default;
-
-  /// Scan the hook for the properties that we know exist.
-  LambdaTemplate(GeneratorOp hook) : templateOp(hook) {
-    for (auto region : hook.getOps<ParamDeclareRegionOp>())
-      canonicalLambda = region;
-    for (auto call : hook.getOps<CallOp>())
-      callUsingLambda = call;
-  }
-  /// The op we are pulling this info from.
-  GeneratorOp templateOp;
-
-  /// This the the template lambda we will clone as the input or output lambda.
-  ParamDeclareRegionOp canonicalLambda;
-
-  // This call shows us how the lambda needs to be bound.
-  CallOp callUsingLambda;
-};
 
 inline bool isXType(LIT::StructType maybeTensor, StringLiteral root,
                     StringLiteral className) {
