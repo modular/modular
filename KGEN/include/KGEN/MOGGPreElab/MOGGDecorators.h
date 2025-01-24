9481baf5302649968bc130daa478a1c6674a45f3
Joshua Peterson <josh@modular.com>
2025-01-24 14:36:51 -0500

    [Kernels] Merge tensor_utils_internal into tensor_internal (#54377)
    ### [Kernels] Merge tensor_utils_internal into tensor_internal
    
    There is no need for this distinction. Merged these two allows the
    public docs to be simpler, giving the users only a tensors section to
    import and explore.
    
    Towards GEX-1704
    
    MODULAR_ORIG_COMMIT_REV_ID: f59d98a5b46dbefc99cdb724fd5efc9478b5a994

--- KGEN/include/KGEN/MOGGPreElab/MOGGDecorators.h
+++ KGEN/include/KGEN/MOGGPreElab/MOGGDecorators.h
@@ -228,10 +228,10 @@ inline bool isDPSKernel(Operation *gen) {
 
 // The stored mojo type symbol name of Tensor type in extensibility kernels.
 constexpr StringLiteral MOJO_DPS_TENSOR_TYPE_NAME =
-    "tensor_utils::ManagedTensorSlice";
+    "tensor_internal::ManagedTensorSlice";
 
 constexpr StringLiteral MOJO_INTERNAL_DPS_TENSOR_TYPE_NAME =
-    "tensor_utils_internal::ManagedTensorSlice";
+    "tensor_internal::ManagedTensorSlice";
 
 constexpr StringLiteral MOJO_INTERNAL_DPS_SIMD_TYPE_NAME = "stdlib::SIMD";
 
--- KGEN/lib/MOGGPreElab/Helpers.h
+++ KGEN/lib/MOGGPreElab/Helpers.h
@@ -24,7 +24,7 @@ inline bool isExtensibilityTensor(LIT::StructType maybeTensor) {
 
 inline bool isDPSTensor(LIT::StructType maybeTensor) {
   return maybeTensor.getSymbol().getRootReference().strref().starts_with(
-             "tensor_utils") &&
+             "tensor") &&
          maybeTensor.getSymbol().getLeafReference() == "ManagedTensorSlice";
 }
 
