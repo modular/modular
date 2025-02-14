modular/modular

commit b482fbf26173f2c064a4e847462eee78f3284b07

[Pipelines] [CI] Work around Llama 3.1 default batch size bug (#55947)
We are broken out-of-the-box on A10 due to logic changes to the default
batch size computation. Our standard policy is to revert the offending
change, but Product has requested that we work around instead. This
should get pipeline logit verification green again.

MAX_INTEGRATION_TESTS_REV_ID: 2779cce11b517991986645d24bb2484378ceb87e

max/tests/integration/pipelines/python/generate_llm_logits.py
@@ -208,6 +208,10 @@ class LlamaPipelineOracle(PipelineOracle):
         config = pipelines.PipelineConfig(
             architecture="LlamaForCausalLM",
             quantization_encoding=pipelines.SupportedEncoding[encoding],
+            # Normally we do not override batch size, preferring to test the
+            # defaults, but the default is known to be broken on A10.  Product
+            # has requested that we work around rather than fix for now.
+            max_batch_size=32,
             max_new_tokens=10,
             huggingface_repo_id=f"modularai/llama-{internal_version}",
             weight_path=[

commit 85468db0dbb20af2fd78799a7532ccdf683f1529

[Pipelines] [CI] Limit batch size workaround to GPU only (#56025)
The CPU estimation is correct in the default case. We need to reduce the
batch size for GPU, but 32 is too large for CPU. Instead of setting to
32 indiscriminately, set to 32 only for GPU.

Fixes breakage introduced in #55947.

MAX_INTEGRATION_TESTS_REV_ID: c03a8a63f78dc45751b8631a1f25d1ad4819b680

max/tests/integration/pipelines/python/generate_llm_logits.py
@@ -210,8 +210,16 @@ class LlamaPipelineOracle(PipelineOracle):
             quantization_encoding=pipelines.SupportedEncoding[encoding],
             # Normally we do not override batch size, preferring to test the
             # defaults, but the default is known to be broken on A10.  Product
-            # has requested that we work around rather than fix for now.
-            max_batch_size=32,
+            # has requested that we work around rather than fix for now.  Do
+            # NOT override, and instead leave as default, for CPU.
+            max_batch_size=(
+                32
+                if any(
+                    device_spec.device_type == "gpu"
+                    for device_spec in device_specs
+                )
+                else None
+            ),
             max_new_tokens=10,
             huggingface_repo_id=f"modularai/llama-{internal_version}",
             weight_path=[