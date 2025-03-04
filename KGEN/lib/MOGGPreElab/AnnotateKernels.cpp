b08aa072d9c2647d2cf223741880e3aa60e479ec
Kenan <kanalbant@gmail.com>
2025-03-04 15:50:15 -0800

    [MOGGPreElab] missed some kernels -- allow enforce_io_param still (#57114)
    MODULAR_ORIG_COMMIT_REV_ID: 9a71821b83638e5037222369f8b314a665df62d6

--- KGEN/lib/MOGGPreElab/AnnotateKernels.cpp
+++ KGEN/lib/MOGGPreElab/AnnotateKernels.cpp
@@ -846,7 +846,7 @@ bool processStructExecuteFunc(ModuleOp moduleOp,
 
   // TODO(GEX-1911): Remove code around enforce_io_param usage as now io_spec
   // specialization is always enforced
-  if (true) {
+  if (enforceIOParamUsage) {
     // Set mogg.num_dps_outputs
     auto numOutputs = llvm::count_if(ioSpecs, [](auto &&elem) {
       return elem.second == IOSpec::OutputTensor;
