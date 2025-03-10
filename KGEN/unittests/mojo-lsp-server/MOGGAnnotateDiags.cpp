505d2640dd005b1fe6b7413cb704876c19d9de62
Kenan <kanalbant@gmail.com>
2025-03-10 17:17:41 -0400

    [LSP] Add tests for various diagnostics mogg-annotate-kernels (#57429)
    Stacked PRs:
     * __->__#57429
     * #57428
    
    --- --- ---
    
    ### [LSP] Add tests for various diagnostics mogg-annotate-kernels
    
    Towards: MAXPLAT-83
    MODULAR_ORIG_COMMIT_REV_ID: 666dcb8f5442fbb42cebda0c1b44b610aadf7c4e

---
+++ KGEN/unittests/mojo-lsp-server/MOGGAnnotateDiags.cpp
@@ -0,0 +1,224 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#include "Support.h"
+#include "gtest/gtest.h"
+
+using namespace M;
+
+TEST(MOGGAnnotateTests, MissingReadWriteParams) {
+  Document doc("test:///foo.mojo", R"(
+import compiler_internal as compiler
+from tensor import ManagedTensorSlice, OutputTensor, InputTensor
+
+@compiler.register("Missing")
+struct Missing:
+    @staticmethod
+    fn execute(a: ManagedTensorSlice):
+        ...
+)");
+
+  createTestClient()
+      .open(doc)
+      .onDiagnostics(
+          doc,
+          [](const std::vector<lsp::Diagnostic> &diags) {
+            ASSERT_EQ((int)diags.size(), 2);
+            EXPECT_EQ(diags[0].message, "Error for argument 'a': 'read' "
+                                        "inferred parameter must be set");
+            EXPECT_EQ(diags[1].message, "Error for argument 'a': 'write' "
+                                        "inferred parameter must be set");
+          })
+      .execute();
+}
+
+TEST(MOGGAnnotateTests, OutputAfterInput) {
+  Document doc("test:///foo.mojo", R"(
+import compiler_internal as compiler
+from tensor import ManagedTensorSlice, OutputTensor, InputTensor
+
+@compiler.register("OutputAfterInput")
+struct OutputAfterInput:
+    @staticmethod
+    fn execute(a: InputTensor, b: OutputTensor):
+        ...
+)");
+
+  createTestClient()
+      .open(doc)
+      .onDiagnostics(doc,
+                     [](const std::vector<lsp::Diagnostic> &diags) {
+                       ASSERT_EQ((int)diags.size(), 1);
+                       EXPECT_EQ(diags[0].message,
+                                 "Output tensor argument 'b' must come before "
+                                 "other non-output tensor arguments");
+                     })
+      .execute();
+}
+
+TEST(MOGGAnnotateTests, InputTensorsForShape) {
+  Document doc("test:///foo.mojo", R"(
+import compiler_internal as compiler
+from tensor import ManagedTensorSlice, OutputTensor, InputTensor
+
+@compiler.register("InputTensorsForShape")
+struct InputTensorsForShape:
+    @staticmethod
+    fn execute(a: InputTensor):
+      pass
+
+    @staticmethod
+    fn shape(a: ManagedTensorSlice):
+      pass
+)");
+
+  createTestClient()
+      .open(doc)
+      .onDiagnostics(doc,
+                     [](const std::vector<lsp::Diagnostic> &diags) {
+                       ASSERT_EQ((int)diags.size(), 1);
+                       EXPECT_EQ(diags[0].message,
+                                 "Error for argument 'a': Tensor arguments to "
+                                 "shape functions must be 'InputTensor'");
+                     })
+      .execute();
+}
+
+TEST(MOGGAnnotateTests, NonInputTensorList) {
+  Document doc("test:///foo.mojo", R"(
+from compiler_internal import StaticTensorSpec
+import compiler_internal as compiler
+from tensor import OutputTensor, MutableInputTensor
+
+@compiler.register("non_input_tensor_list")
+struct NonInputTensorList:
+    @staticmethod
+    fn execute[
+        type: DType,
+        rank: Int,
+        target: StringLiteral,
+        _synchronous: Bool,
+    ](
+        output: List[
+            OutputTensor[
+                static_spec = StaticTensorSpec[type, rank].create_unknown()
+            ]
+        ],
+        mutable: List[
+            MutableInputTensor[
+                static_spec = StaticTensorSpec[type, rank].create_unknown()
+            ]
+        ],
+    ):
+      pass
+)");
+
+  createTestClient()
+      .open(doc)
+      .onDiagnostics(
+          doc,
+          [](const std::vector<lsp::Diagnostic> &diags) {
+            ASSERT_EQ((int)diags.size(), 2);
+            EXPECT_EQ(diags[0].message,
+                      "Only input tensors are allowed as the element type "
+                      "for list arguments at the moment.");
+            EXPECT_EQ(diags[1].message,
+                      "Only input tensors are allowed as the element type "
+                      "for list arguments at the moment.");
+          })
+      .execute();
+}
+
+TEST(MOGGAnnotateTests, PytorchFallbackInvalidArgument) {
+  Document doc("test:///foo.mojo", R"(
+import compiler_internal as compiler
+from python import Python, PythonObject
+
+@compiler.register("pytorch_fallback")
+struct InvalidPytorchFallBackArgument:
+    @staticmethod
+    fn execute():
+      ...
+
+    @staticmethod
+    fn pytorch_fallback(a: PythonObject, b: Int):
+      return
+)");
+
+  createTestClient()
+      .open(doc)
+      .onDiagnostics(
+          doc,
+          [](const std::vector<lsp::Diagnostic> &diags) {
+            ASSERT_EQ((int)diags.size(), 2);
+            EXPECT_EQ(
+                diags[0].message,
+                "Error for argument 'b' all arguments to 'pytorch_fallback' "
+                "functions must have type 'PythonObject'");
+            EXPECT_EQ(diags[1].message,
+                      "Error for result type: the only permitted return type "
+                      "for 'pytorch_fallback' functions is 'PythonObject'");
+          })
+      .execute();
+}
+
+TEST(MOGGAnnotateTests, PytorchFallbackInvalidResult) {
+  Document doc("test:///foo.mojo", R"(
+import compiler_internal as compiler
+from python import Python, PythonObject
+
+@compiler.register("pytorch_fallback")
+struct InvalidPytorchFallBackResult:
+    @staticmethod
+    fn execute():
+      ...
+
+    @staticmethod
+    fn pytorch_fallback(a: PythonObject, b: PythonObject) -> Int:
+      return Int(0)
+)");
+
+  createTestClient()
+      .open(doc)
+      .onDiagnostics(
+          doc,
+          [](const std::vector<lsp::Diagnostic> &diags) {
+            ASSERT_EQ((int)diags.size(), 1);
+            EXPECT_EQ(diags[0].message,
+                      "Error for result type: the only permitted return type "
+                      "for 'pytorch_fallback' functions is 'PythonObject'");
+          })
+      .execute();
+}
+
+TEST(MOGGAnnotateTests, PytorchFallbackInvalidOutResult) {
+  Document doc("test:///foo.mojo", R"(
+import compiler_internal as compiler
+from python import Python, PythonObject
+
+@compiler.register("pytorch_fallback")
+struct InvalidPytorchFallBackResult:
+    @staticmethod
+    fn execute():
+      ...
+
+    @staticmethod
+    fn pytorch_fallback(out output: Int, a: PythonObject, b: PythonObject):
+      return Int(0)
+)");
+
+  createTestClient()
+      .open(doc)
+      .onDiagnostics(
+          doc,
+          [](const std::vector<lsp::Diagnostic> &diags) {
+            ASSERT_EQ((int)diags.size(), 1);
+            EXPECT_EQ(diags[0].message,
+                      "Error for result type: the only permitted return type "
+                      "for 'pytorch_fallback' functions is 'PythonObject'");
+          })
+      .execute();
+}
