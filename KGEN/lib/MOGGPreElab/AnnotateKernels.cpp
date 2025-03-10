591ffd1413c63428414054877e3c1cadc47a4af5
Kenan <kanalbant@gmail.com>
2025-03-10 17:17:05 -0400

    [MOGGPreElab] Error when PythonObjects aren't used for pytorch_fallbacks (#57428)
    Stacked PRs:
     * #57429
     * __->__#57428
    
    --- --- ---
    
    ### [MOGGPreElab] Error when PythonObjects aren't used for
    pytorch_fallbacks
    
    Fixes: MAXPLAT-84
    MODULAR_ORIG_COMMIT_REV_ID: a7ffe73e1b3f11faf04338e520b04316fc816f0e

--- KGEN/lib/MOGGPreElab/AnnotateKernels.cpp
+++ KGEN/lib/MOGGPreElab/AnnotateKernels.cpp
@@ -42,6 +42,8 @@ static constexpr std::array<StringLiteral, 3> kMaxVariadicTensors = {
     "tensor_internal", "managed_tensor_slice", "VariadicTensors"};
 static constexpr std::array<StringLiteral, 4> kMaxList = {
     "stdlib", "collections", "list", "List"};
+static constexpr std::array<StringLiteral, 4> kPythonObject = {
+    "stdlib", "python", "python_object", "PythonObject"};
 
 // TODO(GEX-1822): Should be able to query this information from
 // The lit.struct.decl ops for each of these types rather than hard-coding them.
@@ -657,6 +659,47 @@ processIOSpecs(LIT::FnOp func, bool isShapeFunc = false) {
   return specs;
 }
 
+LogicalResult allTypesArePythonObject(LIT::FnOp pytorch_fallback) {
+
+  auto isPythonObject = [](Type type) {
+    auto structType = getAsDeclRefOrNull(type);
+
+    if (!structType)
+      return false;
+
+    return symbolMatches(structType.getSymbol(), kPythonObject);
+  };
+
+  bool error = false;
+  // Check argument types
+  for (auto &&[argIdx, argType] :
+       llvm::enumerate(pytorch_fallback.getArgumentTypes())) {
+
+    if (isPythonObject(argType))
+      continue;
+
+    auto argName = pytorch_fallback.getFuncTypeGenerator().getArgName(argIdx);
+    auto loc = pytorch_fallback.getBodyRegion().getArgument(argIdx).getLoc();
+
+    emitError(loc, "Error for argument '" + argName.strref() +
+                       "' all arguments to 'pytorch_fallback' functions must "
+                       "have type 'PythonObject'");
+
+    error = true;
+  }
+
+  auto resultType = pytorch_fallback.getFuncTypeGenerator().getUserResultType();
+  if (!isPythonObject(resultType)) {
+    pytorch_fallback.emitError(
+        "Error for result type: the only permitted return type for "
+        "'pytorch_fallback' functions is 'PythonObject'");
+
+    error = true;
+  }
+
+  return failure(error);
+}
+
 // Run standard checks and mutations on a function. emits an
 // error and returns false if an issue was present.
 //
@@ -677,6 +720,11 @@ LogicalResult processStructFuncCommon(
   if (annotation == kMOGGShapeFunctionLabel) {
     auto result = processIOSpecs(func, /*isShapeFunc=*/true);
     if (!result.has_value())
+      return failure();
+  }
+
+  if (annotation == kMOGGPyTorchFallbackFunctionLabel) {
+    if (failed(allTypesArePythonObject(func)))
       return failure();
   }
 
