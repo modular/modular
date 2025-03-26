a222e1ca8d16cdfe333ec3edd2a55b14ba3d0794
Alex Trotta <44127594+Ahajha@users.noreply.github.com>
2025-03-26 14:10:34 -0400

    Revert "[KGEN] Don't split private symbol" (#58505)
    Reverts [Internal link]
    
    This broke the nightly.
    
    MODULAR_ORIG_COMMIT_REV_ID: 33cf5d73e0cf77863b4cbb3c676422eccd8c9a80

--- KGEN/lib/Compiler/ObjectCompiler/LLVMIRUtils.cpp
+++ KGEN/lib/Compiler/ObjectCompiler/LLVMIRUtils.cpp
@@ -769,29 +769,8 @@ void LLVMModulePerFunctionSplitterImpl::collectValueUsers(
 
   // If the current value is a mutable global variable, then it can't be
   // split.
-  // [MOCO-1680] Also don't split private symbols as linking might not work if
-  // there's a use of the symbol in a module where the symbol is external. That
-  // is, if the symbol becomes external in a module, code generation may emit
-  // code assuming the symbol is in GOT. However, private symbols are not
-  // getting entries in GOT, therefore final linkage will fail with "undefined
-  // symbol".
-  //
-  // NOTE: It's possible to support private symbols by switching them
-  // to internal linkage (see https://llvm.org/docs/LangRef.html#linkage-types):
-  // - internal: Similar to private, but the value shows as a local symbol
-  //            (STB_LOCAL in the case of ELF) in the object file.
-  // However, it cannot be done here, because it's not yet known how
-  // many modules will use it.
-  if (auto *global = dyn_cast<llvm::GlobalVariable>(value)) {
-    valueInfos[value].canBeSplit =
-        global->isConstant() && !global->hasPrivateLinkage();
-    LLVM_DEBUG({
-      llvm::dbgs() << "global " << global->getName()
-                   << (valueInfos[value].canBeSplit ? " can be split"
-                                                    : " cannot be split");
-      llvm::dbgs() << '\n';
-    });
-  }
+  if (auto *global = dyn_cast<llvm::GlobalVariable>(value))
+    valueInfos[value].canBeSplit = global->isConstant();
 }
 
 /// Propagate use information through the module.
@@ -852,9 +831,7 @@ void LLVMModulePerFunctionSplitterImpl::propagateUseInfo() {
       }
     }
 
-    // To save time spent in combining dependencies, skip non-immediate
-    // dependencies (users) of the global value that cannot be split.
-    if (info->canBeSplit || !isa_and_nonnull<llvm::GlobalValue>(info->value))
+    if (info->canBeSplit || isa_and_nonnull<llvm::GlobalValue>(info->value))
       continue;
 
     // If a value cannot be split, propagate its dependencies up to its
