9b01910526d63ab598b9e6195fbc9f4bd19e83a1
Evan Ovadia <verdagon@modular.com>
2025-04-02 20:54:06 -0500

    [mojo-lang] Finishes function types, by fixing StructEmitter::createFunction to find more ImplicitOriginRefAttrs to replace (#55241)
    ### [mojo-lang] Finishes function types, by fixing
    StructEmitter::createFunction to find more ImplicitOriginRefAttrs to
    replace
    
    * Fixes StructEmitter::createFunction to accurately replace all the
    implicit origins
     * Adds the getitem calls to variadic thunk's body
    * Adds a final test that reflects what the function-types project is
    going for
    
    StructEmitter::createFunction was previously only shallowly searching
    the argument types for ImplicitOriginRefAttrs so it could replace them
    with ParamDeclRefAttrs. This makes it properly look deeply in the
    argument types (but not too deeply, because we don't want to dive into
    any vtables hidden inside TypeParamAttrs)).
    
    Once StructEmitter::createFunction was fixed, the __getitem__ calls
    finally worked and didn't have depth mismatch errors, and now everything
    works.
    
    MODULAR_ORIG_COMMIT_REV_ID: 46afc35e47b03ffd65427e7bb69b9c48eb5614dd

--- KGEN/lib/MojoParser/ExprNodes.cpp
+++ KGEN/lib/MojoParser/ExprNodes.cpp
@@ -1129,9 +1129,11 @@ static PValue bindToIndirectCall(PValue callable, FnTypeGeneratorType sig,
 
 /// When subscripting a callable with a bound symbol (i.e. a direct method call
 /// or call to a method), apply parameter bindings to it.
-static LogicalResult bindParamValuesToDirectCall(OverloadSet &overloadSet,
-                                                 ArrayRef<Operand> operands,
-                                                 ExprEmitter &emitter) {
+// TODO(MOCO-1106): Removed `static` on this so variadic thunks could use it,
+// let's refactor emitGetterSetterAccess so it can be used by thunks instead.
+LogicalResult bindParamValuesToDirectCall(OverloadSet &overloadSet,
+                                          ArrayRef<Operand> operands,
+                                          ExprEmitter &emitter) {
   // Build a list of all the things we can bind. Remember the number of
   // positional parmaeters that have already been bound.
   SmallVector<InProgressBindings> bindables;
