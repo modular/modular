0e4de54d370d7f4c523a065afd30112415fd300b
Feras Boulala <feras.boulala@modular.com>
2025-04-07 08:28:43 -0400

    [MOGG] Introduce a new helper to interface with MOGGPReElab annotations (#58523)
    These in-memory helpers are meant to be used from `MOGGPreElab` passes
    or subsequent passes in the graph compiler or graph API. They are
    a centralized place to interact with our understanding of kernels.
    Currently, they parse the annotated `LIT` or `KGEN` and build the
    representation.
    
    There are various helper methods attached to these structures. They are
    meant to be extended based on introspection's need. Ideally, code that
    depends on this will not change if we modify the introspected dialect
    (ie annotated KGEN vs annotated LIT vs plain LIT).
    
    MODULAR_ORIG_COMMIT_REV_ID: c5719aecc26d69850a0d7cc5e81e06b8af94c69f

---
+++ KGEN/include/KGEN/MOGGPreElab/MojoKernelAdaptor.h
@@ -0,0 +1,375 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef MOJO_KERNEL_ADAPTOR_H
+#define MOJO_KERNEL_ADAPTOR_H
+
+#include "KGEN/KGENDialect/KGENOps.h"
+#include "KGEN/LITDialect/LITOps.h"
+#include "KGEN/MOGGPreElab/MOGGPreElabDecorators.h"
+
+namespace M::KGEN::MOGGPreElab {
+
+struct TensorOperandAdaptor {
+  static constexpr StringLiteral typeName = MOJO_DPS_TENSOR_TYPE_NAME;
+  // If the tensor is a mutable input or not.
+  bool mut;
+  bool fused;
+
+  bool operator==(const TensorOperandAdaptor &other) const {
+    // Not comparing fused because it can't be specified anywhere except in Mojo
+    // (nothing to compare it to).
+    return mut == other.mut;
+  }
+};
+
+struct VariadicTensorOperandAdaptor : TensorOperandAdaptor {
+  static constexpr StringLiteral typeName = MOJO_VARIADIC_TENSORS_NAME;
+  bool operator==(const VariadicTensorOperandAdaptor &other) const {
+    return mut == other.mut;
+  }
+};
+
+struct ListOfTensorOperandAdaptor : TensorOperandAdaptor {
+  static constexpr StringLiteral typeName = MOJO_TENSOR_LIST_NAME;
+  bool operator==(const ListOfTensorOperandAdaptor &other) const {
+    return mut == other.mut;
+  }
+};
+
+struct ScalarOperandAdaptor {
+  static constexpr StringLiteral typeName = MOJO_INTERNAL_DPS_SIMD_TYPE_NAME;
+  bool operator==(const ScalarOperandAdaptor &other) const { return true; }
+};
+
+struct DevicesContextPtrOperandAdaptor {
+  static constexpr StringLiteral typeName =
+      MOJO_EXTENSIBILITY_API_DEVICE_CONTEXT_PTR_TYPE_NAME;
+  bool operator==(const DevicesContextPtrOperandAdaptor &other) const {
+    return true;
+  }
+};
+
+struct DevicesContextPtrListOperandAdaptor {
+  static constexpr StringLiteral typeName =
+      MOJO_EXTENSIBILITY_API_DEVICE_CONTEXT_PTR_LIST_TYPE_NAME;
+  bool operator==(const DevicesContextPtrListOperandAdaptor &other) const {
+    return true;
+  }
+};
+
+struct OpaqueOperandAdaptor {
+  StringRef typeName;
+  bool operator==(const OpaqueOperandAdaptor &other) const {
+    if (typeName == other.typeName)
+      return true;
+
+    /// FIXME(GEX-2036): This is a gross hack. In `MOGGPreElab`, we maintain
+    /// module names. In other parts of the stack, we use the leaf name.
+    /// Therefore, we check if one name ends with the other. A proper fix would
+    /// be to use the same name at all times.
+    if (typeName.ends_with(other.typeName) ||
+        other.typeName.ends_with(typeName))
+      return true;
+
+    return false;
+  }
+};
+
+struct MojoKernelOperandSourceDescriptor {
+  // The name of the variable in the original Mojo code.
+  StringRef sourceName;
+  // Its position in the function.
+  uint64_t position;
+};
+
+using MojoKernelOperandVariant =
+    std::variant<TensorOperandAdaptor, VariadicTensorOperandAdaptor,
+                 ListOfTensorOperandAdaptor, ScalarOperandAdaptor,
+                 OpaqueOperandAdaptor, DevicesContextPtrOperandAdaptor,
+                 DevicesContextPtrListOperandAdaptor>;
+
+struct MojoKernelOperandAdaptor {
+  // The source information which is optional in the case of a return value.
+  std::optional<MojoKernelOperandSourceDescriptor> sourceDescriptor;
+  // A union between all kinds of supported operands.
+  MojoKernelOperandVariant underlyingType;
+
+  MojoKernelOperandAdaptor(std::optional<uint64_t> positionInFunction,
+                           StringRef typeName,
+                           ArrayAttr positionOfMutableTensors,
+                           ArrayAttr positionOfFusedTensors,
+                           ArrayAttr argumentSourceNames, uint64_t offset = 0);
+
+  template <typename StreamType>
+  StreamType &printNested(StreamType &os, const std::string &nesting) const {
+    if (sourceDescriptor.has_value())
+      os << nesting << *sourceDescriptor;
+
+    os << underlyingType;
+
+    return os;
+  }
+
+  bool operator==(const MojoKernelOperandAdaptor &other) const {
+    if (underlyingType.index() != other.underlyingType.index())
+      return false;
+
+    return std::visit(
+        [&](auto &&arg) { return underlyingType == other.underlyingType; },
+        underlyingType);
+  }
+
+  bool operator!=(const MojoKernelOperandAdaptor &other) const {
+    return !(*this == other);
+  }
+
+  bool isVariadicTensorType() const {
+    return std::holds_alternative<VariadicTensorOperandAdaptor>(underlyingType);
+  }
+
+  bool isOpaqueType() const {
+    return std::holds_alternative<OpaqueOperandAdaptor>(underlyingType);
+  }
+
+  bool isContextType() const {
+    return std::holds_alternative<DevicesContextPtrOperandAdaptor>(
+               underlyingType) ||
+           std::holds_alternative<DevicesContextPtrListOperandAdaptor>(
+               underlyingType);
+  }
+
+  bool isTensorType() const {
+    return std::holds_alternative<TensorOperandAdaptor>(underlyingType) ||
+           std::holds_alternative<VariadicTensorOperandAdaptor>(
+               underlyingType) ||
+           std::holds_alternative<ListOfTensorOperandAdaptor>(underlyingType);
+  }
+};
+
+template <typename FuncOpType>
+struct MojoKernelFunctionAdaptor {
+  // The underlying LIT or KGEN function.
+  // Marked mutable because getSymName is not const for some reason.
+  mutable FuncOpType mojoCode;
+  // Input arguments.
+  SmallVector<MojoKernelOperandAdaptor> inputArguments;
+  // Output arguments (DPS).
+  SmallVector<MojoKernelOperandAdaptor> outputArguments;
+  // Output result which could be an argument if the function throws.
+  std::optional<MojoKernelOperandAdaptor> outputResult;
+
+  MojoKernelFunctionAdaptor(FuncOpType op) : mojoCode(op) {
+    auto argumentTypesNames = mojoCode->template getAttrOfType<ArrayAttr>(
+        KGEN::MOGGPreElab::MOGG_ARG_TYPE_NAMES);
+    auto resultTypeName = mojoCode->template getAttrOfType<StringAttr>(
+        KGEN::MOGGPreElab::MOGG_RESULT_TYPE_NAME);
+    auto numberOfOutputArgumentsAttr =
+        mojoCode->template getAttrOfType<IntegerAttr>(
+            KGEN::MOGGPreElab::kMOGGNumDPSOutputs);
+    auto positionOfMutableTensors = mojoCode->template getAttrOfType<ArrayAttr>(
+        KGEN::MOGGPreElab::kMOGGBufferArgs);
+    auto positionOfFusedTensors = mojoCode->template getAttrOfType<ArrayAttr>(
+        KGEN::MOGGPreElab::kMOGGFusableArgs);
+    auto argumentSourceNames = mojoCode->template getAttrOfType<ArrayAttr>(
+        KGEN::MOGGPreElab::MOGG_ARG_SRC_NAMES);
+
+    uint64_t begOfInputArguments =
+        numberOfOutputArgumentsAttr ? numberOfOutputArgumentsAttr.getInt() : 0;
+    auto funcTypeGenerator = mojoCode.getFuncTypeGenerator().getBody();
+    bool resultAsArgument = false;
+    for (uint64_t i = 0; i < mojoCode.getNumArguments(); ++i) {
+      if (funcTypeGenerator.getArgConvention(i) ==
+          KGEN::ArgConvention::ByRefResult) {
+        resultAsArgument = true;
+        break;
+      }
+    }
+    bool isThrow = funcTypeGenerator.isThrows();
+    uint64_t numberOfArgumentsRelatedToByrefResult =
+        isThrow ? 2 : resultAsArgument;
+    uint64_t endOfInputArguments =
+        argumentTypesNames.size() - numberOfArgumentsRelatedToByrefResult;
+
+    for (size_t i = begOfInputArguments; i < endOfInputArguments; ++i) {
+      auto argTypeName = cast<StringAttr>(argumentTypesNames[i]).strref();
+      inputArguments.emplace_back(i, argTypeName, positionOfMutableTensors,
+                                  positionOfFusedTensors, argumentSourceNames,
+                                  begOfInputArguments);
+    }
+
+    for (size_t i = 0; i < begOfInputArguments; ++i) {
+      auto argTypeName = cast<StringAttr>(argumentTypesNames[i]).strref();
+      outputArguments.emplace_back(i, argTypeName, positionOfMutableTensors,
+                                   positionOfFusedTensors, argumentSourceNames);
+    }
+
+    if (resultAsArgument) {
+      auto argTypeName = dyn_cast<StringAttr>(
+          argumentTypesNames[endOfInputArguments +
+                             numberOfArgumentsRelatedToByrefResult - 1]);
+      if (argTypeName) {
+        outputResult = MojoKernelOperandAdaptor(
+            endOfInputArguments + numberOfArgumentsRelatedToByrefResult - 1,
+            argTypeName.strref(), positionOfMutableTensors,
+            positionOfFusedTensors, argumentSourceNames);
+      }
+    } else if (resultTypeName) {
+      outputResult = MojoKernelOperandAdaptor(
+          {}, resultTypeName.strref(), nullptr, nullptr, argumentSourceNames);
+    }
+  }
+
+  template <typename StreamType>
+  StreamType &printNested(StreamType &os, const std::string &nesting) const {
+    StringRef sourceName = mojoCode.getSymName();
+    if (auto name = mojoCode->getAttr("sourceName"))
+      sourceName = cast<StringAttr>(name).strref();
+    os << nesting << sourceName.str() << "\n" << nesting << "(\n";
+
+    for (auto &arg : outputArguments)
+      arg.printNested(os, nesting + "  [out] ") << ",\n";
+
+    for (auto arg : inputArguments)
+      arg.printNested(os, nesting + "  [in]  ") << ",\n";
+
+    os << nesting << ")";
+    if (outputResult.has_value())
+      os << " -> " << *outputResult;
+
+    return os;
+  }
+};
+
+// A helper struct that provides useful information about a kernel after being
+// lowered through the Mojo compiler.
+template <typename FuncOpType>
+struct MojoKernelAdaptor {
+  StringRef originalStructName;
+  // The name of the op it implements.
+  StringRef registeredOpName;
+  // The kernel's entry point.
+  std::optional<MojoKernelFunctionAdaptor<FuncOpType>> executeFunction;
+  // The kernel's optional shape function.
+  std::optional<MojoKernelFunctionAdaptor<FuncOpType>> shapeFunction;
+
+  MojoKernelAdaptor(StringRef structName, FuncOpType execute, FuncOpType shape)
+      : originalStructName(structName),
+        registeredOpName(execute
+                             ->template getAttrOfType<StringAttr>(
+                                 KGEN::MOGGPreElab::kMOGGExecuteFunctionLabel)
+                             .strref()) {
+    if (execute)
+      executeFunction.emplace(execute);
+    if (shape)
+      shapeFunction.emplace(shape);
+  }
+
+  /// TODO: (GEX-1994) Remove this.
+  bool isCoreMOOperation() const { return registeredOpName.starts_with("mo."); }
+};
+
+//===----------------------------------------------------------------------===//
+// Printers
+//===----------------------------------------------------------------------===//
+
+template <typename StreamType, typename FuncOpType>
+StreamType &operator<<(StreamType &os,
+                       const MojoKernelAdaptor<FuncOpType> &adaptor) {
+  os << "@register(" << adaptor.registeredOpName.str() << ")\n";
+  os << "struct " << adaptor.originalStructName.str();
+  if (adaptor.executeFunction.has_value()) {
+    os << "\n";
+    adaptor.executeFunction->printNested(os, "  ");
+  }
+
+  if (adaptor.shapeFunction.has_value()) {
+    os << "\n";
+    adaptor.shapeFunction->printNested(os, "  ");
+  }
+
+  return os;
+}
+
+template <typename StreamType, typename FuncOpType>
+StreamType &operator<<(StreamType &os,
+                       const MojoKernelFunctionAdaptor<FuncOpType> &func) {
+  return func.printNested(os, "");
+}
+
+template <typename StreamType>
+StreamType &operator<<(StreamType &os,
+                       const MojoKernelOperandAdaptor &operand) {
+  return operand.printNested(os, "");
+
+  return os;
+}
+
+template <typename StreamType>
+StreamType &operator<<(StreamType &os, const MojoKernelOperandVariant &var) {
+  std::visit([&](auto &&arg) { os << arg; }, var);
+
+  return os;
+}
+
+template <typename StreamType>
+StreamType &operator<<(StreamType &os,
+                       const MojoKernelOperandSourceDescriptor &desc) {
+  return os << desc.sourceName.str() << ": ";
+}
+
+static inline const char *bool2str(bool val) { return val ? "True" : "False"; }
+
+template <typename StreamType>
+StreamType &operator<<(StreamType &os, const TensorOperandAdaptor &tensor) {
+  os << "Tensor[mut=" << bool2str(tensor.mut)
+     << ", fused=" << bool2str(tensor.fused) << "]";
+  return os;
+}
+
+template <typename StreamType>
+StreamType &operator<<(StreamType &os,
+                       const VariadicTensorOperandAdaptor &tensors) {
+  os << "*" << *reinterpret_cast<const TensorOperandAdaptor *>(&tensors);
+  return os;
+}
+
+template <typename StreamType>
+StreamType &operator<<(StreamType &os, const ListOfTensorOperandAdaptor &list) {
+  os << "List[" << *reinterpret_cast<const TensorOperandAdaptor *>(&list)
+     << "]";
+  return os;
+}
+
+template <typename StreamType>
+StreamType &operator<<(StreamType &os, const ScalarOperandAdaptor &) {
+  os << "Scalar";
+  return os;
+}
+
+template <typename StreamType>
+StreamType &operator<<(StreamType &os,
+                       const DevicesContextPtrOperandAdaptor &) {
+  os << "Context";
+  return os;
+}
+
+template <typename StreamType>
+StreamType &operator<<(StreamType &os,
+                       const DevicesContextPtrListOperandAdaptor &) {
+  os << "*Context";
+  return os;
+}
+
+template <typename StreamType>
+StreamType &operator<<(StreamType &os, const OpaqueOperandAdaptor &opaque) {
+  os << "<" << opaque.typeName.str() << ">";
+  return os;
+}
+
+} // namespace M::KGEN::MOGGPreElab
+
+#endif // MOJO_KERNEL_ADAPTOR_H
