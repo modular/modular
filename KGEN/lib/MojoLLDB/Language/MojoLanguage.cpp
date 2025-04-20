e052db51099649bda1e156cdb6a23a1f2d1416d5
Chris Lattner <lattner@users.noreply.github.com>
2025-04-19 21:19:08 -0700

    [mojo-stdlib] Rewrite `String` internals to not use `List`
    This rewrites the internals of the `String` type to not use `List`,
    as a step towards improving its design.  It should not be behavior
    changing in any significant way.
    
    This does add a `String.resize(n)` method.
    
    MODULAR_ORIG_COMMIT_REV_ID: 1b8a38e858341ef61805fe51495d688df0cb0043

--- KGEN/lib/MojoLLDB/Language/MojoLanguage.cpp
+++ KGEN/lib/MojoLLDB/Language/MojoLanguage.cpp
@@ -51,25 +51,52 @@ builtinStringSummaryProvider(ValueObject &valobj, Stream &stream,
   // If we fail to read the string, we show some placeholder error text.
   // Otherwise, if we return false, for example, LLDB would print the contents
   // of the inner List.
-  auto onError = [&stream]() {
-    stream << "Summary Unavailable";
+  auto onError = [&stream](llvm::StringRef why = {}) {
+    stream << "Summary Unavailable" << why;
     return true;
   };
 
-  std::optional<std::pair<ValueObjectSP, size_t>> parsed =
-      MojoListSyntheticFrontEnd::parseList(
-          valobj.GetChildMemberWithName("_buffer"));
-  if (!parsed)
+  ValueObjectSP sizeField = valobj.GetChildMemberWithName("_len");
+  if (!sizeField || !sizeField->GetError().Success())
+    return onError("; could not find String._len field");
+
+  // The REPL sees a struct around an int, but DWARF shows directly the int.
+  lldb::ValueObjectSP sizeVal =
+      sizeField->IsScalarType() ? sizeField
+                                : sizeField->GetChildMemberWithName("value");
+  if (!sizeVal || !sizeVal->GetError().Success())
     return onError();
 
-  size_t size = parsed->second;
+  bool success = true;
+  size_t size = sizeVal->GetValueAsUnsigned(0, &success);
+  if (!success)
+    return onError();
 
+  // If the size is 0, the data address might be invalid.
   if (size == 0) {
     stream << "\"\"";
     return true;
   }
 
-  ValueObjectSP &dataPointer = parsed->first;
+  // Otherwise, decode the pointer.
+  ValueObjectSP dataVal = valobj.GetChildMemberWithName("_data");
+  if (!dataVal || !dataVal->GetError().Success())
+    return onError("; could not find String._data field");
+
+  // The REPL sees a struct around a pointer, but DWARF shows directly the
+  // pointer.
+  ValueObjectSP dataPointer = dataVal->IsPointerType()
+                                  ? dataVal
+                                  : dataVal->GetChildMemberWithName("address");
+
+  if (!dataPointer || !dataPointer->GetError().Success())
+    return onError();
+
+  lldb::addr_t data = dataPointer->GetPointerValue();
+  if (!data || data == LLDB_INVALID_ADDRESS)
+    return onError();
+
+  // Now that we have the data pointer, dereference to read the string data.
   StringPrinter::ReadBufferAndDumpToStreamOptions options(valobj);
 
   if (summaryOptions.GetCapping() == TypeSummaryCapping::eTypeSummaryCapped) {
