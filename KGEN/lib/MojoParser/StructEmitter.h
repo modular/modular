063bd8e243cc5d7c0c01172441d7b3a12cdaec5d
Chris Lattner <lattner@users.noreply.github.com>
2025-06-29 15:23:42 -0700

    [mojo-lang] Refactor StructEmitter a bit. (#64628)
    This splits StructEmitter into a FunctionEmitter base, which doesn't
    need a struct. This allows StructEmitter to keep track of the
    StructDecl it is working on, allowing us to pass around less
    redundant state.
    
    MODULAR_ORIG_COMMIT_REV_ID: 008d0776df82bcc59a271da9e191fc14909cbad9

--- KGEN/lib/MojoParser/StructEmitter.h
+++ KGEN/lib/MojoParser/StructEmitter.h
@@ -32,52 +32,9 @@ private:
   ValueInfo() {}
 };
 
-class StructEmitter : public SharedStateUser {
+class FunctionEmitter : public SharedStateUser {
 public:
-  StructEmitter(SharedState &sharedState)
-      : SharedStateUser(sharedState), noneType(shared.getNoneType()) {}
-  /// Generate empty stubs for the destructor, copy constructor, and move
-  /// constructor on the declOp if they are eligible and do not already exist.
-  ///
-  /// A struct is eligible for a destructor if one of its fields has a
-  /// destructor. It is possible that none of the fields of a struct have a
-  /// destructor but that struct has an init that allocates heap memory. In this
-  /// case set the forceGenerateDestructor flag to true to force destructor
-  /// generation.
-  std::optional<ValueInfo>
-  addMissingValueMemberStubsToStruct(ASTDecl &structDecl,
-                                     bool forceGenerateDestructor = false);
-
-  /// Create a FnOp within the scope of the given struct and add function
-  /// terminators.
-  std::pair<FnOp, ASTDecl *>
-  addVoidMethod(ASTDecl &structDecl, StringRef prefix, ArrayRef<Type> argTypes,
-                ArrayRef<ArgConvention> argConventions,
-                PogListAttr argListAttrs, SpecialFunctionKind kind,
-                ArrayRef<ParamDeclAttr> params, PogListAttr paramListAttrs);
-  std::pair<FnOp, ASTDecl *>
-  addVoidMethod(ASTDecl &structDecl, StringRef prefix, ArrayRef<Type> argTypes,
-                ArrayRef<ArgConvention> argConventions,
-                PogListAttr argListAttrs, SpecialFunctionKind kind);
-
-  /// Given a struct that has no explicitly defined `__del__` member, define a
-  /// new one with an empty body. This allows the CheckLifetimes pass to insert
-  /// field dels as needed, and makes sure that anything that refers to this
-  /// struct properly runs its destructor.
-  FnOp synthesizeEmptyDtor(ASTDecl &structDecl);
-  /// Add an empty `__moveinit__` or `__copyinit__` stub for this struct, to be
-  /// filled in later.
-  FnOp synthesizeEmptyMoveOrCopyInit(ASTDecl &structDecl, bool isMove);
-  /// Populate the function with a field by field copy. This will fail if the
-  /// given function does not have the expected signature.
-  LogicalResult populateMoveCopy(ASTDecl &fnDecl, bool isMove);
-
-  /// Add `copy()` method for this struct.
-  FnOp synthesizeEmptyExplicitCopy(ASTDecl &structDecl);
-  void populateExplicitCopy(ASTDecl &fnDecl);
-
-  /// Add a attribute initializer method for this struct with a body.
-  FnOp synthesizeFieldwiseInit(ASTDecl &structDecl);
+  FunctionEmitter(SharedState &shared) : SharedStateUser(shared) {}
 
   /// Emit an empty function stub at the specified location. The block arguments
   /// are added to the body of the function but no ops are added to the body.
@@ -91,6 +48,57 @@ public:
       ImplicitLocOpBuilder &builder, FnEffects fnEffects = FnEffects(),
       StringRef suffix = "", bool synthetic = true,
       InlineLevel inlineLevel = InlineLevel::Automatic);
+};
+
+class StructEmitter : public FunctionEmitter {
+public:
+  // The struct we're working on.
+  ASTDecl &structDecl;
+  StructDeclOp structDeclOp;
+
+  StructEmitter(ASTDecl &structDecl);
+
+  /// Generate empty stubs for the destructor, copy constructor, and move
+  /// constructor on the declOp if they are eligible and do not already exist.
+  ///
+  /// A struct is eligible for a destructor if one of its fields has a
+  /// destructor. It is possible that none of the fields of a struct have a
+  /// destructor but that struct has an init that allocates heap memory. In this
+  /// case set the forceGenerateDestructor flag to true to force destructor
+  /// generation.
+  std::optional<ValueInfo>
+  addMissingValueMemberStubsToStruct(bool forceGenerateDestructor = false);
+
+  /// Create a FnOp within the scope of the given struct and add function
+  /// terminators.
+  std::pair<FnOp, ASTDecl *>
+  addVoidMethod(StringRef prefix, ArrayRef<Type> argTypes,
+                ArrayRef<ArgConvention> argConventions,
+                PogListAttr argListAttrs, SpecialFunctionKind kind,
+                ArrayRef<ParamDeclAttr> params, PogListAttr paramListAttrs);
+  std::pair<FnOp, ASTDecl *>
+  addVoidMethod(StringRef prefix, ArrayRef<Type> argTypes,
+                ArrayRef<ArgConvention> argConventions,
+                PogListAttr argListAttrs, SpecialFunctionKind kind);
+
+  /// Given a struct that has no explicitly defined `__del__` member, define a
+  /// new one with an empty body. This allows the CheckLifetimes pass to insert
+  /// field dels as needed, and makes sure that anything that refers to this
+  /// struct properly runs its destructor.
+  FnOp synthesizeEmptyDtor();
+  /// Add an empty `__moveinit__` or `__copyinit__` stub for this struct, to be
+  /// filled in later.
+  FnOp synthesizeEmptyMoveOrCopyInit(bool isMove);
+  /// Populate the function with a field by field copy. This will fail if the
+  /// given function does not have the expected signature.
+  LogicalResult populateMoveCopy(ASTDecl &fnDecl, bool isMove);
+
+  /// Add `copy()` method for this struct.
+  FnOp synthesizeEmptyExplicitCopy(ASTDecl &structDecl);
+  void populateExplicitCopy(ASTDecl &fnDecl);
+
+  /// Add a attribute initializer method for this struct with a body.
+  FnOp synthesizeFieldwiseInit();
 
   /// This synthesizes an __init__ method that accepts values for every field of
   /// a struct, making it easy for external clients to initialize it.
@@ -98,7 +106,7 @@ public:
   /// method for memory-only types where not all fields are initialized, though
   /// this requires manual modification of the returned FnOp to initialize any
   /// omitted fields.
-  FnOp synthesizeFieldwiseInit(ASTDecl &structDecl, ArrayRef<Type> argTypes,
+  FnOp synthesizeFieldwiseInit(ArrayRef<Type> argTypes,
                                ArrayRef<ArgConvention> argConventions,
                                PogListAttr argListAttrs,
                                // None or Self if register passable.
@@ -110,31 +118,17 @@ public:
       StringRef name, ArrayRef<ParamDeclAttr> params,
       PogListAttr paramListAttrs, ArrayRef<Type> argTypes,
       ArrayRef<ArgConvention> argConventions, PogListAttr argListAttrs,
-      Type resultType, ASTDecl &structDecl, SMLoc loc,
+      Type resultType,
       SpecialFunctionKind specialFnID = SpecialFunctionKind::kNormal,
       FnEffects fnEffects = FnEffects(), StringRef suffix = "",
       bool synthetic = true);
   std::pair<FnOp, ASTDecl *> synthesizeMethodInStruct(
       StringRef name, ArrayRef<Type> argTypes,
       ArrayRef<ArgConvention> argConventions, PogListAttr argListAttrs,
-      Type resultType, ASTDecl &structDecl, SMLoc loc,
+      Type resultType,
       SpecialFunctionKind specialFnID = SpecialFunctionKind::kNormal,
       FnEffects fnEffects = FnEffects(), StringRef suffix = "",
       bool synthetic = true);
-
-private:
-  FnOp createFunction(ASTDecl &parent, StringRef name,
-                      ArrayRef<ParamDeclAttr> params,
-                      PogListAttr paramListAttrs, ArrayRef<Type> argTypes,
-                      ArrayRef<ArgConvention> argConventions,
-                      PogListAttr argListAttrs, Type resultType,
-                      SpecialFunctionKind specialFnID, SMLoc loc,
-                      ImplicitLocOpBuilder &builder, FnEffects fnEffects,
-                      StringRef suffix, bool synthetic,
-                      InlineLevel inlineLevel);
-
-protected:
-  Type noneType;
 };
 
 } // namespace M::KGEN::LIT
