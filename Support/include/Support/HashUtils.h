c1dca7ca0c981d02c8a444ef2facf7141ff9bad8
Spenser Bauman <spenser@modular.com>
2025-06-30 21:13:41 +0000

    [MOGG] Use parallel implementation of bytecode hasher in MojoLibraryAnalysis (#64677)
    MojoLibraryAnalysis spends a non-trivial amount of time to hash its
    contents in JitCompileKernels. The IR module held by MojoLibraryAnalys
    is large and the bytecode serialization process is expensive.
    
    This change adds a bytecode-based hashing procedure which processes the
    contents of an MLIR ModuleOp in parallel and then combines the results
    into a single hash. This takes the time required to has the standard
    library from ~500ms down to ~85ms.
    
    MODULAR_ORIG_COMMIT_REV_ID: 558edab3d2efed9b44887c31443cb79612fd7e64

--- Support/include/Support/HashUtils.h
+++ Support/include/Support/HashUtils.h
@@ -8,7 +8,8 @@
 #define SUPPORT_HASHUTILS_H
 
 #include "Support/LogicalResult.h"
-#include "llvm/ADT/Hashing.h"
+
+#include "mlir/IR/BuiltinOps.h"
 
 namespace mlir {
 class Operation;
@@ -22,6 +23,11 @@ namespace M {
 /// This is useful for deduping operations in a stable way, without relying on
 /// in-memory values (e.g. pointers).
 FailureOr<std::string> getBytecodeHash(mlir::Operation *op);
+
+/// This function computes the bytecode hash similar to getBytecodeHash, but
+/// does so by hashing each individual operation in the module in parallel
+/// (via getBytecodeHash) and then combining the results into a single hash.
+FailureOr<std::string> getModuleBytecodeHash(mlir::ModuleOp module);
 
 } // namespace M
 #endif // SUPPORT_HASHUTILS_H
