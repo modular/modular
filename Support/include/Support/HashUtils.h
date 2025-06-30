1e63bcf39e151436e4f0cff7b53fd2a845f0c486
Spenser Bauman <spenser@modular.com>
2025-06-30 13:08:26 +0000

    [Support][NFC] Remove hashBlockImpl (#64608)
    This function is unused in the codebase now. The declaration was
    removed, this just gets rid of the corresponding definition.
    
    MODULAR_ORIG_COMMIT_REV_ID: 192a0bb936b5a8574a2fb064439cba086fde29f4

--- Support/include/Support/HashUtils.h
+++ Support/include/Support/HashUtils.h
@@ -11,16 +11,10 @@
 #include "llvm/ADT/Hashing.h"
 
 namespace mlir {
-class Block;
 class Operation;
 } // namespace mlir
 
 namespace M {
-
-// Returns a unique hash for a given block. Values will be hashed only by the
-// operation which returns them and the type. So two blocks with one operation
-// will return the same hash, even though the value is a different pointer.
-llvm::hash_code hashBlock(mlir::Block &block);
 
 /// This function serializes an operation to MLIR bytecode and hashes the result
 /// using XXH3's 128-bit variant and then returns the result as a hex string.
