98af9ed7728c2f523efa6d62d26c39adda261919
Spenser Bauman <spenser@modular.com>
2025-07-03 17:15:17 +0000

    [Support][NFC] Refactor HashUtils to expose raw_xxhash_stream (#64777)
    This hash tooling is needed for work on hashing the contents of the
    MojoLibraryAnalysis.
    
    MODULAR_ORIG_COMMIT_REV_ID: 079438377db6daf57ddadc324d1dd9aa605a9592

--- Support/include/Support/HashUtils.h
+++ Support/include/Support/HashUtils.h
@@ -10,12 +10,33 @@
 #include "Support/LogicalResult.h"
 
 #include "mlir/IR/BuiltinOps.h"
+#include "llvm/Support/raw_ostream.h"
+
+#include "xxh3.h"
 
 namespace mlir {
 class Operation;
 } // namespace mlir
 
 namespace M {
+
+/// A raw_ostream that hash the content using the xxhash algorithm.
+class raw_xxhash_stream : public llvm::raw_ostream {
+  XXH3_state_t State;
+
+  /// See raw_ostream::write_impl.
+  void write_impl(const char *Ptr, size_t Size) override {
+    XXH3_128bits_update(&State, (void *)Ptr, Size);
+  }
+
+public:
+  raw_xxhash_stream();
+
+  std::array<uint8_t, 16> hash();
+  std::string hashString();
+
+  uint64_t current_pos() const override { return 0; }
+};
 
 /// This function serializes an operation to MLIR bytecode and hashes the result
 /// using XXH3's 128-bit variant and then returns the result as a hex string.
--- Support/lib/HashUtils.cpp
+++ Support/lib/HashUtils.cpp
@@ -24,32 +24,25 @@
 
 using namespace mlir;
 
-namespace {
-/// A raw_ostream that hash the content using the xxhash algorithm.
-class raw_xxhash_stream : public raw_ostream {
-  XXH3_state_t State;
+M::raw_xxhash_stream::raw_xxhash_stream() : raw_ostream() {
+  XXH3_128bits_reset(&State);
+}
 
-  /// See raw_ostream::write_impl.
-  void write_impl(const char *Ptr, size_t Size) override {
-    XXH3_128bits_update(&State, (void *)Ptr, Size);
-  }
+std::array<uint8_t, 16> M::raw_xxhash_stream::hash() {
+  flush();
 
-public:
-  raw_xxhash_stream() : raw_ostream() { XXH3_128bits_reset(&State); }
+  XXH128_hash_t digest = XXH3_128bits_digest(&State);
+  std::array<uint8_t, 16> result;
+  memcpy(&result[0], (void *)&digest.low64, 8);
+  memcpy(&result[8], (void *)&digest.high64, 8);
+  return result;
+}
 
-  std::array<uint8_t, 16> hash() {
-    flush();
-
-    XXH128_hash_t digest = XXH3_128bits_digest(&State);
-    std::array<uint8_t, 16> result;
-    memcpy(&result[0], (void *)&digest.low64, 8);
-    memcpy(&result[8], (void *)&digest.high64, 8);
-    return result;
-  }
-
-  uint64_t current_pos() const override { return 0; }
-};
-} // namespace
+std::string M::raw_xxhash_stream::hashString() {
+  SmallString<32> output;
+  llvm::toHex(hash(), /*LowerCase=*/true, output);
+  return std::string(output);
+}
 
 FailureOr<std::string> M::getBytecodeHash(mlir::Operation *op) {
   auto unknownLoc = UnknownLoc::get(op->getContext());
@@ -72,9 +65,7 @@ FailureOr<std::string> M::getBytecodeHash(mlir::Operation *op) {
   if (failed(mlir::writeBytecodeToFile(op, ostream, config)))
     return op->emitError("Failed to write bytecode");
 
-  SmallString<32> output;
-  llvm::toHex(ostream.hash(), /*LowerCase=*/true, output);
-  return std::string(output);
+  return ostream.hashString();
 }
 
 FailureOr<std::string> M::getModuleBytecodeHash(mlir::ModuleOp module) {
@@ -98,9 +89,7 @@ FailureOr<std::string> M::getModuleBytecodeHash(mlir::ModuleOp module) {
     for (const CacheT &cache : caches)
       llvm::interleave(cache, hasher, "");
 
-    SmallString<32> output;
-    llvm::toHex(hasher.hash(), /*LowerCase=*/true, output);
-    original.push_back(std::string(output));
+    return hasher.hashString();
   };
 
   CacheT resultCache;
