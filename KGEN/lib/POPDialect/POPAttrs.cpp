a1f5ecda049a40d513d194206f3bad0b10275dd6
Tracy Sharpe <99041446+raiseirql@users.noreply.github.com>
2025-08-05 15:20:18 -0700

    [mojo-stdlib] Add test content for float literal->float8 conversions
    Enable the E5M2 test and add tests for E4M3FNUZ/E5M2FNUZ to test a
    compiler fix for float literal->float8 initializers.
    
    MODULAR_ORIG_COMMIT_REV_ID: 47ceaa030a05d3f446f7855c050a2b3fb8cb55ba

--- KGEN/lib/POPDialect/POPAttrs.cpp
+++ KGEN/lib/POPDialect/POPAttrs.cpp
@@ -813,11 +813,7 @@ static APInt
 floatLiteralConvertGetBitstring(const IPRational &input,
                                 const llvm::fltSemantics &fltSemantics) {
   unsigned totalLength = APFloat::getSizeInBits(fltSemantics);
-  // For semantics without inf e.g. e4m3fn, the max exponent is bias + 1
-  // since inf (all ones for exponent) is used for values.
-  unsigned bias = APFloat::semanticsHasInf(fltSemantics)
-                      ? APFloat::semanticsMaxExponent(fltSemantics)
-                      : APFloat::semanticsMaxExponent(fltSemantics) - 1;
+  unsigned bias = 1 - APFloat::semanticsMinExponent(fltSemantics);
   unsigned exponentLength =
       llvm::Log2_64(APFloat::semanticsMaxExponent(fltSemantics) -
                     APFloat::semanticsMinExponent(fltSemantics) + 3);
