680ff76f4249dd752157ffd3fcd104e073d58e4f
Keith Smiley <keithbsmiley@gmail.com>
2025-09-03 11:11:37 -0700

    [crash-test-dummy] Always setup crashpad-handler (#68300)
    This is required for testing with this CLI.
    
    MODULAR_ORIG_COMMIT_REV_ID: 168fe28452d78ef698c38ac1658f0fc61e105ac3

--- AsyncRT/tools/crash-test-dummy/crash-test-dummy.cpp
+++ AsyncRT/tools/crash-test-dummy/crash-test-dummy.cpp
@@ -23,6 +23,9 @@ int main(int argc, char **argv) {
   CLOptions clOptions;
   llvm::cl::ParseCommandLineOptions(argc, argv, "Modular Crash Test Dummy");
 
+  // TODO: Remove when https://github.com/bazelbuild/bazel/pull/26887 is fixed.
+  setenv("MODULAR_CRASH_REPORTING_ENABLED", "true", 1);
+
   auto ctxOr = Init::createContext("crash-test-dummy");
   if (ctxOr.isError()) {
     llvm::errs() << "could not create context: " << ctxOr.getError() << "\n";
