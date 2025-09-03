28153eead45557e9ec2d677154c82449972cbbc2
Alex Trotta <44127594+Ahajha@users.noreply.github.com>
2025-09-03 18:29:59 -0400

    [KGEN] Simplify CompilationContext::print (#68164)
    MODULAR_ORIG_COMMIT_REV_ID: d45ce90d24ee3201613a923a10461d617ec91fbb

--- KGEN/lib/KGENDialect/KGENCompilationContext.cpp
+++ KGEN/lib/KGENDialect/KGENCompilationContext.cpp
@@ -15,23 +15,7 @@ void CompilationContext::print(llvm::raw_ostream &os) const {
   for (auto entry : mojoDefines) {
     auto k = entry.first;
     os << k << '=';
-    std::visit(
-        [&](auto &&v) {
-          using T = std::decay_t<decltype(v)>;
-          if constexpr (std::is_same_v<T, bool>) {
-            os << v;
-          } else if constexpr (std::is_same_v<T, int>) {
-            os << v;
-          } else if constexpr (std::is_same_v<T, std::string>) {
-            os << v;
-          } else {
-            // NOTE: This should be a static_assert, but that breaks in torch
-            // compile tests on some mac devices.
-            assert("non-exhaustive visitor!");
-          }
-          os << ';';
-        },
-        entry.second);
+    std::visit([&](auto &&v) { os << v << ';'; }, entry.second);
   }
 }
 
