d676e8fae5b3008e6688a2ecdca8695ef4dcb3c5
Ross Campbell <rosscampbell@devhw34.guest.codethink.co.uk>
2025-10-17 17:28:08 +0100

    helpers for testing

--- max/python/max/pipelines/architectures/test_model.py
+++ max/python/max/pipelines/architectures/test_model.py
@@ -6,9 +6,11 @@ from openai import OpenAI
 
 
 def test_text(model_name: str, base_api_url: str):
+    #base_api_url += "/chat/completions"
     print("-- testing text request")
+    print(f"-- target host: {base_api_url} | {model_name}")
     client = OpenAI(base_url=base_api_url, api_key="not_required")
-    response = client.chat.completions.create(
+    completion = client.chat.completions.create(
         model=model_name,
         messages=[
             {
@@ -17,13 +19,13 @@ def test_text(model_name: str, base_api_url: str):
             },
         ],
     )
-    print(response.choices[0].message.content)
+    print(completion.choices[0].message.content)
 
 
 def test_vision(model_name: str, base_api_url: str, image_url: str = "https://gift-a-tree.com/wp-content/uploads/2021/10/Oak-tree-1.jpg"):
     print(f"-- testing vision request with sample image from {image_url}")
     client = OpenAI(base_url=base_api_url, api_key="not_required")
-    response = client.chat.completions.create(
+    completions = client.chat.completions.create(
         model=model_name,
         messages=[
             {
@@ -42,8 +44,9 @@ def test_vision(model_name: str, base_api_url: str, image_url: str = "https://gi
                 ]
             },
         ],
+        # reponse_format={"type": "json_object"}
     )
-    print(response.choices[0].message.content)
+    print(completions.choices[0].message.content)
 
 
 # https://docs.modular.com/max/max-cli#max-serve
@@ -52,12 +55,19 @@ def serve(model_name: str, weight_path: str, custom_architecture: str, serve_loc
     cmd = f"max serve --model {model_name}"
     if custom_architecture:
         cmd += f" --custom-architectures {custom_architecture}"
-    if not serve_local: # if running on DevHW, use GPU
-        cmd += f" --devices=gpu"
     else: # if running locally, use CPU
         cmd += " --devices=cpu"
         if weight_path:
             cmd += f" --weight-path {weight_path}"
+    print(f"-- running: {cmd}")
+    os.system(cmd)
+
+
+def generate(model_name: str, custom_architecture: str, prompt: str):
+    print(f"-- generating with {model_name}, this may take a moment")
+    cmd = f"max generate --model {model_name} --prompt \"{prompt}\"" # --max-new-tokens 256 --temperature 0.7"
+    if custom_architecture:
+        cmd += f" --custom-architectures {custom_architecture}"
     print(f"-- running: {cmd}")
     os.system(cmd)
 
@@ -84,6 +94,11 @@ def main():
         action='store_true',
         help='start the model server on designated host'
     )
+    mode_group.add_argument(
+        '--generate',
+        action='store_true',
+        help='run inference and then shut down the model when done'
+    )
 
     parser.add_argument(
         '--model',
@@ -100,7 +115,7 @@ def main():
     parser.add_argument(
         '--port',
         type=str,
-        default='8001',
+        default='8000',
         help='hostname of DevHW'
     )
     parser.add_argument(
@@ -126,6 +141,8 @@ def main():
         test_vision(model_name=args.model, base_api_url=base_api_url)
     elif args.serve:
         serve(model_name=args.model, weight_path=args.weights, custom_architecture=args.architecture, serve_local=False)
+    elif args.generate:
+        generate(model_name=args.model, custom_architecture=args.architecture, prompt="What is trustable software?")
 
 
 if __name__ == "__main__":
