944e80ba034d89c1279b62df0eaf6a72a01f3c3f
Charlie Barto <cbarto@modular.com>
2025-10-19 18:59:55 -0700

    [stdlib] Indirect KGEN_CompilerRT_AlignedAlloc through a pointer
    In order to support ASAN we need to call the system allocator in asan
    mode. And, since the CompilerRT allocates memory that must be freed by
    mojo we need to add a layer of indirection to
    `KGEN_CompilerRT_AlignedAlloc` and `KGEN_CompilerRT_AlignedFree`
    
    We do this by:
    * adding a KGEN_Allocators global structure with the allocation and
    deallocation pointers. Initialized to TCMalloc's new/delete by default.
    * swtich the parameter order on AsyncRT::TCMallocGlobals::tc_new to
    match system aligned_alloc/memalign.
    
    MODULAR_ORIG_COMMIT_REV_ID: 7935b12a589fd7500b957458861e8db4d9b029e7

--- KGEN/lib/CompilerRT/Memory.cpp
+++ KGEN/lib/CompilerRT/Memory.cpp
@@ -8,8 +8,15 @@
 #include "AsyncRT/Runtime/Globals/Globals.h"
 #include "AsyncRT/Runtime/Runtime.h"
 #include "Support/SymbolExport.h"
-
+#include <malloc.h>
 using namespace M;
+
+namespace {
+struct {
+  void *(*alloc)(size_t, size_t) = AsyncRT::TCMallocGlobals::tc_new;
+  void (*free)(void *) = AsyncRT::TCMallocGlobals::tc_delete;
+} constinit static KGEN_Allocators{};
+} // namespace
 
 /// Returns an alignment allocated memory. If the alignment value is not
 /// positive, then the default alignment is used.
@@ -17,11 +24,11 @@ COMPILERRT_EXPORT COMPILERRT_VISIBILITY_EXPORT void *
 KGEN_CompilerRT_AlignedAlloc(ssize_t alignment, ssize_t size) {
   if (alignment <= 0)
     alignment = kPreferredMemoryAlignment;
-  return AsyncRT::TCMallocGlobals::tc_new(size, alignment);
+  return KGEN_Allocators.alloc(size, alignment);
 }
 
 /// Frees memory allocated via KGEN_CompilerRT_AlignedAlloc.
 COMPILERRT_EXPORT COMPILERRT_VISIBILITY_EXPORT void
 KGEN_CompilerRT_AlignedFree(void *ptr) {
-  return AsyncRT::TCMallocGlobals::tc_delete(ptr);
+  return KGEN_Allocators.free(ptr);
 }
