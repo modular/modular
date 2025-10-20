7d0681d1bbc105be24c06535db269b11375243d0
Alex Trotta <44127594+Ahajha@users.noreply.github.com>
2025-10-20 00:38:42 -0400

    Revert "[stdlib] Indirect KGEN_CompilerRT_AlignedAlloc through a pointer" (#70770)
    Reverts [Internal link]
    
    This broke the Mac build:
    [Internal link]
    
    Not sure if the fix is simple, reverting for now to fix the build.
    
    MODULAR_ORIG_COMMIT_REV_ID: 0bd1c5986be90a0e6a3369b0bc24de761a48d4e0

--- KGEN/lib/CompilerRT/Memory.cpp
+++ KGEN/lib/CompilerRT/Memory.cpp
@@ -8,15 +8,8 @@
 #include "AsyncRT/Runtime/Globals/Globals.h"
 #include "AsyncRT/Runtime/Runtime.h"
 #include "Support/SymbolExport.h"
-#include <malloc.h>
-using namespace M;
 
-namespace {
-struct {
-  void *(*alloc)(size_t, size_t) = AsyncRT::TCMallocGlobals::tc_new;
-  void (*free)(void *) = AsyncRT::TCMallocGlobals::tc_delete;
-} constinit static KGEN_Allocators{};
-} // namespace
+using namespace M;
 
 /// Returns an alignment allocated memory. If the alignment value is not
 /// positive, then the default alignment is used.
@@ -24,11 +17,11 @@ COMPILERRT_EXPORT COMPILERRT_VISIBILITY_EXPORT void *
 KGEN_CompilerRT_AlignedAlloc(ssize_t alignment, ssize_t size) {
   if (alignment <= 0)
     alignment = kPreferredMemoryAlignment;
-  return KGEN_Allocators.alloc(size, alignment);
+  return AsyncRT::TCMallocGlobals::tc_new(size, alignment);
 }
 
 /// Frees memory allocated via KGEN_CompilerRT_AlignedAlloc.
 COMPILERRT_EXPORT COMPILERRT_VISIBILITY_EXPORT void
 KGEN_CompilerRT_AlignedFree(void *ptr) {
-  return KGEN_Allocators.free(ptr);
+  return AsyncRT::TCMallocGlobals::tc_delete(ptr);
 }
