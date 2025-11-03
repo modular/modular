b18c4b53baff14392acbfc40426d5eb23fea1feb
weiwei chen <weiwei.chen@modular.com>
2025-11-03 11:39:54 -0500

    [KGEN] Add an llvm IR pass to set function attributes. (#71560)
    - [x] Add an LLVM IR pass to set function `denormal-fp-math-f32`
    attribute based on cl option value. Clang does similar thing for
    `-fdenormal-fp-math-f32`
    https://github.com/llvm/llvm-project/blob/cc271437553452ede002d871d32abc02084341a8/clang/lib/CodeGen/CGCall.cpp#L1940-L1948.
    
    Stacked PRs:
     * #71561
     * __->__#71560
     * #71559
    
    --- --- ---
    
    ### [KGEN] Add an llvm IR pass to set function attributes.
    
    MODULAR_ORIG_COMMIT_REV_ID: 2772b288e2cee99996695a33bec34aaa0e960f0e

---
+++ KGEN/lib/Compiler/ObjectCompiler/LLVM/Transforms/SetFunctionAttributes.h
@@ -0,0 +1,35 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef KGEN_COMPILER_LLVMIR_TRANSFORMS_SETFUNCTIONATTRIBUTES_H
+#define KGEN_COMPILER_LLVMIR_TRANSFORMS_SETFUNCTIONATTRIBUTES_H
+
+#include "llvm/IR/PassManager.h"
+#include "llvm/Support/CommandLine.h"
+
+namespace llvm {
+class Module;
+} // namespace llvm
+
+namespace M::KGEN {
+
+/// Pass to set some function attributes that are needed for compilation.
+class SetFunctionAttributes
+    : public llvm::PassInfoMixin<SetFunctionAttributes> {
+public:
+  llvm::PreservedAnalyses run(llvm::Module &M,
+                              llvm::ModuleAnalysisManager &MAM);
+
+  static llvm::StringRef name() { return "KGEN::SetFunctionAttributes"; }
+
+private:
+  void runImpl(llvm::Module &M,
+               const llvm::StringMap<llvm::cl::Option *> &options);
+};
+
+} // namespace M::KGEN
+
+#endif // KGEN_COMPILER_LLVMIR_TRANSFORMS_SETFUNCTIONATTRIBUTES_H
