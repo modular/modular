a4fb3fdabb209f71f1c8a715de87129bb452ae15
Joshua Peterson <josh@modular.com>
2025-11-07 15:35:52 -0500

    [AsyncRT] Remove an unnecessary const qualifier (#71970)
    The const on a return-by-value has no effect since the returned value is
    a copy, which is why the compiler was warning about it with
    `-Wignored-qualifiers`.
    
    MODULAR_ORIG_COMMIT_REV_ID: 7782d44c2b6629f08564459328ab614c288c44f0

--- AsyncRT/include/AsyncRT/Support/ConcurrentMPMCQueue.h
+++ AsyncRT/include/AsyncRT/Support/ConcurrentMPMCQueue.h
@@ -109,9 +109,7 @@ struct thread_id_converter<thread_id_t> {
 namespace MoodyCamel {
 namespace Details {
 typedef std::uintptr_t thread_id_t;
-static const thread_id_t invalid_thread_id() {
-  return 0;
-} // Address can't be nullptr
+static thread_id_t invalid_thread_id() { return 0; } // Address can't be nullptr
 static const thread_id_t invalid_thread_id2 =
     1; // Member accesses off a null pointer are also generally invalid. Plus
        // it's not aligned.
