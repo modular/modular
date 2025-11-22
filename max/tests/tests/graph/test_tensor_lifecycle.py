8990a51e8a3cb7e298b4ad50f6fcbee60ce0ab35
Yicheng Wu <yichengdwu@outlook.com>
2025-11-22 15:39:48 -0700

    add test
    Signed-off-by: Yicheng Wu <yichengdwu@outlook.com>

---
+++ max/tests/tests/graph/test_tensor_lifecycle.py
@@ -0,0 +1,48 @@
+# ===----------------------------------------------------------------------=== #
+# Copyright (c) 2025, Modular Inc. All rights reserved.
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions:
+# https://llvm.org/LICENSE.txt
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===----------------------------------------------------------------------=== #
+"""Tests for tensor lifecycle and garbage collection."""
+
+import gc
+import weakref
+
+from max.experimental.tensor import Tensor
+
+
+def test_anonymous_tensors():
+    def build_graph():
+        return Tensor.ones((2, 2)) + 3.0
+
+    z = build_graph()
+    gc.collect()
+
+    val = str(z[0, 0].item())
+    assert val == "4.0"
+
+
+def test_tensor_lifecycle_garbage_collection():
+    t = Tensor.ones((10, 10))
+    t_ref = weakref.ref(t)
+
+    # Trigger execution. 't' is migrated to the fresh graph.
+    str(t + 1.0)
+
+    del t
+    gc.collect()
+
+    # 't' is currently held by the idle graph's safety list.
+    # Trigger one more execution to flush the list.
+    t = Tensor.zeros((1,))
+    str(t)
+    gc.collect()
+
+    assert t_ref() is None
