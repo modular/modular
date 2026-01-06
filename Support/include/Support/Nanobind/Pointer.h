c96a1ba1eb773b3b4b115a4951c5f15188dcdb50
Feras Boulala <feras.boulala@modular.com>
2026-01-06 14:01:59 -0500

    [MAX] Avoid using C Bindings
    MODULAR_ORIG_COMMIT_REV_ID: 609da04379dc9d9506e266ef70388759c3bb99b2

---
+++ Support/include/Support/Nanobind/Pointer.h
@@ -0,0 +1,45 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+
+#include "mlir/IR/Attributes.h"
+#include "mlir/IR/Builders.h"
+#include "mlir/IR/BuiltinAttributes.h"
+#include "mlir/IR/Location.h"
+#include "mlir/IR/OpDefinition.h"
+#include "mlir/IR/Operation.h"
+#include "mlir/IR/SymbolTable.h"
+#include "mlir/IR/Types.h"
+#include "mlir/IR/Value.h"
+#include "mlir/IR/Verifier.h"
+#include "nanobind/nanobind.h"
+#include "nanobind/operators.h"
+#include "nanobind/stl/filesystem.h"
+#include "nanobind/stl/shared_ptr.h"
+#include "nanobind/typing.h"
+
+namespace nb = nanobind;
+
+namespace M::Graph::Python {
+
+// Returning mlir::Operation* from nanobind is annoying and error prone for a
+// few reasons.
+//  - Pointer values are assumed to be passing ownership to Python, while
+//  mlir::Operation* pointers are used as non-owning references
+//  - Operation types don't actually subclass mlir::Operation! They actually
+//  subclass mlir::OpState, but in C++ they may be cast via `mlir::dyn_cast`.
+//  - In order for nanobind to recognize and downcast the op automatically it
+//  needs to follow the correct inheritance. This means bound methods should
+//  always return OpState objects rather than Operation pointers.
+//  - There's no simple way to get an OpState from an Operation*, only the
+//  other way around. This implementation relies on the memory layout of
+//  OpState to cast directly.
+inline nb::handle_t<mlir::OpState> castOp(mlir::Operation *op) {
+  return nb::handle_t<mlir::OpState>(
+      nb::cast(std::make_unique<mlir::OpState>(*(mlir::OpState *)&op))
+          .release());
+}
+
+} // namespace M::Graph::Python
