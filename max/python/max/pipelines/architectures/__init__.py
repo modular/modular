6d0de4f74d1ac58910a4b6643ac7da751b53e4f9
Tolga Cangöz <mtcangoz@gmail.com>
2026-01-11 20:34:51 +0300

    Improve the template

--- max/python/max/pipelines/architectures/__init__.py
+++ max/python/max/pipelines/architectures/__init__.py
@@ -55,6 +55,7 @@ def register_all_models() -> None:
     from .llama3 import llama_arch
     from .llama3_modulev3 import llama_modulev3_arch
     from .llama4 import llama4_arch
+    from .ltx2_module_v3 import ltx2_module_v3_arch
     from .mistral import mistral_arch
     from .mistral3 import mistral3_arch
     from .mpnet import mpnet_arch
@@ -71,7 +72,6 @@ def register_all_models() -> None:
     from .qwen3_embedding import qwen3_embedding_arch
     from .qwen3vl_moe import qwen3vl_arch, qwen3vl_moe_arch
     from .qwen3vl_moe import qwen3vl_moe_arch
-    from .ltx2_module_v3 import ltx2_module_v3_arch
 
     architectures = [
         exaone_arch,
