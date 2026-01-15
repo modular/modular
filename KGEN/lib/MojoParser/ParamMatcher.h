7e22ec701f88b6744211e8f18afc5ccd9b1594f0
Peiming Liu <peiming@modular.com>
2026-01-15 11:54:55 -0800

    [ParamInf] move ParamMatcher into its own file (NFC) (#75466)
    The file is getting bigger. Besides, these two classes are doing
    logically separable stuff.
    
    MODULAR_ORIG_COMMIT_REV_ID: 64b9c62dda6a9ded8b6adb5935ac4a2087d49b8e

---
+++ KGEN/lib/MojoParser/ParamMatcher.h
@@ -0,0 +1,159 @@
+//===----------------------------------------------------------------------===//
+//
+// This file is Modular Inc proprietary.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file provides the ParamMatcher for Mojo parsers to "co-iterate" two
+// types/parameters in order to extract parameter value to be inferred.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef KGEN_MOJOPARSER_PARAMMATCHER_H
+#define KGEN_MOJOPARSER_PARAMMATCHER_H
+
+#include "ParamInf.h"
+
+#include "KGEN/LITDialect/LITTypes.h"
+#include "KGEN/MojoParser/ASTType.h"
+#include "Support/ADT/SmartVariant.h"
+
+namespace M::KGEN::LIT {
+
+class ExprNode;
+class ParamInf;
+class SharedState;
+
+//===----------------------------------------------------------------------===//
+// MatchFailure
+//===----------------------------------------------------------------------===//
+
+/// These are the different failure modes that we know happen.
+struct MatchFailure {
+  /// This failure happens when a parameter is found of the wrong type.
+  struct TypeConflict {
+    size_t paramIdx; // TODO: Render this name.
+    ASTType paramType, argParamType;
+  };
+
+  /// This failure happens when a parameter is inferred to two different values.
+  struct ValueConflict {
+    size_t paramIdx;
+    TypedAttr v1, v2;
+  };
+
+  /// This failure happens when merge* is called, but the expected type/value
+  /// still has an unresolved dependent type which can't be inferred.
+  struct DependsOnUnresolved {
+    size_t paramIdx;
+  };
+
+  /// This failure happens when parameter is inferred, yet the constraint
+  /// attached on it can not be proved.
+  struct UnprovableConstraints {
+    size_t paramIdx;
+  };
+
+  /// This failure hasn't been categorized yet.
+  /// FIXME: Remove this.
+  struct Unclassified {};
+
+  template <typename Failure>
+  MatchFailure(Failure info) : info(info) {}
+
+  // Describe what went wrong.
+  void addExplanation(MojoInflightDiag &diag) const;
+
+  /// If this failure is due to an unresolved parameter, return the index of the
+  /// parameter.
+  std::optional<size_t> getIfDependentOnUnresolved() const {
+    if (isa<DependsOnUnresolved>(info)) {
+      return cast<DependsOnUnresolved>(info).paramIdx;
+    }
+    return std::nullopt;
+  }
+
+private:
+  SmartVariant<TypeConflict, ValueConflict, DependsOnUnresolved, Unclassified,
+               UnprovableConstraints>
+      info;
+};
+
+/// This class implements logic for match parameters between an actual value
+/// present at a call site, and expected value in the callee.  The former value
+/// is always concrete, but the later may contain symbolic parameters from the
+/// callees signature that we're trying to infer.  It is also possible this
+/// candidate is completely invalid!  The result of match is one of several
+/// cases:
+///
+/// - Match: the parameters match.
+/// - Error: the parameters do not match.  The error code is set to indicate
+///   the first reason.
+/// - Retry: the parameters matched and led to a parameter getting inferred! The
+///   parameter # is set to indicate which one.
+///
+/// You might wonder why we need to retry matching from a root when inferring a
+/// parameter.  It turns out that some values can only be matched after
+/// simplification.  Consider a situation like:
+///
+///    struct S[a: Int, b: Int]:
+///    fn take[v: Int](s: S[v, v+1]):
+///
+/// In this case, we *must* stop after inferring the value of `v`, backtrack
+/// up call call stack, and then substitute the value of `v` into the expected
+/// type.  If we don't do this, we won't be able to match calls that pass,
+/// A[1, 2] because the "v+1=2" knowledge can only be had by substituting which
+/// allows the Int addition to fold.
+class ParamMatcher {
+public:
+  ParamMatcher(const ExprNode *expr, ParamInf &state);
+  ~ParamMatcher() {}
+
+  // This is set to the parameter index we successfully inferred.
+  ssize_t retryParamIdx = -1;
+  /// This is set when an error is encountered.
+  std::optional<MatchFailure> failureReason;
+
+  enum ResultCode { Match, Error, Retry };
+  ResultCode matchTypes(Type actualType, Type expectedType);
+  ResultCode matchParams(TypedAttr actualAttr, TypedAttr expectedAttr);
+  ResultCode matchFunctionTypes(FnTypeGeneratorType actual,
+                                FnTypeGeneratorType expected);
+  ResultCode matchSingleEltStruct(TypedAttr actual, TypedAttr expected);
+
+  void resetError() { failureReason.reset(); }
+
+private:
+  // These are methods used by the recursive walker.
+  bool isUnset() const { return retryParamIdx == -1; }
+
+  ResultCode error(MatchFailure &&reason) {
+    failureReason = std::move(reason);
+    return Error;
+  }
+
+private:
+  /// This is how many signature types deep inference is inside parameter
+  /// expressions and determines which index references we match against.
+  ///
+  /// As we search for param-refs, recursively, we'll be recursing past
+  /// `FnTypeGeneratorType`s (and other `ParameterScopeTimeInterface`s),
+  /// which changes what param-ref depths we're watching for; the param-refs'
+  /// depths would be greater (have to reach further outward so to speak, past
+  /// more generator types) to reference param-decls in the
+  /// ParameterInferenceState's original scope. paramIndexRefDepth tracks that
+  /// number.
+  ///
+  /// In other words, these paramIndexRefDepth adjustments are for
+  /// depth-aware searching, see PSTIAIRAID.
+  size_t paramIndexRefDepth = 0;
+
+  /// This is the expression we're inferring within.
+  const ExprNode *const expr;
+  ParamInf &state;
+  SharedState &shared;
+};
+
+} // namespace M::KGEN::LIT
+
+#endif // KGEN_MOJOPARSER_PARAMMATCHER_H
