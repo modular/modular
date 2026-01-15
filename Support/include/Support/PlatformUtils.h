48d47d9870221a3c1a5990e64784e1f707bdba76
Keith Smiley <keithbsmiley@gmail.com>
2026-01-15 10:36:56 -0800

    [All] Use #if over #ifdef for platforms (#75424)
    This makes sure we catch the case where the file attempting to use these
    conditionals actually imports PlatformUtils.h via -Wundef
    
    This makes the define setup a bit sillier, but not a big deal for how
    few we use
    
    MODULAR_ORIG_COMMIT_REV_ID: dd90ae59d0b63dca123af4f70674f84e55fdc1a7

--- Support/include/Support/PlatformUtils.h
+++ Support/include/Support/PlatformUtils.h
@@ -15,20 +15,36 @@
 // allow long #if lines for easier linting
 #if defined(__x86_64__) || defined(__x86_64) || defined(_M_AMD64) || defined(_M_X64)
 #define MODULAR_X86_64 1
-#elif defined(__ARM_NEON__) || defined(__ARM_NEON)
+#else
+#define MODULAR_X86_64 0
+#endif
+
+#if defined(__ARM_NEON__) || defined(__ARM_NEON)
 #define MODULAR_ARM_NEON 1
+#else
+#define MODULAR_ARM_NEON 0
 #endif
 
 #if defined(__aarch64__) || defined(__arm__) || defined(_M_ARM64) || defined(_M_ARM)
 #define MODULAR_ARM 1
+#else
+#define MODULAR_ARM 0
 #endif
 
 #if defined(_WIN32) || defined(_WIN64)
+#define MODULAR_LINUX 0
+#define MODULAR_MACOS 0
 #define MODULAR_WINDOWS 1
 #elif defined(__APPLE__) || defined(__MACH__)
+#define MODULAR_LINUX 0
 #define MODULAR_MACOS 1
+#define MODULAR_WINDOWS 0
 #elif defined(__linux__)
 #define MODULAR_LINUX 1
+#define MODULAR_MACOS 0
+#define MODULAR_WINDOWS 0
+#else
+#error "Could not determine platform"
 #endif
 
 #endif // SUPPORT_PLATFORM_UTILS_H
