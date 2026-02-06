a4104b0ee029548f5e8e5e0baed31e71fa8a30c7
Lily Brown <lbrown@modular.com>
2026-02-06 11:50:11 -0800

    [mojo-lang] Add some assertions to OverloadFitness (#76797)
    Adds some assertions to `OverloadFitness::evaluate` to try to produce a
    more informative stack trace for [this Sentry
    crash](https://modularml.sentry.io/issues/7137815959/events/7d57cd50096c4c56b457b67321215c22/?project=4505785396101120).
    
    Toward MOCO-3240.
    
    MODULAR_ORIG_COMMIT_REV_ID: f5b946f37748c4dbfd59b963e782735d90dce0d0

--- KGEN/lib/MojoParser/OverloadFitness.cpp
+++ KGEN/lib/MojoParser/OverloadFitness.cpp
@@ -918,6 +918,10 @@ OverloadFitness OverloadFitness::evaluate(FnTypeGeneratorType signature,
                          newBindings.getValue().take_front(numBindings))) {
       if (!isEqualCanon(actual, expected)) {
         DeclResolver::DeclScopeChanger x(funcIfDirect);
+        assert(resultType.getDecl(shared) &&
+               "result type should have an associated decl");
+        assert(resultType.getDecl(shared)->getIfOperation() &&
+               "result type decl should have an operation");
         auto declOp =
             cast<StructDeclOp>(resultType.getDecl(shared)->getIfOperation());
         auto paramType = declOp.getSignature().getInputParamTypes()[paramIdx];
