1906f06f192ebf7a6eb202d887001bb740ccffa9
Billy Zhu <billyzhu@modular.com>
2026-02-19 15:42:33 -0800

    [kgen] ParamEvalFrame has-a ParamEvaluator (NFC) (#77677)
    Instead of ParametricParameterEvaluator inheriting from
    ParameterEvaluator, make it contain a ParameterEvaluator. This is one
    more step in removing ParameterEvaluator subclasses and moving state out
    of EvaluationContext.
    
    MODULAR_ORIG_COMMIT_REV_ID: cff98ce0fcc960b681ca424603fe464704e33593

--- KGEN/lib/KGENDialect/ParameterEvaluator.cpp
+++ KGEN/lib/KGENDialect/ParameterEvaluator.cpp
@@ -563,18 +563,3 @@ PartiallySpecializedInputParams::from(
 
   return result;
 }
-
-/// Instantiate a new parameter evaluator with the given parameter values.
-ParametricParameterEvaluator::ParametricParameterEvaluator(
-    ArrayRef<ParamDeclAttr> paramDecls, ArrayRef<TypedAttr> declBindings)
-    : ParameterEvaluator(paramDecls, declBindings) {}
-/// Instantiate a new parameter evaluator with the given input parameters.
-ParametricParameterEvaluator::ParametricParameterEvaluator(
-    ArrayRef<TypedAttr> declBindings)
-    : ParameterEvaluator(declBindings) {}
-
-/// Instantiate a new parameter evaluator with the given parameter values.
-ParametricParameterEvaluator::ParametricParameterEvaluator(
-    DenseMap<StringAttr, TypedAttr> declBindings,
-    ArrayRef<TypedAttr> indexBindings, size_t inputDepth)
-    : ParameterEvaluator(declBindings, indexBindings, inputDepth) {}
