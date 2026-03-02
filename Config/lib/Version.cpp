1d6dbbcef076eff005d7fbcf61ab48502296372f
Alex Trotta <44127594+Ahajha@users.noreply.github.com>
2026-03-02 18:42:02 -0500

    [bazel] Refactor version configuration, add Mojo version
    Mojo 1.0 will require a change in how we plumb our versions, as we won't
    have a single version anymore. We could duplicate all the flags for
    mojo, but this simplifies things a little by merging some of them
    together and then duplicating.
    
    The base version (major.minor.patch) stays the same for an entire
    release cycle, and the label changes frequently (nightly/for RCs), so we
    split to avoid as much extra plumbing as possible. The MAX and Mojo
    labels will be the same for now, but they may differ in the 1.0 beta
    stages.
    
    For now keeps backwards compatibility with the "Modular" named variables
    and functions, but these should be removed at a later point and replaced
    explicitly with either MAX or Mojo versions.
    
    Additionally, when we bump the versions for minor releases, we now only
    need to update one place (oss/modular/BUILD.bazel) instead of several
    different workflows.
    
    MODULAR_ORIG_COMMIT_REV_ID: 4fe12e1de3c7640fa54b360ca9a6f07d84886a8c

--- Config/lib/Version.cpp
+++ Config/lib/Version.cpp
@@ -9,15 +9,34 @@
 
 using namespace M;
 
-ModularVersion M::getModularVersion() {
-  ModularVersion version{};
-  version.major = MODULAR_VERSION_MAJOR;
-  version.minor = MODULAR_VERSION_MINOR;
-  version.patch = MODULAR_VERSION_PATCH;
-  version.label = MODULAR_VERSION_LABEL;
-  version.revision = MODULAR_VERSION_REVISION;
-  version.buildType = MODULAR_BUILD_TYPE_LOWER;
-  return version;
+ProjectVersion M::getMAXVersion() {
+  return ProjectVersion{
+      .major = MAX_VERSION_MAJOR,
+      .minor = MAX_VERSION_MINOR,
+      .patch = MAX_VERSION_PATCH,
+      .label = MAX_VERSION_LABEL,
+      .revision = MODULAR_VERSION_REVISION,
+      .buildType = MODULAR_BUILD_TYPE_LOWER,
+  };
 }
+
+const char *M::getMAXVersionString() { return MAX_VERSION_STRING; }
+
+ProjectVersion M::getMojoVersion() {
+  return ProjectVersion{
+      .major = MOJO_VERSION_MAJOR,
+      .minor = MOJO_VERSION_MINOR,
+      .patch = MOJO_VERSION_PATCH,
+      .label = MOJO_VERSION_LABEL,
+      .revision = MODULAR_VERSION_REVISION,
+      .buildType = MODULAR_BUILD_TYPE_LOWER,
+  };
+}
+
+const char *M::getMojoVersionString() { return MOJO_VERSION_STRING; }
+
+// TODO: Remove
+
+M::ModularVersion M::getModularVersion() { return M::getMAXVersion(); }
 
 const char *M::getModularVersionString() { return MODULAR_VERSION_STRING; }
