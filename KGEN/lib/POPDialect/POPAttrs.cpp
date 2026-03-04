001b41ae409e7d066b9c12dc79e6eb43eedf9d31
Fraser Cormack <fcormack@modular.com>
2026-03-04 10:49:11 -0600

    [mojo-lang] Align comptime & runtime Int pow behaviour
    A recent change was made to integer pow for a LHS of 1 or -1. As a
    result the compile-time version of this function drifed. This commit
    amends that.
    
    MODULAR_ORIG_COMMIT_REV_ID: 0d002b24f7c74949ddaacedd07066c790527e510

--- KGEN/lib/POPDialect/POPAttrs.cpp
+++ KGEN/lib/POPDialect/POPAttrs.cpp
@@ -768,8 +768,8 @@ TypedAttr IntLiteralBinAttr::get(MLIRContext *ctx, TypedAttr lhs, TypedAttr rhs,
     result = l ^ r;
     break;
   case IntLiteralBinKind::Pow:
-    // Exponentiating by a negative amount is not defined for integers.
-    if (r < IPInt(0))
+    // Exponentiating 0 by a negative amount is not defined for integers.
+    if (l == IPInt(0) && r < IPInt(0))
       result = IPInt(0);
     else
       result = l.exponentiate(r);
