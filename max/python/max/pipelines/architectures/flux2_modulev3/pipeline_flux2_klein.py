d192e83c86ad69e5e6a6779d247a525ac5a37a92
pei0033 <eunik.park@squeezebits.com>
2026-03-24 13:06:11 +0000

    add module v2 qwen3 text encoder

--- max/python/max/pipelines/architectures/flux2_modulev3/pipeline_flux2_klein.py
+++ max/python/max/pipelines/architectures/flux2_modulev3/pipeline_flux2_klein.py
@@ -25,7 +25,7 @@ from max.pipelines.core import PixelContext
 from max.pipelines.lib.interfaces.diffusion_pipeline import max_compile
 from max.profiler import Tracer, traced
 
-from ..qwen3.text_encoder import Qwen3TextEncoderKleinModel
+from ..qwen3_modulev3.text_encoder import Qwen3TextEncoderKleinModel
 from .pipeline_flux2 import Flux2ModelInputs, Flux2Pipeline, Flux2PipelineOutput
 
 logger = logging.getLogger("max.pipelines")
