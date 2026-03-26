5f4231b12922ed1938cebe9d551ad806fcf6cfce
npanchen <npanchen@modular.com>
2026-03-26 09:47:06 -0400

    [oss] use f32 reciprocal for Apple GPU
    Apple GPU has no support for `f64` type, therefore need to use `f32` for
    in `mean` reduce
    
    MODULAR_ORIG_COMMIT_REV_ID: df1706e2b6b459358da43937d2a4a522d35a9668

--- KGEN/lib/Compiler/ObjectCompiler/LLVMPassesPipeline.cpp
+++ KGEN/lib/Compiler/ObjectCompiler/LLVMPassesPipeline.cpp
@@ -13,6 +13,7 @@
 #include "LLVM/Transforms/LLVMIRDowngradePass.h"
 #include "LLVM/Transforms/MetalAIRPass.h"
 #include "LLVM/Transforms/MetalRewriteDebugInfo.h"
+#include "LLVM/Transforms/MetalVerifier.h"
 #include "LLVM/Transforms/PointerRewriter.h"
 #include "LLVM/Transforms/SetFunctionAttributes.h"
 #include "llvm/Analysis/GlobalsModRef.h"
@@ -396,6 +397,8 @@ static ModulePassManager buildO3Pipeline(PassBuilder &passBuilder,
     // Rewrite all debuginfo to what Instruments expects (for now just strip
     // debuginfo)
     mpm.addPass(MetalRewriteDebugInfoPass());
+    // Verify that IR is now correct to the best of our knowledge
+    mpm.addPass(MetalVerifierPass());
     return mpm;
   }
 
@@ -580,6 +583,8 @@ static ModulePassManager buildO0Pipeline(PassBuilder &passBuilder,
     // Rewrite all debuginfo to what Instruments expects (for now just strip
     // debuginfo)
     mpm.addPass(MetalRewriteDebugInfoPass());
+    // Verify that IR is now correct to the best of our knowledge
+    mpm.addPass(MetalVerifierPass());
     return mpm;
   }
 
