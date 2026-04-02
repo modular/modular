e15b0c21615fa24ab80bf529fd675f19f7f4d146
Keith Smiley <keithbsmiley@gmail.com>
2026-04-02 15:42:21 -0700

    example

---
+++ bazel/first.cpp
@@ -0,0 +1,4 @@
+#include <iostream>
+void first() {
+  std::cout << "first 1" << std::endl;
+}
---
+++ bazel/second.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+void second() {
+  std::cout << "This is the second function. 2" << std::endl;
+}
