f389bf905bdcb2e1acbd25d1b474fbfaf6bcaf3a
Keith Smiley <keithbsmiley@gmail.com>
2026-04-02 15:45:36 -0700

    Revert "example"
    This reverts commit e15b0c21615fa24ab80bf529fd675f19f7f4d146.

--- bazel/first.cpp
+++
@@ -1,4 +0,0 @@
-#include <iostream>
-void first() {
-  std::cout << "first 1" << std::endl;
-}
--- bazel/second.cpp
+++
@@ -1,5 +0,0 @@
-#include <iostream>
-
-void second() {
-  std::cout << "This is the second function. 2" << std::endl;
-}
