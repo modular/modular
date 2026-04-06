158b1926911ac2130bde9541cc5abfd9f5c26fb6
Gordon Brown <gbrown@modular.com>
2026-04-06 23:17:20 +0100

    [MLRT] Inline createRuntime() into getOrCreateRuntime() + fix (#82547)
    * Inlines createRuntime() into getOrCreateRuntime().
    * Updates call sites to createRuntime() to use getOrCreateRuntime()
    instead.
    * Switch to using mainWillDonate=false in FilesystemBackend.Hammer now
    that the Runtime is shared globally.
    
    Note: this PR was originally introduced in
    [Internal link] and reverted in
    [Internal link] due an post-submit
    failure in //Cache/unittests:unittests.
    
    MODULAR_ORIG_COMMIT_REV_ID: 5e9a24653f026f9f360f65d6d99b081da3d742d3

--- Cache/unittests/CachedTransformTest.cpp
+++ Cache/unittests/CachedTransformTest.cpp
@@ -7,6 +7,7 @@
 #include "Cache/CachedTransform.h"
 #include "MLRT/AsyncRT/Runtime/Algorithms.h"
 #include "MLRT/AsyncRT/Runtime/Runtime.h"
+#include "MLRT/AsyncRT/Runtime/RuntimeManager.h"
 #include "MLRT/AsyncRT/Support/UnknownLocationDecoder.h"
 #include "Support/FileSystemExtras.h"
 #include "Support/Preprocessor.h"
@@ -82,8 +83,8 @@ struct TestPassDiagnosticValidator : public mlir::ScopedDiagnosticHandler {
 // on a cache miss and output of cacheHitFn on cache hit.
 TEST(CachedTransformTest, BufferReturn) {
   TempDir tempDir = createTempDir();
-  RuntimeRef runtime =
-      createRuntime(AsyncRT::RuntimeSource::Test, RuntimeOptions().forDebug());
+  RuntimeRef runtime = getOrCreateRuntime(AsyncRT::RuntimeSource::Test,
+                                          RuntimeOptions().forDebug());
   auto transformBackendChainOr =
       getLocalDefaultBackendChain(tempDir.getPath() / "xform");
   EXPECT_FALSE(failed(transformBackendChainOr));
