14e5365c612ce4f8ed70e325a79e7429c1a3fbe3
Duncan McBain <dmcbain@modular.com>
2026-04-24 17:27:48 +0100

    [Support] Add LogRecord type to Log library (#83979)
    The LogRecord will allow the logger to talk to other languages through
    some sort of FFI by being a fairly neutral representation of log
    arguments. The tag selects between the different union members, then the
    LogArgs are stuffed into an array in the record. The record also tracks
    how many arguments were logged (max 8).
    
    Closes MOTO-1492
    MODULAR_ORIG_COMMIT_REV_ID: 1e308adeaf6cf953b1afaf647f3330c00b9b9d3b

--- Support/include/Support/Log.h
+++ Support/include/Support/Log.h
@@ -6,17 +6,11 @@
 
 // MLOG is a macro that takes a variable number of arguments and logs them to
 // a file or the console.
-//
-// Zero args prints a newline to default INFO level.
-// MLOG(); // "\n" is printed.
-//
+
 // If the single arg is a string, it is printed to the INFO level with a
 // newline. MLOG(""); // "\n" is printed to the INFO level.
 // MLOG("hello");
 // "hello" is printed to the INFO level.
-//
-// If the single arg is a log level, a newline is printed at the given level.
-// MLOG(LogLevel::DEBUG); // "\n" is printed at the DEBUG level.
 //
 // Two args or more is a format string and a value, or values
 // MLOG("{}", "hello"); // hello
@@ -25,8 +19,6 @@
 //
 // ...unless the first arg is a LogLevel, then everything above
 // shifts left
-// MLOG(LogLevel::DEBUG); // new line printed at debug level
-//
 // MLOG(LogLevel::DEBUG, "hi"); // "hi" printed at debug level
 // MLOG(LogLevel::DEBUG, "{}", "hello"); // hello printed at debug level
 // MLOG(LogLevel::DEBUG, "{} {}", "hello", 42); // hello 42
@@ -34,17 +26,14 @@
 #ifndef SUPPORT_LOG_H
 #define SUPPORT_LOG_H
 
-#include "llvm/ADT/StringRef.h"
-
 #define FMT_EXCEPTIONS 0
-#include <fmt/format.h>
+#include <fmt/base.h>
 
+#include <array>
+#include <chrono>
 #include <cstdint>
 #include <cstdlib>
-#include <string>
 #include <string_view>
-#include <tuple>
-#include <type_traits>
 
 #define MLOG(...) M::Log::log(__VA_ARGS__)
 
@@ -75,6 +64,88 @@ enum class LogLevel : uint8_t {
   FATAL = 4,
 };
 
+// Holds the pre-formatted data for a log event. The tag and the union should be
+// kept in sync.
+struct LogArg {
+  enum class Type : uint8_t {
+    Bool,
+    Int64,
+    UInt64,
+    Fp32,
+    Fp64,
+    SmallString,
+    String,
+    Pointer
+  };
+
+  union {
+    bool b;
+    int64_t i64;
+    uint64_t ui64;
+    float fp32;
+    double fp64;
+    // for short strings, stored inline (up to 15 chars + null terminator)
+    std::array<char, sizeof(std::string_view)> ssoStr;
+    std::string_view str = "";
+    const void *ptr;
+  } data;
+  Type tag;
+};
+
+namespace Detail {
+template <typename T>
+LogArg toLogArg(T &&val) {
+  using D = std::decay_t<T>;
+  using enum LogArg::Type;
+  if constexpr (std::is_same_v<D, bool>) {
+    return LogArg{.data = {.b = val}, .tag = Bool};
+  } else if constexpr (std::is_integral_v<D> && std::is_signed_v<D> &&
+                       sizeof(D) <= sizeof(int64_t)) {
+    return LogArg{.data = {.i64 = static_cast<int64_t>(val)}, .tag = Int64};
+  } else if constexpr (std::is_integral_v<D> && std::is_unsigned_v<D> &&
+                       sizeof(D) <= sizeof(uint64_t)) {
+    return LogArg{.data = {.ui64 = static_cast<uint64_t>(val)}, .tag = UInt64};
+  } else if constexpr (std::is_same_v<D, float>) {
+    return LogArg{.data = {.fp32 = val}, .tag = Fp32};
+  } else if constexpr (std::is_same_v<D, double>) {
+    return LogArg{.data = {.fp64 = val}, .tag = Fp64};
+  } else if constexpr (std::is_convertible_v<D, std::string_view>) {
+    std::string_view sv(val);
+    if (sv.size() < sizeof(LogArg::data.ssoStr)) {
+      LogArg arg;
+      arg.tag = LogArg::Type::SmallString;
+      std::copy(sv.data(), sv.data() + sv.size(), arg.data.ssoStr.data());
+      arg.data.ssoStr[sv.size()] = '\0';
+      return arg;
+    } else {
+      return LogArg{.data = {.str = sv}, .tag = String};
+    }
+  } else if constexpr (std::is_pointer_v<D>) {
+    return LogArg{.data = {.ptr = val}, .tag = Pointer};
+  } else {
+    static_assert(!std::is_same_v<T, T>, "Unsupported log argument type.");
+  }
+}
+} // namespace Detail
+
+struct LogRecord {
+  constexpr static size_t maxArgs = 8;
+  using Timestamp = std::chrono::time_point<std::chrono::system_clock>;
+  Timestamp timestamp;
+  std::string_view fmtString;
+  std::array<LogArg, maxArgs> args;
+  uint8_t argCount;
+  LogLevel level;
+
+  template <typename... Args>
+  LogRecord(Timestamp ts, LogLevel lvl, std::string_view fmt, Args &&...args)
+      : timestamp(ts), fmtString(fmt),
+        args{Detail::toLogArg(std::forward<Args>(args))...},
+        argCount(sizeof...(Args)), level(lvl) {
+    static_assert(sizeof...(Args) <= maxArgs, "Too many log arguments");
+  }
+};
+
 class Logger;
 
 Logger &getDefaultLog();
@@ -83,97 +154,31 @@ void setLogLevel(LogLevel level);
 
 LogLevel getLogLevel(const Logger &log = getDefaultLog());
 
-void logWrite(Logger &log, LogLevel level, llvm::StringRef msg);
-
-// Forwards on most arguments unchanged, but adds special handling for pointer
-// types to differentiate between string-like pointers (which should format as
-// strings) and other pointers (which should format as their address).
-template <typename... Args>
-constexpr auto transformArgs(Args &&...args) {
-  return std::make_tuple([](auto &&arg) {
-    using T = std::decay_t<decltype(arg)>;
-    if constexpr (std::is_pointer_v<T>) {
-      // Don't cast string-like pointers - let them format as strings.
-      if constexpr (std::is_same_v<T, const char *> ||
-                    std::is_same_v<T, char *> ||
-                    std::is_same_v<T, const wchar_t *> ||
-                    std::is_same_v<T, wchar_t *>) {
-        return std::forward<decltype(arg)>(arg);
-      } else {
-        return fmt::format("{}", static_cast<const void *>(arg));
-      }
-    } else {
-      return std::forward<decltype(arg)>(arg);
-    }
-  }(args)...);
-}
-
-// Formats the provided message. If no args, returns the message as-is.
-// Otherwise, fmt::vformat to all the arguments in the tuple.
-template <typename... Args>
-inline std::string safeFormat(std::string_view format_str, Args &&...args) {
-  if constexpr (sizeof...(args) == 0) {
-    return std::string(format_str);
-  } else {
-    auto transformedArgs = transformArgs(std::forward<Args>(args)...);
-
-    // Use the provided format string with all arguments.
-    return std::apply(
-        [format_str](auto &&...transformed) {
-          return fmt::vformat(format_str,
-                              fmt::make_format_args(transformed...));
-        },
-        transformedArgs);
-  }
-}
+void logWrite(Logger &log, LogRecord record);
 
 // Checks the log level to see if it should emit a message, and, if so,
-// writes the formatted message.
+// dispatches to the Logger object.
 template <typename... Args>
-inline void logWriteDispatch(Logger &log, LogLevel level, Args &&...args) {
+inline void logWriteDispatch(Logger &log, LogLevel level,
+                             fmt::format_string<Args...> fmt, Args &&...args) {
   if (getLogLevel(log) > level)
     return;
 
-  if constexpr (sizeof...(args) == 0)
-    logWrite(log, level, "");
-  else if constexpr (sizeof...(args) == 1)
-    logWrite(log, level, safeFormat("{}", std::forward<Args>(args)...));
-  else
-    logWrite(log, level, safeFormat(std::forward<Args>(args)...));
+  LogRecord record(std::chrono::system_clock::now(), level,
+                   {fmt.get().data(), fmt.get().size()},
+                   std::forward<Args>(args)...);
+  logWrite(log, std::move(record));
 }
 
-namespace Detail {
-// Helper to check if the type at a given index in the parameter pack is a
-// specific type.
-template <typename T, int Index, typename... Args>
-static constexpr auto is_same_at_index_v = std::is_same_v<
-    std::decay_t<std::tuple_element_t<Index, std::tuple<Args...>>>, T>;
-} // namespace Detail
-
-// Main log entry point (called from the user macros). Checks to see what
-// type the arguments being passed to the macros are, to determine how to
-// route to logWriteDispatch. Users aren't expected to pass in their own
-// Logger instances, but this can still handle the different types if this
-// is added later.
 template <typename... Args>
-inline void log(Args &&...args) {
-  if constexpr (sizeof...(args) == 0)
-    logWriteDispatch(getDefaultLog(), LogLevel::INFO);
-  else if constexpr (sizeof...(args) == 1) {
-    if constexpr (Detail::is_same_at_index_v<LogLevel, 0, Args...>)
-      logWriteDispatch(getDefaultLog(), std::forward<Args>(args)...);
-    else if constexpr (Detail::is_same_at_index_v<Logger, 0, Args...>)
-      logWriteDispatch(std::forward<Args>(args)..., LogLevel::INFO);
-    else
-      logWriteDispatch(getDefaultLog(), LogLevel::INFO,
-                       std::forward<Args>(args)...);
-  } else {
-    if constexpr (Detail::is_same_at_index_v<LogLevel, 0, Args...>)
-      logWriteDispatch(getDefaultLog(), std::forward<Args>(args)...);
-    else
-      logWriteDispatch(getDefaultLog(), LogLevel::INFO,
-                       std::forward<Args>(args)...);
-  }
+void log(fmt::format_string<Args...> fmt, Args &&...args) {
+  logWriteDispatch(getDefaultLog(), LogLevel::INFO, fmt,
+                   std::forward<Args>(args)...);
+}
+
+template <typename... Args>
+void log(LogLevel level, fmt::format_string<Args...> fmt, Args &&...args) {
+  logWriteDispatch(getDefaultLog(), level, fmt, std::forward<Args>(args)...);
 }
 
 } // namespace M::Log
