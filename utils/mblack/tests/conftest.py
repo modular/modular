9abdcbcccdc76f58e7b119f29386b7fa5096282e
Alastair Murray <amurray@modular.com>
2026-04-27 11:33:42 +0100

    [bazel] Add `test_name_prefix` option to `modular_py_test`
    Adds an optional `test_name_prefix` parameter to `modular_py_test`,
    prepended to per-src `py_test` target names in the multi-source branch
    so two macro invocations globbing the same files don't collide.
    
    MODULAR_ORIG_COMMIT_REV_ID: 9ef39bd6a12ae6cd0f50dfb19081a7c287999df6

--- utils/mblack/tests/conftest.py
+++ utils/mblack/tests/conftest.py
@@ -13,4 +13,23 @@
 #
 # ===----------------------------------------------------------------------=== #
 
+from tests import util
+
 pytest_plugins = ["tests.optional"]
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--validate-with-mojo-build",
+        action="store_true",
+        default=False,
+        help=(
+            "Also compile every sample passed to assert_mojo_format with "
+            "`mojo build` to verify it is valid Mojo."
+        ),
+    )
+
+
+def pytest_configure(config):
+    if config.getoption("--validate-with-mojo-build"):
+        util.VALIDATE_WITH_MOJO_BUILD = True
