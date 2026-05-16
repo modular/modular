9484d65f02841e56e4e447647b1f05b4e0b5e083
Danny Byrnes <45842876+danielbyrnes@users.noreply.github.com>
2026-05-15 17:33:41 -0700

    [Revert][Kernels][stdlib] Replace DeviceContextPtr with direct use of DeviceContext"
    Reverting replacement of DeviceContextPtr with direct use of
    DeviceContext since it causes segfault.
    
    MODULAR_ORIG_COMMIT_REV_ID: 0d48f848a7c2b9ecc58efbea1c23152a8cab8efe

--- KGEN/include/KGEN/MOGGPreElab/MOGGPreElabHelpers.h
+++ KGEN/include/KGEN/MOGGPreElab/MOGGPreElabHelpers.h
@@ -29,11 +29,8 @@ inline bool isDPSTensor(LIT::StructType maybeTensor) {
          maybeTensor.getSymbol().getLeafReference() == "ManagedTensorSlice";
 }
 
-inline bool isMojoDeviceContext(LIT::StructType maybeCallContextPtr) {
-  // The public `std::DeviceContext` is the kernel-launch context type.
-  // Kernel `execute` methods that declare it have this argument skipped
-  // when matching execute against the shape function.
-  return isXType(maybeCallContextPtr, "std", "DeviceContext");
+inline bool isMojoDeviceContextPtr(LIT::StructType maybeCallContextPtr) {
+  return isXType(maybeCallContextPtr, "std", "DeviceContextPtr");
 }
 
 inline bool fnNeedsConformances(LIT::FnOp fnOp) {
