5b4320752a2c883544236821ff5b0d724bd18bef
Chris Lattner <lattner@users.noreply.github.com>
2026-09-22 10:19:52 -0500

    [mojo-lang] Continue moving off hlcf.if
    This updates more LowerArgConventions and LowerLoops to
    produce elif instead of if.
    Ref MOCO-855
    
    MODULAR_ORIG_COMMIT_REV_ID: 040439abc0a67c53549721ebebaf1e15082166ba

--- Mojo/lib/Transforms/LowerLoops.cpp
+++ Mojo/lib/Transforms/LowerLoops.cpp
@@ -119,7 +119,7 @@ LogicalResult LowerLoops::lowerForLoop(ForOp forLoop) {
     break;
   }
 
-  // Create IfOp with ThenBlock yields and ElseBlock breaks. `index.cmp`
+  // Create ElifOp with ThenBlock yields and ElseBlock breaks. `index.cmp`
   // produces an `i1`, so normalize to the `scalar<bool>` condition.
   //
   // TODO: this won't be needed after migrating scalar<int>, but we need to
@@ -128,7 +128,7 @@ LogicalResult LowerLoops::lowerForLoop(ForOp forLoop) {
   Value cmpCond = KGEN::POP::CastFromBuiltinOp::create(
                       rewriter, forLoop->getLoc(), boolTy, cmpOp)
                       .getResult();
-  auto ifOp = IfOp::create(rewriter, forLoop->getLoc(), ValueRange{}, cmpCond);
+  auto ifOp = ElifOp::create(rewriter, forLoop->getLoc(), TypeRange{}, cmpCond);
 
   rewriter.createBlock(&ifOp.getThenRegion());
   YieldOp::create(rewriter, forLoop->getLoc());
